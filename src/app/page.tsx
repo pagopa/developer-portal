963781faa7bdb863489b88ae511a66b7ccfda835
AF <alessandro.ferlin@pagopa.it>
2023-01-26 18:07:25 +0100

    Play with home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,24 @@
 'use client';
 
 import { theme } from "@pagopa/mui-italia/dist/theme";
-import { HeaderAccount } from "@pagopa/mui-italia";
+import { HeaderAccount, RootLinkType } from "@pagopa/mui-italia";
 import { ThemeProvider } from "@emotion/react";
 
-const Home = () => {
-  const pagoPALink = {
-    title: "Developer Portal",
-    label: "PagoPA Developer Portal",
-    href: "https://www.pagopa.it/it/",
-    ariaLabel: "Naviga dentro il developer portal di PagoPA",
-  };
-  const onAssistanceClick = () => {};
-
-  return (
-      <ThemeProvider theme={theme}>
-        <HeaderAccount
-            enableLogin={false}
-            rootLink={pagoPALink}
-            onAssistanceClick={onAssistanceClick}
-        />
-      </ThemeProvider>
-  );
+const rootLink: RootLinkType = {
+  title: "Developer Portal",
+  label: "PagoPA Developer Portal",
+  href: "https://www.pagopa.it/it/",
+  ariaLabel: "Naviga dentro il developer portal di PagoPA",
 };
+
+const Home = () => (
+  <ThemeProvider theme={ theme }>
+    <HeaderAccount
+      enableLogin={ false }
+      rootLink={ rootLink }
+      onAssistanceClick={ () => {} }
+    />
+  </ThemeProvider>
+);
 
 export default Home;
