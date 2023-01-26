1db2bd0f049ee523b7292e53fecc53feb6ebc821
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-01-26 11:12:01 +0100

    Create first page using NextJS

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+const Head = () =>
+  (
+    <>
+      <title>PagoPA Developer Portal</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="This is the PagoPA developer portal" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+
+export default Head;
