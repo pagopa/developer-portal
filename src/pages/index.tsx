c288dac5908f777e5332528c8296e582e2ba1ce1
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-02-08 11:17:26 +0100

    Try to add a MUI component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import { InferGetStaticPropsType } from "next";
 import { getCollections } from "@/adapters/gitbook/collections";
-import CollectionList from "@/domain/components/CollectionList";
+import {
+  HeaderAccount,
+  RootLinkType,
+  Showcase,
+  theme,
+} from "@pagopa/mui-italia";
+import { ThemeProvider } from "@emotion/react";
 
 export const getStaticProps = async () => {
   return {
@@ -10,10 +16,36 @@ export const getStaticProps = async () => {
   };
 };
 
+const pagoPALink: RootLinkType = {
+  href: "https://www.pagopa.it/",
+  label: "PagoPA Dev Portal",
+  title: "PagoPA Dev Portal",
+  ariaLabel: "PagoPA Dev Portal",
+};
+
 const Home = ({
   collections,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return <CollectionList collections={collections} />;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <HeaderAccount
+          enableLogin={false}
+          rootLink={pagoPALink}
+          onAssistanceClick={() => {}}
+        />
+      </ThemeProvider>
+
+      <Showcase
+        items={collections.map((coll) => ({
+          title: coll.title,
+          subtitle: coll.title,
+        }))}
+        title="Collections"
+      />
+
+    </>
+  );
 };
 
 export default Home;
