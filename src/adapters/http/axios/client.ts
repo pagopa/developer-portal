3d29907657f7aeccc8104f6ee5ce00c25050d875
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-02-10 11:51:23 +0100

    Create HTTP adapter using axios

---
+++ src/adapters/http/axios/client.ts
@@ -0,0 +1,42 @@
+import * as TE from 'fp-ts/lib/TaskEither';
+import * as E from 'fp-ts/lib/Either';
+import * as t from 'io-ts';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import { Task } from 'fp-ts/Task';
+import { pipe, flow } from 'fp-ts/lib/function';
+import { failure } from 'io-ts/lib/PathReporter';
+
+export type HttpClientConfig = {
+  baseURL: URL;
+  headers?: Record<string, string>;
+};
+
+export type HttpClient = AxiosInstance;
+
+export const makeHttpClient = (config: HttpClientConfig): HttpClient =>
+  axios.create({
+    baseURL: config.baseURL.href,
+    headers: config.headers,
+  });
+
+export const errorsToError = flow(
+  failure,
+  (errors) => new Error(errors.join('\n'))
+);
+
+export const makeHttpRequest =
+  (task: Task<AxiosResponse>) =>
+  <I, O>(decode: (i: I) => t.Validation<O>): TE.TaskEither<Error, O> =>
+    pipe(
+      TE.tryCatch(task, E.toError),
+      TE.bimap(
+        (err) => {
+          console.log(`Error during HTTP request: ${err}`);
+          return new Error(`Error during HTTP request: ${err}`);
+        },
+        // Decode the response
+        ({ data }) => decode(data)
+      ),
+      TE.map(flow(E.mapLeft(errorsToError), TE.fromEither)),
+      TE.flatten
+    );
