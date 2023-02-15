76e49670fa573618cc8a54b566cf91482fe20d11
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-02-15 11:26:47 +0100

    Wrap log messages inside IO

--- src/adapters/http/axios/client.ts
+++ src/adapters/http/axios/client.ts
@@ -3,8 +3,9 @@ import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import * as TE from 'fp-ts/lib/TaskEither';
 import * as E from 'fp-ts/lib/Either';
 import { Task } from 'fp-ts/Task';
-import { pipe, flow } from 'fp-ts/lib/function';
+import { pipe } from 'fp-ts/lib/function';
 import { failure } from 'io-ts/lib/PathReporter';
+import * as IO from 'fp-ts/IO';
 
 export type HttpClientConfig = {
   baseURL: URL;
@@ -19,24 +20,31 @@ export const makeHttpClient = (config: HttpClientConfig): AxiosInstance =>
     timeout: config.timeout || 5000,
   });
 
-export const errorsToError = flow(
-  failure,
-  (errors) => new Error(errors.join('\n'))
-);
+const errorsToError = (errors: t.Errors): Error =>
+  pipe(failure(errors), (errors) => new Error(errors.join('\n')));
+
+// TODO: This should be moved to a separate module
+const log =
+  (msg: string): IO.IO<void> =>
+  () =>
+    console.log(msg);
 
 export const makeHttpRequest =
   (task: Task<AxiosResponse>) =>
   <I, O>(decode: (i: I) => t.Validation<O>): TE.TaskEither<Error, O> =>
     pipe(
       TE.tryCatch(task, E.toError),
-      TE.bimap(
-        (err) => {
-          console.log(`Error during HTTP request: ${err}`);
-          return new Error(`Error during HTTP request: ${err}`);
-        },
-        // Decode the response
-        ({ data }) => decode(data)
-      ),
-      TE.map(flow(E.mapLeft(errorsToError), TE.fromEither)),
-      TE.flatten
+      TE.chain(({ data }) =>
+        pipe(
+          TE.fromIO(
+            pipe(
+              log(`Response received: ${JSON.stringify(data)}`),
+              IO.chain(() => log(`Decoding response`))
+            )
+          ),
+          TE.chain((_) => TE.fromEither(decode(data))),
+          TE.mapLeft(errorsToError),
+          TE.chainFirst((_) => TE.fromIO(log('Response decoded')))
+        )
+      )
     );
