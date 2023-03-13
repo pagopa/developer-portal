cefd108a220a2173959159d1ce753a5007536ffc
AF <alessandro.ferlin@pagopa.it>
2023-03-13 11:07:01 +0100

    Add tutorial cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,12 @@
 import { Card } from '@/domain/product';
 import * as RA from 'fp-ts/ReadonlyArray';
 import * as React from 'react';
-import { Card as MUICard, CardActionArea, CardContent } from '@mui/material';
+import {
+  Card as MUICard,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+} from '@mui/material';
 import { pipe } from 'fp-ts/lib/function';
 import PageElement from './Element';
 
@@ -9,6 +14,14 @@ const Card = (props: Card) => {
   return (
     <MUICard variant='outlined'>
       <CardActionArea component={'a'} href={props.href}>
+        {props.image && (
+          <CardMedia
+            component='img'
+            image={props.image?.src}
+            alt={props.image.alt}
+            height={300}
+          />
+        )}
         <CardContent>{pipe(props.elements, RA.map(PageElement))}</CardContent>
       </CardActionArea>
     </MUICard>
--- src/domain/product.ts
+++ src/domain/product.ts
@@ -18,18 +18,19 @@ export const pageList: ReadonlyArray<Page> = [
       {
         type: 'hero',
         title: 'Firma lorem ipsum dolor sit',
-        subtitle: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+        subtitle:
+          'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
         image: {
           src: 'https://images.unsplash.com/photo-1674594145584-354a3e88e9b5?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwxOTcwMjR8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzcyNDUyNTY&ixlib=rb-4.0.3&q=80',
-          alt: '...'
+          alt: '...',
         },
         primaryRef: {
           title: 'Inizia',
-          href: '/firma-con-io/quick-start'
+          href: '/firma-con-io/quick-start',
         },
         secondaryRef: {
           title: 'Reference API',
-          href: '/firma-con-io/api'
+          href: '/firma-con-io/api',
         },
       },
       {
@@ -105,6 +106,36 @@ export const pageList: ReadonlyArray<Page> = [
           },
         ],
       },
+      {
+        type: 'h2',
+        text: 'Esplora i tutorial',
+      },
+      {
+        type: 'cards',
+        cards: [
+          {
+            type: 'card',
+            image: {
+              src: 'https://images.unsplash.com/photo-1674594145584-354a3e88e9b5?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwxOTcwMjR8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzcyNDUyNTY&ixlib=rb-4.0.3&q=80',
+              alt: '...',
+            },
+            elements: [
+              {
+                type: 'h3',
+                text: 'In evidenza',
+              },
+              {
+                type: 'h3',
+                text: 'Scopri Firma con IO in 3 minuti',
+              },
+              {
+                type: 'paragraph',
+                text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi.',
+              },
+            ],
+          },
+        ],
+      },
     ],
   },
   {
@@ -115,6 +146,11 @@ export const pageList: ReadonlyArray<Page> = [
   },
 ];
 
+export type Image = {
+  src: string;
+  alt: string;
+};
+
 export type Text = {
   type: 'h3' | 'h2' | 'h1' | 'paragraph';
   text: string;
@@ -122,6 +158,7 @@ export type Text = {
 
 export type Card = {
   type: 'card';
+  image?: Image;
   elements: ReadonlyArray<Text>;
   href?: string;
 };
@@ -132,15 +169,12 @@ export type Cards = {
 };
 
 export type Hero = {
-  type: 'hero',
-  title: string,
-  subtitle: string,
-  image: {
-    src: string,
-    alt: string,
-  },
-  primaryRef: PageRef,
-  secondaryRef: PageRef
+  type: 'hero';
+  title: string;
+  subtitle: string;
+  image: Image;
+  primaryRef: PageRef;
+  secondaryRef: PageRef;
 };
 
 export type Element = Text | Cards | Hero;
