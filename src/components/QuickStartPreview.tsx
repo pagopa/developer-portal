cb41ee776fa2d736711703810c0a99e5f55aea3d
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-03-16 14:17:38 +0100

    Remove box and fix alignment

--- src/components/QuickStartPreview.tsx
+++ src/components/QuickStartPreview.tsx
@@ -23,100 +23,96 @@ const items = [
 ];
 
 const QuickStartPreview = () => (
-  <Box bgcolor='#FAFAFA'>
-    <Container
-      maxWidth='xl'
-      sx={{
-        py: {
-          xs: 4,
-          sm: 4,
-          md: 8,
-        },
-      }}
-    >
-      <Stack alignContent='center' spacing={{ xs: 4, md: 8 }}>
-        <Typography variant='h4' textAlign='center'>
-          Scopri quanto è semplice integrarsi
-        </Typography>
-        <Typography variant='body2' textAlign='center'>
-          Firma con IO è una funzionalità che consente agli enti di richiedere
-          la firma di documenti e di gestire i processi relativi in un unico
-          posto.
-        </Typography>
+  <Container
+    maxWidth='xl'
+    sx={{
+      py: {
+        xs: 4,
+        sm: 4,
+        md: 8,
+      },
+    }}
+  >
+    <Stack alignContent='center' spacing={{ xs: 4, md: 8 }}>
+      <Typography variant='h4' textAlign='center'>
+        Scopri quanto è semplice integrarsi
+      </Typography>
+      <Typography variant='body2' textAlign='center'>
+        Firma con IO è una funzionalità che consente agli enti di richiedere la
+        firma di documenti e di gestire i processi relativi in un unico posto.
+      </Typography>
+      <Box
+        sx={{
+          gap: 3,
+          gridTemplateColumns: {
+            xs: 'repeat(6, minmax(0, 1fr))',
+            md: 'repeat(12, minmax(0, 1fr))',
+          },
+        }}
+      >
         <Box
-          sx={{
-            display: 'grid',
-            gap: 3,
-            gridTemplateColumns: {
-              xs: 'repeat(6, minmax(0, 1fr))',
-              md: 'repeat(12, minmax(0, 1fr))',
-            },
+          gridColumn={{
+            xs: 'span 6',
+            md: '2 / span 10',
           }}
         >
-          <Box
-            gridColumn={{
-              xs: 'span 6',
-              md: '2 / span 10',
+          <Stack
+            direction='row'
+            alignItems='flex-start'
+            justifyContent={{ xs: 'flex-start', md: 'center' }}
+            spacing={{ xs: 4, lg: 8 }}
+            sx={{
+              overflowX: { xs: 'scroll', md: 'hidden' },
+              overflowY: 'hidden',
+              scrollSnapType: 'x mandatory',
+              scrollbarWidth: 'none',
+              '&::-webkit-scrollbar': {
+                display: 'none',
+              },
+              '&::after': {
+                content: '""',
+              },
             }}
           >
-            <Stack
-              direction='row'
-              alignItems='flex-start'
-              justifyContent={{ xs: 'flex-start', md: 'center' }}
-              spacing={{ xs: 4, lg: 8 }}
-              sx={{
-                overflowX: { xs: 'scroll', md: 'hidden' },
-                overflowY: 'hidden',
-                scrollSnapType: 'x mandatory',
-                scrollbarWidth: 'none',
-                '&::-webkit-scrollbar': {
-                  display: 'none',
-                },
-                '&::after': {
-                  content: '""',
-                },
-              }}
-            >
-              {items.map(({ title: itemTitle, description }, index) => (
-                <Stack
-                  key={index}
-                  alignContent='center'
-                  justifyContent='flex-start'
-                  spacing={1}
-                  sx={{
-                    minWidth: { xs: '80%', sm: '40%', md: 'auto' },
-                    flex: 1,
-                    scrollSnapAlign: 'start',
-                  }}
+            {items.map(({ title: itemTitle, description }, index) => (
+              <Stack
+                key={index}
+                alignContent='center'
+                justifyContent='flex-start'
+                spacing={1}
+                sx={{
+                  minWidth: { xs: '80%', sm: '40%', md: 'auto' },
+                  flex: 1,
+                  scrollSnapAlign: 'start',
+                }}
+              >
+                <Typography
+                  variant='caption'
+                  color='primary.dark'
+                  alignSelf='flex-start'
                 >
-                  <Typography
-                    variant='caption'
-                    color='primary.dark'
-                    alignSelf='flex-start'
-                  >
-                    {(index + 1).toString().padStart(2, '0')}{' '}
-                  </Typography>
-                  <Stack spacing={1}>
-                    <Typography variant='h5'>{itemTitle}</Typography>
-                    <>
-                      {description && (
-                        <Typography variant='body2'>{description}</Typography>
-                      )}
-                    </>
-                  </Stack>
+                  {(index + 1).toString().padStart(2, '0')}{' '}
+                </Typography>
+                <Stack spacing={1}>
+                  <Typography variant='h5'>{itemTitle}</Typography>
+                  <>
+                    {description && (
+                      <Typography variant='body2'>{description}</Typography>
+                    )}
+                  </>
                 </Stack>
-              ))}
-            </Stack>
-          </Box>
+              </Stack>
+            ))}
+          </Stack>
         </Box>
-        <Box display='flex' justifyContent='center' alignItems='center'>
-          <Button size='medium' variant='contained' color='primary'>
-            Inizia
-          </Button>
-        </Box>
-      </Stack>
-    </Container>
-  </Box>
+      </Box>
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <Button size='medium' variant='contained' color='primary'>
+          Inizia
+        </Button>
+      </Box>
+    </Stack>
+  </Container>
 );
 
 export default QuickStartPreview;
