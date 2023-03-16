fbfa7f335099c397f9a6a0a4f18dcbbb7fc2798c
AF <alessandro.ferlin@pagopa.it>
2023-03-16 14:21:52 +0100

    Change Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,43 +1,34 @@
-import { Box, Container, Stack, Typography } from '@mui/material';
+import { Box, Container, Grid, Stack, Typography } from '@mui/material';
 import Breadcrumbs from './Breadcrumbs';
 
 const Hero = () => (
-  <>
-    <Box
-      bgcolor='#FFFFFF'
-      sx={{
-        backgroundImage: `url(https://github.com/pagopa/mui-italia/blob/main/src/components/Hero/assets/hero_background.png?raw=true)`,
-        backgroundSize: 'cover',
-      }}
-    >
-      <Container maxWidth='xl'>
-        <Box
-          sx={{
-            // maxWidth: '40rem',
-            margin: '0 auto',
-            py: { xs: 4, sm: 4, md: 8 },
-          }}
-        >
-          <Breadcrumbs />
-          <Stack spacing={4}>
-            <Stack spacing={2}>
-              <Typography variant='h1' color='text.primary'>
-                Fai firmare documenti e contratti ai cittadini
-              </Typography>
-              <>
-                <Typography variant='body1' color='text.primary'>
-                  Quis aute iure reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint obcaecat
-                  cupiditat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Typography>
-              </>
-            </Stack>
+  <Box
+    bgcolor='#FFFFFF'
+    sx={{
+      backgroundImage: `url(https://github.com/pagopa/mui-italia/blob/main/src/components/Hero/assets/hero_background.png?raw=true)`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+    }}
+  >
+    <Container maxWidth='xl'>
+      <Breadcrumbs />
+      <Grid container spacing={0} sx={{ py: 10 }} >
+        <Grid item xs={5}>
+          <Stack spacing={2}>
+            <Typography variant='h1' color='text.primary'>
+              Fai firmare documenti e contratti ai cittadini
+            </Typography>
+            <Typography variant='body1' color='text.primary'>
+              Quis aute iure reprehenderit in voluptate velit esse cillum dolore
+              eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.
+            </Typography>
           </Stack>
-        </Box>
-      </Container>
-    </Box>
-  </>
+        </Grid>
+      </Grid>
+    </Container>
+  </Box>
 );
 
 export default Hero;
