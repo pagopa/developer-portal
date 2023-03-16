61f42461a5fcdff98c8eaec797e7a08e864c9ba6
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-03-16 12:54:47 +0100

    Update Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,43 @@
-import HeroSpace from '@/components/HeroSpace';
-import { Container } from '@mui/material';
+import { Box, Container, Stack, Typography } from '@mui/material';
 import Breadcrumbs from './Breadcrumbs';
 
 const Hero = () => (
-  <Container maxWidth='lg'>
-    <Breadcrumbs />
-    <HeroSpace
-      title={'Fai firmare documenti e contratti ai cittadini'}
-      description={
-        'Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      }
-    />
-  </Container>
+  <>
+    <Box
+      bgcolor='#FFFFFF'
+      sx={{
+        backgroundImage: `url(https://github.com/pagopa/mui-italia/blob/main/src/components/Hero/assets/hero_background.png?raw=true)`,
+        backgroundSize: 'cover',
+      }}
+    >
+      <Container maxWidth='xl'>
+        <Box
+          sx={{
+            // maxWidth: '40rem',
+            margin: '0 auto',
+            py: { xs: 4, sm: 4, md: 8 },
+          }}
+        >
+          <Breadcrumbs />
+          <Stack spacing={4}>
+            <Stack spacing={2}>
+              <Typography variant='h1' color='text.primary'>
+                Fai firmare documenti e contratti ai cittadini
+              </Typography>
+              <>
+                <Typography variant='body1' color='text.primary'>
+                  Quis aute iure reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint obcaecat
+                  cupiditat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Typography>
+              </>
+            </Stack>
+          </Stack>
+        </Box>
+      </Container>
+    </Box>
+  </>
 );
 
 export default Hero;
