87409d5cfeae5dcebcc8f4e3a7ba35adc6e31cea
AF <alessandro.ferlin@pagopa.it>
2023-03-16 15:29:49 +0100

    Add HeroHome

--- src/components/HeroHome.tsx
+++ src/components/HeroHome.tsx
@@ -1,3 +1,34 @@
-const HeroHome = () => <></>;
+import { Box, Container, Stack, Typography } from '@mui/material';
+
+const HeroHome = () => (
+  <Box
+    sx={{
+      backgroundImage: `url(https://github.com/pagopa/mui-italia/blob/main/src/components/Hero/assets/hero_background.png?raw=true)`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      backgroundColor: '#0062C3',
+      opacity: 65,
+    }}
+  >
+    <Container maxWidth='md' sx={{ p: 32 }}>
+      <Stack spacing={5}>
+        <Typography
+          variant='body1'
+          component='div'
+          color='primary.contrastText'
+          align='center'
+        >
+          PagoPA{' '}
+          <Box fontWeight='fontWeightMedium' display='inline'>
+            DevPortal
+          </Box>
+        </Typography>
+        <Typography variant='h1' color='primary.contrastText' align='center'>
+          Tutto ciò che serve per integrarsi con l’ecosistema di servizi PagoPA
+        </Typography>
+      </Stack>
+    </Container>
+  </Box>
+);
 
 export default HeroHome;
