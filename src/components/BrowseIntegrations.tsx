d455347d9aa93851bb25eb75e844fd3d95b6f3d6
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-03-17 12:42:25 +0100

    [DEV-260] - Create static homepage (#20)

---
+++ src/components/BrowseIntegrations.tsx
@@ -0,0 +1,44 @@
+import { Box, Container, Grid, Stack, Typography } from '@mui/material';
+import { ButtonNaked } from '@pagopa/mui-italia';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { IntegrationBlock } from '@/domain/home';
+
+const BrowseIntegrations = (props: IntegrationBlock) => (
+  <Container maxWidth='xl'>
+    <Grid container spacing={2} py={6}>
+      <Grid item xs={6}>
+        <Typography variant='h4' py={6}>
+          {props.title}
+        </Typography>
+        <Stack spacing={2}>
+          <Typography variant='h6'>{props.subtitle}</Typography>
+          <Typography variant='body2'>{props.description}</Typography>
+        </Stack>
+        <ButtonNaked
+          size='small'
+          color='primary'
+          sx={{
+            pt: 3,
+          }}
+          endIcon={<ArrowForwardIcon color='primary' />}
+          href={props.findMore.href}
+        >
+          {props.findMore.text}
+        </ButtonNaked>
+      </Grid>
+      <Grid item xs={6}>
+        <Box
+          sx={{
+            width: '100%',
+            height: '100%',
+          }}
+          component='img'
+          alt={props.image.alt}
+          src={props.image.src}
+        />
+      </Grid>
+    </Grid>
+  </Container>
+);
+
+export default BrowseIntegrations;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,27 @@
-import { InferGetStaticPropsType } from 'next';
-import { getCollections } from '@/adapters/gitbook/collections';
-import { Showcase } from '@pagopa/mui-italia';
-import { pipe } from 'fp-ts/lib/function';
-import * as TE from 'fp-ts/lib/TaskEither';
-import * as T from 'fp-ts/lib/Task';
-import { Collection } from '@/domain/collection';
-import { Box, Stack } from '@mui/material';
+import { Box } from '@mui/material';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import ComingSoon from '@/components/ComingSoon';
+import BrowseIntegrations from '@/components/BrowseIntegrations';
+import Highlighted from '@/components/Highlighted';
+import HeroHome from '@/components/HeroHome';
+import { GetStaticProps } from 'next';
+import { HomePage } from '@/domain/home';
+import { staticHomepage } from '@/adapters/static/staticHomepage';
 
-export const getStaticProps = () => {
-  const emptyCollections: ReadonlyArray<Collection> = [];
-  return pipe(
-    getCollections(),
-    TE.getOrElse(() => T.of(emptyCollections)),
-    T.map((collections) => ({
-      props: {
-        collections,
-      },
-    }))
-  )();
-};
+export const getStaticProps: GetStaticProps<HomePage> = async () => ({
+  props: staticHomepage,
+});
 
-const Home = ({
-  collections,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
-  // This is just an example. Here we are rendering a list of collections, using MUI-Italia components.
-  // Data are fetched from GitBook, using GitBook API.
-  return (
-    <Box>
-      <Stack>
-        <Header />
-        <Box>
-          <Showcase
-            items={collections.map((coll) => ({
-              title: coll.title,
-              subtitle: coll.title,
-            }))}
-            title='Collections'
-          />
-        </Box>
-        <Footer />
-      </Stack>
-    </Box>
-  );
-};
+const Home = (props: HomePage) => (
+  <Box>
+    <Header />
+    <HeroHome {...props.hero} />
+    <Highlighted {...props.highlighted} />
+    <BrowseIntegrations {...props.integrations} />
+    <ComingSoon {...props.comingSoon} />
+    <Footer />
+  </Box>
+);
 
 export default Home;
