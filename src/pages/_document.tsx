c41d8f8f78a86723c7a72caa79b5f6985fbbee09
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-03-23 12:27:49 +0100

    [DEV-355] - Add robots file (#25)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <meta name='robots' content='noindex,nofollow' />
+      </Head>
       <body>
         <Main />
         <NextScript />
