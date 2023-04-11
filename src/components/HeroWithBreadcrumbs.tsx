c94c607b2874f7c9e9392a9e7ce2e37e4f077143
AF <alessandro.ferlin@pagopa.it>
2023-04-11 12:09:45 +0200

    Remove useless code

--- src/components/HeroWithBreadcrumbs.tsx
+++ src/components/HeroWithBreadcrumbs.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/router';
 import { HeroBlock } from '@/domain/pageBlock';
 
 const HeroWithBreadcrumbs = ({ title, description, cover }: HeroBlock) => {
-  console.log(JSON.stringify(useRouter()));
   const breadcrumbs = makeBreadcrumbs(staticNav, useRouter().asPath);
   return (
     <Box
