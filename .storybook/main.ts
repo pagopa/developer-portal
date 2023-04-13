dc3ffd1d24f1fc238744ff23631e2f57047e51ec
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-04-13 10:07:21 +0200

    Configure Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,23 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+  core: {
+    disableTelemetry: true,
   },
 };
 export default config;
---
+++ src/stories/TutorialPreview.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/react';
+import TutorialPreview from '@/components/TutorialPreview';
+
+const meta: Meta<typeof TutorialPreview> = {
+  /* The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Components/Tutorial',
+  component: TutorialPreview,
+  /*
+   * This will generate a documentation page.
+   */
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Preview: StoryObj<typeof TutorialPreview> = {
+  args: {
+    // The args you need here will depend on your component
+    type: 'tutorial-preview',
+    title: 'Tutorial Title',
+    preview: {
+      title: 'Preview Title',
+      description: 'This is a tutorial preview',
+      date: '13 luglio 2022',
+      link: 'https://www.pagopa.it/',
+      image: {
+        src: 'https://images.pexels.com/photos/175045/pexels-photo-175045.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        alt: 'An alt text for the image',
+      },
+    },
+  },
+};
