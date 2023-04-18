4c6de2ba91a466f5edf608a55412968ac80c6db6
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-04-18 10:26:57 +0200

    Improve component's style

--- src/components/ProductGuideNav.tsx
+++ src/components/ProductGuideNav.tsx
@@ -1,10 +1,11 @@
-import { Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import { pipe } from 'fp-ts/lib/function';
 import * as RA from 'fp-ts/lib/ReadonlyArray';
 import Link from 'next/link';
 import * as React from 'react';
 import { ProductGuideMenu, ProductGuidePage } from '@/domain/productGuidePage';
 import { Menu } from '@/domain/navigator';
+import { useRouter } from 'next/router';
 
 export type ProductGuidePageProps = ProductGuidePage & {
   navLinks: Menu;
@@ -14,37 +15,64 @@ const ProductGuideNav = ({
   title,
   versionSlug,
   productGuideNav,
-}: ProductGuidePageProps) => (
-  <Stack spacing={2} bgcolor='background.default'>
-    {/* From Figma bgcolor should be #FAFAFA, but it doesn't exist on MUI-Italia */}
-    <Typography variant='h6'>{title}</Typography>
-    <Typography color='text.secondary'>{versionSlug}</Typography>
-    {pipe(
-      productGuideNav,
-      // If the menuItem is a group, render it without the link reference;
-      RA.map((menuItem) =>
-        menuItem.kind === 'group' ? (
-          <Typography
-            key={menuItem.path}
-            textAlign='center'
-            color='text.disabled'
-            textTransform='uppercase'
-          >
-            {menuItem.name}
-          </Typography>
-        ) : (
-          <Typography
-            key={menuItem.path}
-            textAlign='center'
-            component={Link}
-            href={menuItem.path}
-          >
-            {menuItem.name}
-          </Typography>
+}: ProductGuidePageProps) => {
+  const currentPath = useRouter().asPath;
+  return (
+    <Stack spacing={2} bgcolor='background.default' maxWidth='354'>
+      {/* From Figma bgcolor should be #FAFAFA, but it doesn't exist on MUI-Italia */}
+      <Box sx={{ p: 1 }}>
+        <Typography variant='h6'>{title}</Typography>
+      </Box>
+      <Box sx={{ p: 1 }}>
+        <Typography color='text.secondary'>{versionSlug}</Typography>
+      </Box>
+      {pipe(
+        productGuideNav,
+        // If the menuItem is a group, render it without the link reference;
+        RA.map((menuItem) =>
+          menuItem.kind === 'group' ? (
+            <Box key={menuItem.name} component='span' sx={{ p: 1 }}>
+              <Typography
+                key={menuItem.name}
+                color='text.disabled'
+                textTransform='uppercase'
+              >
+                {menuItem.name}
+              </Typography>
+            </Box>
+          ) : currentPath.includes(menuItem.path) ? (
+            // This is the current page, so we render it in a different color
+            // TODO: Missing opacity here
+            <Box key={menuItem.name} component='span' sx={{ p: 1 }}>
+              <Typography
+                variant='sidenav'
+                color='primary.main'
+                key={menuItem.name}
+                component={Link}
+                href={menuItem.path}
+                sx={{ textDecoration: 'none' }}
+              >
+                {menuItem.name}
+              </Typography>
+            </Box>
+          ) : (
+            // This is a link to another page (not the current one)
+            <Box key={menuItem.name} component='span' sx={{ p: 1 }}>
+              <Typography
+                variant='sidenav'
+                key={menuItem.name}
+                component={Link}
+                href={menuItem.path}
+                sx={{ textDecoration: 'none' }}
+              >
+                {menuItem.name}
+              </Typography>
+            </Box>
+          )
         )
-      )
-    )}
-  </Stack>
-);
+      )}
+    </Stack>
+  );
+};
 
 export default ProductGuideNav;
