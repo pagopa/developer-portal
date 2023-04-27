4b8d8532bf0698ce9d6ad7ada36982fa3cb5075f
AF <alessandro.ferlin@pagopa.it>
2023-04-27 14:52:23 +0200

    Add comment to AppEnv

--- src/AppEnv.ts
+++ src/AppEnv.ts
@@ -3,10 +3,19 @@ import * as TE from 'fp-ts/TaskEither';
 import { ProductGuidePageReader } from './domain/productGuidePage';
 import { makeProductGuidePageReader } from './adapters/gitbook/makeProductGuidePageReader';
 
+/**
+ * Defines the shape of the environment that is needed for the application to
+ * run.
+ */
 export type AppEnv = {
   productGuidePageReader: ProductGuidePageReader;
 };
 
+/**
+ * Creates an instance of AppEnv. It takes an environment object as input, and
+ * returns a TaskEither that either resolves to an instance of AppEnv or rejects
+ * with an error.
+ */
 export const makeAppEnv = (
   env: Record<string, string | undefined>
 ): TE.TaskEither<Error, AppEnv> =>
