41a67511ce83d8f47ae3092b16c902da5d70932f
AF <alessandro.ferlin@pagopa.it>
2023-05-03 17:49:46 +0200

    Add ProductGuideVersionsMenu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { theme as muiItaliaTheme } from '@pagopa/mui-italia';
 
-const theme = createTheme(muiItaliaTheme, {
+export const theme = createTheme(muiItaliaTheme, {
   palette: {
     background: {
       code: '#363C42',
