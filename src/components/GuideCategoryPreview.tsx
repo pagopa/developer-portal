36c92ad1aa1c33d050b1a0bb801acf2b964e1ff3
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-05-10 13:24:14 +0200

    Add components to render guide previews

---
+++ src/components/GuideCategoryPreview.tsx
@@ -0,0 +1,38 @@
+import { Container, Stack, Typography } from '@mui/material';
+import React from 'react';
+import { GuideCategoryPreview as GuideCategoryPreviewBlock } from '@/domain/pageBlock';
+import ProductGuidePreview from '@/components/ProductGuidePreview';
+
+const GuideCategoryPreview = ({
+  category,
+  guides,
+}: GuideCategoryPreviewBlock) => (
+  <Container
+    maxWidth='xl'
+    sx={{
+      py: {
+        xs: 4,
+        sm: 4,
+        md: 8,
+      },
+    }}
+  >
+    {guides.length > 0 && (
+      <Stack spacing={5}>
+        <Typography
+          color='text.secondary'
+          textTransform='uppercase'
+          fontSize={14}
+          style={{ textDecoration: 'none' }}
+        >
+          {category.title}
+        </Typography>
+        {guides.map((guide) => (
+          <ProductGuidePreview {...guide} key={guide.title} />
+        ))}
+      </Stack>
+    )}
+  </Container>
+);
+
+export default GuideCategoryPreview;
