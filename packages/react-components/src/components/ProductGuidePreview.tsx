c72bc68c2bcdf43be58bf8fb702b3767a203c777
AF <4775679+datalek@users.noreply.github.com>
2023-05-23 11:20:39 +0200

    Prevent image overflow on guide preview (#57)

--- packages/react-components/src/components/ProductGuidePreview.tsx
+++ packages/react-components/src/components/ProductGuidePreview.tsx
@@ -6,39 +6,46 @@ import Link from 'next/link';
 import { GuidePreviewBlock } from 'core/domain/pageBlock';
 
 const ProductGuidePreview = (props: GuidePreviewBlock) => (
-  <Card raised sx={{ display: 'flex' }}>
-    <CardContent sx={{ flex: '1 0 auto' }}>
-      <Stack spacing={2} justifyContent='center' alignItems='flex-start'>
-        <Typography variant='h6' color='text.primary'>
-          {props.title}
-        </Typography>
-        <Typography variant='subtitle2' component='div' color='text.primary'>
-          {props.preview.title}
-        </Typography>
-        <Typography variant='body2' color='text.primary' component='div'>
-          <ul>
-            {props.preview.description.map((item) => (
-              <li key={item}>{item}</li>
-            ))}
-          </ul>
-        </Typography>
-        <ButtonNaked
-          size='medium'
-          color='primary'
-          href={props.preview.link}
-          endIcon={<ArrowForwardIcon />}
-          component={Link}
-        >
-          {'Scopri'}
-        </ButtonNaked>
-      </Stack>
-    </CardContent>
-    <CardMedia
-      component='img'
-      sx={{ width: 350 }}
-      image={props.preview.image.src}
-      alt={props.preview.image.alt}
-    />
+  <Card raised>
+    <Stack
+      direction='row'
+      justifyContent='space-between'
+      alignItems='stretch'
+      spacing={2}
+    >
+      <CardContent>
+        <Stack spacing={2} justifyContent='center' alignItems='flex-start'>
+          <Typography variant='h6' color='text.primary'>
+            {props.title}
+          </Typography>
+          <Typography variant='subtitle2' component='div' color='text.primary'>
+            {props.preview.title}
+          </Typography>
+          <Typography variant='body2' color='text.primary' component='div'>
+            <ul>
+              {props.preview.description.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </Typography>
+          <ButtonNaked
+            size='medium'
+            color='primary'
+            href={props.preview.link}
+            endIcon={<ArrowForwardIcon />}
+            component={Link}
+          >
+            {'Scopri'}
+          </ButtonNaked>
+        </Stack>
+      </CardContent>
+      <CardMedia
+        component='img'
+        sx={{ width: 350 }}
+        image={props.preview.image.src}
+        alt={props.preview.image.alt}
+      />
+    </Stack>
   </Card>
 );
 
