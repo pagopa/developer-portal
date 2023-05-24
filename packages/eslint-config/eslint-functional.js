2af8352f17ecc13d190e538c6ad95cc2c052e97b
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-05-24 10:38:32 +0200

    Add more rules

--- packages/eslint-config/eslint-functional.js
+++ packages/eslint-config/eslint-functional.js
@@ -6,14 +6,20 @@ module.exports = {
     'functional/no-promise-reject': 'error',
     'functional/no-try-statements': 'error',
     // No mutations
-    'functional/no-let': 'error',
     'functional/immutable-data': 'error',
+    'functional/no-let': 'error',
     // No other paradigms
+    'functional/no-classes': 'error',
+    'functional/no-this-expressions': 'error',
     // No statements
     'functional/no-loop-statements': 'error',
+    'functional/no-return-void': 'error',
     // Stylistic
     'functional/prefer-property-signatures': 'warn',
     'functional/prefer-tacit': 'warn',
     'functional/readonly-type': 'error',
+    // Vanilla
+    'no-var': 'error',
+    'no-param-reassign': 'error',
   }
 }
