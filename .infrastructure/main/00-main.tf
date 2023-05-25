2d791ba88c823192af80396bd3bf02914279693d
AF <alessandro.ferlin@pagopa.it>
2023-05-25 16:54:02 +0200

    Add main terraform

---
+++ .infrastructure/main/00-main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "1.2.8"
+
+  backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = var.tags
+  }
+}
+
+locals {
+  project = format("%s-%s", var.app_name, var.env_short)
+}
+
+data "aws_caller_identity" "current" {}
