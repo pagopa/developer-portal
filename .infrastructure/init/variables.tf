a7d162a620bac22cd4e9535b33c1a3df22bd22e6
AF <alessandro.ferlin@pagopa.it>
2023-05-25 15:19:59 +0200

    Add init terraform configuration

---
+++ .infrastructure/init/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region (default is Milan)"
+  default     = "eu-south-1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment. Possible values are: Dev, Uat, Prod"
+  default     = "Uat"
+}
+
+variable "github_repository" {
+  type        = string
+  description = "This github repository"
+  default     = "developer-portal"
+}
+
+
+variable "tags" {
+  type = map(any)
+  default = {
+    "CreatedBy" : "Terraform",
+    "Environment" : "Uat"
+  }
+}
