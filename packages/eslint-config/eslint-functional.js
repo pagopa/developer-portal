1a7be1797c3b9d69ee726f2eb4d0dea5fb8097f1
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-05-26 11:56:23 +0200

    Update packages/eslint-config/eslint-functional.js
    Co-authored-by: AF <4775679+datalek@users.noreply.github.com>

--- packages/eslint-config/eslint-functional.js
+++ packages/eslint-config/eslint-functional.js
@@ -4,7 +4,7 @@ module.exports = {
   rules: {
     // No exceptions
     'functional/no-promise-reject': 'error',
-    'functional/no-throw-statements': 'warn',
+    'functional/no-throw-statements': 'error',
     'functional/no-try-statements': 'error',
     // No mutations
     'functional/immutable-data': 'error',
