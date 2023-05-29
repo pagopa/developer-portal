de2c5c0fefbda0fb2ae70f9d3ab337151e6dc855
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-05-29 15:22:37 +0200

    Enable prefer-readonly-type and no-expression-statements; disable readonly-type

--- packages/eslint-config/eslint-functional.js
+++ packages/eslint-config/eslint-functional.js
@@ -9,18 +9,35 @@ module.exports = {
     // No mutations
     'functional/immutable-data': 'error',
     'functional/no-let': 'error',
+    'functional/prefer-readonly-type': 'error',
     // No other paradigms
     'functional/no-classes': 'error',
     'functional/no-this-expressions': 'error',
     // No statements
+    'functional/no-expression-statements': 'error',
     'functional/no-loop-statements': 'error',
     'functional/no-return-void': 'error',
     // Stylistic
     'functional/prefer-property-signatures': 'warn',
     'functional/prefer-tacit': 'warn',
-    'functional/readonly-type': 'error',
     // Vanilla
     'no-var': 'error',
     'no-param-reassign': 'error',
-  }
+  },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'functional/prefer-readonly-type': 'off',
+      },
+    },
+    {
+      // Exclude tests from this rule, allowing us to use describe() and it()
+      // Exclude also react components, allowing us to use setState()
+      files: ['**/__tests__/*.ts', '**/*.tsx'],
+      rules: {
+        'functional/no-expression-statements': 'off',
+      },
+    },
+  ],
 }
