cd2d34920d0a8b9841844c330a65aa542ca0b071
AF <alessandro.ferlin@pagopa.it>
2023-06-01 10:06:31 +0200

    Change not found page

--- .infrastructure/60_cdn.tf
+++ .infrastructure/60_cdn.tf
@@ -51,7 +51,7 @@ resource "aws_cloudfront_distribution" "website" {
   custom_error_response {
     error_code         = 404
     response_code      = 404
-    response_page_path = "/it/404.html"
+    response_page_path = "/404.html"
   }
 
   default_cache_behavior {
