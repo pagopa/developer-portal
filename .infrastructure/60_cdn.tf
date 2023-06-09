e9891e7a0bbffbb51cc8429de6856cb9dd8e49cf
AF <alessandro.ferlin@pagopa.it>
2023-06-09 14:29:52 +0200

    Add cloudfront functions deploy workaround

--- .infrastructure/60_cdn.tf
+++ .infrastructure/60_cdn.tf
@@ -33,7 +33,8 @@ resource "aws_cloudfront_response_headers_policy" "websites" {
 resource "aws_cloudfront_function" "website_viewer_request_handler" {
   name    = "website-viewer-request-handler"
   runtime = "cloudfront-js-1.0"
-  publish = false
+  # publish this version only if the env is true
+  publish = var.publish_cloudfront_functions
   code    = file("${path.module}/../apps/cloudfront-functions/dist/viewer-request-handler.js")
 }
 
