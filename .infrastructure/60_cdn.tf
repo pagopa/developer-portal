6833feb77f801dcbbec4ca7203b6485448d18ab3
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-06-09 18:29:21 +0200

    Revert "Update CDN config using DNS"
    This reverts commit 5eec4fb60e4e80734b5316e592bca7058245431e.

--- .infrastructure/60_cdn.tf
+++ .infrastructure/60_cdn.tf
@@ -55,7 +55,7 @@ resource "aws_cloudfront_distribution" "website" {
   comment             = "CloudFront distribution for the static website."
   default_root_object = "index.html"
 
-  aliases = var.enable_cdn_https && var.public_dns_zones != [] ? [format("www.%s", keys(var.public_dns_zones)[0], )] : []
+  aliases = []
 
   custom_error_response {
     error_code         = 404
@@ -95,9 +95,8 @@ resource "aws_cloudfront_distribution" "website" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = var.enable_cdn_https ? false : true
-    acm_certificate_arn            = var.enable_cdn_https ? aws_acm_certificate.website.arn : null
-    ssl_support_method             = var.enable_cdn_https ? "sni-only" : null
+    cloudfront_default_certificate = true
+    acm_certificate_arn            = null
+    ssl_support_method             = null
   }
-
 }
