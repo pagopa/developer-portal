82027abbf7fe18f57071bef701cfef7a4acc42a5
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-06-12 11:50:02 +0200

    Add output for dns records

--- .infrastructure/99_outputs.tf
+++ .infrastructure/99_outputs.tf
@@ -5,3 +5,7 @@ output "terraform_backend_bucket_name" {
 output "terraform_lock_dynamodb_table" {
   value = module.identity.terraform_lock_dynamodb_table
 }
+
+output "name_servers_records" {
+  value = module.dns.name_servers_records
+}
