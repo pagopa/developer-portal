c302c98d341af50e479c8b7a024543d333ad3dab
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-06-13 17:44:10 +0200

    Add alternative names using www

--- .infrastructure/11_certs.tf
+++ .infrastructure/11_certs.tf
@@ -1,6 +1,7 @@
 resource "aws_acm_certificate" "website" {
   domain_name       = var.dns_domain_name
   validation_method = "DNS"
+  subject_alternative_names = [format("www.%s", var.dns_domain_name)]
 
   lifecycle {
     create_before_destroy = true
