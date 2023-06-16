69eeabf54f13e15829c9ddfdac2bdefd17089f82
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-06-16 09:46:05 +0200

    Link CDN to custom domain (#76)

--- .infrastructure/11_certs.tf
+++ .infrastructure/11_certs.tf
@@ -9,20 +9,3 @@ resource "aws_acm_certificate" "website" {
 
   provider = aws.us-east-1
 }
-
-resource "aws_route53_record" "website" {
-  for_each = {
-    for dvo in aws_acm_certificate.website.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 3600 # 1 hour
-  type            = each.value.type
-  zone_id         = aws_route53_zone.dev_portal.zone_id
-}
