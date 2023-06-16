5c7d34ecad05cc0d87fe382e05072add2f5d16fb
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-06-16 14:50:02 +0200

    Add alias records

--- .infrastructure/10_dns.tf
+++ .infrastructure/10_dns.tf
@@ -33,14 +33,26 @@ resource "aws_route53_record" "certificate" {
 }
 
 // This Route53 record will point at our CloudFront distribution.
+resource "aws_route53_record" "www_website" {
+  zone_id = aws_route53_zone.dev_portal.zone_id
+  name    = format("www.%s", var.dns_domain_name)
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "website" {
   zone_id = aws_route53_zone.dev_portal.zone_id
   name    = var.dns_domain_name
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.website.domain_name
-    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    name                   = aws_route53_record.www_website.name
+    zone_id                = aws_route53_record.www_website.zone_id
     evaluate_target_health = false
   }
 }
