4353ad3bfa4790a34be1ebfff7b11066d967b9ec
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-06-16 14:31:35 +0200

    Revert "Remove A record"
    This reverts commit 7218a8cff022a7059c5b911aacc47809bcf52ead.

--- .infrastructure/10_dns.tf
+++ .infrastructure/10_dns.tf
@@ -32,3 +32,15 @@ resource "aws_route53_record" "certificate" {
   zone_id         = aws_route53_zone.dev_portal.zone_id
 }
 
+// This Route53 record will point at our CloudFront distribution.
+resource "aws_route53_record" "website" {
+  zone_id = aws_route53_zone.dev_portal.zone_id
+  name    = var.dns_domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
