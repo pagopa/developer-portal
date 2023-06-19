3f02c1200430429f3eb7102afcc0c2425f9d8c72
Marco Bottaro <marco.bottaro@uqido.com>
2023-06-19 14:13:04 +0200

    Fix linting errors

--- apps/nextjs-website/src/pages/api/hello.ts
+++ apps/nextjs-website/src/pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
-}
+  readonly name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // eslint-disable-next-line functional/no-expression-statements
+  res.status(200).json({ name: 'John Doe' });
 }
