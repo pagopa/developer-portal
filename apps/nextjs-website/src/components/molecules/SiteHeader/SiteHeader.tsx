405bb8dc9af4149d7bf657056f6acbe53018592a
Marco Ponchia <marco.ponchia@uqido.com>
2023-06-27 15:35:10 +0200

    add site header

---
+++ apps/nextjs-website/src/components/molecules/SiteHeader/SiteHeader.tsx
@@ -0,0 +1,37 @@
+import { translations } from '@/_contents/translations';
+import Dropdown from '@/components/atoms/Dropdown/Dropdown';
+import HomepageButton from '@/components/atoms/HomepageButton/HomepageButton';
+import { Product } from '@/lib/types/product';
+import { Divider, Stack } from '@mui/material';
+import React, { Fragment } from 'react';
+
+type SiteHeaderProps = {
+  products: Product[];
+};
+
+const SiteHeader = ({ products }: SiteHeaderProps) => {
+  const { header } = translations;
+  return (
+    <Fragment>
+      <Stack
+        sx={{ m: 2 }}
+        spacing={2}
+        direction='row'
+        justifyContent={{ sm: 'space-between', md: 'start' }}
+        alignItems='center'
+      >
+        <HomepageButton title={header.title} boldTitle={header.boldTitle} />
+        <Dropdown
+          label={header.products}
+          items={products.map((product) => ({
+            href: product.paths.overview.slug,
+            label: product.name,
+          }))}
+        />
+      </Stack>
+      <Divider />
+    </Fragment>
+  );
+};
+
+export default SiteHeader;
