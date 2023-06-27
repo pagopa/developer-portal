cc8a905667b7e7c4dc1e7e24ca688aab8303e3a5
Marco Ponchia <marco.ponchia@uqido.com>
2023-06-27 15:34:49 +0200

    add dropdown components

---
+++ apps/nextjs-website/src/components/atoms/Dropdown/Dropdown.tsx
@@ -0,0 +1,78 @@
+import React, { Fragment } from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Link from 'next/link';
+import { useTheme } from '@mui/material';
+import { ArrowDropDown } from '@mui/icons-material';
+
+type DropdownProps = {
+  label: string;
+  items: { href: string; label: string }[];
+};
+
+const Dropdown = ({ label, items }: DropdownProps) => {
+  const theme = useTheme();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Fragment>
+      <Button
+        aria-controls={open ? `${label}-menu` : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        variant='naked'
+        disableElevation
+        onClick={handleClick}
+        endIcon={<ArrowDropDown />}
+      >
+        {label}
+      </Button>
+      <Menu
+        elevation={0}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        MenuListProps={{
+          'aria-labelledby': `${label}-button`,
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        {items.map((item, index) => {
+          return (
+            <MenuItem
+              key={index}
+              href={item.href}
+              onClick={handleClose}
+              disableRipple
+            >
+              <Link
+                style={{
+                  textDecoration: 'none',
+                  color: theme.palette.common.black,
+                }}
+                href={item.href}
+              >
+                {item.label}
+              </Link>
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    </Fragment>
+  );
+};
+
+export default Dropdown;
