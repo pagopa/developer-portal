44741933826707efe24f4edddaae4db0b32772d3
Marco Ponchia <marco.ponchia@uqido.com>
2023-06-29 10:21:42 +0200

    update translations

--- apps/nextjs-website/src/_contents/translations.ts
+++ apps/nextjs-website/src/_contents/translations.ts
@@ -17,7 +17,7 @@ export const translations = {
       },
     ],
     legalInfo:
-      "<strong>PagoPA S.p.A.</strong>{' '}— società per azioni con socio unico - capitale sociale di euro 1,000,000 interamente versato - sede legale in Roma, Piazza Colonna 370,<br />CAP 00187 - n. di iscrizione a Registro Imprese di Roma, CF e P.IVA 15376371009",
+      '<strong>PagoPA S.p.A.</strong> - Società per azioni con socio unico - Capitale sociale di euro 1,000,000 interamente versato - Sede legale in Roma, Piazza Colonna 370, <br />CAP 00187 - N. di iscrizione a Registro Imprese di Roma, CF e P.IVA 15376371009',
     links: {
       followUs: {
         links: [
@@ -26,14 +26,6 @@ export const translations = {
             href: '#accessibilità',
             label: 'Accessibilità',
             linkType: 'internal',
-          },
-        ],
-        socialLinks: [
-          {
-            title: 'Link: vai al sito LinkedIn di PagoPA S.p.A.',
-            ariaLabel: 'Link: vai al sito LinkedIn di PagoPA S.p.A.',
-            href: 'https://www.linkedin.com/company/pagopa/',
-            icon: 'LinkedIn',
           },
         ],
         title: 'Seguici su',
@@ -47,6 +39,24 @@ export const translations = {
             label: 'Chi siamo',
             linkType: 'internal',
           },
+          {
+            ariaLabel: 'Vai al link: PNRR',
+            href: '#pnrr',
+            label: 'PNRR',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Media',
+            href: '#media',
+            label: 'Media',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Lavora con noi',
+            href: '#lavora-con-noi',
+            label: 'Lavora con noi',
+            linkType: 'internal',
+          },
         ],
       },
       resources: {
@@ -58,6 +68,55 @@ export const translations = {
             label: 'Informativa Privacy',
             linkType: 'internal',
           },
+          {
+            ariaLabel: 'Vai al link: Termini e Condizioni',
+            href: '#terms-conditions',
+            label: 'Termini e Condizioni',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Certificazioni',
+            href: '#certificazioni',
+            label: 'Certificazioni',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Sicurezza delle informazioni',
+            href: '#sicurezza-delle-informazioni',
+            label: 'Sicurezza delle informazioni',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel:
+              'Vai al link: Diritto alla protezione dei dati personali',
+            href: '#diritto-alla-protezione-dei-dati-personali',
+            label: 'Diritto alla protezione dei dati personali',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Preferenze Cookie',
+            href: '#preferenze-cookie',
+            label: 'Preferenze Cookie',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Società trasparente',
+            href: '#societa-trasparente',
+            label: 'Società trasparente',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Responsible Disclosure Policy',
+            href: '#responsible-disclosure-policy',
+            label: 'Responsible Disclosure Policy',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Modello 321',
+            href: '#modello-321',
+            label: 'Modello 321',
+            linkType: 'internal',
+          },
         ],
       },
       services: {
@@ -67,6 +126,43 @@ export const translations = {
             ariaLabel: 'Vai al link: App IO',
             href: 'https://www.pagopa.it/it/prodotti-e-servizi/app-io',
             label: 'App IO',
+            linkType: 'external',
+          },
+          {
+            ariaLabel: 'Vai al link: Piattaforma pagoPA',
+            href: 'https://www.pagopa.it/it/prodotti-e-servizi/piattaforma-pagopa',
+            label: 'Piattaforma pagoPA',
+            linkType: 'external',
+          },
+          {
+            ariaLabel: 'Vai al link: Centro stella',
+            href: 'https://www.pagopa.it/it/prodotti-e-servizi/centro-stella-pagamenti-elettronici',
+            label: 'Centro stella',
+            linkType: 'external',
+          },
+          {
+            ariaLabel: 'Vai al link: Check IBAN',
+            href: 'https://www.pagopa.it/it/prodotti-e-servizi/check-iban',
+            label: 'Check IBAN',
+            linkType: 'external',
+          },
+          {
+            ariaLabel: 'Vai al link: Piattaforma Notifiche Digitali',
+            href: 'https://www.pagopa.it/it/prodotti-e-servizi/piattaforma-notifiche-digitali',
+            label: 'Piattaforma Notifiche Digitali',
+            linkType: 'external',
+          },
+          {
+            ariaLabel:
+              'Vai al link: Piattaforma Digitale Nazionale Dati - Interoperabilità',
+            href: '#piattaforma-digitale-nazionale-dati-interoperabilita',
+            label: 'Piattaforma Digitale Nazionale Dati - Interoperabilità',
+            linkType: 'internal',
+          },
+          {
+            ariaLabel: 'Vai al link: Self Care',
+            href: '#self-care',
+            label: 'Self Care',
             linkType: 'internal',
           },
         ],
