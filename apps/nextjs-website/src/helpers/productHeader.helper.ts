6953a0784def21fdc8ebfb5e5a66b03bffc880ec
Marco Ponchia <marco.ponchia@uqido.com>
2023-06-29 16:54:45 +0200

    fix compile after review

--- apps/nextjs-website/src/helpers/productHeader.helper.ts
+++ apps/nextjs-website/src/helpers/productHeader.helper.ts
@@ -1,4 +1,3 @@
-import { Path } from '@/lib/types/path';
 import { Product } from '@/lib/types/product';
 import { MenuDropdownProp } from '@pagopa/pagopa-editorial-components/dist/components/Header/components/MenuDropdown';
 import { Theme } from '@pagopa/pagopa-editorial-components/dist/types/components';
@@ -12,7 +11,7 @@ export function productToMenuItems(
     .filter(([name, subpath]) => !!name && !!subpath)
     .map(([, subpath]) => {
       return {
-        label: subpath.name
+        label: subpath.name,
         href: subpath.path,
         active: path === subpath.path,
         theme,
