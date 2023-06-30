444611fdd1d8bc0d4ff69def9c9340b56eb49caf
Marco Bottaro <marco.bottaro@uqido.com>
2023-06-30 10:14:50 +0200

    Add ioSign guides

--- apps/nextjs-website/src/_contents/ioSign/ioSign.ts
+++ apps/nextjs-website/src/_contents/ioSign/ioSign.ts
@@ -1,8 +1,9 @@
 import { Product } from '@/lib/types/product';
 import { ioSignOverviewPath } from '@/_contents/ioSign/overviewPath';
+import { ioSignGuidesPath } from '@/_contents/ioSign/guidesPath';
 
 export const ioSign: Product = {
   name: 'Firma con IO',
   path: '/firma-con-io',
-  subpaths: { overview: ioSignOverviewPath },
+  subpaths: { overview: ioSignOverviewPath, guides: ioSignGuidesPath },
 };
--- apps/nextjs-website/src/_contents/products.ts
+++ apps/nextjs-website/src/_contents/products.ts
@@ -5,11 +5,12 @@ import { pagoPaGuides } from '@/_contents/pagoPa/guides';
 import { appIO } from './appIo/appIO';
 import { ioSign } from './ioSign/ioSign';
 import { pagoPa } from './pagoPa/pagoPa';
+import { ioSignGuides } from '@/_contents/ioSign/guides';
 
 export const overviews = [appIoOverview, ioSignOverview, pagoPaOverview];
 export const quickStarts = [];
 export const apis = [];
 export const tutorials = [];
 export const tools = [];
-export const productsGuides = [pagoPaGuides];
+export const productsGuides = [pagoPaGuides, ioSignGuides];
 export const products = [appIO, ioSign, pagoPa];
