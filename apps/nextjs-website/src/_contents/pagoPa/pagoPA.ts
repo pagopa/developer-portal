7abd297833ffc398e812bdeed2859040b7be120f
Marco Bottaro <marco.bottaro@uqido.com>
2023-06-30 09:57:52 +0200

    Add pagoPA Guides path

--- apps/nextjs-website/src/_contents/pagoPa/pagoPA.ts
+++ apps/nextjs-website/src/_contents/pagoPa/pagoPA.ts
@@ -1,8 +1,9 @@
 import { Product } from '@/lib/types/product';
 import { pagoPaOverviewPath } from '@/_contents/pagoPa/overviewPath';
+import { pagoPaGuidesPath } from '@/_contents/pagoPa/guidesPath';
 
 export const pagoPA: Product = {
   name: 'Piattaforma pagoPA',
   path: '/pago-pa',
-  subpaths: { overview: pagoPaOverviewPath },
+  subpaths: { overview: pagoPaOverviewPath, guides: pagoPaGuidesPath },
 };
