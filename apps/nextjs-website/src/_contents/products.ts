939843bb264b03a165613f416ee7b17831dec3ad
Marco Bottaro <marco.bottaro@uqido.com>
2023-06-30 10:40:12 +0200

    Add SEND to products

--- apps/nextjs-website/src/_contents/products.ts
+++ apps/nextjs-website/src/_contents/products.ts
@@ -1,15 +1,23 @@
 import { appIoOverview } from '@/_contents/appIo/overview';
 import { ioSignOverview } from '@/_contents/ioSign/overview';
 import { pagoPaOverview } from '@/_contents/pagoPa/overview';
+import { sendOverview } from '@/_contents/send/overview';
 import { pagoPaGuides } from '@/_contents/pagoPa/guides';
+import { sendGuides } from '@/_contents/send/guides';
 import { appIO } from './appIo/appIO';
 import { ioSign } from './ioSign/ioSign';
 import { pagoPa } from './pagoPa/pagoPa';
+import { send } from '@/_contents/send/send';
 
-export const overviews = [appIoOverview, ioSignOverview, pagoPaOverview];
+export const overviews = [
+  appIoOverview,
+  ioSignOverview,
+  pagoPaOverview,
+  sendOverview,
+];
 export const quickStarts = [];
 export const apis = [];
 export const tutorials = [];
 export const tools = [];
-export const productsGuides = [pagoPaGuides];
-export const products = [appIO, ioSign, pagoPa];
+export const productsGuides = [pagoPaGuides, sendGuides];
+export const products = [appIO, ioSign, pagoPa, send];
