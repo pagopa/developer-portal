018a0b4ed2cb5eab769b3d6dc4d63d68c8156691
marcobottaro <39835990+marcobottaro@users.noreply.github.com>
2023-07-03 18:23:59 +0200

    [DEV-267] - Add ioSign guides (#96)
    * Add ioSign guides
    
    * Rename ioSignGuides to ioSignGuideLists and replace italian-english hybrid with english only path
    
    * Add mobile image to ioSign guide

---
+++ apps/nextjs-website/src/_contents/ioSign/guideLists.ts
@@ -0,0 +1,39 @@
+import { ioSign } from '@/_contents/ioSign/ioSign';
+import { GuideListsData } from '@/lib/types/guideListsData';
+import { ioSignGuideListsPath } from '@/_contents/ioSign/guideListsPath';
+
+export const ioSignGuideLists: GuideListsData = {
+  ...ioSignGuideListsPath,
+  product: ioSign,
+  abstract: {
+    title: 'Richiedi la firma di contratti e documenti',
+    description:
+      'Con Firma con IO puoi inviare alle cittadine e ai cittadini documenti e contratti e richiedere loro di firmarli digitalmente in modo facile, veloce e sicuro.',
+  },
+  guidesSections: [
+    {
+      title: "Per l'utilizzo",
+      guides: [
+        {
+          title: 'Manuale operativo di Firma con IO',
+          description: {
+            title: 'Argomenti trattati',
+            listItems: [
+              'Scopri cos`è e come funziona Firma con IO',
+              'Crea un dossier e fallo firmare',
+              'Scopri le modalità di installazione e utilizzo di Firma con IO',
+              'Naviga fra le API',
+            ],
+          },
+          link: {
+            href: '#',
+            icon: '',
+            title: 'Vai al manuale',
+          },
+          imagePath: '/images/guida-tecnica.png',
+          mobileImagePath: '/images/guida-tecnica-mobile.png',
+        },
+      ],
+    },
+  ],
+};
