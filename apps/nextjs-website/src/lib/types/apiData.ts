bb210e5a30a2a3b0a5fd744fbaf0b09259c28db1
Jessica Corrias <corrias.jessica93@gmail.com>
2023-07-03 12:06:22 +0200

    fix: omit products in ApiData

--- apps/nextjs-website/src/lib/types/apiData.ts
+++ apps/nextjs-website/src/lib/types/apiData.ts
@@ -1,4 +1,4 @@
 import { Page } from '@/lib/types/page';
 import { ApiPageProps } from '@/pages/[productSlug]/api';
 
-export type ApiData = Page<ApiPageProps>;
+export type ApiData = Page<Omit<ApiPageProps, 'products'>>;
