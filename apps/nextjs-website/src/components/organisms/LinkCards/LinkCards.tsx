c8b063a0ece621d1ef2598b5da557b25568e4d42
Marco Ponchia <marco.ponchia@uqido.com>
2023-07-03 15:28:26 +0200

    add link cards component

---
+++ apps/nextjs-website/src/components/organisms/LinkCards/LinkCards.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Box, Grid, Typography, useTheme } from '@mui/material';
+import EContainer from '@pagopa/pagopa-editorial-components/dist/components/EContainer';
+import { translations } from '@/_contents/translations';
+import LinkCard from '@/components/molecules/LinkCard/LinkCard';
+import SectionTitle from '@/components/molecules/SectionTitle/SectionTitle';
+
+type LinkCardsProps = {
+  title: string;
+  subtitle: string;
+  ctaLabel: string;
+  href: string;
+  cardsTitle: string;
+  cards: {
+    title: string;
+    text: string;
+    href: string;
+  }[];
+};
+
+const LinkCards = ({
+  title,
+  subtitle,
+  ctaLabel,
+  href,
+  cardsTitle,
+  cards,
+}: LinkCardsProps) => {
+  const theme = useTheme();
+  const { shared } = translations;
+
+  return (
+    <>
+      <Box py={11} sx={{ backgroundColor: theme.palette.background.default }}>
+        <SectionTitle
+          title={title}
+          subtitle={subtitle}
+          ctaLabel={ctaLabel}
+          href={href}
+        >
+          <Typography
+            content='div'
+            mb={3}
+            mt={6}
+            color={theme.palette.grey[500]}
+            fontSize={14}
+            sx={{
+              fontWeight: theme.typography.fontWeightBold,
+              textTransform: 'uppercase',
+            }}
+          >
+            {cardsTitle}
+          </Typography>
+        </SectionTitle>
+        <EContainer>
+          <Box pb={4}>
+            <Grid container spacing={2}>
+              {cards.map(({ title, text, href }, index) => {
+                return (
+                  <Grid key={index} item xs={12} md={6} lg>
+                    <LinkCard
+                      minHeight={155}
+                      title={title}
+                      text={text}
+                      link={{
+                        href,
+                        label: shared.goToModel,
+                      }}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Box>
+        </EContainer>
+      </Box>
+    </>
+  );
+};
+
+export default LinkCards;
