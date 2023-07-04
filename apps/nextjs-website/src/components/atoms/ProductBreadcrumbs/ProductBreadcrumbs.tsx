15dee2efb8234625fb0c4fa2479ca5494223781e
Marco Ponchia <marco.ponchia@uqido.com>
2023-07-04 15:19:15 +0200

    add breadcrumbs to layout

--- apps/nextjs-website/src/components/atoms/ProductBreadcrumbs/ProductBreadcrumbs.tsx
+++ apps/nextjs-website/src/components/atoms/ProductBreadcrumbs/ProductBreadcrumbs.tsx
@@ -23,19 +23,13 @@ const ProductBreadcrumbs = ({ breadcrumbs }: ProductBreadcrumbsProps) => {
       <Box
         component={'div'}
         sx={{ display: { xs: 'none', md: 'block' } }}
-        mx={15}
-        mb={2}
-        mt={4}
+        ml={26}
+        my={2}
       >
-        <Breadcrumbs aria-label='breadcrumb'>
+        <Breadcrumbs sx={{ paddingTop: 2 }} aria-label='breadcrumb'>
           {breadcrumbs.map((breadcrumb, index) => {
             return index === breadcrumbs.length - 1 ? (
-              <Typography
-                key={index}
-                fontSize={16}
-                lineHeight={24}
-                fontWeight={600}
-              >
+              <Typography key={index} fontSize={16} fontWeight={600}>
                 {breadcrumb.name}
               </Typography>
             ) : (
@@ -44,7 +38,6 @@ const ProductBreadcrumbs = ({ breadcrumbs }: ProductBreadcrumbsProps) => {
                 component={Link}
                 underline='hover'
                 fontSize={16}
-                lineHeight={24}
                 color={
                   index === 0
                     ? theme.palette.text.primary
--- apps/nextjs-website/src/helpers/breadcrumbs.helpers.ts
+++ apps/nextjs-website/src/helpers/breadcrumbs.helpers.ts
@@ -4,8 +4,14 @@ import { Product } from '@/lib/types/product';
 
 export function productPageToBreadcrumbs(
   product: Product,
+  path?: string,
   paths?: readonly Path[]
 ): readonly Path[] {
+  const subpath = [
+    ...Object.entries(product.subpaths)
+      .filter((entry) => path?.includes(entry[1].path))
+      .map((entry) => entry[1]),
+  ];
   return [
     {
       name: translations.breadcrumbs.title,
@@ -13,8 +19,9 @@ export function productPageToBreadcrumbs(
     },
     {
       name: product.name,
-      path: product.path,
+      path: product.subpaths.overview.path,
     },
+    ...subpath,
     ...(paths || []),
   ];
 }
