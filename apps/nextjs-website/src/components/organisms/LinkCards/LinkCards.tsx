4d56f97493e61f4a67edfd1e6d7253f1543b1854
Marco Ponchia <marco.ponchia@uqido.com>
2023-07-04 11:28:15 +0200

    Fix lint issue

--- apps/nextjs-website/src/components/organisms/LinkCards/LinkCards.tsx
+++ apps/nextjs-website/src/components/organisms/LinkCards/LinkCards.tsx
@@ -30,49 +30,49 @@ const LinkCards = ({
   const { shared } = translations;
 
   return (
-      <Box py={11} sx={{ backgroundColor: theme.palette.background.default }}>
-        <SectionTitle
-          title={title}
-          subtitle={subtitle}
-          ctaLabel={ctaLabel}
-          href={href}
+    <Box py={11} sx={{ backgroundColor: theme.palette.background.default }}>
+      <SectionTitle
+        title={title}
+        subtitle={subtitle}
+        ctaLabel={ctaLabel}
+        href={href}
+      >
+        <Typography
+          content='div'
+          mb={3}
+          mt={6}
+          color={theme.palette.grey[500]}
+          fontSize={14}
+          sx={{
+            fontWeight: theme.typography.fontWeightBold,
+            textTransform: 'uppercase',
+          }}
         >
-          <Typography
-            content='div'
-            mb={3}
-            mt={6}
-            color={theme.palette.grey[500]}
-            fontSize={14}
-            sx={{
-              fontWeight: theme.typography.fontWeightBold,
-              textTransform: 'uppercase',
-            }}
-          >
-            {cardsTitle}
-          </Typography>
-        </SectionTitle>
-        <EContainer>
-          <Box pb={4}>
-            <Grid container spacing={2}>
-              {cards.map(({ title, text, href }, index) => {
-                return (
-                  <Grid key={index} item xs={12} md={6} lg>
-                    <LinkCard
-                      minHeight={155}
-                      title={title}
-                      text={text}
-                      link={{
-                        href,
-                        label: shared.goToModel,
-                      }}
-                    />
-                  </Grid>
-                );
-              })}
-            </Grid>
-          </Box>
-        </EContainer>
-      </Box>
+          {cardsTitle}
+        </Typography>
+      </SectionTitle>
+      <EContainer>
+        <Box pb={4}>
+          <Grid container spacing={2}>
+            {cards.map(({ title, text, href }, index) => {
+              return (
+                <Grid key={index} item xs={12} md={6} lg>
+                  <LinkCard
+                    minHeight={155}
+                    title={title}
+                    text={text}
+                    link={{
+                      href,
+                      label: shared.goToModel,
+                    }}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
+      </EContainer>
+    </Box>
   );
 };
 
