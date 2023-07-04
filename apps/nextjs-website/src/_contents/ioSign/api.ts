73f1439373e39198055c732cc332a0006f3fa160
Jessica Corrias <corrias.jessica93@gmail.com>
2023-07-04 15:07:42 +0200

    [DEV-611]: add API page in "Firma con IO" (#113)
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>

---
+++ apps/nextjs-website/src/_contents/ioSign/api.ts
@@ -0,0 +1,24 @@
+import { ApiData } from '@/lib/types/apiData';
+import { ioSign } from '@/_contents/ioSign/ioSign';
+import { ioSignApiPath } from '@/_contents/ioSign/apiPath';
+
+export const ioSignApi: ApiData = {
+  ...ioSignApiPath,
+  product: ioSign,
+  specURL:
+    'https://raw.githubusercontent.com/pagopa/io-sign/latest/apps/io-func-sign-issuer/openapi.yaml',
+  bannerLinks: [
+    {
+      theme: 'dark',
+      title: "Serve aiuto durante l'integrazione?",
+      decoration: 'HeadsetMic',
+      body: 'Scrivi un’e-mail in cui descrivi il tuo problema o dubbio all’indirizzo <strong>firmaconio-tech@pagopa.it</strong>',
+    },
+    {
+      theme: 'light',
+      title: 'Assistenza continua',
+      decoration: 'Feedback',
+      body: "Per problemi o dubbi dopo l'integrazione, scrivici all'indirizzo <strong>enti-firmaconio@assistenza.pagopa.it</strong>",
+    },
+  ],
+};
