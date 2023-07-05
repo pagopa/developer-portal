46323361418131807c964cb8ea72e102d35f1463
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-05 17:20:40 +0200

    [DEV-636] - Add pagoPA overview (#127)

--- apps/nextjs-website/src/_contents/pagoPa/overview.ts
+++ apps/nextjs-website/src/_contents/pagoPa/overview.ts
@@ -1,6 +1,8 @@
 import { OverviewData } from '@/lib/types/overviewData';
 import { pagoPa } from '@/_contents/pagoPa/pagoPa';
 import { pagoPaOverviewPath } from '@/_contents/pagoPa/overviewPath';
+import { ioSign } from '@/_contents/ioSign/ioSign';
+import { tutorials as pagoPaTutorials } from '@/_contents/pagoPa/tutorials';
 
 export const pagoPaOverview: OverviewData = {
   ...pagoPaOverviewPath,
@@ -10,11 +12,98 @@ export const pagoPaOverview: OverviewData = {
     backgroundImage: '/images/hero.jpg',
     title: 'La piattaforma per gestire e rendicontare i pagamenti',
     subtitle:
-      'pagoPA permette alle Pubbliche Amministrazioni di gestire gli incassi in modo centralizzato, offrendo servizi automatici di rendicontazione e riconciliazione con un significativo risparmio nei costi di gestione.',
+      'pagoPA permette agli Enti Creditori di gestire gli incassi in modo centralizzato, offrendo servizi automatici di rendicontazione e riconciliazione con un significativo risparmio nei costi di gestione.',
   },
   feature: {
-    title: 'title',
-    subtitle: 'subtitle',
-    items: [],
+    title: 'Perché usare pagoPA',
+    subtitle:
+      'Che tu sia un ente pubblico, privato, centrale o locale, tramite pagoPA potrai:',
+    items: [
+      {
+        stackIcon: {
+          icon: 'AccountBalance',
+        },
+        subtitle:
+          'Un unico accordo quadro che abilita EC e PSP a operare e definisce gli standard di comunicazione, senza la necessità di stipulare accordi multipli',
+        title: 'Un unico accordo per Enti Creditori e PSP',
+      },
+      {
+        stackIcon: {
+          icon: 'Payment',
+        },
+        subtitle:
+          'Una gestione centralizzata degli incassi per gli Enti Creditori, tramite servizi automatici di rendicontazione e riconciliazione',
+        title: 'Un modo semplice per gestire i pagamenti',
+      },
+      {
+        stackIcon: {
+          icon: 'PeopleAlt',
+        },
+        subtitle:
+          "Accesso diretto al settore dei pagamenti per i servizi pubblici e possibilità di promuovere la tua offerta multicanale da un'unica piattaforma",
+        title: 'Milioni di cittadini e imprese raggiunti',
+      },
+      {
+        stackIcon: {
+          icon: 'TrendingUp',
+        },
+        subtitle:
+          'Esiti immediati e riduzione di tempi e costi sviluppo IT per gli Enti Creditori. Risparmio e accesso ad un più regolato mercato dei pagamenti per i PSP',
+        title: 'Più efficienza su tempi  e costi',
+      },
+    ],
   },
+  startCards: [
+    {
+      title: 'Quick Start',
+      text: 'Cinque semplici passi per aderire a Nuova connettività',
+      href: '/',
+      iconName: 'FlagOutlined',
+    },
+    {
+      title: 'Documentazione API REST',
+      text: 'Esplora le API Rest per la gestione delle posizioni debitorie',
+      href: ioSign.subpaths.api?.path ?? '#',
+      iconName: 'FolderOutlined',
+    },
+    {
+      title: 'Documentazione  SOAP',
+      text: 'Consulta tutti gli schemi XSD e WSDL che seguono le diverse release SANP',
+      href: '#',
+      iconName: 'FolderOutlined',
+    },
+  ],
+  tutorials: pagoPaTutorials,
+  relatedLinks: [
+    {
+      path: 'https://survey.pagopa.it/238313',
+      name: 'Contribuisci al miglioramento della Tassonomia',
+    },
+    {
+      path: 'https://docs.pagopa.it/linee-guida-brand-pagopa/',
+      name: 'Consulta le linee guida brand pagoPA',
+    },
+    {
+      path: 'https://www.gazzettaufficiale.it/eli/id/2018/07/03/18A04494/sg',
+      name: 'Consulta le linee Guida Gazzetta Ufficiale',
+    },
+    {
+      path: 'https://docs.pagopa.it/portale-delle-adesioni/portale-delle-adesioni',
+      name: 'Leggi il documento sul Portale delle Adesioni predisposto per gli Enti Creditori',
+    },
+  ],
+  bannerLinks: [
+    {
+      theme: 'dark',
+      title: 'Hai bisogno di aiuto?',
+      decoration: 'HeadsetMic',
+      body: 'Apri un ticket utilizzando l’apposita funzione all’interno della tua area riservata di Self Care.',
+    },
+    {
+      theme: 'light',
+      title: 'Dicci cosa ne pensi',
+      decoration: 'Feedback',
+      body: 'Per chiarimenti sulle specifiche d’implementazione di pagoPA, come ad esempio SACI e SANP, puoi aprire una segnalazione su GitHub',
+    },
+  ],
 };
---
+++ apps/nextjs-website/src/_contents/pagoPa/tutorials.ts
@@ -0,0 +1,22 @@
+import { Tutorial } from '@/lib/types/tutorialData';
+
+export const tutorials: readonly Tutorial[] = [
+  {
+    title: 'Come richiedere pagamenti che contengono marca da bollo digitale',
+    dateString: '2023-06-29T22:15:53.780Z',
+    path: '/pago-pa/tutorial/1',
+    name: 'tutorial 1',
+  },
+  {
+    title: 'Come avviare un esercizio come Ente Creditore su pagoPA',
+    dateString: '2023-06-29T22:15:53.780Z',
+    path: '/pago-pa/tutorial/2',
+    name: 'tutorial 2',
+  },
+  {
+    title: 'Come stampare un avviso di pagamento in formato PDF',
+    dateString: '2023-06-29T22:15:53.780Z',
+    path: '/pago-pa/tutorial/3',
+    name: 'tutorial 3',
+  },
+];
