57122fae60e468a9260767406b40d042ad81ed58
AF <alessandro.ferlin@pagopa.it>
2023-07-05 14:47:02 +0200

    Update jest.config.ts

--- packages/gitbook-docs/jest.config.ts
+++ packages/gitbook-docs/jest.config.ts
@@ -2,11 +2,10 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.+(ts|tsx)'],
-  moduleFileExtensions: ['ts', 'js', 'tsx'],
+  testMatch: ['**/__tests__/**/*.+(ts)'],
+  moduleFileExtensions: ['ts', 'js'],
   transform: {
-    // See the discussion https://github.com/vercel/next.js/issues/8663
-    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+    '^.+\\.(ts)$': 'ts-jest',
   },
   coverageThreshold: {
     global: {
