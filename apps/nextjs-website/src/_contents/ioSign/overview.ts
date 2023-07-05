c10e1a8b889008c290004fb0215be4fe7cf4a46d
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-05 14:33:51 +0200

    Add banner links to io-sign overview

--- apps/nextjs-website/src/_contents/ioSign/overview.ts
+++ apps/nextjs-website/src/_contents/ioSign/overview.ts
@@ -59,4 +59,18 @@ export const ioSignOverview: OverviewData = {
     },
   ],
   tutorials: tutorials,
+  bannerLinks: [
+    {
+      theme: 'dark',
+      title: 'Serve aiuto durante l’integrazione?',
+      decoration: 'LiveHelp',
+      body: 'Scrivi un’e-mail in cui descrivi il tuo problema o dubbio all’indirizzo <strong>firmaconio-tech@pagopa.it</strong>',
+    },
+    {
+      theme: 'light',
+      title: 'Assistenza continua',
+      decoration: 'HeadsetMic',
+      body: 'Per problemi o dubbi dopo l’integrazione, scrivici all’indirizzo <strong>enti-firmaconio@assistenza.pagopa.it</strong>',
+    },
+  ],
 };
