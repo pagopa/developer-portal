8939476395c5e92170ab6cb4ca2ccc8a41054daf
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-05 12:50:00 +0200

    [DEV-699] - Add App IO guide index page (#121)

---
+++ apps/nextjs-website/src/_contents/appIo/guideLists.ts
@@ -0,0 +1,121 @@
+import { GuideListsData } from '@/lib/types/guideListsData';
+import { appIO } from '@/_contents/appIo/appIO';
+import { appIOGuideListsPath } from '@/_contents/appIo/guideListsPath';
+
+export const appIOGuideLists: GuideListsData = {
+  ...appIOGuideListsPath,
+  product: appIO,
+  abstract: {
+    title: 'Guide e manuali',
+    description:
+      'Per una conoscenza approfondita o dubbi puntuali, consulta i manuali e le guide disponibili per l’app IO.',
+  },
+  guidesSections: [
+    {
+      title: "Per l'integrazione",
+      guides: [
+        {
+          title: 'Guida Tecnica all’integrazione dei servizi',
+          description: {
+            title: 'Argomenti trattati',
+            listItems: [
+              'Scopri come aderire',
+              'Crea e pubblica un servizio',
+              'Testa e invia un messaggio',
+              'Esegui test sulle funzionalità',
+            ],
+          },
+          link: {
+            href: '#',
+            label: 'Vai alla guida',
+          },
+          imagePath: '/images/guida-tecnica.png',
+          mobileImagePath: '/images/guida-tecnica-mobile.png',
+        },
+      ],
+    },
+    {
+      title: "Per l'utilizzo",
+      guides: [
+        {
+          title: 'Manuale dei servizi dell’app IO',
+          description: {
+            title: 'Argomenti trattati',
+            listItems: [
+              'Dai il nome al servizio e scrivi la sua scheda',
+              'Scopri quali e che tipo di messaggi puoi inviare',
+              'Leggi le indicazioni sul tono di voce',
+              'Usa i modelli dei servizi più comuni',
+            ],
+          },
+          link: {
+            href: '#',
+            label: 'Vai al manuale',
+          },
+          imagePath: '/images/manuale-dei-servizi.png',
+          mobileImagePath: '/images/manuale-dei-servizi-mobile.png',
+        },
+        {
+          title: 'Supporto agli Enti',
+          description: {
+            title: 'Argomenti trattati',
+            listItems: [
+              'Domande e risposte su Adesione,Accordi e Assistenza',
+              'Tutorial e Domande Frequenti sui Servizi',
+              'Tutorial e Domande Frequenti sui Messaggi',
+              'Tutorial e Domande Frequenti sui Pagamenti',
+            ],
+          },
+          link: {
+            href: '#',
+            label: 'Vai al manuale',
+          },
+          imagePath: '/images/manuale-dei-servizi.png',
+          mobileImagePath: '/images/manuale-dei-servizi-mobile.png',
+        },
+        {
+          title: 'Kit di comunicazione',
+          description: {
+            title: 'Argomenti trattati',
+            listItems: [
+              'Crea una campagna di sensibilizzazione',
+              'Usa i modelli grafici, copy e social',
+              'Leggi i suggerimenti per una comunicazione efficace',
+              'Raccontaci la tua esperienza',
+            ],
+          },
+          link: {
+            href: '#',
+            label: 'Vai al kit',
+          },
+          imagePath: '/images/kit-di-comunicazione.png',
+          mobileImagePath: '/images/kit-di-comunicazione-mobile.png',
+        },
+      ],
+    },
+    {
+      title: 'Programmi e iniziative',
+      guides: [
+        {
+          title: 'Carta Giovani Nazionale',
+          description: {
+            title: 'Argomenti trattati',
+            listItems: [
+              'Accreditati come operatore',
+              'Identifica i beneficiari',
+              'Crea e gestisci un’agevolazione',
+              'Modifica e integra le informazioni',
+            ],
+          },
+          link: {
+            href: '#',
+            label: 'Vai al manuale',
+          },
+          imagePath: '/images/manuale-di-carta-giovani-nazionale.png',
+          mobileImagePath:
+            '/images/manuale-di-carta-giovani-nazionale-mobile.png',
+        },
+      ],
+    },
+  ],
+};
