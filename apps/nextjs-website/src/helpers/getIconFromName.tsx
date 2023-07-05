bfebbbea3369265b82d9c1964790444587c967af
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-05 15:08:54 +0200

    Handle icons for SEND

--- apps/nextjs-website/src/helpers/getIconFromName.tsx
+++ apps/nextjs-website/src/helpers/getIconFromName.tsx
@@ -1,7 +1,17 @@
 import HeadsetMicIcon from '@mui/icons-material/HeadsetMic';
 import FeedbackIcon from '@mui/icons-material/Feedback';
+import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import TimerSharpIcon from '@mui/icons-material/TimerSharp';
+import ApprovalIcon from '@mui/icons-material/Approval';
 
-export type IconName = 'HeadsetMic' | 'Feedback';
+export type IconName =
+  | 'HeadsetMic'
+  | 'Feedback'
+  | 'MarkEmailRead'
+  | 'QueryStats'
+  | 'TimerSharp'
+  | 'Approval';
 
 type IconFromNameParams = {
   type: IconName;
@@ -19,6 +29,14 @@ export const getIconFromName = ({
       return <HeadsetMicIcon sx={{ color: fill, fontSize: size }} />;
     case 'Feedback':
       return <FeedbackIcon sx={{ color: fill, fontSize: size }} />;
+    case 'MarkEmailRead':
+      return <MarkEmailReadIcon sx={{ color: fill, fontSize: size }} />;
+    case 'QueryStats':
+      return <QueryStatsIcon sx={{ color: fill, fontSize: size }} />;
+    case 'TimerSharp':
+      return <TimerSharpIcon sx={{ color: fill, fontSize: size }} />;
+    case 'Approval':
+      return <ApprovalIcon sx={{ color: fill, fontSize: size }} />;
     default:
       return <></>;
   }
