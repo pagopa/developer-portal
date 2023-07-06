25c9c2d4962add5dabb5489db4a483d3742f26a9
Marco Ponchia <ponchia.marco1994@gmail.com>
2023-07-06 16:18:35 +0200

    Refactor overview content translation (#138)
    Co-authored-by: Marco Comi <9998393+kin0992@users.noreply.github.com>

--- apps/nextjs-website/src/components/organisms/TutorialsOverview/TutorialsOverview.tsx
+++ apps/nextjs-website/src/components/organisms/TutorialsOverview/TutorialsOverview.tsx
@@ -5,21 +5,29 @@ import { Path } from '@/lib/types/path';
 import News from '@/components/organisms/News/News';
 
 type TutorialsOverviewProps = {
+  title: string;
+  subtitle: string;
+  ctaLabel: string;
   tutorialPath: Path;
   tutorials: Tutorial[];
 };
 
 const TutorialsOverview = ({
+  title,
+  subtitle,
+  ctaLabel,
   tutorials,
   tutorialPath,
 }: TutorialsOverviewProps) => {
-  const { overview, shared } = translations;
+  const { shared } = translations;
   return (
     <News
-      title={overview.tutorial.title}
-      subtitle={overview.tutorial.subtitle}
-      ctaLabel={overview.tutorial.ctaLabel}
-      href={tutorialPath.path}
+      title={title}
+      subtitle={subtitle}
+      cta={{
+        label: ctaLabel,
+        href: tutorialPath.path,
+      }}
       cards={tutorials.map((tutorial) => ({
         ...tutorial,
         href: {
