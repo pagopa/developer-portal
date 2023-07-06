903b9d42f9726ecbe4e503597de90a2c446ec186
Marco Ponchia <marco.ponchia@uqido.com>
2023-07-06 12:42:28 +0200

    Fix navigation

--- apps/nextjs-website/src/components/molecules/HeroSwiper/HeroSwiper.tsx
+++ apps/nextjs-website/src/components/molecules/HeroSwiper/HeroSwiper.tsx
@@ -1,35 +1,42 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useCallback, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
 import NavigationArrow from '@/components/atoms/NavigationArrow/NavigationArrow';
 import CtaSlide, { CtaSlideProps } from '@/components/atoms/CtaSlide/CtaSlide';
+import { Swiper as SwiperCore } from 'swiper';
 
 export type HeroSwiperProps = {
   cards: CtaSlideProps[];
 };
 
 const HeroSwiper = ({ cards }: HeroSwiperProps) => {
+  const [swiperCore, setSwiperCore] = useState<SwiperCore>();
   const [swiperIndex, setSwiperIndex] = useState(0);
-  const navigationPrevRef = useRef(null);
-  const navigationNextRef = useRef(null);
+
+  const nextSlide = useCallback(() => {
+    swiperCore?.slideTo(swiperIndex + 1);
+  }, [swiperCore, swiperIndex]);
+
+  const previusSlide = useCallback(() => {
+    swiperCore?.slideTo(swiperIndex - 1);
+  }, [swiperCore, swiperIndex]);
 
   return (
     <Swiper
-      initialSlide={swiperIndex}
-      navigation={{
-        prevEl: navigationPrevRef.current,
-        nextEl: navigationNextRef.current,
+      onInit={(swiper) => {
+        setSwiperCore(swiper);
       }}
+      initialSlide={swiperIndex}
       onActiveIndexChange={(swiperCore) => {
         setSwiperIndex(swiperCore.activeIndex);
       }}
       pagination={{ clickable: true }}
       modules={[Navigation, Pagination]}
     >
-      <div ref={navigationPrevRef}>
+      <div onClick={() => previusSlide()}>
         <NavigationArrow direction={'left'} hidden={swiperIndex === 0} />
       </div>
-      <div ref={navigationNextRef}>
+      <div onClick={() => nextSlide()}>
         <NavigationArrow
           direction={'right'}
           hidden={swiperIndex === cards.length - 1}
