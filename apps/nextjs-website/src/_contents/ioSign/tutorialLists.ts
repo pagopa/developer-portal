a967752f597cddf16af5f8cf90d67eefaa728f12
Marco Bottaro <marco.bottaro@uqido.com>
2023-07-06 19:01:21 +0200

    Fix paths

--- apps/nextjs-website/src/_contents/ioSign/tutorialLists.ts
+++ apps/nextjs-website/src/_contents/ioSign/tutorialLists.ts
@@ -7,19 +7,19 @@ export const ioSignTutorials: readonly Tutorial[] = [
   {
     title: 'Come creare e preparare il documento da firmare digitalmente',
     dateString: '2023-06-29T22:15:53.780Z',
-    path: '/io-sign/tutorials/1',
+    path: `${ioSignTutorialListsPath.path}/1`,
     name: 'tutorial 1',
   },
   {
     title: 'Come creare il Dossier per la richiesta di firma',
     dateString: '2023-06-29T22:15:53.780Z',
-    path: '/io-sign/tutorials/2',
+    path: `${ioSignTutorialListsPath.path}/2`,
     name: 'tutorial 2',
   },
   {
     title: 'Come effettuare l’upload dei documenti',
     dateString: '2023-06-29T22:15:53.780Z',
-    path: '/io-sign/tutorials/3',
+    path: `${ioSignTutorialListsPath.path}/3`,
     name: 'tutorial 3',
   },
 ];
