08a61b67c97aa90d41ef0224fabf70048d445c93
Marco Bottaro <marco.bottaro@uqido.com>
2023-07-06 19:01:21 +0200

    Fix paths

--- apps/nextjs-website/src/_contents/pagoPa/tutorialLists.ts
+++ apps/nextjs-website/src/_contents/pagoPa/tutorialLists.ts
@@ -7,19 +7,19 @@ export const pagoPaTutorials: readonly Tutorial[] = [
   {
     title: 'Come richiedere pagamenti che contengono marca da bollo digitale',
     dateString: '2023-06-29T22:15:53.780Z',
-    path: '/pago-pa/tutorials/come-richiedere-pagamenti-che-contengono-marca-da-bollo-digitale',
+    path: `${pagoPaTutorialListsPath.path}/come-richiedere-pagamenti-che-contengono-marca-da-bollo-digitale`,
     name: 'Come richiedere pagamenti che contengono marca da bollo digitale',
   },
   {
     title: 'Come avviare un esercizio come Ente Creditore su pagoPA',
     dateString: '2023-06-29T22:15:53.780Z',
-    path: '/pago-pa/tutorials/come-avviare-un-esercizio-come-ente-creditore-su-pagopa',
+    path: `${pagoPaTutorialListsPath.path}/come-avviare-un-esercizio-come-ente-creditore-su-pagopa`,
     name: 'Come avviare un esercizio come Ente Creditore su pagoPA',
   },
   {
     title: 'Come stampare un avviso di pagamento in formato PDF',
     dateString: '2023-06-29T22:15:53.780Z',
-    path: '/pago-pa/tutorials/come-stampare-un-avviso-di-pagamento-in-formato-pdf',
+    path: `${pagoPaTutorialListsPath.path}/come-stampare-un-avviso-di-pagamento-in-formato-pdf`,
     name: 'Come stampare un avviso di pagamento in formato PDF',
   },
 ];
