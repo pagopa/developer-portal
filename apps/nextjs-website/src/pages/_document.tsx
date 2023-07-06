0dfa9de184935f01ab3756e52c01d464b1293e8c
Jessica Corrias <corrias.jessica93@gmail.com>
2023-07-06 16:09:51 +0200

    feat: add matomo analytics

--- apps/nextjs-website/src/pages/_document.tsx
+++ apps/nextjs-website/src/pages/_document.tsx
@@ -1,14 +1,36 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const matomo = `
+  var _paq = (window._paq = window._paq || []);
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+  (function () {
+    var u = "https://pagopa.matomo.cloud/";
+    _paq.push(["setTrackerUrl", u + "matomo.php"]);
+    _paq.push(["setSiteId", "8"]);
+    var d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0];
+    g.async = true;
+    g.src = "//cdn.matomo.cloud/pagopa.matomo.cloud/matomo.js";
+    s.parentNode.insertBefore(g, s);
+  })();
+  `;
+
   return (
     <Html lang='en'>
       <Head>
         <meta name='robots' content='noindex,nofollow' />
+        <script
+          key='script-matomo'
+          dangerouslySetInnerHTML={{ __html: matomo }}
+        />
       </Head>
       <body>
         <Main />
-        <NextScript />
+        <NextScript></NextScript>
       </body>
     </Html>
   );
