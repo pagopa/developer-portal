11d6796f05fd9f9cc7db40823f7145d260208624
Marco Ponchia <marco.ponchia@uqido.com>
2023-07-07 13:26:36 +0200

    remove divider

--- apps/nextjs-website/src/components/atoms/ProductBreadcrumbs/ProductBreadcrumbs.tsx
+++ apps/nextjs-website/src/components/atoms/ProductBreadcrumbs/ProductBreadcrumbs.tsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Breadcrumbs,
-  Divider,
   Link as MuiLink,
   Typography,
   useTheme,
@@ -19,40 +18,37 @@ type ProductBreadcrumbsProps = {
 const ProductBreadcrumbs = ({ breadcrumbs }: ProductBreadcrumbsProps) => {
   const theme = useTheme();
   return (
-    <>
-      <Box
-        component={'div'}
-        sx={{ display: { xs: 'none', md: 'block' } }}
-        ml={18}
-        my={2}
-      >
-        <Breadcrumbs sx={{ paddingTop: 2 }} aria-label='breadcrumb'>
-          {breadcrumbs.map((breadcrumb, index) => {
-            return index === breadcrumbs.length - 1 ? (
-              <Typography key={index} fontSize={16} fontWeight={600}>
-                {breadcrumb.name}
-              </Typography>
-            ) : (
-              <MuiLink
-                key={index}
-                component={Link}
-                underline='hover'
-                fontSize={16}
-                color={
-                  index === 0
-                    ? theme.palette.text.primary
-                    : theme.palette.text.disabled
-                }
-                href={breadcrumb.path}
-              >
-                {breadcrumb.name}
-              </MuiLink>
-            );
-          })}
-        </Breadcrumbs>
-      </Box>
-      <Divider />
-    </>
+    <Box
+      component={'div'}
+      sx={{ display: { xs: 'none', md: 'block' } }}
+      ml={18}
+      my={2}
+    >
+      <Breadcrumbs sx={{ paddingTop: 2 }} aria-label='breadcrumb'>
+        {breadcrumbs.map((breadcrumb, index) => {
+          return index === breadcrumbs.length - 1 ? (
+            <Typography key={index} fontSize={16} fontWeight={600}>
+              {breadcrumb.name}
+            </Typography>
+          ) : (
+            <MuiLink
+              key={index}
+              component={Link}
+              underline='hover'
+              fontSize={16}
+              color={
+                index === 0
+                  ? theme.palette.text.primary
+                  : theme.palette.text.disabled
+              }
+              href={breadcrumb.path}
+            >
+              {breadcrumb.name}
+            </MuiLink>
+          );
+        })}
+      </Breadcrumbs>
+    </Box>
   );
 };
 
