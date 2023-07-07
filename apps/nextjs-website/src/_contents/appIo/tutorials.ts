c4196af6dbd51441991e6b35833fe6653533d501
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-07 15:55:08 +0200

    Change images

--- apps/nextjs-website/src/_contents/appIo/tutorials.ts
+++ apps/nextjs-website/src/_contents/appIo/tutorials.ts
@@ -2,31 +2,9 @@ import { pipe } from 'fp-ts/lib/function';
 import * as E from 'fp-ts/lib/Either';
 import * as RA from 'fp-ts/lib/ReadonlyArray';
 import { docsAssetsPath, docsPath } from '@/config';
-import { Tutorial } from '@/lib/types/tutorialData';
 import { parseDoc } from 'gitbook-docs/parseDoc';
 import { appIo } from '@/_contents/appIo/appIo';
 import { appIoBannerLinks } from '@/_contents/appIo/bannerLinks';
-
-export const tutorials: readonly Tutorial[] = [
-  {
-    title: 'Quali sono i possibili accordi di adesione all’app IO',
-    dateString: '2023-06-29T22:15:53.780Z',
-    path: '/app-io/tutorial/quale-accordo-di-adesione-scegliere',
-    name: 'Quale accordo di adesione scegliere',
-  },
-  {
-    title: 'Come inviare un messaggio con un avviso di pagamento',
-    dateString: '2023-06-29T22:15:53.780Z',
-    path: '/app-io/tutorial/come-spedire-un-avviso-di-pagamento-in-un-messaggio',
-    name: 'Come spedire un avviso di pagamento in un messaggio',
-  },
-  {
-    title: 'Come allegare documenti a un messaggio',
-    dateString: '2023-06-29T22:15:53.780Z',
-    path: '/app-io/tutorial/come-allegare-documenti-a-un-messaggio',
-    name: 'Come allegare documenti a un messaggio',
-  },
-];
 
 export const appIoTutorials = pipe(
   [
