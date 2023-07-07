10660b8321fcccb3f1ccf53efa850ae029ed70a1
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-07 14:23:23 +0200

    Add strong tag

--- apps/nextjs-website/src/_contents/pagoPa/overview.ts
+++ apps/nextjs-website/src/_contents/pagoPa/overview.ts
@@ -131,13 +131,13 @@ export const pagoPaOverview: OverviewData = {
       theme: 'dark',
       title: 'Hai bisogno di aiuto?',
       decoration: 'HeadsetMic',
-      body: 'Apri un ticket utilizzando l’apposita funzione all’interno della tua area riservata di Self Care.',
+      body: 'Apri un ticket utilizzando l’apposita funzione all’interno della tua <strong>area riservata di Self Care</strong>.',
     },
     {
       theme: 'light',
       title: 'Dicci cosa ne pensi',
       decoration: 'Feedback',
-      body: 'Per chiarimenti sulle specifiche d’implementazione di pagoPA, come ad esempio SACI e SANP, puoi aprire una segnalazione su GitHub',
+      body: 'Per chiarimenti sulle specifiche d’implementazione di pagoPA, come ad esempio SACI e SANP, puoi aprire una segnalazione su <strong>GitHub</strong>',
     },
   ],
 };
