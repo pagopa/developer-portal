1ea27dae1dfea8a6a9e989141e92f3bd3e86f10d
Marco Ponchia <marco.ponchia@uqido.com>
2023-07-12 10:08:11 +0200

    Fix header style

--- apps/nextjs-website/src/components/molecules/SiteHeader/SiteHeader.tsx
+++ apps/nextjs-website/src/components/molecules/SiteHeader/SiteHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { translations } from '@/_contents/translations';
 import Dropdown from '@/components/atoms/Dropdown/Dropdown';
 import { Product } from '@/lib/types/product';
-import { Divider, Stack } from '@mui/material';
+import { Box, Divider, Stack } from '@mui/material';
 import HomepageButton from '@/components/molecules/HomepageButton/HomepageButton';
 
 type SiteHeaderProps = {
@@ -13,22 +13,24 @@ const SiteHeader = ({ products }: SiteHeaderProps) => {
   const { header } = translations;
   return (
     <>
-      <Stack
-        sx={{ m: 2 }}
-        spacing={2}
-        direction='row'
-        justifyContent={{ sm: 'space-between', md: 'start' }}
-        alignItems='center'
-      >
-        <HomepageButton title={header.title} boldTitle={header.boldTitle} />
-        <Dropdown
-          label={header.products}
-          items={products.map((product) => ({
-            href: product.subpaths.overview.path,
-            label: product.name,
-          }))}
-        />
-      </Stack>
+      <Box paddingX={{ xs: 1, sm: 3 }}>
+        <Stack
+          sx={{ my: 2 }}
+          spacing={2}
+          direction='row'
+          justifyContent={{ sm: 'space-between', md: 'start' }}
+          alignItems='center'
+        >
+          <HomepageButton title={header.title} boldTitle={header.boldTitle} />
+          <Dropdown
+            label={header.products}
+            items={products.map((product) => ({
+              href: product.subpaths.overview.path,
+              label: product.name,
+            }))}
+          />
+        </Stack>
+      </Box>
       <Divider />
     </>
   );
