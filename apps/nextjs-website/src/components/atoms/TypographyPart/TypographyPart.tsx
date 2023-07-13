66f2636e4ecf3fa1f807400e5da521c61e661d84
Marco Bottaro <marco.bottaro@uqido.com>
2023-07-13 16:27:59 +0200

    Add margin to TypographyPart component

--- apps/nextjs-website/src/components/atoms/TypographyPart/TypographyPart.tsx
+++ apps/nextjs-website/src/components/atoms/TypographyPart/TypographyPart.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, useTheme } from '@mui/material';
 import { TypographyProps } from '@mui/material/Typography/Typography';
 
 export type TypographyPartProps = TypographyProps & { text: string };
 
 const TypographyPart = (props: TypographyPartProps) => {
-  return <Typography {...(props as TypographyProps)}>{props.text}</Typography>;
+  const { spacing } = useTheme();
+
+  return (
+    <Typography
+      {...(props as TypographyProps)}
+      sx={{ marginBottom: spacing(5) }}
+    >
+      {props.text}
+    </Typography>
+  );
 };
 
 export default TypographyPart;
