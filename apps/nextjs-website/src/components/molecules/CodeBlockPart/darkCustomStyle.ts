83fa65e337abf4f3b5629cd2301a60d57f0820a2
Marco Bottaro <marco.bottaro@uqido.com>
2023-07-13 20:29:15 +0200

    Add CodeBlockPart component

---
+++ apps/nextjs-website/src/components/molecules/CodeBlockPart/darkCustomStyle.ts
@@ -0,0 +1,198 @@
+export const darkCustomStyle: {
+  readonly [p: string]: React.CSSProperties;
+} = {
+  'code[class*="language-"]': {
+    background: '#414141',
+    color: '#eee',
+    fontFamily: 'Roboto Mono, monospace',
+    fontSize: '1em',
+    hyphens: 'none',
+    lineHeight: '1.5em',
+    MozHyphens: 'none',
+    MozTabSize: '4',
+    msHyphens: 'none',
+    OTabSize: '4',
+    tabSize: '4',
+    textAlign: 'left',
+    WebkitHyphens: 'none',
+    whiteSpace: 'pre',
+    wordBreak: 'normal',
+    wordSpacing: 'normal',
+    wordWrap: 'normal',
+  },
+  'pre[class*="language-"]': {
+    background: '#414141',
+    borderRadius: '0 0.375rem 0.375rem 0',
+    color: '#eee',
+    fontFamily: 'Roboto Mono, monospace',
+    fontSize: '1em',
+    hyphens: 'none',
+    lineHeight: '1.5em',
+    margin: '0.5em 0',
+    MozHyphens: 'none',
+    MozTabSize: '4',
+    msHyphens: 'none',
+    OTabSize: '4',
+    overflow: 'auto',
+    padding: '1.25em 1em',
+    position: 'relative',
+    tabSize: '4',
+    textAlign: 'left',
+    WebkitHyphens: 'none',
+    whiteSpace: 'pre',
+    wordBreak: 'normal',
+    wordSpacing: 'normal',
+    wordWrap: 'normal',
+  },
+  'code[class*="language-"]::-moz-selection': {
+    background: '#363636',
+  },
+  'pre[class*="language-"]::-moz-selection': {
+    background: '#363636',
+  },
+  'code[class*="language-"] ::-moz-selection': {
+    background: '#363636',
+  },
+  'pre[class*="language-"] ::-moz-selection': {
+    background: '#363636',
+  },
+  'code[class*="language-"]::selection': {
+    background: '#363636',
+  },
+  'pre[class*="language-"]::selection': {
+    background: '#363636',
+  },
+  'code[class*="language-"] ::selection': {
+    background: '#363636',
+  },
+  'pre[class*="language-"] ::selection': {
+    background: '#363636',
+  },
+  ':not(pre) > code[class*="language-"]': {
+    whiteSpace: 'normal',
+    borderRadius: '0.2em',
+    padding: '0.1em',
+  },
+  '.language-css > code': {
+    color: '#fd9170',
+  },
+  '.language-sass > code': {
+    color: '#fd9170',
+  },
+  '.language-scss > code': {
+    color: '#fd9170',
+  },
+  '[class*="language-"] .namespace': {
+    opacity: '0.7',
+  },
+  atrule: {
+    color: '#c792ea',
+  },
+  'attr-name': {
+    color: '#ffcb6b',
+  },
+  'attr-value': {
+    color: '#a5e844',
+  },
+  attribute: {
+    color: '#a5e844',
+  },
+  boolean: {
+    color: '#c792ea',
+  },
+  builtin: {
+    color: '#ffcb6b',
+  },
+  cdata: {
+    color: '#80cbc4',
+  },
+  char: {
+    color: '#80cbc4',
+  },
+  class: {
+    color: '#ffcb6b',
+  },
+  'class-name': {
+    color: '#f2ff00',
+  },
+  comment: {
+    color: '#616161',
+  },
+  constant: {
+    color: '#c792ea',
+  },
+  deleted: {
+    color: '#ff6666',
+  },
+  doctype: {
+    color: '#616161',
+  },
+  entity: {
+    color: '#ff6666',
+  },
+  function: {
+    color: '#c792ea',
+  },
+  hexcode: {
+    color: '#f2ff00',
+  },
+  id: {
+    color: '#c792ea',
+    fontWeight: 'bold',
+  },
+  important: {
+    color: '#c792ea',
+    fontWeight: 'bold',
+  },
+  inserted: {
+    color: '#80cbc4',
+  },
+  keyword: {
+    color: '#c792ea',
+  },
+  number: {
+    color: '#fd9170',
+  },
+  operator: {
+    color: '#89ddff',
+  },
+  prolog: {
+    color: '#616161',
+  },
+  property: {
+    color: '#80cbc4',
+  },
+  'pseudo-class': {
+    color: '#a5e844',
+  },
+  'pseudo-element': {
+    color: '#a5e844',
+  },
+  punctuation: {
+    color: '#89ddff',
+  },
+  regex: {
+    color: '#f2ff00',
+  },
+  selector: {
+    color: '#ff6666',
+  },
+  string: {
+    color: '#a5e844',
+  },
+  symbol: {
+    color: '#c792ea',
+  },
+  tag: {
+    color: '#ff6666',
+  },
+  unit: {
+    color: '#fd9170',
+  },
+  url: {
+    color: '#ff6666',
+  },
+  variable: {
+    color: '#ff6666',
+  },
+};
