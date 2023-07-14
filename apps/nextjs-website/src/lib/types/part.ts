99969a9dd20bf0c30fa1a1122402538bf9f02bd9
Marco Bottaro <marco.bottaro@uqido.com>
2023-07-14 10:09:20 +0200

    Fix prop types in Part type

--- apps/nextjs-website/src/lib/types/part.ts
+++ apps/nextjs-website/src/lib/types/part.ts
@@ -1,7 +1,14 @@
 import { ComponentType } from '@/lib/enums/componentType';
-import { TypographyProps } from '@mui/material/Typography/Typography';
+import { AlertPartProps } from '@/components/atoms/AlertPart/AlertPart';
+import { CodeBlockPartProps } from '@/components/molecules/CodeBlockPart/CodeBlockPart';
+import { InnerHtmlPartProps } from '@/components/atoms/InnerHtmlPart/InnerHtmlPart';
+import { TypographyPartProps } from '@/components/atoms/TypographyPart/TypographyPart';
 
 export type Part = {
   readonly componentType: ComponentType;
-  readonly props: (TypographyProps & { readonly text: string }) | unknown; // TODO: add contentType related prop types
+  readonly props:
+    | AlertPartProps
+    | CodeBlockPartProps
+    | InnerHtmlPartProps
+    | TypographyPartProps;
 };
