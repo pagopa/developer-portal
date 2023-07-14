064dc6a5d4ba686c8985f4075e5ca1064fddaeea
Marco Bottaro <marco.bottaro@uqido.com>
2023-07-14 15:15:17 +0200

    Fix part type

--- apps/nextjs-website/src/lib/types/part.ts
+++ apps/nextjs-website/src/lib/types/part.ts
@@ -1,16 +1,12 @@
-import { ComponentType } from '@/lib/enums/componentType';
 import { AlertPartProps } from '@/components/atoms/AlertPart/AlertPart';
 import { ApiTesterPartProps } from '@/components/organisms/ApiTesterPart/ApiTesterPart';
 import { CodeBlockPartProps } from '@/components/molecules/CodeBlockPart/CodeBlockPart';
 import { InnerHtmlPartProps } from '@/components/atoms/InnerHtmlPart/InnerHtmlPart';
 import { TypographyPartProps } from '@/components/atoms/TypographyPart/TypographyPart';
 
-export type Part = {
-  readonly componentType: ComponentType;
-  readonly props:
-    | AlertPartProps
-    | ApiTesterPartProps
-    | CodeBlockPartProps
-    | InnerHtmlPartProps
-    | TypographyPartProps;
-};
+export type Part =
+  | (AlertPartProps & { readonly componentType: 'alert' })
+  | (ApiTesterPartProps & { readonly componentType: 'apiTester' })
+  | (CodeBlockPartProps & { readonly componentType: 'codeBlock' })
+  | (InnerHtmlPartProps & { readonly componentType: 'innerHTML' })
+  | (TypographyPartProps & { readonly componentType: 'typography' });
