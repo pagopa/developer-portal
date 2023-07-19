681a70ebc7eba4becb69ceb2070877b57c0993c2
AF <4775679+datalek@users.noreply.github.com>
2023-07-19 12:43:28 +0200

    Add missing components to implement (#187)

--- packages/gitbook-docs/src/renderContent.ts
+++ packages/gitbook-docs/src/renderContent.ts
@@ -9,12 +9,24 @@ import { ParagraphProps } from './markdoc/schema/paragraph';
 import { HeadingProps } from './markdoc/schema/heading';
 
 export type RenderingComponents<A> = {
-  readonly Swagger: (props: SwaggerProps) => A;
   readonly Link: (props: LinkProps<A>) => A;
-  readonly Hint: (props: HintProps<A>) => A;
-  readonly Figure: (props: FigureProps<A>) => A;
   readonly Paragraph: (props: ParagraphProps<A>) => A;
   readonly Heading: (props: HeadingProps<A>) => A;
+  // readonly UnorderList: (props: UnorderedListProps<A>) => A;
+  // readonly OrderedList: (props: OrderedListProps<A>) => A;
+  // readonly TaskList: (props: TaskListProps<A>) => A;
+  readonly Hint: (props: HintProps<A>) => A;
+  // readonly Quote: (props: QuoteProps<A>) => A;
+  // readonly CodeBlock: (props: CodeBlockProps<A>) => A;
+  // readonly File: (props: FileProps<A>) => A;
+  readonly Figure: (props: FigureProps<A>) => A;
+  // readonly Embed: (props: EmbedProps<A>) => A;
+  // readonly Table: (props: TableProps<A>) => A;
+  // readonly Cards: (props: CardsProps<A>) => A;
+  // readonly Tabs: (props: TabsProps<A>) => A;
+  // readonly Expandable: (props: ExpandableProps<A>) => A;
+  readonly Swagger: (props: SwaggerProps) => A;
+  // readonly PageLink: (props: PageLinkProps<A>) => A;
 };
 
 export const renderContent = (
