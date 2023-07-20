ee45d7dc6e82bcbd5541e9b3f7854f76d0ed441c
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-20 16:04:44 +0200

    Simplify blockquote

--- packages/gitbook-docs/src/markdoc/schema/blockquote.ts
+++ packages/gitbook-docs/src/markdoc/schema/blockquote.ts
@@ -1,16 +1,16 @@
 import Markdoc, { Schema } from '@markdoc/markdoc';
 
+export type QuoteProps<A> = {
+  readonly children: A;
+};
+
 export const blockquote: Schema = {
   attributes: {},
   transform: (node, config) => {
-    const children = node.children
-      .find(({ type }) => type === 'paragraph')
-      ?.children.find(({ type }) => type === 'inline')
-      ?.transformChildren({
-        ...config,
-      });
-    return new Markdoc.Tag('Quote', node.transformAttributes(config), [
-      new Markdoc.Tag('Paragraph', {}, children),
-    ]);
+    return new Markdoc.Tag(
+      'Quote',
+      node.transformAttributes(config),
+      node.transformChildren(config)
+    );
   },
 };
