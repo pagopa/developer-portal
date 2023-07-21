98ea8c3614b4789fcb8d9062027ae12b07b786c7
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-21 09:13:31 +0200

    Simplify blockquote schema

--- packages/gitbook-docs/src/markdoc/schema/blockquote.ts
+++ packages/gitbook-docs/src/markdoc/schema/blockquote.ts
@@ -1,16 +1,9 @@
-import Markdoc, { Schema } from '@markdoc/markdoc';
+import { Schema } from '@markdoc/markdoc';
 
 export type QuoteProps<A> = {
   readonly children: A;
 };
 
 export const blockquote: Schema = {
-  attributes: {},
-  transform: (node, config) => {
-    return new Markdoc.Tag(
-      'Quote',
-      node.transformAttributes(config),
-      node.transformChildren(config)
-    );
-  },
+  render: 'Quote',
 };
