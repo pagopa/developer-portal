0d6ff135295c7777a0fd9236858893270c3fd602
AF <alessandro.ferlin@pagopa.it>
2023-07-24 16:02:32 +0200

    Parse title

---
+++ packages/gitbook-docs/src/parseTitle.ts
@@ -0,0 +1,28 @@
+import Markdoc, { Node } from '@markdoc/markdoc';
+import { pipe } from 'fp-ts/lib/function';
+import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
+
+// Return the content of a Node of type 'text'
+const parseText = ({ type, attributes }: Node): O.Option<string> =>
+  type === 'text' && typeof attributes.content === 'string'
+    ? O.fromNullable(attributes.content)
+    : O.none;
+
+export const parseTitle = (markdown: string): O.Option<string> => {
+  const ast = Markdoc.parse(markdown);
+  const result = pipe(
+    Array.from(ast.walk()),
+    RA.findFirst(
+      ({ type, attributes }) => type === 'heading' && attributes.level === 1
+    ),
+    O.map((node) =>
+      pipe(
+        Array.from(node.walk()),
+        RA.filterMap(parseText),
+        RA.reduce('', (a, b) => a + b)
+      )
+    )
+  );
+  return result;
+};
