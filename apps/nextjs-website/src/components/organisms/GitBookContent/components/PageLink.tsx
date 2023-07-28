8a8988c30e8c1b20c7441d9abc8efa32e8238363
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-28 10:16:32 +0200

    Add pageLink component

---
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/PageLink.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+import MUILink from '@mui/material/Link';
+import NextLink from 'next/link';
+import { PageLinkProps } from 'gitbook-docs/markdoc/schema/pageLink';
+
+export const PageLink = ({ url, children }: PageLinkProps<ReactNode>) => (
+  <MUILink
+    component={NextLink}
+    href={url}
+    sx={{ textDecoration: 'none' }}
+  >
+    {children}
+  </MUILink>
+);
+
+export default PageLink;
