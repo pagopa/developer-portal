2cda554a7288504241cb2c9a3e9780ed148bc341
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-07-28 10:17:41 +0200

    Lint code

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/PageLink.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/PageLink.tsx
@@ -4,11 +4,7 @@ import NextLink from 'next/link';
 import { PageLinkProps } from 'gitbook-docs/markdoc/schema/pageLink';
 
 export const PageLink = ({ url, children }: PageLinkProps<ReactNode>) => (
-  <MUILink
-    component={NextLink}
-    href={url}
-    sx={{ textDecoration: 'none' }}
-  >
+  <MUILink component={NextLink} href={url} sx={{ textDecoration: 'none' }}>
     {children}
   </MUILink>
 );
