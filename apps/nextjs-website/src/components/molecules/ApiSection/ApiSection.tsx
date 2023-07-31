b1f12a5e422b9e9ac628db76d00e3f3619db0d28
redtom <tommasorosso1gmail.com>
2023-07-31 10:41:37 +0200

    api param

--- apps/nextjs-website/src/components/molecules/ApiSection/ApiSection.tsx
+++ apps/nextjs-website/src/components/molecules/ApiSection/ApiSection.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from "react";
 import {
   MenuItem,
   Select,
@@ -6,13 +6,14 @@ import {
   Stack,
   Typography,
   useTheme,
-} from '@mui/material';
-import { ApiViewer } from '@/components/atoms/ApiViewer';
-import { Product } from '@/lib/types/product';
-import { styles } from '@/components/molecules/ApiSection/ApiSection.styles';
-import Link from 'next/link';
-import { ButtonNaked } from '@pagopa/mui-italia';
-import IconWrapper from '@/components/atoms/IconWrapper/IconWrapper';
+} from "@mui/material";
+import { ApiViewer } from "@/components/atoms/ApiViewer";
+import { Product } from "@/lib/types/product";
+import { styles } from "@/components/molecules/ApiSection/ApiSection.styles";
+import Link from "next/link";
+import { ButtonNaked } from "@pagopa/mui-italia";
+import IconWrapper from "@/components/atoms/IconWrapper/IconWrapper";
+import { useRouter } from "next/router";
 
 export type ApiPageProps = {
   readonly product: Product;
@@ -33,6 +34,20 @@ const ApiSection = ({ product, specURLs, soapDocumentation }: ApiPageProps) => {
   const { palette, spacing } = useTheme();
 
   const [selectedItemURL, setSelectedItemURL] = useState(specURLs[0].url);
+
+  const router = useRouter();
+
+  // if a spec query param is present, try to match it with the specURLs, if found, set it as selectedItemURL
+  useEffect(() => {
+    const specName = router.query?.spec;
+    if (specName) {
+      const spec = specURLs.find((item) => item?.name === specName);
+      if (spec) {
+        setSelectedItemURL(spec.url);
+      }
+    }
+  }, [router.query, specURLs]);
+
   const handleChange = (event: SelectChangeEvent) => {
     setSelectedItemURL(event.target.value);
   };
@@ -51,8 +66,8 @@ const ApiSection = ({ product, specURLs, soapDocumentation }: ApiPageProps) => {
             <Select
               value={selectedItemURL}
               onChange={handleChange}
-              size='small'
-              variant='outlined'
+              size="small"
+              variant="outlined"
               sx={styles.select}
             >
               {specURLs.map((item, index) => (
@@ -67,19 +82,19 @@ const ApiSection = ({ product, specURLs, soapDocumentation }: ApiPageProps) => {
       {soapDocumentation && (
         <Stack
           sx={styles.soapContainer}
-          alignItems='center'
-          justifyContent='flex-end'
-          direction='row'
+          alignItems="center"
+          justifyContent="flex-end"
+          direction="row"
           height={spacing(8)}
           gap={spacing(2)}
         >
-          <Typography variant='body2' color={textColor}>
+          <Typography variant="body2" color={textColor}>
             {soapDocumentation.title}
           </Typography>
           <Stack
-            alignItems='center'
-            justifyContent='flex-end'
-            direction='row'
+            alignItems="center"
+            justifyContent="flex-end"
+            direction="row"
             gap={spacing(1)}
           >
             <ButtonNaked
