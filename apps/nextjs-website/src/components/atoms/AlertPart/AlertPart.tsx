351ced08420575bc21d4bdd35b5a1951b1993c9e
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-08-03 10:43:29 +0200

    Simplify component just to see how size changes

--- apps/nextjs-website/src/components/atoms/AlertPart/AlertPart.tsx
+++ apps/nextjs-website/src/components/atoms/AlertPart/AlertPart.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import { AlertTitle, SxProps, useTheme } from '@mui/material';
-import Alert, { AlertColor } from '@mui/material/Alert';
 
 export type AlertPartProps = {
-  alertStyle?: SxProps;
-  severity: AlertColor;
   text: string;
   title: string;
 };
 
-const AlertPart = ({ alertStyle, severity, text, title }: AlertPartProps) => {
-  const { spacing } = useTheme();
-
-  return (
-    <Alert
-      severity={severity}
-      sx={{ borderRadius: '4px', marginBottom: spacing(5), ...alertStyle }}
-    >
-      <AlertTitle>{title}</AlertTitle>
-      {text}
-    </Alert>
-  );
+const AlertPart = ({ text, title }: AlertPartProps) => {
+  return <div title={title}>{text}</div>;
 };
 
 export default AlertPart;
