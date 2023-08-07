1549168c50ec3c084d31659ec954279d53ba326c
Marco Bottaro <marco.bottaro@uqido.com>
2023-08-07 18:08:02 +0200

    Fix Heading.tsx

--- apps/nextjs-website/src/components/organisms/GuideInPageMenu/components/Heading.tsx
+++ apps/nextjs-website/src/components/organisms/GuideInPageMenu/components/Heading.tsx
@@ -2,30 +2,37 @@ import { HeadingProps } from 'gitbook-docs/markdoc/schema/heading';
 import { ReactNode } from 'react';
 import MUILink from '@mui/material/Link';
 import { removeEmojis } from '@/markdoc/helpers';
+import { Typography, useTheme } from '@mui/material';
 
 const Heading = ({ level, id, children }: HeadingProps<ReactNode>) => {
   const isString = typeof children === 'string';
+  const theme = useTheme();
 
   if (level < 2 || level > 4) {
     return;
   }
 
   return (
-    <>
-      <MUILink
-        href={`#${id}`}
-        title={isString ? removeEmojis(children) : ''}
+    <MUILink
+      href={`#${id}`}
+      title={isString ? removeEmojis(children) : ''}
+      sx={{
+        display: 'block',
+        fontFamily: 'Titillium Web',
+        marginBottom: '12px',
+        textDecoration: 'none',
+      }}
+    >
+      <Typography
         sx={{
-          color: '#0062C3',
-          fontSize: '18px',
-          fontWeight: 600,
-          textDecoration: 'none',
+          color: theme.palette.text.secondary,
+          fontSize: '16px',
+          fontWeight: 400,
         }}
       >
         {isString ? removeEmojis(children) : children}
-      </MUILink>
-      <br />
-    </>
+      </Typography>
+    </MUILink>
   );
 };
 
