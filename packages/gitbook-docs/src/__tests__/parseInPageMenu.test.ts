eab9f017f82d934832b70527283ca9927189a2d1
Marco Bottaro <marco.bottaro@uqido.com>
2023-08-09 17:40:11 +0200

    Fix parsing test

--- packages/gitbook-docs/src/__tests__/parseInPageMenu.test.ts
+++ packages/gitbook-docs/src/__tests__/parseInPageMenu.test.ts
@@ -11,10 +11,9 @@ const config = {
 describe('parseInPageMenu', () => {
   it('should return only heading elements', () => {
     expect(
-      parseInPageMenu('# Title 1\n## Title 2\nParagraph', config)
+      parseInPageMenu('# Title 1\n## Title 2\n### Title 3\nParagraph', config)
     ).toStrictEqual([
-      new Markdoc.Tag('Heading', { level: 1, id: 'title-1' }, ['Title 1']),
-      new Markdoc.Tag('Heading', { level: 2, id: 'title-2' }, ['Title 2']),
+      new Markdoc.Tag('Heading', { level: 3, id: 'title-3' }, ['Title 3']),
     ]);
   });
 });
