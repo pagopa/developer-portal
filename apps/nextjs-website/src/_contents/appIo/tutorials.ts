382c90b51de87be3e655ab8b3a63c73af25c8e72
AF <alessandro.ferlin@pagopa.it>
2023-09-05 12:17:21 +0200

    Refactor tutorial parsing

--- apps/nextjs-website/src/_contents/appIo/tutorials.ts
+++ apps/nextjs-website/src/_contents/appIo/tutorials.ts
@@ -1,30 +1,9 @@
-import { pipe } from 'fp-ts/lib/function';
-import * as E from 'fp-ts/lib/Either';
-import * as RA from 'fp-ts/lib/ReadonlyArray';
-import { docsAssetsPath, docsPath } from '@/config';
-import { parseDoc } from 'gitbook-docs/parseDoc';
 import { appIo } from '@/_contents/appIo/appIo';
 import { appIoBannerLinks } from '@/_contents/appIo/bannerLinks';
+import { makeTutorials } from '../makeDocs';
 
-export const appIoTutorials = pipe(
-  [
-    {
-      product: appIo,
-      source: {
-        pathPrefix: `${appIo.path}/tutorials`,
-        assetsPrefix: `${docsAssetsPath}/I4tX18g9wQQvTbyNkmIT`,
-        dirPath: `${docsPath}/I4tX18g9wQQvTbyNkmIT`,
-      },
-      bannerLinks: appIoBannerLinks,
-    },
-  ],
-  RA.traverse(E.Applicative)(parseDoc),
-  E.fold((e) => {
-    // eslint-disable-next-line functional/no-expression-statements
-    console.log(e);
-    // eslint-disable-next-line functional/no-throw-statements
-    throw e;
-  }, RA.flatten),
-  // This is a workaround that removes the 'index' space from tutorial docs
-  RA.filter(({ page: { path } }) => path !== `${appIo.path}/tutorials`)
-);
+export const appIoTutorials = makeTutorials({
+  product: appIo,
+  dirName: 'I4tX18g9wQQvTbyNkmIT',
+  bannerLinks: appIoBannerLinks,
+});
