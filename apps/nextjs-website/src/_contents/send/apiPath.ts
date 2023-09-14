b117bec0bde1cf542f3e8895e3771fd10f25bbef
Marco Bottaro <marco.bottaro@uqido.com>
2023-09-14 09:49:20 +0200

    Add different links to Send API using 'spec' query param

--- apps/nextjs-website/src/_contents/send/apiPath.ts
+++ apps/nextjs-website/src/_contents/send/apiPath.ts
@@ -4,3 +4,13 @@ export const sendApiPath: Path = {
   name: 'API',
   path: '/send/api',
 };
+
+export const sendB2BApiPath: Path = {
+  name: 'B2B API',
+  path: `${sendApiPath.path}?spec=API%20B2B%20per%20le%20Pubbliche%20Amministrazioni`,
+};
+
+export const sendNotificationsApiPath: Path = {
+  name: 'Notification API',
+  path: `${sendApiPath.path}?spec=API%20B2B%20avanzamento%20notifiche`,
+};
