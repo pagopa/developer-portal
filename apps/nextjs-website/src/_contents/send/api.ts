daa202bb8ce6428d4001da33e0b651242264986b
Marco Bottaro <marco.bottaro@uqido.com>
2023-09-18 14:41:18 +0200

    Fix linting

--- apps/nextjs-website/src/_contents/send/api.ts
+++ apps/nextjs-website/src/_contents/send/api.ts
@@ -1,6 +1,10 @@
 import { ApiData } from '@/lib/types/apiData';
 import { send } from '@/_contents/send/send';
-import { sendApiPath, sendB2BApiPathName, sendNotificationsApiPathName } from '@/_contents/send/apiPath';
+import {
+  sendApiPath,
+  sendB2BApiPathName,
+  sendNotificationsApiPathName,
+} from '@/_contents/send/apiPath';
 import { sendBannerLinks } from '@/_contents/send/bannerLinks';
 
 export const sendApi: ApiData = {
