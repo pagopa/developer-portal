0146e3b1a54136e77a133a2c685635ee07773e21
Marco Ponchia <marco.ponchia@uqido.com>
2023-09-19 13:15:55 +0200

    fix after review

--- apps/strapi-cms/config/middlewares.ts
+++ apps/strapi-cms/config/middlewares.ts
@@ -2,11 +2,9 @@ export default [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
-  'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
