2cae6adf97f988d69fffa571bbf93bb6ff195117
marcobottaro <39835990+marcobottaro@users.noreply.github.com>
2023-09-21 16:25:12 +0200

    Fix "Go to GitHub" link style in ApiSection component (#263)

--- apps/nextjs-website/src/components/molecules/ApiSection/ApiSection.tsx
+++ apps/nextjs-website/src/components/molecules/ApiSection/ApiSection.tsx
@@ -142,6 +142,9 @@ const ApiSection = ({
             <ButtonNaked
               sx={{
                 color: textColor,
+                '&:hover': {
+                  color: textColor,
+                },
               }}
               component={Link}
               aria-label={soapDocumentation.buttonLabel}
