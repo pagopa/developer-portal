3963e443c3d5156a9c98dff314eb2054e960fbcf
Marco Bottaro <marco.bottaro@uqido.com>
2023-09-25 16:32:25 +0200

    Fix linting

--- apps/nextjs-website/src/_contents/send/guides.ts
+++ apps/nextjs-website/src/_contents/send/guides.ts
@@ -25,12 +25,12 @@ const manualeOperativo: GuideDefinition = {
   },
   versions: [
     {
-      version:'v1.1',
-      dirName:'fJIYTgeiXFHm8gUu4gAD',
+      version: 'v1.1',
+      dirName: 'fJIYTgeiXFHm8gUu4gAD',
     },
     {
-      version:'v1.1.1',
-      dirName:'XbcyIfLqbQFM4OsEpBQY',
+      version: 'v1.1.1',
+      dirName: 'XbcyIfLqbQFM4OsEpBQY',
     },
     {
       version: 'v1.0.1',
