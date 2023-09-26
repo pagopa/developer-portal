eab51276c1d5bc4c5a341b04a62685a2a9678d1f
AF <alessandro.ferlin@pagopa.it>
2023-09-26 16:31:27 +0200

    Add ses dkim records

--- .infrastructure/65_ses.tf
+++ .infrastructure/65_ses.tf
@@ -3,6 +3,10 @@ resource "aws_ses_domain_identity" "devportal" {
   domain = var.dns_domain_name
 }
 
-resource aws_ses_email_identity "noreply_email" {
+resource "aws_ses_domain_dkim" "devportal" {
+  domain = aws_ses_domain_identity.devportal.domain
+}
+
+resource "aws_ses_email_identity" "noreply_email" {
   email = format("no-reply@%s", var.dns_domain_name)
 }
