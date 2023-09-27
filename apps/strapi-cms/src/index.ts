a9c74676bd32cf63f26911b4b0fe8db5ae6eb2fe
AF <alessandro.ferlin@pagopa.it>
2023-09-27 17:00:44 +0200

    Add strong eslint rules on strapi-cms package

--- apps/strapi-cms/src/index.ts
+++ apps/strapi-cms/src/index.ts
@@ -1,22 +1,22 @@
 export default {
   /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {
-    // do nothing for now
-  },
-
-  /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.
    *
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {
+  bootstrap: (): undefined => {
+    // do nothing for now
+  },
+
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register: (): undefined => {
     // do nothing for now
   },
 };
