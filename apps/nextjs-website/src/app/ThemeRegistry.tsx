1894d645dedc8e6378d915e1100886e1108ebd7a
Marco Ponchia <ponchia.marco1994@gmail.com>
2023-09-27 17:42:33 +0200

    Update ThemeRegistry.tsx
    remove no explict any

--- apps/nextjs-website/src/app/ThemeRegistry.tsx
+++ apps/nextjs-website/src/app/ThemeRegistry.tsx
@@ -12,7 +12,6 @@ export const theme = createTheme(muiItaliaTheme);
 
 // This implementation is from emotion-js
 // https://github.com/emotion-js/emotion/issues/2928#issuecomment-1319747902
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function ThemeRegistry({
   options,
   children,
