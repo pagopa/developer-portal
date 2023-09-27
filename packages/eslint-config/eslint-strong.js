6226fd22b9ded59019be55cad3a21edb10d1d747
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-09-27 15:28:55 +0200

    Disable no longer available rule and enable the replacement

--- packages/eslint-config/eslint-strong.js
+++ packages/eslint-config/eslint-strong.js
@@ -6,4 +6,21 @@ module.exports = {
     // Load pagopa eslint config
     "@pagopa/eslint-config/strong",
   ],
+  overrides: [
+    {
+      /*
+       * Version 5.0.0 (https://github.com/eslint-functional/eslint-plugin-functional/releases/tag/v5.0.0)
+       * add a breaking change that renames the rule no-method-signature to
+       * prefer-property-signatures.
+       *
+       * Disable rule coming from @pagopa/eslint-rules and enable (with the very same level
+       * used by the PagoPA's linter) the rule renamed in version 5.
+       */
+      files: ['*'],
+      rules: {
+        'functional/no-method-signature': 'off',
+        'functional/prefer-property-signatures': 'error'
+      },
+    },
+  ],
 }
