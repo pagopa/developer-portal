861066d0012843b7f4ae1219ba8349d777aea7cc
Marco Bottaro <marco.bottaro@uqido.com>
2023-09-28 12:53:18 +0200

    Fix linting error

--- packages/gitbook-docs/src/parseDoc.ts
+++ packages/gitbook-docs/src/parseDoc.ts
@@ -46,7 +46,7 @@ const transformPath = (
   path
     .replace(dirPath, `${pathPrefix}`)
     .replace('/README.md', '')
-    // remove spaces because in some systems (e.g. NextJS) 
+    // remove spaces because in some systems (e.g. NextJS)
     // they cause errors when used in paths
     .replace(' ', '')
     .replace('.md', '');
