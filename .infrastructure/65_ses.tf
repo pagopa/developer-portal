51b1d119bdff56c756a339f741641116a69fd920
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-09-28 17:26:16 +0200

    Add email addresses of team members (just to test sign up)
    AWS SES sandbox env allow only to send email to validated addresses. With this commit, we are adding email addresses we are going to use during tests until we switch to production env of SES

--- .infrastructure/65_ses.tf
+++ .infrastructure/65_ses.tf
@@ -14,3 +14,40 @@ module "ses_developer_pagopa_it" {
   ses_group_name      = "DevPortalSES"
   user_name           = "DevPortal"
 }
+
+# These are just email addresses used during test
+# With sandbox env of SES we can only send email to verified email addresses
+# TODO: remove these when we switch to production
+resource "aws_sesv2_email_identity" "marco" {
+  email_identity = "marco.comi@pagopa.it"
+}
+resource "aws_sesv2_email_identity" "alessandro" {
+  email_identity = "alessandro.ferlin@pagopa.it"
+}
+resource "aws_sesv2_email_identity" "kada" {
+  email_identity = "carloalberto.degliatti@pagopa.it"
+}
+resource "aws_sesv2_email_identity" "andrea" {
+  email_identity = "andrea.gurrieri@pagopa.it"
+}
+resource "aws_sesv2_email_identity" "fabrizio" {
+  email_identity = "fabrizio.ulisse@pagopa.it"
+}
+resource "aws_sesv2_email_identity" "monica" {
+  email_identity = "monica.costantini@pagopa.it"
+}
+resource "aws_sesv2_email_identity" "norma" {
+  email_identity = "norma.cocito@pagopa.it"
+}
+resource "aws_sesv2_email_identity" "marco_bot" {
+  email_identity = "marco.bottaro@uqido.com"
+}
+resource "aws_sesv2_email_identity" "marco_ponchia" {
+  email_identity = "marco.ponchia@uqido.com"
+}
+resource "aws_sesv2_email_identity" "jeremy" {
+  email_identity = "jeremy.gordillo@uqido.com"
+}
+resource "aws_sesv2_email_identity" "tommaso" {
+  email_identity = "tommaso.rosso@uqido.com"
+}
