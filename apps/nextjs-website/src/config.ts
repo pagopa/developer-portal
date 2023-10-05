e97f53da2a9605f8a2f9b0fa4813c9ba3b9ce589
Marco Ponchia <marco.ponchia@uqido.com>
2023-10-05 17:36:28 +0200

    add robots.ts and related new env variable ALLOW_CRAWLER

--- apps/nextjs-website/src/config.ts
+++ apps/nextjs-website/src/config.ts
@@ -3,3 +3,4 @@ export const docsPath = process.env.PATH_TO_GITBOOK_DOCS;
 export const cookieDomainScript = process.env.COOKIE_DOMAIN_SCRIPT;
 export const environment = process.env.ENVIRONMENT;
 export const docsAssetsPath = '/gitbook/docs';
+export const allowCrawler = process.env.ALLOW_CRAWLER === 'true';
