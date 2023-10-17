908d4209af3708e87a08b07aadae8ff1cc6a2ec3
Monica Costantini <122867940+certevol@users.noreply.github.com>
2023-10-17 12:58:06 +0200

    [DPC-69] API Send Update

--- apps/nextjs-website/src/_contents/send/api.ts
+++ apps/nextjs-website/src/_contents/send/api.ts
@@ -10,15 +10,10 @@ import { sendBannerLinks } from '@/_contents/send/bannerLinks';
 export const sendApi: ApiData = {
   ...sendApiPath,
   product: send,
-  specURLsName: 'Documentazione API di SEND',
   specURLs: [
     {
       name: sendB2BApiPathName,
-      url: 'https://raw.githubusercontent.com/pagopa/pn-delivery/main/docs/openapi/api-external-b2b-pa.yaml',
-    },
-    {
-      name: sendNotificationsApiPathName,
-      url: 'https://raw.githubusercontent.com/pagopa/pn-delivery-push/main/docs/openapi/api-external-b2b-webhook.yaml',
+      url: 'https://raw.githubusercontent.com/pagopa/pn-delivery/release/2.1.0/docs/openapi/api-external-b2b-pa-bundle.yaml',
     },
   ],
   bannerLinks: sendBannerLinks,
