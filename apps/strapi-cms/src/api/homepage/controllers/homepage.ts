a67af49985fb1eb897502153600913f060997bd8
Marco Ponchia <marco.ponchia@uqido.com>
2023-10-19 16:22:18 +0200

    Fix linting problem

--- apps/strapi-cms/src/api/homepage/controllers/homepage.ts
+++ apps/strapi-cms/src/api/homepage/controllers/homepage.ts
@@ -2,6 +2,6 @@
  * homepage controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::homepage.homepage');
