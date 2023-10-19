d66b1900849cf2cedd82a226d81f3b8499a3a6a5
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-10-19 10:25:29 +0200

    Do not let terraform module handle the DNS records

--- .infrastructure/65_ses.tf
+++ .infrastructure/65_ses.tf
@@ -3,9 +3,6 @@ module "ses_developer_pagopa_it" {
   source        = "github.com/pagopa/terraform-aws-ses.git?ref=58c1263afa441692e67d1be5dca809e65d6852df" # v1.2.1
   aws_region    = var.aws_region
   domain        = var.dns_domain_name
-  verify_dkim   = true
-  verify_domain = true
-  zone_id       = aws_route53_zone.dev_portal.zone_id
 
   iam_permissions = [
     "ses:SendCustomVerificationEmail",
@@ -16,5 +13,4 @@ module "ses_developer_pagopa_it" {
 
   ses_group_name = "DevPortalSES"
   user_name      = "DevPortal"
-
 }
