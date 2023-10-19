a852829e7b2de516c9acfad3d36e583923f81697
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-10-19 14:50:04 +0200

    Return PageNotFound on prod environment

--- apps/nextjs-website/src/app/auth/login/page.tsx
+++ apps/nextjs-website/src/app/auth/login/page.tsx
@@ -1,8 +1,12 @@
 import LoginForm from '@/components/organisms/Auth/LoginForm';
 import { Box } from '@mui/material';
+import { environment } from '@/config';
+import PageNotFound from '@/app/not-found';
 
 const Login = () => {
-  return (
+  return environment === 'prod' ? (
+    <PageNotFound />
+  ) : (
     <Box
       sx={{
         display: 'flex',
