5e58a1c9a4aaeba16a04155d0bcb5c48e08a1377
tommaso1 <tommasorosso1@gmail.com>
2023-10-24 20:49:25 +0000

    menu style + card scaffold

--- apps/nextjs-website/src/app/profile/page.tsx
+++ apps/nextjs-website/src/app/profile/page.tsx
@@ -1,23 +1,25 @@
 'use client';
 
-import GuideMenu from "@/components/atoms/GuideMenu/GuideMenu";
-import ProfileMenu from "@/components/atoms/ProfileMenu/ProfileMenu";
-import { Box, useTheme, Typography } from "@mui/material";
-
+import GuideMenu from '@/components/atoms/GuideMenu/GuideMenu';
+import ProfileMenu from '@/components/atoms/ProfileMenu/ProfileMenu';
+import { Box, useTheme, Typography, Stack, Card, Button } from '@mui/material';
 
 const TermsOfService = () => {
-    const { palette } = useTheme();
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <ProfileMenu name="Mario Rossi"></ProfileMenu>
-
-            <Box py={6}>
-                BBB
-            </Box>
-
-        </Box>
-
-    );
+  const { palette } = useTheme();
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <ProfileMenu name='Mario Rossi'></ProfileMenu>
+      <Stack sx={{ margin: '30px' }}>
+        <Typography variant='h3'>I tuoi dati</Typography>
+        <Card elevation={2}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Typography variant='h6'>Profilo</Typography>
+            <Button variant='outlined'>Modifica</Button>
+          </Box>
+        </Card>
+      </Stack>
+    </Box>
+  );
 };
 
 export default TermsOfService;
--- apps/nextjs-website/src/components/atoms/ProfileMenu/ProfileMenu.tsx
+++ apps/nextjs-website/src/components/atoms/ProfileMenu/ProfileMenu.tsx
@@ -6,72 +6,83 @@ import { Box, useTheme } from '@mui/material';
 import { translations } from '@/_contents/translations';
 
 type ProfileMenuProps = {
-    name: string;
+  name: string;
 };
 
+const ProfileMenu = ({ name }: ProfileMenuProps) => {
+  const { palette } = useTheme();
+  const { shared } = translations;
 
-const ProfileMenu = ({
-    name,
-}: ProfileMenuProps) => {
-    const { palette } = useTheme();
-    const { shared } = translations;
-
-    return (
-        <Box
-            sx={{
-                backgroundColor: palette.grey[50],
-                display: 'flex',
-                flexDirection: 'column',
-                position: 'relative',
-            }}
+  return (
+    <Box
+      sx={{
+        backgroundColor: palette.grey[50],
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'relative',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '80px 0',
+          width: { lg: '347px' },
+          flexGrow: { lg: 0 },
+          flexShrink: { lg: 0 },
+          position: 'sticky',
+          overflowY: 'auto',
+          top: 50,
+          scrollbarWidth: 'thin',
+        }}
+      >
+        <Typography
+          variant='h6'
+          sx={{
+            padding: '0px 32px',
+            verticalAlign: 'middle',
+            fontWeight: 'regular',
+          }}
         >
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    padding: '80px 0',
-                    width: { lg: '347px' },
-                    flexGrow: { lg: 0 },
-                    flexShrink: { lg: 0 },
-                    position: 'sticky',
-                    overflowY: 'auto',
-                    top: 50,
-                    scrollbarWidth: 'thin',
-                }}
-            >
-                <Typography
-                    variant='h6'
-                    sx={{
-                        padding: '0px 32px',
-                        verticalAlign: 'middle',
-                        fontWeight: 'regular',
-                    }}
-                >
-                    Ciao,
-                </Typography>
-                <Typography
-                    variant='h5'
-                    sx={{
-                        padding: '0px 32px',
-                        verticalAlign: 'middle',
-                    }}
-                >
-                    {name}
-                </Typography>
+          Ciao,
+        </Typography>
+        <Typography
+          variant='h5'
+          sx={{
+            padding: '0px 32px',
+            verticalAlign: 'middle',
+          }}
+        >
+          {name}
+        </Typography>
 
-
-                <Box sx={{
-                    borderRight: '3px solid ' + palette.primary.main ,
-                }}>
-                    I tuoi dati
-                </Box>
-                <Box>
-                    Consensi e privacy
-                </Box>
-
-            </Box>
+        <Box
+          sx={{
+            borderRight: '3px solid ' + palette.primary.main,
+            backgroundColor: palette.primary.main + '14',
+            backgroundOpacity: 0.08,
+            padding: '10px 32px',
+            marginTop: '32px',
+            cursor: 'pointer'
+          }}
+        >
+          I tuoi dati
         </Box>
-    );
+        <Box
+          onClick={() => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            window.OneTrust.ToggleInfoDisplay()
+          }}
+          sx={{
+            marginTop: '12px',
+            padding: '10px 32px',
+            cursor: 'pointer'
+          }}
+        >Consensi e privacy</Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ProfileMenu;
