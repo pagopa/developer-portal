ab26f913775e2c33d70e55cadb273a021092d80c
jeremygordillo <jere.gordi@gmail.com>
2023-10-26 17:43:17 +0200

    add error placeholder

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/Swagger/Swagger.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/Swagger/Swagger.tsx
@@ -3,25 +3,42 @@ import { SwaggerProps } from 'gitbook-docs/markdoc/schema/swagger';
 import { OpenAPI, OpenAPIV3 } from 'openapi-types';
 import { useEffect, useState } from 'react';
 
+import { Card, Typography } from '@mui/material';
 import { Operations } from './Operations';
 
 const useSpec = (src?: string) => {
   const [spec, setSpec] = useState<OpenAPI.Document>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (src) {
-      SwaggerParser.dereference(src).then(setSpec).catch(console.error);
+      setLoading(true);
+      SwaggerParser.dereference(src)
+        .then(setSpec)
+        .catch(console.error)
+        .finally(() => setLoading(false));
     }
   }, [src]);
 
-  return spec;
+  return { spec, loading };
 };
 
 const Swagger = ({ path, method, src }: SwaggerProps) => {
-  const spec = useSpec(src);
+  const { spec, loading } = useSpec(src);
 
-  if (!src) return null;
-  if (!spec) return <h3> Unexpected error during spec loading!</h3>;
+  if (!src || loading) return null;
+  if (!spec)
+    return (
+      <Card sx={{ borderRadius: 1, p: 2 }} variant='outlined'>
+        <Typography sx={{ fontWeight: 'bold' }} variant='body1'>
+          Oops, manca qualcosa.
+        </Typography>
+        <Typography variant='body2' color='GrayText'>
+          Non siamo riusciti a trovare la fonte originale per visualizzare
+          questo contenuto.
+        </Typography>
+      </Card>
+    );
 
   const validOperations = { [path]: [method] as OpenAPIV3.HttpMethods[] };
 
