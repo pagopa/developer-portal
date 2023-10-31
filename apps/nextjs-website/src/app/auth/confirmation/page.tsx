dada28f61639b43b330002c44c5d6c17099ec796
Marco Ponchia <marco.ponchia@uqido.com>
2023-10-31 17:56:42 +0100

    Add confirmation page

---
+++ apps/nextjs-website/src/app/auth/confirmation/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+import { Box, CircularProgress, Stack } from '@mui/material';
+import { environment } from '@/config';
+import PageNotFound from '@/app/not-found';
+import { Auth } from 'aws-amplify';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const Confirmation = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const username = searchParams.get('username');
+  const code = searchParams.get('code');
+
+  const [renderNotFound, setRenderNotFound] = useState(environment === 'prod');
+
+  useEffect(() => {
+    if (username && code) {
+      Auth.confirmSignUp(username, code)
+        .then(() => {
+          setRenderNotFound(false);
+          // eslint-disable-next-line functional/immutable-data
+          router.push('/auth/account-activated');
+        })
+        .catch(() => {
+          setRenderNotFound(true);
+        });
+    } else {
+      setRenderNotFound(true);
+    }
+  }, [username, code]);
+
+  return renderNotFound ? (
+    <PageNotFound />
+  ) : (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Stack
+        justifyContent={'center'}
+        height={500}
+        padding={2}
+        alignItems='center'
+      >
+        <CircularProgress />
+      </Stack>
+    </Box>
+  );
+};
+
+export default Confirmation;
