cd3384b63cbc087436346ff1a5397adf7757c397
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-11-07 13:18:42 +0100

    [DEV-1057] - Send welcome email when user confirms email address (#361)

--- apps/cognito-functions/src/send-email-handler.ts
+++
@@ -1,8 +0,0 @@
-import { PostConfirmationConfirmSignUpTriggerEvent } from 'aws-lambda';
-
-export const handler = async (
-  event: PostConfirmationConfirmSignUpTriggerEvent
-) => {
-  // eslint-disable-next-line functional/no-expression-statements
-  console.log('Event', JSON.stringify(event, null, 2));
-};
