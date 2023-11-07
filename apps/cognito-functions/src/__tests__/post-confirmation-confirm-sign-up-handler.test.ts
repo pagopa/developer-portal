3200790c7616aa8d3ad4c5edafa8e827b3872e20
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-11-07 09:47:18 +0100

    Add unit tests for post confirmation handler

--- apps/cognito-functions/src/__tests__/post-confirmation-confirm-sign-up-handler.test.ts
+++ apps/cognito-functions/src/__tests__/post-confirmation-confirm-sign-up-handler.test.ts
@@ -1,6 +1,9 @@
 import { PostConfirmationConfirmSignUpTriggerEvent } from 'aws-lambda';
+import { makeHandler } from '../post-confirmation-confirm-sign-up-handler';
+import { SendEmailCommand, SES } from '@aws-sdk/client-ses';
+import { mock, mockReset, captor } from 'jest-mock-extended';
 
-const event: PostConfirmationConfirmSignUpTriggerEvent = {
+const eventMock: PostConfirmationConfirmSignUpTriggerEvent = {
   version: 'aVersion',
   region: 'eu-south-1',
   userPoolId: 'aUserPoolId',
@@ -27,8 +30,46 @@ const event: PostConfirmationConfirmSignUpTriggerEvent = {
   triggerSource: 'PostConfirmation_ConfirmSignUp',
 };
 
-describe('Handler', () => {
-  it('send the welcome email', async () => {
-    expect(true).toStrictEqual(true);
+describe('Post Confirmation Confirm Sign Up', () => {
+  const env = {
+    config: {
+      fromEmailAddress: 'from@email.com',
+    },
+    ses: mock<SES>(),
+  };
+
+  beforeEach(() => {
+    mockReset(env.ses);
+  });
+
+  it('should send the email to the specified email address', async () => {
+    const { ses: sesMock } = env;
+
+    const sendEmailCommandCaptor = captor<SendEmailCommand>();
+
+    await makeHandler(env)(eventMock);
+    expect(sesMock.send).toHaveBeenCalled();
+    expect(sesMock.send).toHaveBeenCalledWith(sendEmailCommandCaptor);
+
+    expect(sendEmailCommandCaptor.value.input.Source).toStrictEqual(
+      env.config.fromEmailAddress
+    );
+    expect(
+      sendEmailCommandCaptor.value.input.Destination?.ToAddresses
+    ).toContainEqual(eventMock.request.userAttributes.email);
+  });
+
+  it('should not send the email because email address is missing', async () => {
+    const { ses: sesMock } = env;
+    const noEmailEvent = {
+      ...eventMock,
+    };
+    // Delete email from the event, so we can test that SES is not invoked.
+    // Use delete because of StringMap type doesn't allow nullish values
+    // eslint-disable-next-line functional/immutable-data
+    delete noEmailEvent['request']['userAttributes']['email'];
+
+    await makeHandler(env)(noEmailEvent);
+    expect(sesMock.send).not.toHaveBeenCalled();
   });
 });
