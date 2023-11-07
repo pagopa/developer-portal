c17cf56205d791cfac31024c50e1e60eba449d0c
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-11-07 18:18:09 +0100

    Add company type attribute

--- .infrastructure/55_cognito.tf
+++ .infrastructure/55_cognito.tf
@@ -181,6 +181,18 @@ resource "aws_cognito_user_pool" "devportal" {
     }
   }
 
+  schema {
+    name                     = "company_type"
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    required                 = false
+    string_attribute_constraints = {
+      min_length = 1
+      max_length = 256
+    }
+  }
+
 }
 
 resource "aws_cognito_user_pool_client" "devportal_website" {
