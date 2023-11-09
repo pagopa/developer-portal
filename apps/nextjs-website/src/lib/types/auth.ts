084d9f3a850e5faa47a7afde56a4fe5be8217d41
Michele Da Rin Fioretto <michele.darinfioretto@uqido.com>
2023-11-09 15:20:36 +0100

    feat(DEV-1045): add missing fields to type

--- apps/nextjs-website/src/lib/types/auth.ts
+++ apps/nextjs-website/src/lib/types/auth.ts
@@ -7,5 +7,9 @@ export type DevPortalUser = CognitoUser & {
     readonly family_name: string;
     readonly given_name: string;
     readonly sub: string;
+    readonly 'custom:company_type': string;
+    readonly 'custom:job_role': string;
+    readonly 'custom:mailinglist_accepted': boolean;
+    readonly 'custom:privacy_accepted': boolean;
   };
 };
