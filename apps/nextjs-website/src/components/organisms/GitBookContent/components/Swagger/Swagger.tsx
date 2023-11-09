f395c1e5f126d15f7991611477c9be66f938a70b
jeremygordillo <jere.gordi@gmail.com>
2023-11-09 14:39:13 +0100

    refactor & add messages

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/Swagger/Swagger.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/Swagger/Swagger.tsx
@@ -1,41 +1,24 @@
-import SwaggerParser from '@apidevtools/swagger-parser';
-import { SwaggerProps } from 'gitbook-docs/markdoc/schema/swagger';
-import { OpenAPI, OpenAPIV3 } from 'openapi-types';
-import { useEffect, useState } from 'react';
-
 import { Card, Typography } from '@mui/material';
+import { SwaggerProps } from 'gitbook-docs/markdoc/schema/swagger';
+import { useTranslations } from 'next-intl';
+import { OpenAPIV3 } from 'openapi-types';
+
 import { Operations } from './Operations';
-
-const useSpec = (src?: string) => {
-  const [spec, setSpec] = useState<OpenAPI.Document>();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (src) {
-      setLoading(true);
-      SwaggerParser.dereference(src)
-        .then(setSpec)
-        .catch(console.error)
-        .finally(() => setLoading(false));
-    }
-  }, [src]);
-
-  return { spec, loading };
-};
+import { useSpec } from './hooks/use-spec';
 
 const Swagger = ({ path, method, src }: SwaggerProps) => {
   const { spec, loading } = useSpec(src);
+  const t = useTranslations('swagger');
 
   if (!src || loading) return null;
   if (!spec)
     return (
       <Card sx={{ borderRadius: 1, p: 2 }} variant='outlined'>
         <Typography sx={{ fontWeight: 'bold' }} variant='body1'>
-          Oops, manca qualcosa.
+          {t('errorCard.header')}
         </Typography>
         <Typography variant='body2' color='GrayText'>
-          Non siamo riusciti a trovare la fonte originale per visualizzare
-          questo contenuto.
+          {t('errorCard.message')}
         </Typography>
       </Card>
     );
