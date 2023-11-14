3a7dff3c16774b20188d98c3e818350133fce7d9
AF <alessandro.ferlin@pagopa.it>
2023-11-14 17:54:40 +0100

    Add create auth challenge handler

--- .infrastructure/55_cognito.tf
+++ .infrastructure/55_cognito.tf
@@ -64,12 +64,12 @@ module "cognito_post_confirmation_function" {
   }
 }
 
-module "cognito_define_auth_challenge_function" {
+module "cognito_verify_auth_challenge_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "cognito_define_auth_challenge"
-  description   = "This Lambda function is invoked to initiate the custom authentication flow."
-  handler       = "main.defineAuthChallengeHandler"
+  function_name = "cognito_verify_auth_challenge"
+  description   = "This Lambda function is invoked to verify if the response from the user for a custom Auth Challenge is valid or not."
+  handler       = "main.verifyAuthChallengeHandler"
   runtime       = "nodejs18.x"
 
   create_package                          = false
@@ -88,12 +88,12 @@ module "cognito_define_auth_challenge_function" {
   }
 }
 
-module "cognito_verify_auth_challenge_function" {
+module "cognito_create_auth_challenge_function" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "cognito_verify_auth_challenge"
-  description   = "This Lambda function is invoked to verify if the response from the user for a custom Auth Challenge is valid or not."
-  handler       = "main.verifyAuthChallengeHandler"
+  function_name = "cognito_create_auth_challenge"
+  description   = "This Lambda function is invoked to create a challenge to present to the user."
+  handler       = "main.createAuthChallengeHandler"
   runtime       = "nodejs18.x"
 
   create_package                          = false
@@ -103,6 +103,13 @@ module "cognito_verify_auth_challenge_function" {
   environment_variables = local.lambda_env_variables
 
   attach_policy_statements = true
+  policy_statements = {
+    ses = {
+      effect    = "Allow",
+      actions   = ["ses:SendEmail", "ses:SendRawEmail"],
+      resources = [module.ses_developer_pagopa_it.ses_domain_identity_arn]
+    },
+  }
 
   allowed_triggers = {
     cognito_devportal = {
