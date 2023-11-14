71b000cd1294b24658dff05001d95c4ecca20006
Marco Bottaro <marco.bottaro@uqido.com>
2023-11-14 10:24:03 +0100

    Fix wrong recipient text in confirmation message

--- apps/cognito-functions/src/templates/confirmation-message.ts
+++ apps/cognito-functions/src/templates/confirmation-message.ts
@@ -10,7 +10,7 @@ const TRANSLATIONS = {
   companyLegalDetails:
     'PagoPA S.p.A. - Società per azioni con socio unico capitale sociale di euro 1,000,000 i.v.Sede legale in Roma, Piazza Colonna 370, CAP 00187Sede operativa in Roma, Via Sardegna 38, CAP 00187N. di iscrizione a Registro Imprese di Roma, CF e P.IVA 15376371009',
   wrongRecipient:
-    'Ricevi questo messaggio in quanto bla bla bla.<br>Non sei tu? Ignora o cancella questa e-mail.',
+    'Ricevi questa e-mail perché hai creato un account su PagoPA DevPortal.<br>Non sei tu? Ignora o cancella questa e-mail.',
   buttonFallbackText: 'Il bottone non funziona? Puoi usare il seguente link:',
 };
 
