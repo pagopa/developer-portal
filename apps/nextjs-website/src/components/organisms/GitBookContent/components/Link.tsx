370418c2116154f740cf69ba0bf5b97d00b5f658
tommaso1 <tommasorosso1@gmail.com>
2023-11-14 21:33:04 +0000

    [DEV-1106] Fix mail to continuing to open

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/Link.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/Link.tsx
@@ -2,16 +2,15 @@ import { ReactNode } from 'react';
 import MUILink from '@mui/material/Link';
 import NextLink from 'next/link';
 import { LinkProps } from 'gitbook-docs/markdoc/schema/link';
+import { useTheme } from '@mui/material';
 
-export const Link = ({ title, href, children }: LinkProps<ReactNode>) => (
-  <MUILink
-    component={NextLink}
-    href={href}
-    title={title}
-    sx={{ textDecoration: 'none' }}
-  >
-    {children}
-  </MUILink>
-);
+export const Link = ({ title, href, children }: LinkProps<ReactNode>) => {
+  const { palette } = useTheme();
+  return (
+    <a href={href} title={title} style={{ color: palette.primary.main }}>
+      {children}
+    </a>
+  );
+};
 
 export default Link;
