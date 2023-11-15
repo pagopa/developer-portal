3fd91ba12f28a0255d136d67b4c3eb55621abc35
Marco Bottaro <marco.bottaro@uqido.com>
2023-11-15 15:23:19 +0100

    Remove unused imports

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/Link.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/Link.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from 'react';
-import MUILink from '@mui/material/Link';
-import NextLink from 'next/link';
 import { LinkProps } from 'gitbook-docs/markdoc/schema/link';
 import { useTheme } from '@mui/material';
 
