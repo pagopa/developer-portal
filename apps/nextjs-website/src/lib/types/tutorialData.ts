5e1c8825d46f6eaa96eb1e7d4eb017a848072003
marcobottaro <39835990+marcobottaro@users.noreply.github.com>
2023-11-17 13:08:42 +0100

    Add the showInOverview property to TutorialData to select Tutorials on overview pages (#409)

--- apps/nextjs-website/src/lib/types/tutorialData.ts
+++ apps/nextjs-website/src/lib/types/tutorialData.ts
@@ -2,6 +2,7 @@ import { Path } from '@/lib/types/path';
 
 export type Tutorial = {
   readonly comingSoon?: boolean;
+  readonly showInOverview?: boolean;
   readonly image?: {
     readonly url: string;
     readonly alt: string;
