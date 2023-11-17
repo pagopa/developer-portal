ce3a480d30ecfaf2f2cbca8e871a544d497ed518
marcobottaro <39835990+marcobottaro@users.noreply.github.com>
2023-11-17 14:55:56 +0100

    Update apps/nextjs-website/src/app/profile/agreements/page.tsx
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>

--- apps/nextjs-website/src/app/profile/agreements/page.tsx
+++ apps/nextjs-website/src/app/profile/agreements/page.tsx
@@ -41,7 +41,7 @@ const Agreements = () => {
     .map((node, index) => <span key={index}>{node}</span>);
 
   return (
-    <Stack sx={{ margin: '30px', width: '100%', maxWidth: '694px' }}>
+    <Stack sx={{ padding: '30px', width: '100%', maxWidth: '694px' }}>
       <Typography variant='h4' sx={{ marginBottom: '40px' }}>
         {t('title')}
       </Typography>
