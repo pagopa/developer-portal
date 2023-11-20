d1b8329b431411d440873ad572d18aa6ff1c56c3
Marco Comi <9998393+kin0992@users.noreply.github.com>
2023-11-20 12:14:15 +0100

    Add user preferences attribute

--- .infrastructure/55_cognito.tf
+++ .infrastructure/55_cognito.tf
@@ -271,6 +271,18 @@ resource "aws_cognito_user_pool" "devportal" {
     }
   }
 
+  schema {
+    name                     = "user_preferences"
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    required                 = false
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 131072
+    }
+  }
+
 }
 
 resource "aws_cognito_user_pool_client" "devportal_website" {
