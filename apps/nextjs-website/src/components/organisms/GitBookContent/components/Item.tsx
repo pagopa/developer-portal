87786d0a036c9fa3d26763c7e226eaaee6bf6595
tommaso1 <tommasorosso1@gmail.com>
2023-11-20 18:58:25 +0000

    [DEV-1090] Fix inline images

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/Item.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/Item.tsx
@@ -1,15 +1,35 @@
 import Typography from '@mui/material/Typography';
 import { ItemProps } from 'gitbook-docs/markdoc/schema/item';
+import React, { useEffect } from 'react';
 import { ReactNode } from 'react';
 
-const Item = ({ children }: ItemProps<ReactNode>) => (
-  <Typography
-    component='li'
-    variant='body1'
-    style={{ wordBreak: 'break-word' }}
-  >
-    {children}
-  </Typography>
-);
+const Item = ({ children }: ItemProps<ReactNode>) => {
+
+  return (
+    <Typography
+      component='li'
+      variant='body1'
+      style={{
+        wordBreak: 'break-word',
+      }}
+      sx={{
+        '& .MuiContainer-root': {
+          width: 'initial',
+          marginLeft: '1rem',
+        },
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'start',
+        }}
+      >
+        {children}
+      </div>
+    </Typography>
+  );
+};
 
 export default Item;
