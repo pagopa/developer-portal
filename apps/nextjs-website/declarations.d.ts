6676abc2d621f21710df7b7736dc1aa5c436186a
jeremygordillo <jere.gordi@gmail.com>
2023-11-29 18:42:01 +0100

    add comments in declarations.d.ts

--- apps/nextjs-website/declarations.d.ts
+++ apps/nextjs-website/declarations.d.ts
@@ -1,3 +1,6 @@
+// I added this declaration to fix the problem with swagger-ui types.
+// I should access the System property of SwaggerUI but the swagger-ui types don't declare it.
+// See apps/nextjs-website/src/helpers/swagger.ts for more details
 declare module 'swagger-ui' {
   const SwaggerUI: any;
   export default SwaggerUI;
