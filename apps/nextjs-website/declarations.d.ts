c0091c4a387e77b4860d5dae5a74b906bbcbcaac
jeremygordillo <jere.gordi@gmail.com>
2023-11-29 18:09:07 +0100

    fix: build swagger-ui error

--- apps/nextjs-website/declarations.d.ts
+++ apps/nextjs-website/declarations.d.ts
@@ -2,3 +2,8 @@ declare module 'swagger-ui' {
   const SwaggerUI: any;
   export default SwaggerUI;
 }
+
+declare module 'next/dist/compiled/undici' {
+  const File: any;
+  export default File;
+}
--- apps/nextjs-website/src/app/layout.tsx
+++ apps/nextjs-website/src/app/layout.tsx
@@ -4,6 +4,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import '@/styles/globals.css';
+import '@/polyfill';
 import ThemeRegistry from './ThemeRegistry';
 import { getProducts } from '@/lib/api';
 import SiteFooter from '@/components/atoms/SiteFooter/SiteFooter';
