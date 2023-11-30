88edd4f0d47f71465d82bcae53200d014d1991db
tommaso1 <tommasorosso1@gmail.com>
2023-11-30 14:24:02 +0000

    redirect on webinar

--- apps/nextjs-website/src/components/molecules/SubscribeToWebinar/SubscribeToWebinar.tsx
+++ apps/nextjs-website/src/components/molecules/SubscribeToWebinar/SubscribeToWebinar.tsx
@@ -9,7 +9,7 @@ import {
 } from '@/helpers/userPreferences.helpers';
 import { useTranslations } from 'next-intl';
 import { DevPortalUser } from '@/lib/types/auth';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 export type SubscribeButtonProps = {
   webinarSlug?: string;
@@ -35,6 +35,7 @@ const SubscribeToWebinar = ({
   const t = useTranslations('webinar');
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     if (userAttributes && webinarSlug) {
@@ -87,7 +88,7 @@ const SubscribeToWebinar = ({
   const onSubscribeWithoutUser = () => {
     setIsLoading(true);
     // eslint-disable-next-line functional/immutable-data
-    router.push('/auth/login');
+    router.push(`/auth/login?redirect=${pathname}`);
     return null;
   };
 
