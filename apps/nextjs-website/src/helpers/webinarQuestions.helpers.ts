9c40a40e6b9a7c43133c6c5bc3c5c5e05d6ba02e
Marco Ponchia <marco.ponchia@uqido.com>
2023-12-07 17:09:20 +0100

    add guard on missing variable

--- apps/nextjs-website/src/helpers/webinarQuestions.helpers.ts
+++ apps/nextjs-website/src/helpers/webinarQuestions.helpers.ts
@@ -2,6 +2,9 @@ import { webinarQuestionConfig } from '@/config';
 
 export async function addWebinarQuestion(params: Record<string, string>) {
   const { url, resource, token } = webinarQuestionConfig;
+  if (!url || !resource || !token) {
+    return { status: 'Missing configuration' };
+  }
   const requestOptions = {
     method: 'POST',
     body: JSON.stringify({
