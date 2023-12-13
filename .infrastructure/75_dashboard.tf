0c7eb2e45e55aabbeb78dfd7e0927b5ec1d1cbc2
AF <alessandro.ferlin@pagopa.it>
2023-12-13 15:47:09 +0100

    Update dashboard

--- .infrastructure/75_dashboard.tf
+++ .infrastructure/75_dashboard.tf
@@ -4,49 +4,121 @@ resource "aws_cloudwatch_dashboard" "main" {
   dashboard_body = jsonencode({
     widgets = [
       {
-        type   = "metric"
-        x      = 0
-        y      = 0
-        width  = 12
-        height = 6
+        "height" : 1,
+        "width" : 24,
+        "y" : 0,
+        "x" : 0,
+        "type" : "text",
+        "properties" : {
+          "markdown" : "## FE (CDN) \n"
+        }
+      },
+      {
+        "height" : 6,
+        "width" : 6,
+        "y" : 1,
+        "x" : 0,
+        "type" : "metric",
+        "properties" : {
+          "timezone" : "UTC",
+          "region" : var.aws_region,
+          "view" : "timeSeries",
+          "stacked" : false,
+          "metrics" : [
+            ["AWS/CloudFront", "Requests", "Region", "Global", "DistributionId", aws_cloudfront_distribution.website.id, { "region" : var.aws_region }]
+          ],
+          "title" : "Requests"
+        }
+      },
+      {
+        "height" : 6,
+        "width" : 6,
+        "y" : 1,
+        "x" : 6,
+        "type" : "metric",
+        "properties" : {
+          "timezone" : "UTC",
+          "region" : var.aws_region,
+          "view" : "timeSeries",
+          "stacked" : false,
+          "metrics" : [
+            ["AWS/CloudFront", "4xxErrorRate", "Region", "Global", "DistributionId", aws_cloudfront_distribution.website.id, { "region" : var.aws_region }],
+            [".", "5xxErrorRate", ".", ".", ".", ".", { "region" : "us-east-1" }]
+          ],
+          "title" : "4xxErrorRate, 5xxErrorRate"
+        }
+      },
+      {
+        "height" : 6,
+        "width" : 6,
+        "y" : 1,
+        "x" : 12,
+        "type" : "metric",
+        "properties" : {
+          "timezone" : "UTC",
+          "region" : var.aws_region,
+          "view" : "timeSeries",
+          "stacked" : false,
+          "metrics" : [
+            ["AWS/CloudFront", "BytesDownloaded", "Region", "Global", "DistributionId", aws_cloudfront_distribution.website.id, { "region" : var.aws_region }]
+          ],
+          "title" : "BytesDownloaded"
+        }
+      },
+      {
+        "height" : 1,
+        "width" : 24,
+        "y" : 2,
+        "x" : 0,
+        "type" : "text",
+        "properties" : {
+          "markdown" : "## Cognito \n"
+        }
+      },
+      {
+        "type" : "metric"
+        "y" : 3
+        "x" : 0
+        "width" : 12
+        "height" : 6
 
-        properties = {
-          metrics = [
+        "properties" : {
+          "metrics" : [
             ["AWS/Lambda", "Errors", "FunctionName", module.cognito_post_confirmation_function.lambda_function_name, { "period" : 300, "stat" : "Sum", "region" : var.aws_region }],
             ["...", module.cognito_custom_message_function.lambda_function_name, { "period" : 300, "stat" : "Sum", "region" : var.aws_region }],
             ["...", module.cognito_verify_auth_challenge_function.lambda_function_name, { "period" : 300, "stat" : "Sum", "region" : var.aws_region }],
             ["...", module.cognito_create_auth_challenge_function.lambda_function_name, { "period" : 300, "stat" : "Sum", "region" : var.aws_region }],
             ["...", module.cognito_define_auth_challenge_function.lambda_function_name, { "period" : 300, "stat" : "Sum", "region" : var.aws_region }]
           ],
-          legend = {
-            position = "right"
+          "legend" : {
+            "position" : "right"
           },
-          title    = "Lambda Errors: Sum",
-          view     = "timeSeries",
-          stacked  = false,
-          region   = var.aws_region
+          "title" : "Lambda Errors: Sum",
+          "view" : "timeSeries",
+          "stacked" : false,
+          "region" : var.aws_region
         }
       },
       {
-        type   = "metric"
-        x      = 0
-        y      = 0
-        width  = 12
-        height = 6
+        "type" : "metric"
+        "y" : 3
+        "x" : 12
+        "width" : 12
+        "height" : 6
 
-        properties = {
-          sparkline = false,
-          metrics = [
+        "properties" : {
+          "sparkline" : false,
+          "metrics" : [
             ["AWS/Cognito", "SignInSuccesses", "UserPool", aws_cognito_user_pool.devportal.id, "UserPoolClient", aws_cognito_user_pool_client.devportal_website.id],
             [".", "SignUpSuccesses", ".", ".", ".", "."],
             [".", "TokenRefreshSuccesses", ".", ".", ".", "."]
           ],
-          view    = "timeSeries",
-          stacked = false,
-          region  = var.aws_region,
-          stat    = "SampleCount",
-          period  = 300,
-          title   = "Cognito"
+          "view" : "timeSeries",
+          "stacked" : false,
+          "region" : var.aws_region,
+          "stat" : "SampleCount",
+          "period" : 300,
+          "title" : "Cognito"
         }
       }
     ]
