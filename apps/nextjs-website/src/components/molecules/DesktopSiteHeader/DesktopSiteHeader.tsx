90762bdff9c0707fdd99a8fbc73aa9528e44ff45
Marco Bottaro <marco.bottaro@uqido.com>
2024-01-18 19:22:58 +0100

    Hide webinar link in production environment

--- apps/nextjs-website/src/components/molecules/DesktopSiteHeader/DesktopSiteHeader.tsx
+++ apps/nextjs-website/src/components/molecules/DesktopSiteHeader/DesktopSiteHeader.tsx
@@ -5,6 +5,7 @@ import DesktopUserInfo from '@/components/atoms/DesktopUserInfo/DesktopUserInfo'
 import React from 'react';
 import { SiteHeaderProps } from '@/components/molecules/SiteHeader/SiteHeader';
 import { useTranslations } from 'next-intl';
+import { isProduction } from '@/config';
 
 const DesktopSiteHeader = ({ products }: SiteHeaderProps) => {
   const t = useTranslations('devPortal');
@@ -27,15 +28,17 @@ const DesktopSiteHeader = ({ products }: SiteHeaderProps) => {
           label: product.name,
         }))}
       />
-      <LinkMui
-        component={Link}
-        color='primary.main'
-        underline='none'
-        href={'/webinars'}
-        sx={{ fontSize: '16px', fontWeight: 600 }}
-      >
-        {t('siteHeader.webinars')}
-      </LinkMui>
+      {!isProduction && (
+        <LinkMui
+          component={Link}
+          color='primary.main'
+          underline='none'
+          href={'/webinars'}
+          sx={{ fontSize: '16px', fontWeight: 600 }}
+        >
+          {t('siteHeader.webinars')}
+        </LinkMui>
+      )}
       <DesktopUserInfo />
     </Box>
   );
