04246c9fa364355fa11f1f57423572c8028dcfcf
tommaso1 <tommasorosso1@gmail.com>
2024-01-19 11:26:06 +0100

    [DEV-1022] Editable profile (#499)
    * editable profile
    
    * linter
    
    * pr comments
    
    * null check
    
    * pr comments
    
    * linter
    
    * fix merge artifact
    
    * Update apps/nextjs-website/src/components/molecules/InfoCard/InfoCardProfile.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Allowing empty values
    
    * required fields
    
    * Update apps/nextjs-website/src/app/profile/personal-data/page.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/app/profile/personal-data/page.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/app/profile/personal-data/page.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/app/profile/personal-data/page.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/app/profile/personal-data/page.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/app/profile/personal-data/page.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/components/molecules/InfoCard/InfoCardProfile.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/components/atoms/InfoCardItem/InfoCardItemProfile.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/components/atoms/InfoCardItem/InfoCardItemProfile.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Fix select label
    
    * Fix profile layout
    
    ---------
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    Co-authored-by: Marco Ponchia <marco.ponchia@uqido.com>

--- apps/nextjs-website/src/app/profile/personal-data/page.tsx
+++ apps/nextjs-website/src/app/profile/personal-data/page.tsx
@@ -2,20 +2,18 @@
 import { Box, Divider, Stack, Typography } from '@mui/material';
 import { Auth } from 'aws-amplify';
 import { useTranslations } from 'next-intl';
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { translations } from '@/_contents/translations';
+import { InfoCardItemProfileProps } from '@/components/atoms/InfoCardItem/InfoCardItemProfile';
 import {
   InfoCardItem,
   InfoCardItemProps,
 } from '@/components/atoms/InfoCardItem/InfoCardItem';
 import DeleteSection from '@/components/molecules/DeleteSection/DeleteSection';
-import { InfoCard } from '@/components/molecules/InfoCard/InfoCard';
 import { ProfileInfoCard } from '@/components/organisms/Auth/ProfileInfoCard';
 import { useUser } from '@/helpers/user.helper';
-import { useRouter } from 'next/navigation';
-import ConfirmationModal from '@/components/atoms/ConfirmationModal/ConfirmationModal';
 import PasswordFormWrapper from '@/components/organisms/Auth/PasswordFormWrapper';
+import { InfoCardProfile } from '@/components/molecules/InfoCard/InfoCardProfile';
 
 const PersonalData = () => {
   const {
@@ -24,49 +22,62 @@ const PersonalData = () => {
     },
   } = translations;
   const t = useTranslations('profile');
-  const router = useRouter();
-  const { user } = useUser();
+
+  const { user, setUserAttributes } = useUser();
+
+  const [profileDataSectionItems, setProfileDataSectionItems] = useState<
+    InfoCardItemProfileProps[]
+  >([]);
+
+  useEffect(() => {
+    setProfileDataSectionItems([
+      {
+        title: t('personalData.fields.name'),
+        value: user?.attributes.given_name,
+        editable: true,
+        type: 'text',
+        required: true,
+      },
+      {
+        title: t('personalData.fields.surname'),
+        value: user?.attributes.family_name,
+        editable: true,
+        type: 'text',
+        required: true,
+      },
+      {
+        title: t('personalData.fields.role'),
+        value: user?.attributes['custom:job_role'],
+        editable: true,
+        type: 'text',
+        required: false,
+      },
+      {
+        title: t('personalData.fields.sector'),
+        value: user?.attributes['custom:company_type'],
+        editable: true,
+        type: 'select',
+        values: companyRoles,
+        required: false,
+      },
+    ]);
+  }, [user?.attributes]);
+
   const [editItem, setEditItem] = useState<InfoCardItemProps | null>(null);
-  const [showModal, setShowModal] = useState(false);
 
   async function handleChangePassword(
     oldPassword: string,
     newPassword: string
   ) {
     await Auth.changePassword(user, oldPassword, newPassword);
-    setShowModal(true);
   }
-
-  const dataSectionItems: InfoCardItemProps[] = [
-    {
-      name: 'name',
-      title: t('personalData.fields.name'),
-      value: user?.attributes.given_name,
-    },
-    {
-      name: 'surname',
-      title: t('personalData.fields.surname'),
-      value: user?.attributes.family_name,
-    },
-    {
-      name: 'role',
-      title: t('personalData.fields.role'),
-      value: user?.attributes['custom:job_role'],
-    },
-    {
-      name: 'sector',
-      title: t('personalData.fields.sector'),
-      value: companyRoles.find(
-        (role) => role.value === user?.attributes['custom:company_type']
-      )?.title,
-    },
-  ];
 
   const accountSectionItems: InfoCardItemProps[] = [
     {
       name: 'email',
       title: t('personalData.fields.email'),
       value: user?.attributes.email,
+      editable: false,
     },
     {
       name: 'password',
@@ -108,30 +119,43 @@ const PersonalData = () => {
 
   return (
     <>
-      <ConfirmationModal
-        setOpen={() => null}
-        open={showModal}
-        title={t('changePassword.dialog.title')}
-        text={t('changePassword.dialog.text')}
-        confirmCta={{
-          label: t('changePassword.dialog.confirmLabel'),
-          onClick: () => {
-            Auth.signOut().then(() => {
-              router.replace('/auth/login');
-            });
-            return null;
-          },
-        }}
-      />
       <Stack
         gap={5}
         sx={{ padding: { xs: '40px 24px', md: '80px 40px' }, width: '100%' }}
       >
         <Typography variant='h4'>{t('personalData.title')}</Typography>
-        <InfoCard
+        <InfoCardProfile
           cardTitle={t('personalData.dataSection')}
-          items={dataSectionItems}
+          items={profileDataSectionItems}
+          onValue={(items: InfoCardItemProfileProps[]) => {
+            const oldItems = [...profileDataSectionItems];
+
+            if (!user) return null;
+
+            setUserAttributes(
+              {
+                ...user.attributes,
+                ...{
+                  given_name: items[0].value || user.attributes.given_name,
+                  family_name: items[1].value || user.attributes.family_name,
+                  'custom:job_role': items[2].value || '',
+                  'custom:company_type':
+                    items[3].value || user.attributes['custom:company_type'],
+                },
+              },
+              () => {
+                setProfileDataSectionItems(items);
+                return null;
+              },
+              () => {
+                setProfileDataSectionItems(oldItems);
+                return null;
+              }
+            );
+            return null;
+          }}
         />
+
         <ProfileInfoCard
           cardTitle={t('personalData.accountSection')}
           items={accountSectionItems}
