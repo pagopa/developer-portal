0108c4847219285680439bef77c0595de6974d5b
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-01-22 18:42:13 +0100

    Fix Terraform module source

--- .infrastructure/50_storage.tf
+++ .infrastructure/50_storage.tf
@@ -49,7 +49,7 @@ resource "aws_s3_bucket_policy" "cloudfront" {
 # Webinar questions database
 
 module "dynamodb_webinar_questions" {
-  source = "git::git@github.com:terraform-aws-modules/terraform-aws-dynamodb-table.git?ref=715399dbe24f6443820bf5de80f6100b35d56355" # v4.0.0
+  source = "git::github.com/terraform-aws-modules/terraform-aws-dynamodb-table.git?ref=715399dbe24f6443820bf5de80f6100b35d56355" # v4.0.0
 
   billing_mode                = "PAY_PER_REQUEST"
   deletion_protection_enabled = true
