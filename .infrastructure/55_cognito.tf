81507d5d3c5a224ebe32188c511528a0553a9cc3
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-01-23 11:14:16 +0100

    Extract IAM policy document to avoid duplication

--- .infrastructure/55_cognito.tf
+++ .infrastructure/55_cognito.tf
@@ -348,56 +348,40 @@ resource "aws_cognito_identity_pool" "devportal" {
 
 }
 
-resource "aws_iam_role" "devportal_authenticated_user" {
-  name        = "DevPortalAuthenticatedUser"
-  description = "The role assumed by the authenticated devportal users"
+data "aws_iam_policy_document" "authenticated_users_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Principal = {
-          "Federated" : "cognito-identity.amazonaws.com"
-        }
-        Condition = {
-          StringEquals = {
-            "cognito-identity.amazonaws.com:aud" : aws_cognito_identity_pool.devportal.id
-          }
-          "ForAnyValue:StringLike" = {
-            "cognito-identity.amazonaws.com:amr" : "authenticated"
-          }
-        }
-      }
-    ]
-  })
+    principals {
+      type        = "Service"
+      identifiers = ["cognito-identity.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "cognito-identity.amazonaws.com:aud"
+      values   = [aws_cognito_identity_pool.devportal.id]
+    }
+
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "cognito-identity.amazonaws.com:amr"
+      values   = ["authenticated"]
+    }
+  }
+}
+
+resource "aws_iam_role" "devportal_authenticated_user" {
+  name               = "DevPortalAuthenticatedUser"
+  description        = "The role assumed by the authenticated devportal users"
+  assume_role_policy = data.aws_iam_policy_document.authenticated_users_policy.json
 }
 
 resource "aws_iam_role" "devportal_authenticated_host_user" {
-  name        = "DevPortalAuthenticatedHostUser"
-  description = "The role assumed by the authenticated host devportal users"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Principal = {
-          "Federated" : "cognito-identity.amazonaws.com"
-        }
-        Condition = {
-          StringEquals = {
-            "cognito-identity.amazonaws.com:aud" : aws_cognito_identity_pool.devportal.id
-          }
-          "ForAnyValue:StringLike" = {
-            "cognito-identity.amazonaws.com:amr" : "authenticated"
-          }
-        }
-      }
-    ]
-  })
+  name               = "DevPortalAuthenticatedHostUser"
+  description        = "The role assumed by the authenticated host devportal users"
+  assume_role_policy = data.aws_iam_policy_document.authenticated_users_policy.json
 }
 
 resource "aws_cognito_identity_pool_roles_attachment" "main" {
