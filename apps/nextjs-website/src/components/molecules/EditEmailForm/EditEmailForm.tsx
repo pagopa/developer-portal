eadf4e3fa8837beb70bce604a4b9a57a6cf580b6
marcobottaro <39835990+marcobottaro@users.noreply.github.com>
2024-01-24 16:33:42 +0100

    [DEV-1272] update attribute email (#532)
    * feat: update password from user profile
    
    * add translations & improve errors
    
    * changes after review
    
    * changes after review
    
    * add error state to password text field label
    
    * update message
    
    * Refactor personal-data page to handle both change password and change email
    
    * Add UX to edit email user attribute
    
    * Remove duplicated import
    
    * Fix distance from divider
    
    * Fix it.json indentation
    
    * Add changeset file
    
    * Handle errors and add expiredCodePage
    
    * Add error case and move signOut after router push
    
    * Use PageBackgroundWrapper component instead of Box
    
    * Rename state constant
    
    * Remove ExpiredCodeCard component
    
    * Move components to their own folder
    
    * Add 'use client'
    
    ---------
    
    Co-authored-by: jeremygordillo <jere.gordi@gmail.com>

---
+++ apps/nextjs-website/src/components/molecules/EditEmailForm/EditEmailForm.tsx
@@ -0,0 +1,124 @@
+import { ButtonNaked } from '@pagopa/mui-italia';
+import { Stack, Typography } from '@mui/material';
+import { useCallback, useState } from 'react';
+
+import { useTranslations } from 'next-intl';
+import { emailMatcher } from '@/helpers/auth.helpers';
+import RequiredTextField, {
+  ValidatorFunction,
+} from '@/components/molecules/RequiredTextField/RequiredTextField';
+
+type EditEmailFormProps = {
+  onSave: (email: string) => Promise<void>;
+  // eslint-disable-next-line functional/no-return-void
+  onCancel: () => void;
+};
+
+type FormSchema = {
+  email: string;
+};
+
+const EditEmailForm = ({ onSave, onCancel }: EditEmailFormProps) => {
+  const t = useTranslations('profile');
+
+  const [errors, setErrors] = useState<Partial<FormSchema>>({});
+  const [formValue, setFormValue] = useState<FormSchema>({
+    email: '',
+  });
+
+  const emailValidators: ValidatorFunction[] = [
+    (value: string) => ({
+      valid: emailMatcher.test(value),
+      error: t('changeEmail.wrongEmail'),
+    }),
+  ];
+
+  const validateForm = useCallback(() => {
+    const { email } = formValue;
+    // eslint-disable-next-line functional/no-let
+    let err = {};
+
+    if (!emailMatcher.test(email)) {
+      err = { ...err, email: t('changeEmail.wrongEmail') };
+    }
+
+    setErrors(err);
+    const hasErrors = Object.keys(err).length > 0;
+    return !hasErrors;
+  }, [formValue, t]);
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+
+    setFormValue(() => ({ email: value }));
+  };
+
+  const handleSave = () => {
+    if (!validateForm()) return;
+    onSave(formValue.email).catch((error) => {
+      if (error.code === 'NotAuthorizedException') {
+        setErrors({ email: t('changeEmail.notAuthorizedException') });
+      } else {
+        console.error(error);
+      }
+    });
+  };
+
+  const actions = (
+    <>
+      <ButtonNaked
+        color='primary'
+        sx={{ paddingLeft: 0, paddingRight: 0 }}
+        onClick={onCancel}
+      >
+        {t('changeEmail.cancel')}
+      </ButtonNaked>
+      <ButtonNaked variant='contained' color='primary' onClick={handleSave}>
+        {t('changeEmail.save')}
+      </ButtonNaked>
+    </>
+  );
+
+  return (
+    <Stack gap={3}>
+      <Stack
+        alignItems={{ xs: 'flex-start', md: 'center' }}
+        flexDirection={{ xs: 'column', md: 'row' }}
+        gap={2}
+        mt={{ xs: 1, md: 3 }}
+      >
+        <Typography
+          variant='body2'
+          flexGrow={1}
+          fontSize={16}
+          minWidth={{ xs: 'auto', md: '170px' }}
+        >
+          {t('changeEmail.title')}
+        </Typography>
+        <Stack flexDirection='row' gap={4} display={{ xs: 'none', md: 'flex' }}>
+          {actions}
+        </Stack>
+      </Stack>
+      <RequiredTextField
+        label={t('personalData.fields.email')}
+        value={formValue.email}
+        onChange={handleEmailChange}
+        helperText={t('changeEmail.wrongEmail')}
+        customValidators={emailValidators}
+        sx={{ marginBottom: { xs: 0, md: 3 } }}
+      />
+      <Stack
+        flexDirection='row'
+        gap={4}
+        display={{ xs: 'flex', md: 'none' }}
+        sx={{ marginBottom: { xs: 3, md: 0 } }}
+      >
+        {actions}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default EditEmailForm;
