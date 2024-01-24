e99ad8c49d9e277c82caef4059774df9dcde7bae
jeremygordillo <jere.gordi@gmail.com>
2024-01-24 16:09:55 +0100

    add test

--- apps/nextjs-website/jest.config.ts
+++ apps/nextjs-website/jest.config.ts
@@ -4,6 +4,7 @@ const jestConfig: JestConfigWithTsJest = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx)'],
   moduleFileExtensions: ['ts', 'js', 'tsx'],
+  testEnvironment: 'jsdom',
   transform: {
     // See the discussion https://github.com/vercel/next.js/issues/8663
     '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
@@ -12,6 +13,9 @@ const jestConfig: JestConfigWithTsJest = {
     // Needed or else if some test files imports a file from the src folder using the Next naming convention, it will fail.
     // Example: import { shared } from '@/_contents/translations'; returns Cannot find module '@/_contents/translations' from 'src/some-file.ts'
     '@/(.*)': '<rootDir>/src/$1',
+    // '@fontsource/(.*)': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$':
+      '<rootDir>/src/__tests__/__mocks__/fileMock.js',
   },
 };
 
---
+++ apps/nextjs-website/src/__tests__/helpers/EmailFormWrapper.test.tsx
@@ -0,0 +1,134 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { PropsWithChildren } from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import EmailFormWrapper from '@/components/organisms/Auth/EmailFormWrapper';
+import messages from '@/messages/it.json';
+
+const Wrapper = ({ children }: PropsWithChildren) => (
+  <NextIntlClientProvider locale={'it'} messages={messages}>
+    {children}
+  </NextIntlClientProvider>
+);
+
+describe('EmailFormWrapper', () => {
+  const mockOnCancel = jest.fn();
+  const mockOnSave = jest.fn((email: string) => Promise.resolve());
+  const mockOnEdit = jest.fn();
+
+  const item = {
+    editable: true,
+    name: 'email',
+    title: 'Email',
+    value: 'mockEmail@mock.it',
+    onEdit: mockOnEdit,
+  };
+
+  it('should render EditEmailForm when isEditing is true', () => {
+    const { getByRole } = render(
+      <Wrapper>
+        <EmailFormWrapper
+          item={item}
+          isEditing={true}
+          onCancel={mockOnCancel}
+          onSave={mockOnSave}
+          onEdit={mockOnEdit}
+        />
+      </Wrapper>
+    );
+
+    expect(getByRole('textbox')).toBeTruthy();
+  });
+
+  it('should render InfoCardItem when isEditing is false', () => {
+    const { getAllByText } = render(
+      <Wrapper>
+        <EmailFormWrapper
+          item={item}
+          isEditing={false}
+          onCancel={mockOnCancel}
+          onSave={mockOnSave}
+          onEdit={mockOnEdit}
+        />
+      </Wrapper>
+    );
+
+    expect(getAllByText(item.value)).toBeTruthy();
+  });
+
+  it('should call onEdit when edit button is clicked', () => {
+    const { getByRole } = render(
+      <Wrapper>
+        <EmailFormWrapper
+          item={item}
+          isEditing={false}
+          onCancel={mockOnCancel}
+          onSave={mockOnSave}
+          onEdit={mockOnEdit}
+        />
+      </Wrapper>
+    );
+
+    fireEvent.click(getByRole('button'));
+    expect(mockOnEdit).toHaveBeenCalled();
+  });
+
+  it('should call onCancel when cancel button is clicked', () => {
+    const { getAllByRole } = render(
+      <Wrapper>
+        <EmailFormWrapper
+          item={item}
+          isEditing={true}
+          onCancel={mockOnCancel}
+          onSave={mockOnSave}
+          onEdit={mockOnEdit}
+        />
+      </Wrapper>
+    );
+
+    fireEvent.click(getAllByRole('button', { name: /annulla/i })[0]);
+    expect(mockOnCancel).toHaveBeenCalled();
+  });
+
+  it('should not call onSave when save button is clicked and email input has not valid value', () => {
+    const { getByRole, getAllByRole } = render(
+      <Wrapper>
+        <EmailFormWrapper
+          item={item}
+          isEditing={true}
+          onCancel={mockOnCancel}
+          onSave={mockOnSave}
+          onEdit={mockOnEdit}
+        />
+      </Wrapper>
+    );
+
+    fireEvent.change(getByRole('textbox'), {
+      target: { value: 'updated' },
+    });
+
+    fireEvent.click(getAllByRole('button', { name: /conferma/i })[0]);
+    expect(mockOnSave).not.toHaveBeenCalled();
+  });
+
+  it('should call onSave when save button is clicked and email input is valid', () => {
+    const { getByRole, getAllByRole } = render(
+      <Wrapper>
+        <EmailFormWrapper
+          item={item}
+          isEditing={true}
+          onCancel={mockOnCancel}
+          onSave={mockOnSave}
+          onEdit={mockOnEdit}
+        />
+      </Wrapper>
+    );
+
+    fireEvent.change(getByRole('textbox'), {
+      target: { value: 'updated@mockmail.com' },
+    });
+
+    fireEvent.click(getAllByRole('button', { name: /conferma/i })[0]);
+    expect(mockOnSave).toHaveBeenCalled();
+  });
+});
