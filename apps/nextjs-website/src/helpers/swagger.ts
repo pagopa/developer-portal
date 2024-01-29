0735b1f82a971c903c0f9826ddc7ebfcdf75597f
AF <alessandro.ferlin@pagopa.it>
2024-01-29 16:02:03 +0100

    Code review suggestion

--- apps/nextjs-website/src/helpers/swagger.ts
+++ apps/nextjs-website/src/helpers/swagger.ts
@@ -1,7 +1,12 @@
 import SwaggerUI from 'swagger-ui';
 
-// https://github.com/swagger-api/swagger-ui/wiki/JSON-Schema-samples-API
+//@ts-expect-error the types are not exported as expected, the following code
+//should type
+//https://github.com/swagger-api/swagger-ui/wiki/JSON-Schema-samples-API
 export const system = new SwaggerUI.System({
+  //@ts-expect-error same as above, the types are not exported as expected, the
+  //following code should type
+  //https://github.com/swagger-api/swagger-ui/wiki/JSON-Schema-samples-API
   plugins: [SwaggerUI.plugins.JSONSchema5Samples],
   presets: [],
 }).getSystem();
