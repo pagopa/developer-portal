92555e2d7152fa98beb5e6f0fa85599ecab90e93
tommaso1 <tommasorosso1@gmail.com>
2024-01-30 10:02:28 +0000

    fix config jest

--- apps/nextjs-website/jest.config.ts
+++ apps/nextjs-website/jest.config.ts
@@ -16,6 +16,7 @@ const jestConfig: JestConfigWithTsJest = {
     // '@fontsource/(.*)': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$':
       '<rootDir>/src/__tests__/__mocks__/fileMock.js',
+      "^uuid$": require.resolve("uuid")
   },
 };
 
