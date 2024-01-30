1ea08685e7835a2741508d312bedc710b2dadf40
jeremygordillo <jere.gordi@gmail.com>
2024-01-30 11:07:43 +0100

    update jest config

--- apps/nextjs-website/jest.config.ts
+++ apps/nextjs-website/jest.config.ts
@@ -2,7 +2,7 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.+(ts|tsx)'],
+  testMatch: ['**/__tests__/**/*test.+(ts|tsx)'],
   moduleFileExtensions: ['ts', 'js', 'tsx'],
   testEnvironment: 'jsdom',
   transform: {
@@ -13,10 +13,9 @@ const jestConfig: JestConfigWithTsJest = {
     // Needed or else if some test files imports a file from the src folder using the Next naming convention, it will fail.
     // Example: import { shared } from '@/_contents/translations'; returns Cannot find module '@/_contents/translations' from 'src/some-file.ts'
     '@/(.*)': '<rootDir>/src/$1',
-    // '@fontsource/(.*)': 'identity-obj-proxy',
+    '^uuid$': require.resolve('uuid'),
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css|less)$':
       '<rootDir>/src/__tests__/__mocks__/fileMock.js',
-      "^uuid$": require.resolve("uuid")
   },
 };
 
