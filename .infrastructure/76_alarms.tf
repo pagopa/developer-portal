31e0474ca6eae8104c9f59ce24c1a629ef0f28ab
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-01-31 16:31:38 +0100

    Add unit

--- .infrastructure/76_alarms.tf
+++ .infrastructure/76_alarms.tf
@@ -5,6 +5,7 @@ module "metric_alarm" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-cloudwatch.git//modules/metric-alarm?ref=0b4aa2b9aa19060205965a938de89a7bf0ff477b" # v5.1.0
 
   alarm_name        = "DevPortal | Website | SES | Bounce Rate"
+  actions_enabled   = true
   alarm_description = "Alarm to monitor the bounce rate"
   metric_name       = "Bounce"
   namespace         = "AWS/SES"
@@ -12,6 +13,7 @@ module "metric_alarm" {
   comparison_operator = "GreaterThanOrEqualToThreshold"
   threshold           = "0.05" # 5%
   statistic           = "Average"
+  unit                = "Count"
   period              = 300 # 5 minutes
   evaluation_periods  = 1
 }
