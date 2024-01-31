80fbbd216268095211bb91ff2eb84b126877d34d
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-01-31 17:23:49 +0100

    Add SES send rate limit alarm

--- .infrastructure/76_alarms.tf
+++ .infrastructure/76_alarms.tf
@@ -53,3 +53,21 @@ module "ses_daily_sending_quota_alarm" {
   period              = 3600 # 1 hour
   evaluation_periods  = 1
 }
+
+## Send rate limit alarm
+module "ses_sending_rate_limit_alarm" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-cloudwatch.git//modules/metric-alarm?ref=0b4aa2b9aa19060205965a938de89a7bf0ff477b" # v5.1.0
+
+  alarm_name        = "DevPortal | Website | SES | Send Rate"
+  actions_enabled   = true
+  alarm_description = "Alarm to monitor the rate limit per second"
+  metric_name       = "Send"
+  namespace         = "AWS/SES"
+
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "140" # At the moment we can send 14 emails/second, so 140 in 10 seconds
+  statistic           = "Sum"
+  unit                = "Count"
+  period              = 10 # 10 seconds
+  evaluation_periods  = 1
+}
