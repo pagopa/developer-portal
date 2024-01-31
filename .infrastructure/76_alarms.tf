eff144c738fe4c4966682335dda624467d6f499e
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-01-31 17:11:35 +0100

    Add SES daily sending quota alarm

--- .infrastructure/76_alarms.tf
+++ .infrastructure/76_alarms.tf
@@ -35,3 +35,21 @@ module "ses_reputation_complaint_rate_alarm" {
   period              = 300 # 5 minutes
   evaluation_periods  = 1
 }
+
+## Daily sending quota alarm
+module "ses_daily_sending_quota_alarm" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-cloudwatch.git//modules/metric-alarm?ref=0b4aa2b9aa19060205965a938de89a7bf0ff477b" # v5.1.0
+
+  alarm_name        = "DevPortal | Website | SES | Send"
+  actions_enabled   = true
+  alarm_description = "Alarm to monitor the daily sending quota limit"
+  metric_name       = "Send"
+  namespace         = "AWS/SES"
+
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = "40000"
+  statistic           = "Sum"
+  unit                = "Count"
+  period              = 3600 # 1 hour
+  evaluation_periods  = 1
+}
