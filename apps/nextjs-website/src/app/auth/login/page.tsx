ec6ad0b615cc8737edfd4bc868756d05c39c0a79
Marco Bottaro <marco.bottaro@uqido.com>
2024-02-05 14:28:54 +0100

    Fix sign-in and confirm sign-in UI and UX

--- apps/nextjs-website/src/app/auth/login/page.tsx
+++ apps/nextjs-website/src/app/auth/login/page.tsx
@@ -1,29 +1,59 @@
 'use client';
 import LoginForm from '@/components/organisms/Auth/LoginForm';
 import ConfirmLogIn from '@/components/organisms/Auth/ConfirmLogin';
-import { Box, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import { useCallback, useState } from 'react';
 import { Auth } from 'aws-amplify';
 import { LoginSteps } from '@/lib/types/loginSteps';
 import { LoginFunction } from '@/lib/types/loginFunction';
+import ConfirmSignUp from '@/components/organisms/Auth/ConfirmSignUp';
 import { useRouter, useSearchParams } from 'next/navigation';
+import PageBackgroundWrapper from '@/components/atoms/PageBackgroundWrapper/PageBackgroundWrapper';
 
 const Login = () => {
   const router = useRouter();
   const [logInStep, setLogInStep] = useState(LoginSteps.LOG_IN);
   const [user, setUser] = useState(null);
   const [userName, setUserName] = useState<string | null>(null);
+  const [noAccount, setNoAccount] = useState<boolean>(false);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const onLogin: LoginFunction = useCallback(async ({ username, password }) => {
+    setNoAccount(false);
+    setUsername(username);
+    setPassword(password);
+
     const user = await Auth.signIn({
       username,
       password,
+    }).catch((error) => {
+      if (error.code === 'UserNotConfirmedException') {
+        setUserName(username);
+        setLogInStep(LoginSteps.CONFIRM_ACCOUNT);
+      } else {
+        setNoAccount(true);
+      }
+      return false;
     });
 
     setUserName(username);
-    setUser(user);
-    setLogInStep(LoginSteps.MFA_CHALLENGE);
+
+    if (user) {
+      setUser(user);
+      setLogInStep(LoginSteps.MFA_CHALLENGE);
+    }
   }, []);
+
+  const resendCode = useCallback(async () => {
+    const result = await onLogin({ username, password })
+      .then(() => true)
+      .catch(() => false);
+
+    return result;
+  }, [onLogin, password, username]);
+
   const searchParams = useSearchParams();
 
   const confirmLogin = useCallback(
@@ -33,36 +63,45 @@ const Login = () => {
       const redirect = searchParams.get('redirect');
       router.replace(redirect ? redirect : '/');
     },
-    [router, user]
+    [router, searchParams, user]
   );
 
+  const onBackStep = useCallback(() => {
+    router.replace(
+      `/auth/login?email=${encodeURIComponent(userName || '')}&step=${
+        LoginSteps.LOG_IN
+      }`
+    );
+    setLogInStep(LoginSteps.LOG_IN);
+    return null;
+  }, [router, userName]);
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        minHeight: '100vh',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100vw',
-        backgroundImage: 'url(/images/hero.jpg)',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'bottom right',
-      }}
-    >
-      <Grid
-        container
-        justifyContent='center'
-        sx={{ mx: 'auto' }}
-        my={6}
-        spacing={6}
-      >
-        {logInStep === LoginSteps.LOG_IN && <LoginForm onLogin={onLogin} />}
-        {logInStep === LoginSteps.MFA_CHALLENGE && (
-          <ConfirmLogIn email={userName} onConfirmLogin={confirmLogin} />
-        )}
-      </Grid>
-    </Box>
+    <>
+      <PageBackgroundWrapper>
+        <Grid
+          container
+          justifyContent='center'
+          sx={{ mx: 'auto' }}
+          my={6}
+          spacing={6}
+        >
+          {logInStep === LoginSteps.LOG_IN && (
+            <LoginForm noAccount={noAccount} onLogin={onLogin} />
+          )}
+          {logInStep === LoginSteps.MFA_CHALLENGE && (
+            <ConfirmLogIn
+              email={userName}
+              onConfirmLogin={confirmLogin}
+              resendCode={resendCode}
+            />
+          )}
+          {logInStep === LoginSteps.CONFIRM_ACCOUNT && (
+            <ConfirmSignUp email={userName || ''} onBack={onBackStep} />
+          )}
+        </Grid>
+      </PageBackgroundWrapper>
+    </>
   );
 };
 
