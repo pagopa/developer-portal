8e3de0405354b0d08a3b97016c2e0c6dac8e9a6b
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-07 17:44:21 +0100

    Add first implementation of function that fetch data from Strapi

---
+++ apps/nextjs-website/src/lib/strapi/fetchFromStrapi.ts
@@ -0,0 +1,46 @@
+import * as t from 'io-ts';
+import { pipe } from 'fp-ts/lib/function';
+import * as R from 'fp-ts/lib/Reader';
+import * as E from 'fp-ts/lib/Either';
+import * as TE from 'fp-ts/lib/TaskEither';
+import * as PR from 'io-ts/lib/PathReporter';
+import { StrapiEnv } from '@/lib/strapi/StapiEnv';
+
+// Function to invoke in order to retrieve data from Strapi.
+export const fetchFromStrapi = <A, O, I>(
+  path: string,
+  populate: '*' | string,
+  codec: t.Type<A, O, I>
+) =>
+  pipe(
+    R.ask<StrapiEnv>(),
+    R.map(({ strapiEndpoint, strapiApiToken, fetchFun }) =>
+      pipe(
+        // handle any promise result
+        TE.tryCatch(
+          () =>
+            fetchFun(`${strapiEndpoint}/api/${path}/?populate=${populate}`, {
+              method: 'GET',
+              headers: {
+                Authorization: `Bearer ${strapiApiToken}`,
+              },
+            }),
+          E.toError
+        ),
+        // handle any error on json function
+        TE.chain((response) => TE.tryCatch(() => response.json(), E.toError)),
+        TE.chainEitherK((json) =>
+          // decode the response with the given codec
+          pipe(
+            codec.decode(json),
+            E.mapLeft((errors) => new Error(PR.failure(errors).join('\n')))
+          )
+        ),
+        TE.fold(
+          // eslint-disable-next-line functional/no-promise-reject
+          (errors) => () => Promise.reject(errors),
+          (result) => () => Promise.resolve(result)
+        )
+      )()
+    )
+  );
