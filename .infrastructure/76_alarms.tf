be571d8c66c31596a6a4c485ab23a55078bcf1a7
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-07 10:59:51 +0100

    Add Cognito CreateAuthChallenge Lamdba alarm to monitor the concurrent executions limit

--- .infrastructure/76_alarms.tf
+++ .infrastructure/76_alarms.tf
@@ -497,3 +497,26 @@ module "cognito_create_auth_challenge_lambda_duration_alarm" {
     FunctionName = module.cognito_create_auth_challenge_function.lambda_function_name
   }
 }
+
+### Create auth challenge concurrent executions
+module "cognito_create_auth_challenge_lambda_concurrent_executions_alarm" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-cloudwatch.git//modules/metric-alarm?ref=0b4aa2b9aa19060205965a938de89a7bf0ff477b" # v5.1.0
+
+  alarm_name        = "DevPortal | Website | Cognito | Lambda | CreateAuthChallenge | ConcurrentExecutions"
+  actions_enabled   = true
+  alarm_description = "This alarm can proactively detect if the concurrency of the function is approaching the Region-level concurrency quota of the account"
+  metric_name       = "ConcurrentExecutions"
+  namespace         = "AWS/Lambda"
+
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = 90 # 90% of the concurrency quota
+  statistic           = "Maximum"
+  period              = 60
+  evaluation_periods  = 10
+  datapoints_to_alarm = 10
+  treat_missing_data  = "notBreaching" # No data in the period is considered as good.
+
+  dimensions = {
+    FunctionName = module.cognito_create_auth_challenge_function.lambda_function_name
+  }
+}
