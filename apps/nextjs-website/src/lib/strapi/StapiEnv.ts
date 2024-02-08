dc41beade16f418d5b9ea9a645fcc99000b85f93
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-08 15:15:50 +0100

    Remove unused functions in StrapiEnv file

--- apps/nextjs-website/src/lib/strapi/StapiEnv.ts
+++ apps/nextjs-website/src/lib/strapi/StapiEnv.ts
@@ -1,24 +1,7 @@
-import { pipe } from 'fp-ts/lib/function';
-import * as E from 'fp-ts/lib/Either';
-import { makeStrapiConfig, StrapiConfig } from '@/lib/strapi/StrapiConfig';
-import { publicEnv } from '@/BrowserConfig';
+import { StrapiConfig } from '@/lib/strapi/StrapiConfig';
 
 // This type represents the environment of Strapi.
 export type StrapiEnv = {
   readonly config: StrapiConfig;
   readonly fetchFun: typeof fetch;
 };
-
-const makeStrapiEnv = (config: StrapiConfig): StrapiEnv => ({
-  config,
-  fetchFun: fetch,
-});
-
-export const strapiEnv = pipe(
-  makeStrapiConfig(publicEnv),
-  E.map(makeStrapiEnv),
-  E.getOrElseW((errors) => {
-    // eslint-disable-next-line functional/no-throw-statements
-    throw errors;
-  })
-);
