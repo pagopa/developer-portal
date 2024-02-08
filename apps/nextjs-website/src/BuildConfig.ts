40b03d13cf164f77c8000d51488101d780c116b7
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-08 15:18:00 +0100

    Refactor on BuildConfig and BuildEnv

--- apps/nextjs-website/src/BuildConfig.ts
+++ apps/nextjs-website/src/BuildConfig.ts
@@ -3,10 +3,14 @@ import * as E from 'fp-ts/lib/Either';
 import * as PR from 'io-ts/lib/PathReporter';
 import * as t from 'io-ts';
 import * as tt from 'io-ts-types';
+import { StrapiConfig } from '@/lib/strapi/StrapiConfig';
 
-const BuildConfigCodec = t.type({
-  FETCH_FROM_STRAPI: t.string.pipe(tt.BooleanFromString),
-});
+const BuildConfigCodec = t.intersection([
+  t.type({
+    FETCH_FROM_STRAPI: t.string.pipe(tt.BooleanFromString),
+  }),
+  StrapiConfig,
+]);
 
 export type BuildConfig = t.TypeOf<typeof BuildConfigCodec>;
 
--- apps/nextjs-website/src/BuildEnv.ts
+++ apps/nextjs-website/src/BuildEnv.ts
@@ -1,15 +1,18 @@
-import { BuildConfig, makeBuildConfig } from './BuildConfig';
 import { pipe } from 'fp-ts/function';
-import { publicEnv } from '@/BrowserConfig';
 import * as E from 'fp-ts/Either';
+import { BuildConfig, makeBuildConfig } from '@/BuildConfig';
+import { publicEnv } from '@/BrowserConfig';
+import { StrapiEnv } from '@/lib/strapi/StapiEnv';
 
+// BuildEnv
 export type BuildEnv = {
   readonly config: BuildConfig;
-};
+} & StrapiEnv;
 
 // given environment variables produce an BuildEnv
 const makeBuildEnv = (config: BuildConfig): BuildEnv => ({
   config,
+  fetchFun: fetch,
 });
 
 export const buildEnv = pipe(
