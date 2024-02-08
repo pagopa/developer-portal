b521c7da1e554df478bf63e286502b153e8a479e
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-08 15:36:19 +0100

    Remove buildEnv since it's not used at the moment

--- apps/nextjs-website/src/BuildEnv.ts
+++ apps/nextjs-website/src/BuildEnv.ts
@@ -1,7 +1,4 @@
-import { pipe } from 'fp-ts/function';
-import * as E from 'fp-ts/Either';
-import { BuildConfig, makeBuildConfig } from '@/BuildConfig';
-import { publicEnv } from '@/BrowserConfig';
+import { BuildConfig } from '@/BuildConfig';
 import { StrapiEnv } from '@/lib/strapi/StapiEnv';
 
 // BuildEnv
@@ -10,16 +7,7 @@ export type BuildEnv = {
 } & StrapiEnv;
 
 // given environment variables produce an BuildEnv
-const makeBuildEnv = (config: BuildConfig): BuildEnv => ({
+export const makeBuildEnv = (config: BuildConfig): BuildEnv => ({
   config,
   fetchFun: fetch,
 });
-
-export const buildEnv = pipe(
-  makeBuildConfig(publicEnv),
-  E.map(makeBuildEnv),
-  E.getOrElseW((errors) => {
-    // eslint-disable-next-line functional/no-throw-statements
-    throw errors;
-  })
-);
