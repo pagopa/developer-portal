7271b33b7023ab79e490f5a6c9cdb83e0b8244be
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-09 10:35:23 +0100

    Update StrapiHomepageCodec

--- apps/nextjs-website/src/lib/strapi/homepage.ts
+++ apps/nextjs-website/src/lib/strapi/homepage.ts
@@ -1,12 +1,25 @@
 import * as t from 'io-ts/lib';
 import { fetchFromStrapi } from './fetchFromStrapi';
 
-const StrapiHomepageCodec = t.strict({
+const LinkHomepageCodec = t.strict({
+  text: t.string,
+  href: t.string,
+  target: t.union([
+    t.null,
+    t.literal('_self'),
+    t.literal('_blank'),
+    t.literal('_parent'),
+    t.literal('_top'),
+  ]),
+});
+
+export const StrapiHomepageCodec = t.strict({
   data: t.strict({
     id: t.number,
     attributes: t.strict({
       comingsoonDocumentation: t.type({
         title: t.string,
+        links: t.array(LinkHomepageCodec),
       }),
     }),
   }),
@@ -16,6 +29,6 @@ export type StrapiHomepage = t.TypeOf<typeof StrapiHomepageCodec>;
 
 export const fetchHomepage = fetchFromStrapi(
   'homepage',
-  '*',
+  'populate[comingsoonDocumentation][populate][0]=links',
   StrapiHomepageCodec
 );
