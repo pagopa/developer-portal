7e0c5f900da029a64e4a3dc0ddb050de65fb10c2
AF <4775679+datalek@users.noreply.github.com>
2024-02-09 15:30:13 +0100

    Update .infrastructure/50_storage.tf

--- .infrastructure/50_storage.tf
+++ .infrastructure/50_storage.tf
@@ -58,6 +58,7 @@ module "dynamodb_webinar_questions" {
   hash_key                       = "webinarId"
   range_key                      = "createdAt"
   ttl_enabled                    = false
+  ttl_attribute_name             = "expireAt"
   server_side_encryption_enabled = true
 
   attributes = [
