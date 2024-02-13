f626f690c3fd186bb7ef98f07809088979ce0edb
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-02-13 21:22:58 +0100

    add-dns-record-strapi
    add-dns-record-strapi

--- .infrastructure/10_dns.tf
+++ .infrastructure/10_dns.tf
@@ -107,3 +107,33 @@ resource "aws_route53_record" "devportal_google_site_verification_txt" {
   records = ["google-site-verification=Z94dFrXZD0YqP-r5BY5ODb4NsbQBAggTGRZM9fNtOj0"]
   ttl     = 3600
 }
+
+# Add DNS record for CMS Strapi
+module "records" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-route53.git//modules/records?ref=bc63328714550fd903d2574b263833c9ce1c867e" # v2.11.0"
+
+  zone_id = module.dns_zone_cms.route53_zone_zone_id[keys(var.dns_domain_name_cms)[0]]
+
+  records = [
+    {
+      name = ""
+      type = "A"
+      alias = {
+        name                   = module.cms_load_balancer.dns_name
+        zone_id                = module.cms_load_balancer.zone_id
+        evaluate_target_health = false
+      }
+    },
+    {
+      name = "www"
+      type = "A"
+      alias = {
+        name                   = module.cms_load_balancer.dns_name
+        zone_id                = module.cms_load_balancer.zone_id
+        evaluate_target_health = false
+      }
+    }
+  ]
+
+  depends_on = [module.dns_zone_cms]
+}
