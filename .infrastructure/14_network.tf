be0578c227a862c3f2514d231da7ad287293bd00
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-02-13 12:29:07 +0100

    Update .infrastructure/14_network.tf
    Co-authored-by: AF <4775679+datalek@users.noreply.github.com>

--- .infrastructure/14_network.tf
+++ .infrastructure/14_network.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "cms_lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ## https://developer.hashicorp.com/terraform/language/meta-arguments/lifecycle#create_before_destroy
+  # https://registry.terraform.io/providers/hashicorp/aws/5.35.0/docs/resources/security_group#recreating-a-security-group
   lifecycle {
     create_before_destroy = true
   }
