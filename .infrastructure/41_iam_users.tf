d33a0a14c324bd1c8085a74ff33cdb42d5afd146
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-14 16:11:51 +0100

    Create developers readonly group and iam user

---
+++ .infrastructure/41_iam_users.tf
@@ -0,0 +1,24 @@
+module "iam_user_mauro_dandrea" {
+  source                        = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-user?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
+  force_destroy                 = true
+  password_reset_required       = true
+  create_iam_user_login_profile = false
+
+  policy_arns = ["arn:aws:iam::aws:policy/IAMUserChangePassword"]
+
+  name = "mauro.dandrea@dgsspa.com"
+  tags = {
+    Company = "DGS"
+  }
+}
+
+module "iam_group_developers_readonly_access" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-group-with-policies?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
+
+  name                     = "developers_read_only"
+  create_group             = true
+  group_users              = [module.iam_user_mauro_dandrea.iam_user_name]
+  custom_group_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+  # Attach IAM policy which allows IAM users to manage their credentials and MFA
+  attach_iam_self_management_policy = true
+}
