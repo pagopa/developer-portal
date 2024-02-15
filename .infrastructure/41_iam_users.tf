3e3d8b629f4b48d67eba3dc9914277991b567c0c
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-15 17:53:34 +0100

    Rollback to version without the module

--- .infrastructure/41_iam_users.tf
+++ .infrastructure/41_iam_users.tf
@@ -1,28 +1,19 @@
-module "iam_user_mauro_dandrea" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-user?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
-  count  = var.environment == "dev" ? 1 : 0
-
-  force_destroy                 = true
-  password_reset_required       = true
-  create_iam_user_login_profile = true
-  create_iam_access_key         = false
-
-  policy_arns = ["arn:aws:iam::aws:policy/IAMUserChangePassword"]
+resource "aws_iam_user" "mauro_dandrea" {
+  # This force the deletion of the user and its login profile
+  # because we manually give access to the console
+  force_destroy = true
 
   name = "mauro.dandrea@dgsspa.com"
+
   tags = {
     Company = "DGS"
   }
 }
 
-module "iam_group_developers_readonly_access" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-group-with-policies?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
-  count  = var.environment == "dev" ? 1 : 0
-
-  name                     = "developers_read_only"
-  create_group             = true
-  group_users              = [module.iam_user_mauro_dandrea[0].iam_user_name]
-  custom_group_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
-  # Attach IAM policy which allows IAM users to manage their credentials and MFA
-  attach_iam_self_management_policy = true
+# Allow IAM User to change the password
+# Attach IAM User policy because with IAM Group policy we have the following error
+# Error: deleting IAM User  DeleteConflict: Cannot delete entity, must delete login profile first.
+resource "aws_iam_user_policy_attachment" "change_password" {
+  user       = aws_iam_user.mauro_dandrea.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
 }
