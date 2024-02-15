33c4c2328069c0c9ab26682ae59e32b382ba17c6
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-15 14:31:10 +0100

    Do not wait for validation
    According to [documentation](https://github.com/terraform-aws-modules/terraform-aws-acm/blob/8d0b22f1f242a1b36e29b8cb38aaeac9b887500d/README.md?plain=1#L174), in CI environment we should not wait or we could encounter errors

--- .infrastructure/11_certs.tf
+++ .infrastructure/11_certs.tf
@@ -36,7 +36,7 @@ module "cms_ssl_certificate" {
     "www.${keys(var.dns_domain_name_cms)[0]}"
   ]
 
-  wait_for_validation = true
+  wait_for_validation = false # https://github.com/terraform-aws-modules/terraform-aws-acm/blob/8d0b22f1f242a1b36e29b8cb38aaeac9b887500d/README.md?plain=1#L174
   validation_method   = "DNS"
   dns_ttl             = 3600
 }
