8114f460d483f6c6829c15db29a4026266cf03b8
AF <alessandro.ferlin@pagopa.it>
2024-02-16 14:32:05 +0100

    Move iam policy to reuse it

--- .infrastructure/40_iam.tf
+++ .infrastructure/40_iam.tf
@@ -1,34 +1,41 @@
 data "aws_caller_identity" "current" {}
 
+data "aws_iam_policy_document" "deploy_github" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_repository}:*"]
+    }
+
+    condition {
+      test     = "ForAllValues:StringEquals"
+      variable = "token.actions.githubusercontent.com:iss"
+      values   = ["https://token.actions.githubusercontent.com"]
+    }
+
+    condition {
+      test     = "ForAllValues:StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+
 ###############################################################################
-#                      Define IAM Role to use on deploy                       #
+#                Define IAM Role to use on website deploy                     #
 ###############################################################################
 resource "aws_iam_role" "deploy_website" {
   name        = "GitHubActionDeployWebsite"
   description = "Role to assume to deploy the website"
-
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          "Federated" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:${var.github_repository}:*"
-          },
-          "ForAllValues:StringEquals" = {
-            "token.actions.githubusercontent.com:iss" : "https://token.actions.githubusercontent.com",
-            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.deploy_github.json
 }
 
 resource "aws_iam_policy" "deploy_website" {
@@ -75,6 +82,60 @@ resource "aws_iam_policy" "deploy_website" {
 resource "aws_iam_role_policy_attachment" "deploy_website" {
   role       = aws_iam_role.deploy_website.name
   policy_arn = aws_iam_policy.deploy_website.arn
+}
+
+###############################################################################
+#                Define IAM Role to use on strapi deploy                      #
+###############################################################################
+
+resource "aws_iam_role" "deploy_cms" {
+  name        = "GitHubActionDeployCms"
+  description = "Role to assume to deploy the cms"
+  assume_role_policy = data.aws_iam_policy_document.deploy_github.json
+}
+
+resource "aws_iam_policy" "deploy_cms" {
+  name        = "DeployCms"
+  description = "Policy to allow to deploy the website"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecs:DescribeTaskDefinition",
+          "ecs:RegisterTaskDefinition",
+          "ecs:DescribeServices",
+          "ecs:UpdateService",
+          "ecr:GetAuthorizationToken",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+          "ecr:UploadLayerPart",
+          "ecr:InitiateLayerUpload",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:BatchGetImage"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:PassRole"
+        ]
+        Effect = "Allow"
+        Resource = [
+          module.iam_role_ecs_task_execution.iam_role_arn,
+          module.iam_role_task_role.iam_role_arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "deploy_cms" {
+  role       = aws_iam_role.deploy_cms.name
+  policy_arn = aws_iam_policy.deploy_cms.arn
 }
 
 ## IAM Role and policy ECS for CMS Strapi
@@ -217,90 +278,4 @@ module "iam_policy_cms" {
       },
     ]
   })
-}
-
-## IAM Role Deploy GitHub for CMS Strapi
-data "aws_iam_policy_document" "trusted_cms_github" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    principals {
-      type        = "Federated"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
-    }
-
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repository}:*"]
-    }
-
-    condition {
-      test     = "ForAllValues:StringEquals"
-      variable = "token.actions.githubusercontent.com:iss"
-      values   = ["https://token.actions.githubusercontent.com"]
-    }
-
-    condition {
-      test     = "ForAllValues:StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-  }
-}
-
-module "iam_policy_deploy_cms_github" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-policy?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
-
-  name        = "DeployECS"
-  path        = "/"
-  description = "Policy to allow deploy on ECS."
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ecs:DescribeTaskDefinition",
-          "ecs:RegisterTaskDefinition",
-          "ecs:DescribeServices",
-          "ecs:UpdateService",
-          "ecr:GetAuthorizationToken",
-          "ecr:CompleteLayerUpload",
-          "ecr:GetAuthorizationToken",
-          "ecr:UploadLayerPart",
-          "ecr:InitiateLayerUpload",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:PutImage",
-          "ecr:BatchGetImage"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        Action = [
-          "iam:PassRole"
-        ]
-        Effect = "Allow"
-        Resource = [
-          module.iam_role_ecs_task_execution.iam_role_arn,
-          module.iam_role_task_role.iam_role_arn
-        ]
-      }
-    ]
-  })
-}
-
-module "iam_role_deploy_cms_github" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-assumable-role?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
-
-  create_role = true
-  role_name   = "GitHubActionDeployECS"
-
-  custom_role_policy_arns = [
-    module.iam_policy_deploy_cms_github.arn
-  ]
-  number_of_custom_role_policy_arns = 1
-  custom_role_trust_policy          = data.aws_iam_policy_document.trusted_cms_github.json
-  role_requires_mfa                 = false
 }
