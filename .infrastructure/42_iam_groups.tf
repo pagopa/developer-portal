3baac157d4b74c6a6f27059c65fb21b7e439db09
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-21 15:40:50 +0100

    [DEV-1400] Create developers readonly group and iam user (#649)

---
+++ .infrastructure/42_iam_groups.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_group" "developers_read_only" {
+  name = "developers_read_only"
+}
+
+# IAM Group Membership - DGS
+resource "aws_iam_group_membership" "dgs" {
+  name = "DGS"
+
+  users = [
+    aws_iam_user.mauro_dandrea.name
+  ]
+
+  group = aws_iam_group.developers_read_only.name
+}
+
+resource "aws_iam_group_policy_attachment" "read_only" {
+  group = aws_iam_group.developers_read_only.name
+  # The AWS ReadOnly Access Policy
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
