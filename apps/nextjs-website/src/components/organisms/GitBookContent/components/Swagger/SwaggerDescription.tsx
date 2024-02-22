7a2b499dd20195282b4e3fc4e363e29e891c17ea
tommaso1 <tommasorosso1@gmail.com>
2024-02-22 15:01:00 +0100

    Update apps/nextjs-website/src/components/organisms/GitBookContent/components/Swagger/SwaggerDescription.tsx
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/Swagger/SwaggerDescription.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/Swagger/SwaggerDescription.tsx
@@ -1,10 +1,6 @@
-import { Card, Typography } from '@mui/material';
+'use client';
+import { Typography } from '@mui/material';
 import { SwaggerDescriptionProps } from 'gitbook-docs/markdoc/schema/swagger';
-import { useTranslations } from 'next-intl';
-import { OpenAPIV3 } from 'openapi-types';
-
-import { Operations } from './Operations';
-import { useSpec } from './hooks/useSpec';
 import { ReactNode } from 'react';
 
 const SwaggerDescription = ({
