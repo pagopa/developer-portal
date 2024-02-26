8e18a767673bd8276e86b989855f5242a9bfa8eb
AF <4775679+datalek@users.noreply.github.com>
2024-02-26 14:59:44 +0100

    Revert "[DEV-1383, DEV-1384] - Create ECS and ALB for CMS Strapi (#637)" (#677)
    This reverts commit c0c4026fc18203826303b69aaee6e36bc4a81a5f.

--- .infrastructure/variables.tf
+++ .infrastructure/variables.tf
@@ -67,18 +67,6 @@ variable "cms_app_port" {
   default     = 1337
 }
 
-## CMS Strapi CPU
-variable "cms_app_cpu" {
-  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "1024" ##### 1 vCPU
-}
-
-## CMS Strapi RAM
-variable "cms_app_memory" {
-  description = "Fargate instance memory to provision (in MiB)"
-  default     = "3072" ##### 3 GB RAM
-}
-
 # CMS Strapi DNS
 variable "dns_domain_name_cms" {
   description = "DNS domain name of the Developer Portal's CMS"
