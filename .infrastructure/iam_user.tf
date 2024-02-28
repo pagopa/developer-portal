b2d5806e50d90d397489fce20aaf0da6188da096
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-28 08:55:28 +0100

    Move iam_user to iam_user file

--- .infrastructure/iam_user.tf
+++ .infrastructure/iam_user.tf
@@ -17,3 +17,13 @@ resource "aws_iam_user_policy_attachment" "change_password" {
   user       = aws_iam_user.mauro_dandrea.name
   policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
 }
+
+## IAM User Strapi with Access and Secret Key for CMS Strapi
+module "iam_user_cms" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-user?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
+
+  name                          = "strapi"
+  create_iam_user_login_profile = false
+  create_iam_access_key         = true
+  policy_arns                   = [module.iam_policy_cms.arn]
+}
