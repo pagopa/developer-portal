91627e487e454960384a098c2109698d5ba07093
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-28 22:25:58 +0100

    Move cognito identity resources to proper file

--- .infrastructure/cognito.tf
+++ .infrastructure/cognito.tf
@@ -187,23 +187,6 @@ resource "aws_cognito_user_pool_domain" "devportal" {
   user_pool_id    = aws_cognito_user_pool.devportal.id
 }
 
-resource "aws_cognito_identity_pool" "devportal" {
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  identity_pool_name               = "devportal-identity"
-  allow_unauthenticated_identities = false
-  allow_classic_flow               = false
-
-  cognito_identity_providers {
-    client_id               = aws_cognito_user_pool_client.devportal_website.id
-    provider_name           = aws_cognito_user_pool.devportal.endpoint
-    server_side_token_check = false
-  }
-
-}
-
 data "aws_iam_policy_document" "authenticated_users_policy" {
   statement {
     effect  = "Allow"
@@ -238,23 +221,6 @@ resource "aws_iam_role" "devportal_authenticated_host_user" {
   name               = "DevPortalAuthenticatedHostUser"
   description        = "The role assumed by the authenticated host devportal users"
   assume_role_policy = data.aws_iam_policy_document.authenticated_users_policy.json
-}
-
-resource "aws_cognito_identity_pool_roles_attachment" "main" {
-  identity_pool_id = aws_cognito_identity_pool.devportal.id
-
-  role_mapping {
-    identity_provider = format(
-      "cognito-idp.%s.amazonaws.com/%s:%s",
-      var.aws_region, aws_cognito_user_pool.devportal.id, aws_cognito_user_pool_client.devportal_website.id
-    )
-    ambiguous_role_resolution = "AuthenticatedRole"
-    type                      = "Token"
-  }
-
-  roles = {
-    authenticated = aws_iam_role.devportal_authenticated_user.arn
-  }
 }
 
 resource "aws_cognito_user_group" "hosts" {
