7ed64e2f9d7a0db4a0db38a42adb3188b1070c29
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-28 09:30:47 +0100

    Move cognito's iam_policy to dedicated file

--- .infrastructure/iam_policy.tf
+++ .infrastructure/iam_policy.tf
@@ -204,3 +204,27 @@ module "iam_policy_cms" {
     ]
   })
 }
+
+data "aws_iam_policy_document" "authenticated_users_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = ["cognito-identity.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "cognito-identity.amazonaws.com:aud"
+      values   = [aws_cognito_identity_pool.devportal.id]
+    }
+
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "cognito-identity.amazonaws.com:amr"
+      values   = ["authenticated"]
+    }
+  }
+}
