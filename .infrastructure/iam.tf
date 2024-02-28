6980d094a05848c2e7b6df8956566c1a601ad8b0
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-02-28 08:55:28 +0100

    Move iam_user to iam_user file

--- .infrastructure/iam.tf
+++ .infrastructure/iam.tf
@@ -245,16 +245,6 @@ module "iam_role_task_role" {
   role_requires_mfa = false
 }
 
-## IAM User Strapi with Access and Secret Key for CMS Strapi
-module "iam_user_cms" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-user?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
-
-  name                          = "strapi"
-  create_iam_user_login_profile = false
-  create_iam_access_key         = true
-  policy_arns                   = [module.iam_policy_cms.arn]
-}
-
 module "iam_policy_cms" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-iam.git//modules/iam-policy?ref=f37809108f86d8fbdf17f735df734bf4abe69315" # v5.34.0
 
