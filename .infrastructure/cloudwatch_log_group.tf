3db27cd200b9236cf2ff17c5d5416f7df8d24919
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2024-02-29 14:31:14 +0100

    terraform-fmt
    terraform-fmt

--- .infrastructure/cloudwatch_log_group.tf
+++ .infrastructure/cloudwatch_log_group.tf
@@ -2,6 +2,6 @@
 module "cms_log_group" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-cloudwatch.git//modules/log-group?ref=bf969da953bdbea229392255d2b36e7b720e917e" # v5.3.0
 
-  name = "strapi-cms"
+  name              = "strapi-cms"
   retention_in_days = 90
 }
