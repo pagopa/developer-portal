7254d176073a1caae03718300c48e5d26ff80b92
tommaso1 <tommasorosso1@gmail.com>
2024-03-05 16:59:43 +0000

    Handle data-size image attribute to support inline image rendering

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/Item.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/Item.tsx
@@ -16,11 +16,7 @@ const Item = ({ children }: ItemProps<ReactNode>) => {
           width: 'initial',
           marginLeft: '1rem',
         },
-        '& div.list-item-container:has(li)': {
-          display: 'flex',
-          flexDirection: 'column',
-        },
-        '& div.list-item-container:has(img)': {
+        '& div.list-item-container:has(img.data-size-line)': {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'start',
