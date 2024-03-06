9de8bd8b28d5a4d587982279537c6518128b732e
jeremygordillo <jere.gordi@gmail.com>
2024-03-06 17:26:33 +0100

    add test

--- apps/nextjs-website/src/components/organisms/Auth/__tests__/SignUpForm.test.tsx
+++ apps/nextjs-website/src/components/organisms/Auth/__tests__/SignUpForm.test.tsx
@@ -81,6 +81,62 @@ describe('SignUpForm', () => {
     expect(mockOnSignUp).not.toHaveBeenCalled();
   });
 
+  it('should reset form fields errors', () => {
+    const { getByRole, getAllByLabelText } = render(
+      <Wrapper>
+        <SignUpForm
+          onSignUp={mockOnSignUp}
+          userAlreadyExist={mockUserAlreadyExist}
+        />
+      </Wrapper>
+    );
+    const firstNameInput = getByRole('textbox', {
+      name: /firstname/i,
+    }) as HTMLInputElement;
+    const lastNameInput = getByRole('textbox', {
+      name: /lastname/i,
+    }) as HTMLInputElement;
+    const usernameInput = getByRole('textbox', {
+      name: /email/i,
+    }) as HTMLInputElement;
+    const { confirmPasswordInput, passwordInput } = getPasswordInputs(
+      getAllByLabelText(/password/i)
+    );
+    const submitButton = getByRole('button', { name: /Iscriviti/i });
+
+    fireEvent.change(firstNameInput, { target: { value: '' } });
+    fireEvent.change(lastNameInput, { target: { value: '' } });
+    fireEvent.change(usernameInput, { target: { value: 'invalid email' } });
+    fireEvent.change(passwordInput, { target: { value: 'short' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: 'mismatch' } });
+    fireEvent.click(submitButton);
+
+    expect(firstNameInput).toBeInvalid();
+    expect(lastNameInput).toBeInvalid();
+    expect(usernameInput).toBeInvalid();
+    expect(passwordInput).toBeInvalid();
+    expect(confirmPasswordInput).toBeInvalid();
+    expect(mockOnSignUp).not.toHaveBeenCalled();
+
+    fireEvent.change(firstNameInput, { target: { value: 'John' } });
+    fireEvent.change(lastNameInput, { target: { value: 'Doe' } });
+    fireEvent.change(usernameInput, {
+      target: { value: 'john.doe@example.com' },
+    });
+    fireEvent.change(passwordInput, { target: { value: 'StrongPassword23!' } });
+    fireEvent.change(confirmPasswordInput, {
+      target: { value: 'StrongPassword23!' },
+    });
+    fireEvent.click(submitButton);
+
+    expect(firstNameInput).toBeValid();
+    expect(lastNameInput).toBeValid();
+    expect(usernameInput).toBeValid();
+    expect(passwordInput).toBeValid();
+    expect(confirmPasswordInput).toBeValid();
+    expect(mockOnSignUp).toHaveBeenCalled();
+  });
+
   it('should call onSignUp when form is valid', () => {
     const { getByRole, getAllByLabelText } = render(
       <Wrapper>
