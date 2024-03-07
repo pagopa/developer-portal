ef918df50a163205b79754fdaa12edfaafe84769
jeremygordillo <jere.gordi@gmail.com>
2024-03-07 15:22:19 +0100

    refactor

--- apps/nextjs-website/src/app/auth/sign-up/page.tsx
+++ apps/nextjs-website/src/app/auth/sign-up/page.tsx
@@ -5,71 +5,53 @@ import SignUpForm from '@/components/organisms/Auth/SignUpForm';
 import { SignUpSteps } from '@/lib/types/signUpSteps';
 import { Box, Grid, Typography, useMediaQuery } from '@mui/material';
 import { Auth } from 'aws-amplify';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useCallback, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 import { SignUpUserData } from '@/lib/types/sign-up';
 import { useTranslations } from 'next-intl';
 import { signUpAdvantages } from '@/_contents/auth';
-import AuthStatus from '@/components/organisms/Auth/AuthStatus';
 import { generateSignUpData } from '@/helpers/auth.helpers';
+import { useAuthenticatedUserRedirect } from '@/helpers/user.helper';
+import { useState } from 'react';
 
 const SignUp = () => {
+  const loading = useAuthenticatedUserRedirect();
+
   const params = useSearchParams();
-  const router = useRouter();
   const isSmallScreen = useMediaQuery('(max-width: 1000px)');
   const signUp = useTranslations('auth.signUp');
 
+  const [email, setEmail] = useState('');
   const [submitting, setSubmitting] = useState(false);
   const [userAlreadyExist, setUserAlreadyExist] = useState(false);
   const [signUpStep, setSignUpStep] = useState(
     params.get('step') || SignUpSteps.SIGN_UP
   );
 
-  const email = params.get('email') || '';
-
-  const goToConfirmSignUp = useCallback(
-    (email: string) => {
-      router.replace(
-        `/auth/sign-up?email=${encodeURIComponent(email)}&step=${
-          SignUpSteps.CONFIRM_SIGN_UP
-        }`
-      );
-      setSignUpStep(SignUpSteps.CONFIRM_SIGN_UP);
-    },
-    [router]
-  );
-
-  const onBackStep = useCallback(() => {
-    router.replace(
-      `/auth/sign-up?email=${encodeURIComponent(email)}&step=${
-        SignUpSteps.SIGN_UP
-      }`
-    );
+  const onBackStep = () => {
     setSignUpStep(SignUpSteps.SIGN_UP);
-    return null;
-  }, [router, email]);
+  };
 
-  const onSignUp = useCallback(
-    (userData: SignUpUserData) => {
-      setUserAlreadyExist(false);
-      setSubmitting(true);
-      Auth.signUp(generateSignUpData(userData))
-        .then(() => {
-          goToConfirmSignUp(userData.username);
-        })
-        .catch((error) => {
-          if (error.code === 'UsernameExistsException') {
-            setUserAlreadyExist(true);
-          } else {
-            setUserAlreadyExist(false);
-          }
-        })
-        .finally(() => {
-          setSubmitting(false);
-        });
-    },
-    [goToConfirmSignUp]
-  );
+  const onSignUp = (userData: SignUpUserData) => {
+    setUserAlreadyExist(false);
+    setSubmitting(true);
+    Auth.signUp(generateSignUpData(userData))
+      .then(() => {
+        setEmail(userData.username);
+        setSignUpStep(SignUpSteps.CONFIRM_SIGN_UP);
+      })
+      .catch((error) => {
+        if (error.code === 'UsernameExistsException') {
+          setUserAlreadyExist(true);
+        } else {
+          setUserAlreadyExist(false);
+        }
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
+
+  if (loading) return null;
 
   return (
     <>
@@ -112,13 +94,11 @@ const SignUp = () => {
           </Grid>
           <Grid item xs={isSmallScreen ? 1 : 5}>
             {signUpStep === SignUpSteps.SIGN_UP && (
-              <AuthStatus>
-                <SignUpForm
-                  userAlreadyExist={userAlreadyExist}
-                  submitting={submitting}
-                  onSignUp={onSignUp}
-                />
-              </AuthStatus>
+              <SignUpForm
+                userAlreadyExist={userAlreadyExist}
+                submitting={submitting}
+                onSignUp={onSignUp}
+              />
             )}
             {signUpStep === SignUpSteps.CONFIRM_SIGN_UP && (
               <ConfirmSignUp email={email} onBack={onBackStep} />
