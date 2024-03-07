f7dc79cde5c8871ca2a91c5f558033662ed7db5f
Monica Costantini <122867940+certevol@users.noreply.github.com>
2024-03-07 13:13:33 +0100

    [DEV-1478] remove past webinars from the homepage

--- apps/nextjs-website/src/_contents/webinars.ts
+++ apps/nextjs-website/src/_contents/webinars.ts
@@ -63,7 +63,7 @@ export const webinars: readonly Webinar[] = [
       `<br /> \n` +
       `<p>Gli interventi dei nostri esperti saranno intervallati da momenti di Q&A per condividere le migliori pratiche per un'esperienza fluida e di successo nella gestione dei vostri servizi. </p> \n`,
     slug: 'nuove-api-io',
-    isVisibleInHome: true,
+    isVisibleInHome: false,
     imagePath: '/images/webinar-cover-nuove-api-io.png',
     speakers: [
       {
