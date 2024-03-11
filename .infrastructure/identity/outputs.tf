696268da71c06465a8ee411f80dcd2310898b69d
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-03-11 15:04:53 +0100

    Remove .infrastructure folder

--- .infrastructure/identity/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "terraform_backend_bucket_name" {
-  value = aws_s3_bucket.terraform_states.bucket
-}
-
-output "terraform_lock_dynamodb_table" {
-  value = aws_dynamodb_table.dynamodb-terraform-state-lock.name
-}
