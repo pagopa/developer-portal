32da61b5692eb6db435b807acbbf9d630e1a0987
AF <alessandro.ferlin@pagopa.it>
2024-03-18 15:28:24 +0100

    Remove minor from engine_version

--- apps/infrastructure/src/rds_aurora.tf
+++ apps/infrastructure/src/rds_aurora.tf
@@ -5,7 +5,7 @@ module "cms_rds" {
   name                        = "cms-database"
   engine                      = "aurora-postgresql"
   engine_mode                 = "provisioned"
-  engine_version              = "14.6"
+  engine_version              = "14"
   auto_minor_version_upgrade  = true
   database_name               = "strapidb"
   master_username             = "postgres"
