cfd71d440d6014574861b9b776517046f29a1f8b
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-03-26 15:25:43 +0100

    Create SNS topic and subscription

---
+++ apps/infrastructure/src/sns_topic.tf
@@ -0,0 +1,10 @@
+resource "aws_sns_topic" "metric_alarm" {
+  name         = "cloudwatch_metric_alarms"
+  display_name = "DevPortal Alarms Topic"
+}
+
+resource "aws_sns_topic_subscription" "metric_alarm" {
+  protocol  = "email"
+  endpoint  = format("devportal-alerts+%s@pagopa.it", var.environment)
+  topic_arn = aws_sns_topic.metric_alarm.arn
+}
