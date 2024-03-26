d53cc0a3def00b86982a65468bf9147ac1f54806
tommaso1 <tommasorosso1@gmail.com>
2024-03-26 08:59:00 +0000

    linting autogenerated strapi files, changeset

--- apps/strapi-cms/src/api/webinar-author/controllers/webinar-author.ts
+++ apps/strapi-cms/src/api/webinar-author/controllers/webinar-author.ts
@@ -2,6 +2,8 @@
  * webinar-author controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::webinar-author.webinar-author');
+export default factories.createCoreController(
+  'api::webinar-author.webinar-author'
+);
--- apps/strapi-cms/src/api/webinar-author/services/webinar-author.ts
+++ apps/strapi-cms/src/api/webinar-author/services/webinar-author.ts
@@ -4,4 +4,6 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::webinar-author.webinar-author');
+export default factories.createCoreService(
+  'api::webinar-author.webinar-author'
+);
