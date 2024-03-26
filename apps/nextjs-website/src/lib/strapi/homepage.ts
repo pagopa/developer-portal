b732619cb9001f4aff7a920e3904904859ad257e
Michele Da Rin Fioretto <michele.darinfioretto@uqido.com>
2024-03-26 15:54:36 +0100

    feat(DEV-1546): add missing code

--- apps/nextjs-website/src/lib/strapi/homepage.ts
+++ apps/nextjs-website/src/lib/strapi/homepage.ts
@@ -2,38 +2,14 @@ import * as t from 'io-ts/lib';
 import * as tt from 'io-ts-types';
 import * as qs from 'qs';
 import { fetchFromStrapi } from './fetchFromStrapi';
-import { pipe } from 'fp-ts/lib/function';
-import * as E from 'fp-ts/lib/Either';
-
-/**
- * This type is used to convert null to undefined when decoding.
- * This is useful becuase Strapi returns null for missing fields, so we want to convert those to undefined to be more consistent with the rest of our codebase.
- *
- * @example
- * const codec = t.union([NullToUndefined, t.string]);
- * const result = codec.decode(null);
- * // result will be Right(undefined)
- */
-export const NullToUndefined = new t.Type<undefined, null, unknown>(
-  // name: a unique name for this codec
-  'NullToUndefined',
-  // is: a custom type guard
-  t.undefined.is,
-  // validate: succeeds if a value of type I can be decoded to a value of type A
-  (u, c) =>
-    pipe(
-      t.null.validate(u, c),
-      E.map(() => undefined)
-    ),
-  // encode: converts a value of type A to a value of type O
-  () => null
-);
+import { BlocksContentCodec } from './codecs/BlocksContentCodec';
+import { NullToUndefinedCodec } from './codecs/NullToUndefinedCodec';
 
 const LinkCodec = t.strict({
   text: t.string,
   href: t.string,
   target: t.union([
-    NullToUndefined,
+    NullToUndefinedCodec,
     t.literal('_self'),
     t.literal('_blank'),
     t.literal('_parent'),
@@ -66,7 +42,7 @@ const ProductCodec = t.strict({
 const CallToActionCodec = t.strict({
   link: LinkCodec,
   variant: t.union([
-    NullToUndefined,
+    NullToUndefinedCodec,
     t.literal('text'),
     t.literal('contained'),
     t.literal('outlined'),
@@ -75,15 +51,18 @@ const CallToActionCodec = t.strict({
 
 const HeroSlideCodec = t.strict({
   title: t.string,
-  callToAction: t.union([NullToUndefined, CallToActionCodec]),
+  subhead: t.union([NullToUndefinedCodec, BlocksContentCodec]),
+  callToAction: t.union([NullToUndefinedCodec, CallToActionCodec]),
   titleColor: t.union([
-    NullToUndefined,
+    NullToUndefinedCodec,
     t.literal('contrastText'),
     t.literal('main'),
     t.literal('light'),
     t.literal('dark'),
   ]),
-  backgroundImage: t.strict({ data: t.union([NullToUndefined, MediaCodec]) }),
+  backgroundImage: t.strict({
+    data: t.union([NullToUndefinedCodec, MediaCodec]),
+  }),
 });
 
 const NewsItemCodec = t.strict({
