df5ee9ea3d8533b809b8da6d2d8d30dcd1a54bfe
AF <4775679+datalek@users.noreply.github.com>
2024-03-29 11:47:11 +0100

    [DEV-1562] Add cms_google_* parameters to ecs_task_execution (#794)

--- apps/infrastructure/src/iam_policy.tf
+++ apps/infrastructure/src/iam_policy.tf
@@ -132,7 +132,10 @@ data "aws_iam_policy_document" "ecs_task_execution" {
       module.secret_cms_jwt_secret.ssm_parameter_arn,
       module.secret_cms_access_key_id.ssm_parameter_arn,
       module.secret_cms_access_key_secret.ssm_parameter_arn,
-      module.secret_cms_github_pat.ssm_parameter_arn
+      module.secret_cms_github_pat.ssm_parameter_arn,
+      module.secret_cms_google_gsuite_hd.ssm_parameter_arn,
+      module.secret_cms_google_oauth_client_id.ssm_parameter_arn,
+      module.secret_cms_google_oauth_client_secret.ssm_parameter_arn,
     ]
   }
 
