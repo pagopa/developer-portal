e56b34279532e098a68400e48a59be44b0233706
tommaso1 <tommasorosso1@gmail.com>
2024-04-19 15:16:31 +0200

    [DEV-1365] mermaid charts (#834)
    * mermaid charts
    
    * lock and changeset
    
    * dynamic
    
    * change id
    
    * Update apps/nextjs-website/src/components/atoms/MermaidDiagram/MermaidDiagram.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/nextjs-website/src/components/atoms/MermaidDiagram/MermaidDiagram.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

---
+++ apps/nextjs-website/src/components/atoms/MermaidDiagram/MermaidDiagram.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import mermaid from 'mermaid';
+
+interface MermaidDiagramProps {
+  chart: string;
+}
+
+mermaid.initialize({});
+
+const MermaidDiagram: React.FC<MermaidDiagramProps> = ({ chart }) => {
+  // Generate a unique id for the diagram based on the chart content
+  const id = chart
+    .split('')
+    .map((s) => s.charCodeAt(0))
+    .reduce((a, b) => a + b)
+    .toString();
+
+  useEffect(() => {
+    document.getElementById(id)?.removeAttribute('data-processed');
+    mermaid.contentLoaded();
+  }, [id]);
+
+  return (
+    <div className='mermaid' id={id}>
+      {chart}
+    </div>
+  );
+};
+
+export default MermaidDiagram;
