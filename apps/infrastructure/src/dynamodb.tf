dde303353eb824a107cfe644abaed2161190b433
christian-calabrese <christian.calabrese@pagopa.it>
2024-05-06 17:17:48 +0200

    [DEV-1616] Created dynamodb table containing webinar subscriptions (#845)
    * feat: dynamodb containing webinar subscriptions created
    
    * created changeset for new dynamodb table

--- apps/infrastructure/src/dynamodb.tf
+++ apps/infrastructure/src/dynamodb.tf
@@ -22,3 +22,28 @@ module "dynamodb_webinar_questions" {
     },
   ]
 }
+
+# Webinar subscriptions
+
+module "dynamodb_webinar_subscriptions" {
+  source = "git::github.com/terraform-aws-modules/terraform-aws-dynamodb-table.git?ref=715399dbe24f6443820bf5de80f6100b35d56355" # v4.0.0
+
+  billing_mode                = "PAY_PER_REQUEST"
+  deletion_protection_enabled = true
+
+  name                           = "WebinarSubscriptions"
+  hash_key                       = "username"
+  range_key                      = "webinarId"
+  server_side_encryption_enabled = true
+
+  attributes = [
+    {
+      name = "username"
+      type = "S"
+    },
+    {
+      name = "webinarId"
+      type = "S"
+    },
+  ]
+}
