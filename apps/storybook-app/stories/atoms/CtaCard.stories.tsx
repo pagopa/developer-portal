e559eac04c91c9f0cb2b5c08ea30422baf47dcf3
MikeAtUqido <146933303+MikeAtUqido@users.noreply.github.com>
2024-05-08 16:49:45 +0200

    [DEV-1252]: install storybook custom app (#719)
    * feat(DEV-1252): install storybook custom app
    
    * feat(DEV-1252): fix runtime error
    
    * feat(DEV-1252): add --no-open option to storybook script
    
    * feat(DEV-1252): move stories to storybook-app
    
    * feat(DEV-1252): move stories to storybook-app
    
    * feat(DEV-1252): update CODEOWNERS file
    
    * feat(DEV-1252): fix PR comments
    
    * feat(DEV-1252): fix package lock
    
    * feat(DEV-1252): fix tsconfig
    
    * feat(DEV-1252): remove first showcase on CtaCard story
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

---
+++ apps/storybook-app/stories/atoms/CtaCard.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import CtaCard from '../../../nextjs-website/src/components/atoms/CtaCard/CtaCard';
+import IconWrapper from '../../../nextjs-website/src/components/atoms/IconWrapper/IconWrapper';
+
+const meta: Meta<typeof CtaCard> = {
+  title: 'Atoms/CtaCard',
+  component: CtaCard,
+};
+
+export default meta;
+
+export const ShowcaseWithIcon: StoryObj<typeof CtaCard> = {
+  args: {
+    title: 'Title',
+    text: 'Text',
+    cta: {
+      label: 'Label',
+      href: 'https://example.com',
+    },
+    icon:
+      <IconWrapper
+        icon='https://cdn.dev.developer.pagopa.it/app_Io_d9bffd556b.svg'
+        isSvg={true}
+      />,
+  },
+};
+
