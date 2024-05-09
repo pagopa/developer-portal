148988c11b94039d3d826e63ed89dced7af53f33
tommaso1 <tommasorosso1@gmail.com>
2024-05-09 13:47:19 +0200

    Update apps/strapi-cms/src/api/tutorial/controllers/tutorial.ts
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

--- apps/strapi-cms/src/api/tutorial/controllers/tutorial.ts
+++ apps/strapi-cms/src/api/tutorial/controllers/tutorial.ts
@@ -2,6 +2,6 @@
  * tutorial controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::tutorial.tutorial');
