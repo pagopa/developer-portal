06161035814d248dd4c9079bdade8e328010ecd2
Marco Ponchia <marco.ponchia@uqido.com>
2024-05-20 09:44:58 +0200

    Update strapi rules and files

--- packages/eslint-config/eslint-strapi.js
+++ packages/eslint-config/eslint-strapi.js
@@ -14,6 +14,7 @@ module.exports = {
         'functional/immutable-data': 'off',
         'functional/prefer-readonly-type': 'off',
         'functional/no-expression-statements': 'off',
+        'functional/no-throw-statements': 'off',
         'functional/no-return-void': 'off',
         'prefer-arrow/prefer-arrow-functions': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
