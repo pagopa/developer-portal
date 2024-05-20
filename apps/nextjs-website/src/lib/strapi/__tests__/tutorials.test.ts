4275bec7e8aeea1badd1d04114a8e2469becb94d
tommaso1 <tommasorosso1@gmail.com>
2024-05-20 09:29:34 +0000

    test and codec wip

---
+++ apps/nextjs-website/src/lib/strapi/__tests__/tutorials.test.ts
@@ -0,0 +1,91 @@
+import * as E from 'fp-ts/lib/Either';
+import { StrapiTutorialsCodec } from '@/lib/strapi/tutorial';
+
+const makeStrapiResponseJson = () => ({
+  data: [
+    {
+      "id": 1,
+      "attributes": {
+        "title": "ttt",
+        "slug": "ttttt",
+        "content": [
+          {
+            "type": "paragraph",
+            "children": [
+              {
+                "type": "text",
+                "text": "asadasdasdasd"
+              }
+            ]
+          }
+        ],
+        "createdAt": "2024-05-16T08:10:17.203Z",
+        "updatedAt": "2024-05-20T09:12:13.509Z",
+        "publishedAt": null,
+        "locale": "en",
+        "dirName": "dddddd",
+        "name": "aaaaa",
+        "image": {
+          "data": null
+        },
+        "bannerLinks": [
+          {
+            "id": 1,
+            "title": "asd",
+            "body": [
+              {
+                "type": "paragraph",
+                "children": [
+                  {
+                    "type": "text",
+                    "text": "asdasd"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "title": null,
+            "body": null
+          }
+        ],
+        "relatedLinks": {
+          "id": 3,
+          "title": "yghf"
+        },
+        "product": {
+          "data": {
+            "id": 1,
+            "attributes": {
+              "name": "firma-con-io",
+              "description": "firma-con-io",
+              "slug": "firma-con-io",
+              "createdAt": "2024-05-20T09:12:02.036Z",
+              "updatedAt": "2024-05-20T09:12:02.036Z",
+              "publishedAt": null,
+              "locale": "en"
+            }
+          }
+        }
+      }
+    }
+  ],
+  meta: {
+    pagination: {
+      page: 1,
+      pageSize: 25,
+      pageCount: 1,
+      total: 1,
+    },
+  },
+});
+
+describe('StrapiTutorialCodec', () => {
+  it('should decode strapi webinars', () => {
+    const jsonFromStrapi = makeStrapiResponseJson();
+    const actual = StrapiTutorialsCodec.decode(jsonFromStrapi);
+    expect(E.isRight(actual)).toBeTruthy();
+  });
+
+});
---
+++ apps/nextjs-website/src/lib/strapi/tutorial.ts
@@ -0,0 +1,65 @@
+import * as t from 'io-ts/lib';
+import * as tt from 'io-ts-types';
+import * as qs from 'qs';
+import { fetchFromStrapi } from './fetchFromStrapi';
+import { NullToUndefinedCodec } from './codecs/NullToUndefinedCodec';
+import { BlocksContentCodec } from './codecs/BlocksContentCodec';
+import { MediaCodec } from './codecs/MediaCodec';
+import { RelatedLinksCodec } from './codecs/RelatedLinksCodec';
+
+const WebinarSpeakerCodec = t.strict({
+  id: t.number,
+  attributes: t.strict({
+    name: t.string,
+    jobTitle: t.string,
+    description: t.union([NullToUndefinedCodec, BlocksContentCodec]),
+    avatar: t.strict({ data: t.union([NullToUndefinedCodec, MediaCodec]) }),
+    publishedAt: tt.DateFromISOString,
+  }),
+});
+
+export const WebinarCodec = t.strict({
+  id: t.number,
+  attributes: t.strict({
+    title: t.string,
+    // description: t.string,
+    // slug: t.string,
+    // publishedAt: tt.DateFromISOString,
+    // isVisibleInList: t.boolean,
+    // coverImage: t.strict({ data: MediaCodec }),
+    // bodyContent: t.union([NullToUndefinedCodec, BlocksContentCodec]),
+    // playerSrc: t.union([NullToUndefinedCodec, t.string]),
+    // startDatetime: t.union([NullToUndefinedCodec, tt.DateFromISOString]),
+    // endDatetime: t.union([NullToUndefinedCodec, tt.DateFromISOString]),
+    // subscribeParagraphLabel: t.union([NullToUndefinedCodec, t.string]),
+    // relatedLinks: t.union([NullToUndefinedCodec, RelatedLinksCodec]),
+    // webinarSpeakers: t.strict({ data: t.array(WebinarSpeakerCodec) }),
+  }),
+});
+
+const PaginationCodec = t.strict({
+  pagination: t.strict({
+    page: t.number,
+    pageSize: t.number,
+    pageCount: t.number,
+    total: t.number,
+  }),
+});
+
+export const StrapiTutorialsCodec = t.strict({
+  data: t.array(WebinarCodec),
+  meta: PaginationCodec,
+});
+
+export type StrapiTutorials = t.TypeOf<typeof StrapiTutorialsCodec>;
+
+const makeStrapiTutorialsPopulate = () =>
+  qs.stringify({
+    populate: 'image,bannerLinks,relatedLinks,product',
+  });
+
+export const fetchTutorials = fetchFromStrapi(
+  'webinars',
+  makeStrapiTutorialsPopulate(),
+  StrapiTutorialsCodec
+);
