187179445a65b3021e62ba0c2557b995283fb3ec
Marco Ponchia <ponchia.marco1994@gmail.com>
2024-05-22 10:27:27 +0200

    [DEV-1631] Add slug custom uniq validator (#851)
    * add custom validator for slug uniqueness
    
    * add changeset
    
    * Add strapi config
    
    * Update strapi files according to config and remove eslint-disable
    
    * Fix after review
    
    * Update strapi rules and files
    
    * remove lint disable
    
    ---------
    
    Co-authored-by: tommaso1 <tommasorosso1@gmail.com>
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

--- apps/strapi-cms/src/api/webinar/content-types/webinar/lifecycles.ts
+++ apps/strapi-cms/src/api/webinar/content-types/webinar/lifecycles.ts
@@ -1,15 +1,22 @@
 import { errors } from '@strapi/utils';
 
+interface IWebinar {
+  readonly slug?: string;
+  readonly locale?: string;
+  readonly startDatetime?: string;
+  readonly endDatetime?: string;
+}
+
 interface IWebinarEvent {
   readonly params: {
-    readonly data: {
-      readonly startDatetime?: string;
-      readonly endDatetime?: string;
+    readonly data: IWebinar;
+    readonly where?: {
+      readonly id?: string;
     };
   };
 }
 
-const validateDates = (event: IWebinarEvent) => {
+const validateDates = (event: IWebinarEvent): boolean => {
   const { data } = event.params;
 
   const startDateTime = data.startDatetime
@@ -26,14 +33,62 @@ const validateDates = (event: IWebinarEvent) => {
   if (startDateTime && endDateTime && endDateTime <= startDateTime) {
     throw new errors.ApplicationError('End date must be after start date');
   }
+  return true;
+};
+
+const getLocale = async (event: IWebinarEvent): Promise<string> => {
+  // eslint-disable-next-line functional/no-let
+  let currentLocale: string | undefined;
+  if (event.params.data.locale) {
+    currentLocale = event.params.data.locale;
+  } else if (event.params.where?.id) {
+    const webinar: IWebinar | undefined = await strapi
+      .service('api::webinar.webinar')
+      .findOne(event.params.where?.id);
+    currentLocale = webinar?.locale;
+  }
+  return currentLocale || 'it';
+};
+
+const validateSlugUniqByLocale = async (
+  event: IWebinarEvent
+): Promise<boolean> => {
+  const currentLocale = await getLocale(event);
+
+  const query: { where: Record<string, unknown> } = {
+    where: {
+      locale: currentLocale,
+      slug: event.params.data.slug,
+    },
+  };
+  if (event.params.where?.id) {
+    query.where = {
+      id: {
+        $notIn: [event.params.where.id],
+      },
+      ...query.where,
+    };
+  }
+
+  const webinarWithSameSlug: IWebinar | undefined = await strapi.db
+    .query('api::webinar.webinar')
+    .findOne(query);
+
+  if (webinarWithSameSlug) {
+    throw new errors.ApplicationError(
+      'Webinar with the same slug already exists for the current locale'
+    );
+  }
+  return true;
 };
 
 module.exports = {
-  beforeCreate(event: IWebinarEvent) {
+  async beforeCreate(event: IWebinarEvent) {
     validateDates(event);
+    await validateSlugUniqByLocale(event);
   },
-
-  beforeUpdate(event: IWebinarEvent) {
+  async beforeUpdate(event: IWebinarEvent) {
     validateDates(event);
+    await validateSlugUniqByLocale(event);
   },
 };
