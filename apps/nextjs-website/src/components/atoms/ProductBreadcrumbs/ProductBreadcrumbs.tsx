77ca4d54e2b3220ba43e039b7c43982e7ff8c5a4
jeremygordillo <jere.gordi@gmail.com>
2024-05-23 14:19:17 +0200

    update ProductBreadcrumbs styles

--- apps/nextjs-website/src/components/atoms/ProductBreadcrumbs/ProductBreadcrumbs.tsx
+++ apps/nextjs-website/src/components/atoms/ProductBreadcrumbs/ProductBreadcrumbs.tsx
@@ -6,7 +6,6 @@ import {
   Typography,
   useTheme,
 } from '@mui/material';
-import EContainer from '@/editorialComponents/EContainer/EContainer';
 import Link from 'next/link';
 import React from 'react';
 import { useTranslations } from 'next-intl';
@@ -20,38 +19,38 @@ const ProductBreadcrumbs = ({ breadcrumbs }: ProductBreadcrumbsProps) => {
   const theme = useTheme();
   const t = useTranslations('breadcrumbs');
   return (
-    <EContainer>
-      <Box
-        component={'div'}
-        sx={{ display: { xs: 'none', md: 'block' } }}
-        my={2}
-      >
-        <Breadcrumbs sx={{ paddingTop: 2 }} aria-label='breadcrumb'>
-          {breadcrumbs.map((breadcrumb, index) => {
-            return index === breadcrumbs.length - 1 ? (
-              <Typography key={index} fontSize={16} fontWeight={600}>
-                {breadcrumb.translate ? t(breadcrumb.name) : breadcrumb.name}
-              </Typography>
-            ) : (
-              <MuiLink
-                key={index}
-                component={Link}
-                underline='hover'
-                fontSize={16}
-                color={
-                  index === 0
-                    ? theme.palette.text.primary
-                    : theme.palette.text.disabled
-                }
-                href={breadcrumb.path}
-              >
-                {breadcrumb.translate ? t(breadcrumb.name) : breadcrumb.name}
-              </MuiLink>
-            );
-          })}
-        </Breadcrumbs>
-      </Box>
-    </EContainer>
+    <Box
+      component='div'
+      sx={{
+        display: { xs: 'none', md: 'block' },
+        paddingY: '20px',
+      }}
+    >
+      <Breadcrumbs aria-label='breadcrumb'>
+        {breadcrumbs.map((breadcrumb, index) => {
+          return index === breadcrumbs.length - 1 ? (
+            <Typography key={index} fontSize={16} fontWeight={600}>
+              {breadcrumb.translate ? t(breadcrumb.name) : breadcrumb.name}
+            </Typography>
+          ) : (
+            <MuiLink
+              key={index}
+              component={Link}
+              underline='hover'
+              fontSize={16}
+              color={
+                index === 0
+                  ? theme.palette.text.primary
+                  : theme.palette.text.disabled
+              }
+              href={breadcrumb.path}
+            >
+              {breadcrumb.translate ? t(breadcrumb.name) : breadcrumb.name}
+            </MuiLink>
+          );
+        })}
+      </Breadcrumbs>
+    </Box>
   );
 };
 
