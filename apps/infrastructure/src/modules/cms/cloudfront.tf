d1d8fa9c416148fd60868a5b6541fcd00ea7d7f3
christian-calabrese <christian.calabrese@pagopa.it>
2024-05-27 09:09:18 +0200

    [EC-310] Revise terraform code structure to follow engineering standard (#870)
    * feat: create bucket containing ai knowledge base
    
    * chore: applying newly introduced engineering standards for terraform repository structure
    
    * fix: formatted modules
    
    * fix: added missing providers to modules
    
    * fix: added missing providers to modules
    
    * fix: added missing providers to modules
    
    * feat: subdivided in cms, website and chatbot
    
    * fix: added moved.tf file containing the moves from old repo structure to the new
    
    * fix: update terraform lock
    
    * feat: returning name servers records as output
    
    * fix: some resources not moved correctly
    
    * fix: some resources not moved correctly
    
    * fix: moving production website certificate records
    
    * chore: changed modules folder from _modules to modules

---
+++ apps/infrastructure/src/modules/cms/cloudfront.tf
@@ -0,0 +1,53 @@
+## CDN to Media Library for CMS Strapi
+module "cloudfront_cms" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-cloudfront.git?ref=ed0f1f983f606304e00ad9f48399bd2fe0b79233" # v3.2.2
+
+  create_origin_access_identity = true
+  origin_access_identities = {
+    s3_cms = "Identity to access S3 bucket"
+  }
+
+  origin = {
+    s3_one = {
+      domain_name = module.s3_bucket_cms.s3_bucket_bucket_regional_domain_name
+      s3_origin_config = {
+        origin_access_identity = "s3_cms"
+      }
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "CloudFront distribution for the CMS Media Library"
+
+  viewer_certificate = {
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = module.strapi_media_library_ssl_certificate.acm_certificate_arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  aliases = module.strapi_media_library_ssl_certificate.distinct_domain_names
+
+  default_cache_behavior = {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id       = "s3_one"
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0     # min time for objects to live in the distribution cache
+    default_ttl            = 3600  # default time for objects to live in the distribution cache
+    max_ttl                = 86400 # max time for objects to live in the distribution cache
+
+    forwarded_values = {
+      query_string = false
+      headers      = []
+      cookies = {
+        forward = "none"
+      }
+    }
+  }
+
+  geo_restriction = {
+    restriction_type = "none"
+  }
+}
---
+++ apps/infrastructure/src/modules/core/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to create resources. Default Milan"
+  default     = "eu-south-1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    CreatedBy = "Terraform"
+  }
+}
+
+variable "dns_domain_name" {
+  description = "DNS domain for the Developer Portal product"
+  type        = string
+}
+
+variable "dns_delegate_records" {
+  type        = map(any)
+  description = "DNS delegate records"
+  default     = {}
+}
