00307c6fd7cf448b5b0341528d383bd402ec5750
Marco Ponchia <marco.ponchia@uqido.com>
2024-05-28 16:16:26 +0200

    Add make tutorial props

---
+++ apps/nextjs-website/src/lib/tutorials.ts
@@ -0,0 +1,25 @@
+import { StrapiTutorials } from './strapi/tutorial';
+import { Tutorial } from './types/tutorialData';
+
+export type TutorialsProps = readonly Tutorial[];
+
+export function makeTutorialsProps(
+  strapiTutorials: StrapiTutorials
+): TutorialsProps {
+  return strapiTutorials.data.map(({ attributes }) => ({
+    comingSoon: false, // TODO: add this field to the Strapi model
+    showInOverview: true, // TODO: add this field to the Strapi model
+    image: attributes.image.data
+      ? {
+          url: attributes.image.data.attributes.url,
+          alternativeText:
+            attributes.image.data.attributes.alternativeText || '',
+        }
+      : undefined,
+    title: attributes.title,
+    publishedAt: attributes.publishedAt,
+    name: attributes.title,
+    path: `/${attributes.product.data.attributes.slug}/tutorials/${attributes.slug}`,
+    content: attributes.content,
+  }));
+}
