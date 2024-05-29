e26aa4b6f4ef23a139c6eb4491c7d2a6a5042fc2
Michele Da Rin Fioretto <michele.darinfioretto@uqido.com>
2024-05-29 08:16:30 +0200

    feat(DEV-1677): add quote component

---
+++ apps/nextjs-website/src/components/atoms/Quote/Quote.tsx
@@ -0,0 +1,78 @@
+import { FormatQuote } from '@mui/icons-material';
+import { Box, Stack, Typography, useTheme } from '@mui/material';
+import Image from 'next/image';
+
+export type QuoteProps = {
+  readonly quote: string;
+  readonly color?: 'contrastText' | 'main' | 'light' | 'dark';
+  readonly backgroundImage?: {
+    readonly name: string;
+    readonly width: number;
+    readonly height: number;
+    readonly ext: string;
+    readonly mime: string;
+    readonly url: string;
+  };
+};
+
+const Quote = ({
+  quote,
+  color,
+  backgroundImage = {
+    name: 'hero-swiper.png',
+    width: 1920,
+    height: 1080,
+    ext: '.png',
+    mime: 'image/png',
+    url: '/images/hero-swiper.png',
+  },
+}: QuoteProps) => {
+  const { palette } = useTheme();
+  const textColor = color
+    ? palette.primary[color]
+    : palette.primary.contrastText;
+
+  return (
+    <Stack>
+      <Stack
+        height={{ xs: 468, sm: 560 }}
+        justifyContent={'start'}
+        alignItems={'center'}
+        direction={'row'}
+      >
+        <Stack
+          px={{ xs: 4, md: 18 }}
+          direction={'column'}
+          justifyContent={'start'}
+          zIndex={10}
+          sx={{ width: 'fit-content', maxWidth: '40%' }}
+        >
+          <FormatQuote sx={{ color: textColor, rotate: '180deg' }} />
+          <Typography color={textColor} variant='body1'>
+            {quote}
+          </Typography>
+          <FormatQuote
+            sx={{
+              color: textColor,
+              alignSelf: 'flex-end',
+              position: 'relative',
+              left: '24px',
+            }}
+          />
+        </Stack>
+      </Stack>
+      {backgroundImage && (
+        <Box zIndex={0} position={'absolute'} height={'100%'} width={'100%'}>
+          <Image
+            style={{ objectFit: 'cover' }}
+            src={backgroundImage.url}
+            alt={quote}
+            fill={true}
+          />
+        </Box>
+      )}
+    </Stack>
+  );
+};
+
+export default Quote;
