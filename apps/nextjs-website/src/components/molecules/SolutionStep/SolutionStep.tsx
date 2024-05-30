c9a51038d960c65c07a64c144fd1b39d03bbebf5
jeremygordillo <jere.gordi@gmail.com>
2024-05-30 15:04:19 +0200

    add SolutionStep component

---
+++ apps/nextjs-website/src/components/molecules/SolutionStep/SolutionStep.tsx
@@ -0,0 +1,49 @@
+import { Stack, Typography, Box, Chip } from '@mui/material';
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+
+type SolutionStepProps = {
+  title: string;
+  content: string;
+  products: { label: string; href: string }[];
+};
+
+const SolutionStep = ({ title, content, products }: SolutionStepProps) => {
+  const t = useTranslations('solution');
+  return (
+    <Stack flexBasis={{ lg: '334px' }} gap={2} justifyContent='space-between'>
+      <Stack gap={2}>
+        <Typography color='#5C6F82' sx={{ fontWeight: 700 }} variant='caption'>
+          {title}
+        </Typography>
+        <Typography variant='body1'>{content}</Typography>
+      </Stack>
+      <Stack gap={2}>
+        <Typography
+          color='#5C6F82'
+          sx={{ textTransform: 'uppercase' }}
+          variant='subtitle2'
+        >
+          {t('steps.platforms')}
+        </Typography>
+        <Box display='flex' gap={1}>
+          {products.map((product, index) => (
+            <Link key={index} href={product.href}>
+              <Chip
+                label={product.label}
+                variant='filled'
+                sx={{
+                  color: '#215C76',
+                  background: '#E1F5FE',
+                  borderRadius: 1,
+                }}
+              />
+            </Link>
+          ))}
+        </Box>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default SolutionStep;
