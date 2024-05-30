c7eb7c73156121faea7ff7ac77a8317432b757eb
MikeAtUqido <146933303+MikeAtUqido@users.noreply.github.com>
2024-05-30 16:55:53 +0200

    [DEV-1677]: add quote component (#880)
    * feat(DEV-1677): add quote component
    
    * chore: add changeset
    
    * feat(DEV-1677): fix PR comments
    
    * feat(DEV-1677): fix PR comments
    
    ---------
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

---
+++ apps/nextjs-website/src/components/atoms/Quote/Quote.tsx
@@ -0,0 +1,87 @@
+import { FormatQuote } from '@mui/icons-material';
+import { Box, Stack, Typography, useTheme } from '@mui/material';
+import Image from 'next/image';
+
+export type QuoteProps = {
+  readonly quote: string;
+  readonly color?: 'contrastText' | 'main' | 'light' | 'dark';
+  readonly backgroundImage?: {
+    readonly name: string;
+    readonly width: number;
+    readonly height: number;
+    readonly ext: string;
+    readonly mime: string;
+    readonly url: string;
+  };
+};
+
+const Quote = ({
+  quote,
+  color,
+  backgroundImage = {
+    name: 'hero-swiper.png',
+    width: 1920,
+    height: 1080,
+    ext: '.png',
+    mime: 'image/png',
+    url: '/images/hero-swiper.png',
+  },
+}: QuoteProps) => {
+  const { palette } = useTheme();
+  const textColor = color
+    ? palette.primary[color]
+    : palette.primary.contrastText;
+
+  return (
+    <Stack>
+      <Stack
+        height={{ xs: 468, sm: 560 }}
+        justifyContent={'start'}
+        alignItems={'center'}
+        direction={'row'}
+      >
+        <Stack
+          px={{ xs: 4, md: 18 }}
+          direction={'column'}
+          justifyContent={'start'}
+          zIndex={10}
+          // TODO: Check with design team
+          sx={{ width: 'fit-content', maxWidth: { xs: '100%', lg: '40%' } }}
+        >
+          <FormatQuote
+            fontSize='large'
+            sx={{
+              color: palette.grey[300],
+              rotate: '180deg',
+              position: 'relative',
+              left: '4px',
+              bottom: '8px',
+            }}
+          />
+          <Typography color={textColor} variant='h6'>
+            {quote}
+          </Typography>
+          <FormatQuote
+            fontSize='large'
+            sx={{
+              color: palette.grey[300],
+              alignSelf: 'flex-end',
+              position: 'relative',
+              left: '24px',
+            }}
+          />
+        </Stack>
+      </Stack>
+      <Box zIndex={0} position={'absolute'} height={'100%'} width={'100%'}>
+        <Image
+          style={{ objectFit: 'cover' }}
+          src={backgroundImage.url}
+          alt={quote}
+          fill={true}
+        />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Quote;
