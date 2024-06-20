cb684a8c631f15ad278d997f393c5f0d255a82b3
Marcello Bertoli <marcello.bertoli@uqido.com>
2024-06-20 11:23:50 +0200

    add linting to storybook

--- apps/storybook-app/stories/atoms/BannerLink.stories.tsx
+++ apps/storybook-app/stories/atoms/BannerLink.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { BannerLink } from '../../../nextjs-website/src/components/atoms/BannerLink/BannerLink';
-import { mockTextBlock, mockText } from '../mock-content.helper';
+import { mockTextBlock } from '../mock-content.helper';
 
 const meta: Meta<typeof BannerLink> = {
   title: 'Atoms/BannerLink',
@@ -18,41 +18,41 @@ export const Showcase: StoryObj<typeof BannerLink> = {
     title: 'Titolo',
     count: 1,
     content: [
-      {...mockTextBlock({type: 'paragraph', wordCount: 10})},
+      { ...mockTextBlock({ type: 'paragraph', wordCount: 10 }) },
       {
-        'type': 'list',
-        'format': 'unordered',
-        'children': [
+        type: 'list',
+        format: 'unordered',
+        children: [
           {
-            'type': 'list-item',
-            'children': [
+            type: 'list-item',
+            children: [
               {
-                'type': 'text',
-                'text': 'Le comunicazioni in tempo reale',
-                'bold': true
+                type: 'text',
+                text: 'Le comunicazioni in tempo reale',
+                bold: true,
               },
               {
-                'type': 'text',
-                'text': ' consentono al cittadino di agire con tempestività sul pagamento di sanzioni'
-              }
-            ]
+                type: 'text',
+                text: ' consentono al cittadino di agire con tempestività sul pagamento di sanzioni',
+              },
+            ],
           },
           {
-            'type': 'list-item',
-            'children': [
+            type: 'list-item',
+            children: [
               {
-                'type': 'text',
-                'text': 'La scadenza sugli avvisi di pagamento',
-                'bold': true
+                type: 'text',
+                text: 'La scadenza sugli avvisi di pagamento',
+                bold: true,
               },
               {
-                'type': 'text',
-                'text': ' riduce la possibilità di incorrere in sanzioni aggiuntive'
-              }
-            ]
-          }
-        ]
+                type: 'text',
+                text: ' riduce la possibilità di incorrere in sanzioni aggiuntive',
+              },
+            ],
+          },
+        ],
       },
     ],
-  }
+  },
 };
--- apps/storybook-app/stories/molecules/BannerLinks.stories.tsx
+++ apps/storybook-app/stories/molecules/BannerLinks.stories.tsx
@@ -1,46 +1,35 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from '@storybook/react';
 import { BannerLinks } from '../../../nextjs-website/src/components/molecules/BannerLinks/BannerLinks';
-import { mockTextBlock } from "../mock-content.helper";
+import { mockTextBlock } from '../mock-content.helper';
 
 const meta: Meta<typeof BannerLinks> = {
-    title: 'Molecules/BannerLinks',
-    component: BannerLinks,
-  };
-  
+  title: 'Molecules/BannerLinks',
+  component: BannerLinks,
+};
+
 export default meta;
 
 export const Showcase: StoryObj<typeof BannerLinks> = {
-    args:{
-        banners:[
-            {
-                contentMaxWidth: 450,
-                justify: 'right',
-                theme: 'light',
-                icon: 'Feedback',
-                title: 'Titolo',
-                content: [
-                  {...mockTextBlock({type: 'paragraph', wordCount: 30})},
-                ],
-              },
-              {
-                contentMaxWidth: 450,
-                justify: 'center',
-                theme: 'dark',
-                icon: 'Feedback',
-                title: 'Titolo',
-                content: [
-                  {...mockTextBlock({type: 'paragraph', wordCount: 28})},
-                ],
-              },{
-                contentMaxWidth: 450,
-                justify: 'left',
-                theme: 'light',
-                icon: 'Feedback',
-                title: 'Titolo',
-                content: [
-                  {...mockTextBlock({type: 'paragraph', wordCount: 32})},
-                ],
-              }
-        ]
-    }
-}
+  args: {
+    banners: [
+      {
+        theme: 'light',
+        icon: 'Feedback',
+        title: 'Titolo',
+        content: [{ ...mockTextBlock({ type: 'paragraph', wordCount: 30 }) }],
+      },
+      {
+        theme: 'dark',
+        icon: 'Feedback',
+        title: 'Titolo',
+        content: [{ ...mockTextBlock({ type: 'paragraph', wordCount: 28 }) }],
+      },
+      {
+        theme: 'light',
+        icon: 'Feedback',
+        title: 'Titolo',
+        content: [{ ...mockTextBlock({ type: 'paragraph', wordCount: 32 }) }],
+      },
+    ],
+  },
+};
