f01879c024294213c08aea4c2caec566a32f38e1
Marcello Bertoli <marcello.bertoli@uqido.com>
2024-06-25 19:04:42 +0200

    rename tags to tag

--- apps/strapi-cms/src/api/api-data/controllers/api-data.ts
+++ apps/strapi-cms/src/api/api-data/controllers/api-data.ts
@@ -2,6 +2,6 @@
  * api-data controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::api-data.api-data');
