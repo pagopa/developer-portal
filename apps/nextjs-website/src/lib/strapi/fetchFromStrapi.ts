2710295e128d0ed6ee03cd2120c0bb790755d7c1
Marco Bottaro <marco.bottaro@uqido.com>
2024-07-04 15:33:56 +0200

    Remove fetchFun cache option

--- apps/nextjs-website/src/lib/strapi/fetchFromStrapi.ts
+++ apps/nextjs-website/src/lib/strapi/fetchFromStrapi.ts
@@ -28,7 +28,6 @@ export const fetchFromStrapi = <A, O, I>(
             () =>
               fetchFun(`${strapiEndpoint}/api/${path}/?${populate}`, {
                 method: 'GET',
-                cache: 'no-cache',
                 headers: {
                   Authorization: `Bearer ${strapiApiToken}`,
                 },
