24003166228e99da27f25cd3c9f1967f2bfbfa08
MarBert <41899883+MarBert@users.noreply.github.com>
2024-07-10 14:52:08 +0200

    [DEV-1738] Add ApiDataListPageTemplate (#927)
    * move bannerLinks to atoms, update imports
    
    * refactor prop for bannerlink
    
    * fix ul tag order, add margins, add max width
    
    * add title, need update to icon
    
    * fix issue in appIO content
    
    * refactor icon to accept string or object
    
    * fix banner link layout, fix content
    
    * remove unused imports
    
    * refactor to icon received from strapi
    
    * add comment for clarification
    
    * add typeof mediacodec to image in props
    
    * update banner links to keep text inside reading area
    
    * fix to layout and refactor
    
    * fix bannerlinks with 3 options width
    
    * fix contents props
    
    * update changeset
    
    * update storybook for bannerlink
    
    * remove useless images and indentifiers
    
    * fix import in SolutionPageTemplate
    
    * add default value for width and justify
    
    * remove empty content
    
    * add linting to storybook
    
    * Add media type to icon, update contents and storybook, add svg icons
    
    * remove stories useles fields, revert mockimage
    
    * remove linting rules, reverted package-lock
    
    * Fix storybook url mock content
    
    * remove usless import
    
    * Add template, add template.stories
    
    * fix linting in bannerlink, fix import error in case history page tamplate
    
    * add optional cta label to cta card grid, update storybook content, fix hero layout
    
    * add banner link to page
    
    * add title variant to hero and template
    
    * add code.svg icon
    
    * update changeset
    
    * update svg color to white, fix storybook mock content
    
    * simplify banner links to show a maximum of two banner links
    
    * refactor template
    
    * add local icon reference for storybook
    
    * Add icons to stoybook
    
    * remove useless file
    
    * Refactor banner links props
    
    * Update apps/nextjs-website/src/components/templates/ApiListPageTemplate/ApiListPageTemplate.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/nextjs-website/src/components/templates/ApiListPageTemplate/ApiListPageTemplate.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/storybook-app/stories/templates/ApiListPageTemplate.stories.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/storybook-app/stories/templates/ApiListPageTemplate.stories.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/storybook-app/stories/templates/ApiListPageTemplate.stories.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/nextjs-website/src/components/templates/ApiListPageTemplate/ApiListPageTemplate.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/nextjs-website/src/components/templates/ApiListPageTemplate/ApiListPageTemplate.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/nextjs-website/src/components/templates/ApiListPageTemplate/ApiListPageTemplate.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * refactor to ApiDataListPageTemplate
    
    * Update .changeset/rare-files-punch.md
    
    * fix issue with cards pagination
    
    * Update apps/nextjs-website/src/components/templates/ApiDataListPageTemplate/ApiDataListPageTemplate.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

---
+++ apps/storybook-app/stories/templates/ApiDataListPageTemplate.stories.tsx
@@ -0,0 +1,127 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ApiDataListPageTemplate from '../../../nextjs-website/src/components/templates/ApiDataListPageTemplate/ApiDataListPageTemplate';
+import { nextIntlContextDecorator } from '../next-intl-context.helper';
+import { mockText, mockTextBlock } from '../mock-content.helper';
+
+const meta: Meta<typeof ApiDataListPageTemplate> = {
+  title: 'Templates/ApiDataListPageTemplate',
+  component: ApiDataListPageTemplate,
+};
+
+export default meta;
+
+export const Showcase: StoryObj<typeof ApiDataListPageTemplate> = {
+  decorators: [nextIntlContextDecorator],
+  args: {
+    breadcrumbs: {
+      product: {
+        name: 'Piattaforma pagoPA',
+        description: 'Il portale per gli sviluppatori di PagoPA',
+        slug: 'pagopa',
+        logo: {
+          url: 'https://cdn.dev.developer.pagopa.it/app_Io_d9bffd556b.svg',
+        },
+        subpaths: {
+          overview: {
+            name: 'api',
+            path: 'api',
+          },
+        },
+      },
+      path: 'PagoPA',
+      paths: [
+        {
+          name: 'API',
+          path: 'API',
+        },
+      ],
+    },
+    hero: {
+      title: 'API',
+      subtitle:
+        'Se vuoi sapere come iscriverti a un e-service partendo dal catalogo e ottenere un voucher per la fruizione del sevizio.',
+    },
+    cards: [
+      {
+        title: mockText(3),
+        text: mockText(10),
+        icon: '/icons/code.svg',
+        ctaLabel: 'Esplora le API',
+        href: '#',
+        tags: [
+          {
+            label: 'REST',
+          },
+        ],
+      },
+      {
+        title: mockText(3),
+        text: mockText(10),
+        icon: '/icons/code.svg',
+        ctaLabel: 'Esplora le API',
+        href: '#',
+        tags: [
+          {
+            label: 'SOAP',
+          },
+        ],
+      },
+      {
+        title: mockText(3),
+        text: mockText(10),
+        icon: '/icons/code.svg',
+        ctaLabel: 'Esplora le API',
+        href: '#',
+        tags: [
+          {
+            label: 'REST',
+          },
+        ],
+      },
+      {
+        title: mockText(3),
+        text: mockText(10),
+        icon: '/icons/code.svg',
+        ctaLabel: 'Esplora le API',
+        href: '#',
+        tags: [
+          {
+            label: 'REST',
+          },
+        ],
+      },
+    ],
+    bannerLinks: [
+      {
+        theme: 'light',
+        icon: {
+          name: 'headset.svg',
+          alternativeText: null,
+          caption: null,
+          width: 60,
+          height: 61,
+          ext: '.svg',
+          mime: 'image/svg+xml',
+          url: '/icons/headset.svg',
+        },
+        title: mockText(2),
+        content: [{ ...mockTextBlock({ type: 'paragraph', wordCount: 30 }) }],
+      },
+      {
+        theme: 'dark',
+        icon: {
+          name: 'headset.svg',
+          alternativeText: null,
+          caption: null,
+          width: 60,
+          height: 61,
+          ext: '.svg',
+          mime: 'image/svg+xml',
+          url: '/icons/headset.svg',
+        },
+        title: mockText(2),
+        content: [{ ...mockTextBlock({ type: 'paragraph', wordCount: 28 }) }],
+      },
+    ],
+  },
+};
