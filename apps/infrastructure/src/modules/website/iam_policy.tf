9ce6513d4595a832854c73f38fbb147744b1fc45
christian-calabrese <christian.calabrese@pagopa.it>
2024-07-17 14:30:39 +0200

    fix: edited correct bucket policy

--- apps/infrastructure/src/modules/website/iam_policy.tf
+++ apps/infrastructure/src/modules/website/iam_policy.tf
@@ -72,8 +72,9 @@ resource "aws_iam_policy" "deploy_website" {
 
 data "aws_iam_policy_document" "website_iam_policy" {
   statement {
-    actions = ["s3:GetObject"]
+    actions = ["s3:GetObject", "s3:ListBucket"]
     resources = [
+      aws_s3_bucket.website.arn,
       "${aws_s3_bucket.website.arn}/*"
     ]
 
