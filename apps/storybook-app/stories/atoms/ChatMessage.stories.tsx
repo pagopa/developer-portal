90e2a2944c7b0227f61555e07a02639e8affb38f
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2024-07-22 17:32:52 +0200

    Add intl decorator and fix lint issiues

--- apps/storybook-app/stories/atoms/ChatMessage.stories.tsx
+++ apps/storybook-app/stories/atoms/ChatMessage.stories.tsx
@@ -1,6 +1,7 @@
 import { Decorator, Meta, StoryObj } from '@storybook/react';
 import ChatMessage from '../../../nextjs-website/src/components/atoms/ChatMessage/ChatMessage';
 import React from 'react';
+import { nextIntlContextDecorator } from '../next-intl-context.helper';
 
 const meta: Meta<typeof ChatMessage> = {
   title: 'Atoms/ChatMessage',
@@ -8,9 +9,7 @@ const meta: Meta<typeof ChatMessage> = {
 };
 
 const decorator: Decorator = (story) => (
-  <div style={{ backgroundColor: '#D9D9D9', padding: '2rem' }}>
-    {story()}
-  </div>
+  <div style={{ backgroundColor: '#D9D9D9', padding: '2rem' }}>{story()}</div>
 );
 
 export default meta;
@@ -20,7 +19,7 @@ export const ChatBotMessage: StoryObj<typeof ChatMessage> = {
     message: "Hello I'm a ChatBot",
     timestamp: '11:22',
   },
-  decorators: [decorator],
+  decorators: [decorator, nextIntlContextDecorator],
 };
 
 export const UserMessage: StoryObj<typeof ChatMessage> = {
@@ -29,5 +28,5 @@ export const UserMessage: StoryObj<typeof ChatMessage> = {
     sender: 'John Doe',
     timestamp: '11:22',
   },
-  decorators: [decorator],
+  decorators: [decorator, nextIntlContextDecorator],
 };
