3ccccdce9204f222488226e83469a7b393479d13
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2024-07-22 17:54:30 +0200

    Fix modal story

--- apps/storybook-app/stories/organisms/ChatModal.stories.tsx
+++ apps/storybook-app/stories/organisms/ChatModal.stories.tsx
@@ -1,6 +1,7 @@
 import { Decorator, Meta, StoryObj } from '@storybook/react';
 import ChatModal from '../../../nextjs-website/src/components/organisms/ChatModal/ChatModal';
 import React from 'react';
+import { nextIntlContextDecorator } from '../next-intl-context.helper';
 
 const meta: Meta<typeof ChatModal> = {
   title: 'Organisms/ChatModal',
@@ -21,36 +22,39 @@ export const NewChatSession: StoryObj<typeof ChatModal> = {
   args: {
     chatMessages: [
       {
-        message: "Ciao, sono [Nome Chatbot], il chatbot di DevPortal.\nPrima di incominciare ti invito a leggere l'Informativa sulla Privacy.\nScrivi Accetto per proseguire.",
-        timestamp: '11:21'
-      }
-    ]
+        message:
+          "Ciao, sono [Nome Chatbot], il chatbot di DevPortal.\nPrima di incominciare ti invito a leggere l'Informativa sulla Privacy.\nScrivi Accetto per proseguire.",
+        timestamp: '11:21',
+      },
+    ],
   },
-  decorators: [decorator],
+  decorators: [decorator, nextIntlContextDecorator],
 };
 
 export const ChatSessionWithMessages: StoryObj<typeof ChatModal> = {
   args: {
     chatMessages: [
       {
-        message: "Ciao, sono [Nome Chatbot], il chatbot di DevPortal.\nPrima di incominciare ti invito a leggere l'Informativa sulla Privacy.\nScrivi Accetto per proseguire.",
-        timestamp: '11:21'
+        message:
+          "Ciao, sono [Nome Chatbot], il chatbot di DevPortal.\nPrima di incominciare ti invito a leggere l'Informativa sulla Privacy.\nScrivi Accetto per proseguire.",
+        timestamp: '11:21',
       },
       {
-        message: 'Accetto', 
-        sender: 'Mario Rossi', 
-        timestamp: '11:22'
-      },
-      {
-        message: "Ottimo! Adesso possiamo incominciare. Come posso aiutarti?",
-        timestamp: '11:22'
-      },
-      {
-        message: "E' possibile pagare una posizione debitoria una volta scaduta?",
+        message: 'Accetto',
         sender: 'Mario Rossi',
-        timestamp: '11:22'
-      }
-    ]
+        timestamp: '11:22',
+      },
+      {
+        message: 'Ottimo! Adesso possiamo incominciare. Come posso aiutarti?',
+        timestamp: '11:22',
+      },
+      {
+        message:
+          "E' possibile pagare una posizione debitoria una volta scaduta?",
+        sender: 'Mario Rossi',
+        timestamp: '11:22',
+      },
+    ],
   },
-  decorators: [decorator],
+  decorators: [decorator, nextIntlContextDecorator],
 };
