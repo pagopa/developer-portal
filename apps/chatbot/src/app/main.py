f17f868490098a2c1d9ad10e56a6b55aa70c943d
Devis Battisti <devis.battisti@uqido.com>
2024-07-23 15:51:59 +0200

    feat(chatbot api): cors configuration

--- apps/chatbot/src/app/main.py
+++ apps/chatbot/src/app/main.py
@@ -2,6 +2,7 @@ import yaml
 import mangum
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from src.modules.chatbot import Chatbot
 
@@ -16,6 +17,20 @@ class Query(BaseModel):
   queriedAt: str | None = None
 
 app = FastAPI()
+
+
+origins = [
+  "http://localhost",
+  "http://localhost:3000",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.get("/healthz")
 async def healthz ():
