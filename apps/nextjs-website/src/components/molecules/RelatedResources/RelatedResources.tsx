7bf17f9f1f3563e04022fd34930b02cb8f9426d8
Marco Ponchia <marco.ponchia@uqido.com>
2024-07-24 18:37:22 +0200

    fix

--- apps/nextjs-website/src/components/molecules/RelatedResources/RelatedResources.tsx
+++ apps/nextjs-website/src/components/molecules/RelatedResources/RelatedResources.tsx
@@ -1,5 +1,5 @@
 import EContainer from '@/editorialComponents/EContainer/EContainer';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useTranslations } from 'next-intl';
 import { GuideCard, GuideCardProps } from '../GuideCard/GuideCard';
 import DownloadableDocuments, {
@@ -22,8 +22,10 @@ const RelatedResources = ({
       <Typography variant='h4' sx={{ mt: 8, mb: 4, width: '100%' }}>
         {t('title')}
       </Typography>
-      {resources &&
+      <Box>
+        {resources &&
           resources.map((props, index) => <GuideCard key={index} {...props} />)}
+      </Box>
       <DownloadableDocuments documents={downloadableDocuments} />
     </EContainer>
   );
