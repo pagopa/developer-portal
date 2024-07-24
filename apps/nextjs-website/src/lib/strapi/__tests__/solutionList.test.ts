b5fca3722972b49874f7a75fbf84460f4069b73a
Marco Ponchia <marco.ponchia@uqido.com>
2024-07-24 12:57:03 +0200

    fix solution list test

--- apps/nextjs-website/src/lib/strapi/__tests__/solutionList.test.ts
+++ apps/nextjs-website/src/lib/strapi/__tests__/solutionList.test.ts
@@ -1,124 +1,151 @@
 import * as E from 'fp-ts/lib/Either';
 import { StrapiSolutionListCodec } from '../solutionListCodec';
+import { baseSolutionJson } from './solutions.test';
+import { baseCaseHistoryJson } from './caseHistories.test';
 
 const makeStrapiResponseJson = () => ({
   data: {
     id: 1,
     attributes: {
-      title: 'Solutions List',
-      description: 'Solutions List page desc',
-      createdAt: '2024-06-10T15:31:59.140Z',
-      updatedAt: '2024-06-10T15:31:59.893Z',
-      publishedAt: '2024-06-10T15:31:59.889Z',
+      title: 'test',
+      description: 'sdfdfsdfsd',
+      createdAt: '2024-07-24T09:21:10.194Z',
+      updatedAt: '2024-07-24T10:45:18.577Z',
+      publishedAt: '2024-07-24T09:21:10.856Z',
       locale: 'it',
       solutions: {
-        data: [
+        data: [baseSolutionJson],
+      },
+      caseHistories: {
+        id: 4,
+        title: 'etew',
+        description: 'dssasa',
+        case_histories: {
+          data: [baseCaseHistoryJson],
+        },
+      },
+      features: {
+        id: 3,
+        title: 'dfdsfsfds',
+        items: [
           {
-            id: 1,
-            attributes: {
-              slug: 'solution-1',
-              kickerTitle: 'kickerTitle',
-              title: 'Title Test',
-              description: null,
-              dirName: 'lAIZmjrusC6qV8ki9zsZ',
-              landingUseCaseFile: 'README.md',
-              createdAt: '2024-06-06T15:36:07.300Z',
-              updatedAt: '2024-06-10T08:37:35.961Z',
-              publishedAt: '2024-06-06T15:36:10.333Z',
-              locale: 'it',
-              icon: {
-                data: {
-                  id: 5,
-                  attributes: {
-                    name: 'Screenshot from 2024-05-29 14-15-04.png',
-                    alternativeText: null,
-                    caption: null,
-                    width: 83,
-                    height: 86,
-                    formats: null,
-                    hash: 'Screenshot_from_2024_05_29_14_15_04_8e9393832f',
-                    ext: '.png',
-                    mime: 'image/png',
-                    size: 0.51,
-                    url: 'http://localhost:1337/uploads/Screenshot_from_2024_05_29_14_15_04_8e9393832f.png',
-                    previewUrl: null,
-                    provider: 'strapi-provider-upload-custom',
-                    provider_metadata: null,
-                    createdAt: '2024-05-29T12:15:28.241Z',
-                    updatedAt: '2024-06-06T15:35:44.363Z',
+            id: 11,
+            title: 'fsdfdsfsd',
+            content: [
+              {
+                type: 'paragraph',
+                children: [
+                  {
+                    type: 'text',
+                    text: 'sfdsfdsfdsfsd',
                   },
+                ],
+              },
+            ],
+            theme: 'light',
+            icon: {
+              data: {
+                id: 3,
+                attributes: {
+                  name: 'face.png',
+                  alternativeText: null,
+                  caption: null,
+                  width: 320,
+                  height: 320,
+                  formats: null,
+                  hash: 'face_f36a6f701f',
+                  ext: '.png',
+                  mime: 'image/png',
+                  size: 0.98,
+                  url: 'http://localhost:1337/uploads/face_f36a6f701f.png',
+                  previewUrl: null,
+                  provider: 'strapi-provider-upload-custom',
+                  provider_metadata: null,
+                  createdAt: '2024-04-15T14:11:04.915Z',
+                  updatedAt: '2024-04-15T14:11:04.915Z',
+                },
+              },
+            },
+          },
+          {
+            id: 12,
+            title: 'ffff',
+            content: [
+              {
+                type: 'paragraph',
+                children: [
+                  {
+                    type: 'text',
+                    text: 'fdsfsdfsdfdsfdsf',
+                  },
+                ],
+              },
+            ],
+            theme: 'light',
+            icon: {
+              data: {
+                id: 3,
+                attributes: {
+                  name: 'face.png',
+                  alternativeText: null,
+                  caption: null,
+                  width: 320,
+                  height: 320,
+                  formats: null,
+                  hash: 'face_f36a6f701f',
+                  ext: '.png',
+                  mime: 'image/png',
+                  size: 0.98,
+                  url: 'http://localhost:1337/uploads/face_f36a6f701f.png',
+                  previewUrl: null,
+                  provider: 'strapi-provider-upload-custom',
+                  provider_metadata: null,
+                  createdAt: '2024-04-15T14:11:04.915Z',
+                  updatedAt: '2024-04-15T14:11:04.915Z',
+                },
+              },
+            },
+          },
+          {
+            id: 13,
+            title: 'dddd',
+            content: [
+              {
+                type: 'paragraph',
+                children: [
+                  {
+                    type: 'text',
+                    text: 'fsdfdsfsd',
+                  },
+                ],
+              },
+            ],
+            theme: 'light',
+            icon: {
+              data: {
+                id: 5,
+                attributes: {
+                  name: 'Screenshot from 2024-05-29 14-15-04.png',
+                  alternativeText: null,
+                  caption: null,
+                  width: 83,
+                  height: 86,
+                  formats: null,
+                  hash: 'Screenshot_from_2024_05_29_14_15_04_8e9393832f',
+                  ext: '.png',
+                  mime: 'image/png',
+                  size: 0.51,
+                  url: 'http://localhost:1337/uploads/Screenshot_from_2024_05_29_14_15_04_8e9393832f.png',
+                  previewUrl: null,
+                  provider: 'strapi-provider-upload-custom',
+                  provider_metadata: null,
+                  createdAt: '2024-05-29T12:15:28.241Z',
+                  updatedAt: '2024-07-17T14:59:45.174Z',
                 },
               },
             },
           },
         ],
-      },
-      caseHistories: {
-        id: 1,
-        title: 'Stories',
-        description: 'dsfsdf dfs sdff f sf asadf  ',
-        case_histories: {
-          data: [
-            {
-              id: 1,
-              attributes: {
-                slug: 'case-history-1',
-                title: 'Case History',
-                description: 'desc',
-                createdAt: '2024-06-05T13:04:34.614Z',
-                updatedAt: '2024-06-05T14:05:24.891Z',
-                publishedAt: '2024-06-05T13:08:21.443Z',
-                locale: 'it',
-                image: {
-                  data: {
-                    id: 4,
-                    attributes: {
-                      name: 'webinar-cover-io-remote-content.jpg',
-                      alternativeText: null,
-                      caption: null,
-                      width: 728,
-                      height: 416,
-                      formats: {
-                        thumbnail: {
-                          name: 'thumbnail_webinar-cover-io-remote-content.jpg',
-                          hash: 'thumbnail_webinar_cover_io_remote_content_62f1f615b5',
-                          ext: '.jpg',
-                          mime: 'image/jpeg',
-                          path: null,
-                          width: 245,
-                          height: 140,
-                          size: 5.38,
-                          url: 'http://localhost:1337/uploads/thumbnail_webinar_cover_io_remote_content_62f1f615b5.jpg',
-                        },
-                        small: {
-                          name: 'small_webinar-cover-io-remote-content.jpg',
-                          hash: 'small_webinar_cover_io_remote_content_62f1f615b5',
-                          ext: '.jpg',
-                          mime: 'image/jpeg',
-                          path: null,
-                          width: 500,
-                          height: 286,
-                          size: 16.03,
-                          url: 'http://localhost:1337/uploads/small_webinar_cover_io_remote_content_62f1f615b5.jpg',
-                        },
-                      },
-                      hash: 'webinar_cover_io_remote_content_62f1f615b5',
-                      ext: '.jpg',
-                      mime: 'image/jpeg',
-                      size: 30.06,
-                      url: 'http://localhost:1337/uploads/webinar_cover_io_remote_content_62f1f615b5.jpg',
-                      previewUrl: null,
-                      provider: 'strapi-provider-upload-custom',
-                      provider_metadata: null,
-                      createdAt: '2024-04-15T14:25:47.773Z',
-                      updatedAt: '2024-04-15T14:25:47.773Z',
-                    },
-                  },
-                },
-              },
-            },
-          ],
-        },
       },
     },
   },
@@ -126,7 +153,7 @@ const makeStrapiResponseJson = () => ({
 });
 
 describe('StrapiSolutionsListCodec', () => {
-  it('should decode strapi homepage', () => {
+  it('should decode strapi SolutionsList', () => {
     const jsonFromStrapi = makeStrapiResponseJson();
     const actual = StrapiSolutionListCodec.decode(jsonFromStrapi);
     expect(E.isRight(actual)).toBeTruthy();
