7694f9a764721a71c782b288513bf3e67c30c180
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2024-08-01 16:06:13 +0200

    add mobile style and remove badge

--- apps/nextjs-website/src/components/atoms/ChatButton/ChatButton.tsx
+++ apps/nextjs-website/src/components/atoms/ChatButton/ChatButton.tsx
@@ -1,61 +1,42 @@
 'use client';
 import Fab from '@mui/material/Fab';
-import { Badge, Box, useTheme } from '@mui/material';
+import { Box, Theme, useMediaQuery, useTheme } from '@mui/material';
 import IconWrapper from '../IconWrapper/IconWrapper';
 
 type ChatButtonProps = {
   isChatOpen: boolean;
-  hasNewMessages: boolean;
   onOpenChat: (event: React.MouseEvent<HTMLButtonElement>) => null;
 };
 
-const ChatButton = ({
-  isChatOpen,
-  hasNewMessages,
-  onOpenChat,
-}: ChatButtonProps) => {
+const ChatButton = ({ isChatOpen, onOpenChat }: ChatButtonProps) => {
   const { palette } = useTheme();
+  const isDesktop = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
   return (
     <Box sx={{ opacity: isChatOpen ? 0 : 1 }}>
-      <Badge
-        badgeContent={hasNewMessages ? 1 : 0}
-        color='success'
-        overlap='circular'
-        variant='dot'
+      <Box
         sx={{
-          '& .MuiBadge-badge': {
-            fontSize: 9,
-            height: 15,
-            minWidth: 15,
-            borderRadius: 20,
-            zIndex: 1051,
-          },
+          backgroundColor: palette.text.primary,
+          padding: '3px',
+          borderRadius: '100%',
         }}
       >
-        <Box
+        <Fab
+          aria-label='chat'
+          onClick={onOpenChat}
+          size={isDesktop ? 'large' : 'medium'}
           sx={{
-            backgroundColor: palette.text.primary,
-            padding: '3px',
-            borderRadius: '100%',
+            backgroundColor: 'white !important',
+            paddingTop: 1,
           }}
         >
-          <Fab
-            aria-label='chat'
-            onClick={onOpenChat}
-            sx={{
-              backgroundColor: 'white !important',
-              paddingTop: 1,
-            }}
-          >
-            <IconWrapper
-              icon={'/icons/chatbotAvatar.svg'}
-              isSvg={true}
-              color={palette.text.secondary}
-              size={48}
-            />
-          </Fab>
-        </Box>
-      </Badge>
+          <IconWrapper
+            icon={'/icons/chatbotAvatar.svg'}
+            isSvg={true}
+            color={palette.text.secondary}
+            size={isDesktop ? 48 : 40}
+          />
+        </Fab>
+      </Box>
     </Box>
   );
 };
