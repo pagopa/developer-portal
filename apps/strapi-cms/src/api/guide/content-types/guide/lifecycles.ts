e5590d1302feb4137df18b931b1b85e80dbbbd22
redtom <tommasorosso1gmail.com>
2024-08-06 15:33:55 +0200

    guides and guides list page on strapi

---
+++ apps/strapi-cms/src/api/guide/content-types/guide/lifecycles.ts
@@ -0,0 +1,51 @@
+import { errors } from '@strapi/utils';
+
+interface IGuide {
+  readonly versions?: Array<{
+    id: number;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    __pivot: { field: string; component_type: string };
+  }>;
+}
+
+interface IGuideEvent {
+  readonly params: {
+    readonly data: IGuide;
+    readonly where?: {
+      readonly id?: string;
+    };
+  };
+}
+
+const validateGuideVersions = async (event: IGuideEvent) => {
+  const { data } = event.params;
+
+  if (data.versions && Array.isArray(data.versions)) {
+    const versionIds = data.versions.map((v) => v.id);
+
+    // Fetch the full version data
+    const versions = await strapi.db.connection
+      .select('*')
+      .from(`components_common_guide_versions`)
+      .whereIn('id', versionIds);
+
+    const mainVersions = versions.filter((version) => version.main === 1);
+
+    if (mainVersions.length > 1) {
+      throw new errors.ApplicationError(
+        'Only one version can have main set to true'
+      );
+    }
+  }
+
+  return true;
+};
+
+module.exports = {
+  async beforeCreate(event: IGuideEvent) {
+    await validateGuideVersions(event);
+  },
+  async beforeUpdate(event: IGuideEvent) {
+    await validateGuideVersions(event);
+  },
+};
