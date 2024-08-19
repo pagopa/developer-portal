59aacfd9795b5cdc08a0e4025528f5b952e43de5
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2024-08-19 17:23:17 +0200

    Add chatbot feedback button atom

---
+++ apps/nextjs-website/src/components/atoms/ChatbotFeedbackButton/ChatbotFeedbackButton.tsx
@@ -0,0 +1,58 @@
+import { Check, ThumbDownAlt, ThumbDownAltOutlined } from '@mui/icons-material';
+import { IconButton, Tooltip } from '@mui/material';
+import { useTranslations } from 'next-intl';
+import { useState } from 'react';
+
+const TOOLTIP_DURATION = 2000;
+
+type ChatbotFeedbackButtonProps = {
+  isNegativeFeedbackGiven: boolean;
+  onToggleNegativeFeedback: (negativeFeedback: boolean) => null;
+};
+
+const ChatbotFeedbackButton = ({
+  isNegativeFeedbackGiven,
+  onToggleNegativeFeedback,
+}: ChatbotFeedbackButtonProps) => {
+  const t = useTranslations();
+  const [feedbackGiven, setFeedbackGiven] = useState(false);
+
+  const onToggle = (negativeFeedback: boolean) => {
+    onToggleNegativeFeedback(negativeFeedback);
+    setFeedbackGiven(true);
+    setTimeout(() => setFeedbackGiven(false), TOOLTIP_DURATION);
+  };
+
+  return (
+    <>
+      <IconButton
+        color='primary'
+        onClick={() => onToggle(!isNegativeFeedbackGiven)}
+        disabled={feedbackGiven}
+        sx={{
+          cursor: 'pointer',
+        }}
+      >
+        {feedbackGiven ? (
+          <Tooltip
+            sx={{
+              display: 'block',
+            }}
+            open={true}
+            placement='bottom'
+            title={t('chatbot.feedbackGiven')}
+            arrow
+          >
+            <Check color='success' fontSize='small' />
+          </Tooltip>
+        ) : isNegativeFeedbackGiven ? (
+          <ThumbDownAlt fontSize='small' />
+        ) : (
+          <ThumbDownAltOutlined fontSize='small' />
+        )}
+      </IconButton>
+    </>
+  );
+};
+
+export default ChatbotFeedbackButton;
