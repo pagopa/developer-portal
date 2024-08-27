8d0d78b6c150cc8e74054a54c8f342a583f25607
Marco Domenico Cirillo <59966344+mdciri@users.noreply.github.com>
2024-08-27 18:45:23 +0200

    [CAI-128] Redis (#1065)
    * Update README
    
    * Update poetry with Redis with llamaindex
    
    * Update scripts with Redis
    
    * Update params
    
    * Update README
    
    * feat: changeset added
    
    ---------
    
    Co-authored-by: christian-calabrese <christian.calabrese@pagopa.it>

--- apps/chatbot/src/modules/create_vector_index.py
+++ apps/chatbot/src/modules/create_vector_index.py
@@ -6,17 +6,21 @@ import logging
 from llama_index.embeddings.bedrock import BedrockEmbedding
 
 from src.modules.async_bedrock import AsyncBedrock
-from src.modules.vector_database import build_automerging_index
+from src.modules.vector_database import build_automerging_index_s3, build_automerging_index_redis
 
+from dotenv import load_dotenv
 
+load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 AWS_DEFAULT_REGION = os.getenv('CHB_AWS_DEFAULT_REGION', os.getenv('AWS_DEFAULT_REGION'))
 AWS_S3_BUCKET = os.getenv("CHB_AWS_S3_BUCKET", os.getenv("AWS_S3_BUCKET"))
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--params", type=str, default="config/params.yaml", help="params path")
+    parser.add_argument("--use-redis", action="store_true", help="store the index on Redis")
     args = parser.parse_args()
 
     # load parameters
@@ -35,12 +39,21 @@ if __name__ == "__main__":
     )
 
     # create vector index
-    index = build_automerging_index(
-        model,
-        embed_model,
-        documentation_dir=params["documentation"]["path"],
-        save_dir=params["vector_index"]["path"],
-        s3_bucket_name=AWS_S3_BUCKET,
-        chunk_sizes=params["vector_index"]["chunk_sizes"],
-        chunk_overlap=params["vector_index"]["chunk_overlap"]
-    )
+    if args.use_redis:
+        index = build_automerging_index_redis(
+            model,
+            embed_model,
+            documentation_dir=params["documentation"]["path"],
+            chunk_sizes=params["vector_index"]["chunk_sizes"],
+            chunk_overlap=params["vector_index"]["chunk_overlap"]
+        )
+    else:
+        index = build_automerging_index_s3(
+            model,
+            embed_model,
+            documentation_dir=params["documentation"]["path"],
+            save_dir=params["vector_index"]["path"],
+            s3_bucket_name=AWS_S3_BUCKET,
+            chunk_sizes=params["vector_index"]["chunk_sizes"],
+            chunk_overlap=params["vector_index"]["chunk_overlap"]
+        )
