845bd121af1260369b129d65f87b04e248590ec9
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2024-09-09 15:34:57 +0200

    Create fixture file for chatbot Stories

--- apps/storybook-app/stories/organisms/ChatbotHistoryLayout.stories.tsx
+++ apps/storybook-app/stories/organisms/ChatbotHistoryLayout.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import ChatbotHistoryLayout from 'nextjs-website/src/components/organisms/ChatbotHistoryLayout/ChatbotHistoryLayout';
-import { mockText } from '../mock-content.helper';
+import { sessionsList } from '../fixtures/chatbotFixture';
 
 const meta: Meta<typeof ChatbotHistoryLayout> = {
   title: 'Organisms/ChatbotHistoryLayout',
@@ -12,58 +12,7 @@ export default meta;
 export const NewChatSession: StoryObj<typeof ChatbotHistoryLayout> = {
   args: {
     paginatedSessions: {
-      items: [
-        {
-          title: mockText(25),
-          id: '111',
-          createdAt: '2024-07-24T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '112',
-          createdAt: '2024-07-23T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '113',
-          createdAt: '2024-07-22T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '114',
-          createdAt: '2024-07-21T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '115',
-          createdAt: '2024-06-24T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '116',
-          createdAt: '2024-06-23T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '117',
-          createdAt: '2024-06-22T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '118',
-          createdAt: '2024-06-21T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '119',
-          createdAt: '2024-06-20T17:14:07.129Z',
-        },
-        {
-          title: mockText(25),
-          id: '120',
-          createdAt: '2024-05-24T17:14:07.129Z',
-        },
-      ],
+      items: sessionsList,
       page: 1,
       pages: 5,
       size: 44,
