58da7a4a02d068f7de5dc75be2b0ff28fb464a4a
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2024-09-10 15:42:40 +0200

    Add guest message

--- apps/nextjs-website/src/components/molecules/Chat/Chat.tsx
+++ apps/nextjs-website/src/components/molecules/Chat/Chat.tsx
@@ -10,6 +10,8 @@ import { compact } from 'lodash';
 import { useTranslations } from 'next-intl';
 import { ChatCatbotWriting } from '@/components/atoms/ChatChatbotWriting/ChatChatbotWriting';
 import { ChatSkeleton } from '@/components/atoms/ChatSkeleton/ChatSkeleton';
+import { useUser } from '@/helpers/user.helper';
+import { baseUrl } from '@/config';
 
 type ChatProps = {
   queries: Query[];
@@ -31,9 +33,14 @@ const Chat = ({
   const t = useTranslations();
   const { palette } = useTheme();
   const [instantScroll, setInstantScroll] = useState(scrollToBottom);
+  const { user } = useUser();
   const messages = useMemo(
     () => [
-      firstMessage(t('chatBot.welcomeMessage')),
+      firstMessage(
+        user
+          ? t('chatBot.welcomeMessage')
+          : t('chatBot.guestMessage', { host: baseUrl })
+      ),
       ...compact(
         queries.flatMap((q) => [
           q.question && q.queriedAt
@@ -57,7 +64,7 @@ const Chat = ({
         ])
       ),
     ],
-    [queries, t]
+    [queries, t, user]
   ) satisfies Message[];
 
   const scrollRef = useRef<HTMLDivElement>(null);
