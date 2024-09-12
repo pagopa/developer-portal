04cb1c3f1d53e06b97fac56d783c920731e7452e
Marco Ponchia <marco.ponchia@uqido.com>
2024-09-12 12:00:22 +0200

    add structured data make functions

--- apps/nextjs-website/src/helpers/structuredData.helpers.ts
+++ apps/nextjs-website/src/helpers/structuredData.helpers.ts
@@ -1,10 +1,19 @@
+import { ApiPageProps } from '@/components/molecules/ApiSection/ApiSection';
 import { baseUrl, organizationInfo, websiteName } from '@/config';
 import { Media } from '@/lib/strapi/codecs/MediaCodec';
+import { Webinar } from '@/lib/types/webinar';
 import {
+  Article,
   BreadcrumbList,
+  Event,
+  FAQPage,
+  HowTo,
   ImageObject,
   ListItem,
+  MonetaryAmount,
+  Offer,
   Organization,
+  SoftwareApplication,
   WebPage,
   WebSite,
   WithContext,
@@ -36,14 +45,6 @@ export type StructuredDataBreadcrumbList = readonly Pick<
   'name' | 'item'
 >[];
 
-export type StructuredDataMedia = Partial<
-  Pick<Media, 'url' | 'width' | 'height'>
->;
-
-export type StructuredDataWebPage = Omit<WebPage, '@type'> & {
-  readonly media?: StructuredDataMedia;
-};
-
 export function makeBreadcrumbList(
   items: StructuredDataBreadcrumbList
 ): WithContext<BreadcrumbList> {
@@ -58,6 +59,10 @@ export function makeBreadcrumbList(
   };
 }
 
+export type StructuredDataMedia = Partial<
+  Pick<Media, 'url' | 'width' | 'height'>
+>;
+
 function mediaToImageObject(media: StructuredDataMedia): ImageObject {
   return {
     '@type': 'ImageObject',
@@ -66,6 +71,10 @@ function mediaToImageObject(media: StructuredDataMedia): ImageObject {
     height: `${media.height}`,
   };
 }
+
+export type StructuredDataWebPage = Omit<WebPage, '@type'> & {
+  readonly media?: StructuredDataMedia;
+};
 
 export function makeWebPage(
   webPage: StructuredDataWebPage
@@ -79,5 +88,108 @@ export function makeWebPage(
     image:
       webPage.image ||
       (webPage.media ? mediaToImageObject(webPage.media) : undefined),
+  };
+}
+
+export function makeFAQPage(
+  faqs: ReadonlyArray<{
+    readonly question: string;
+    readonly answer: string;
+  }>
+): WithContext<FAQPage> {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: faqs.map(({ question, answer }) => ({
+      '@type': 'Question',
+      name: question,
+      acceptedAnswer: {
+        '@type': 'Answer',
+        text: answer,
+      },
+    })),
+  };
+}
+
+const defaultMonetaryAmount: MonetaryAmount = {
+  '@type': 'MonetaryAmount',
+  currency: 'EUR',
+  value: '0',
+};
+
+export function makeHowTo(howTo: Omit<HowTo, '@type'>): WithContext<HowTo> {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'HowTo',
+    ...howTo,
+    estimatedCost: defaultMonetaryAmount,
+  };
+}
+
+const defaultOffers: Offer = {
+  '@type': 'Offer',
+  price: '0',
+  availability: 'https://schema.org/InStock',
+};
+
+export function makeEvent(event: Omit<Event, '@type'>): WithContext<Event> {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'Event',
+    ...event,
+    offers: event.offers || defaultOffers,
+  };
+}
+
+export function convertWebinarToStracturedDataEvent(
+  webinar: Webinar
+): WithContext<Event> {
+  return makeEvent({
+    name: webinar.title,
+    startDate: webinar.startDateTime,
+    endDate: webinar.endDateTime,
+    eventStatus: 'https://schema.org/EventScheduled',
+    description: webinar.description,
+    image: webinar.imagePath,
+    offers: defaultOffers,
+    organizer: organization,
+    location: {
+      '@type': 'VirtualLocation',
+      url: `${baseUrl}/webinars/${webinar.slug}`,
+    },
+    performers: webinar.speakers?.map((speaker) => ({
+      '@type': 'Person',
+      name: speaker.name,
+      jobTitle: speaker.jobTitle,
+      image: speaker.avatar ? mediaToImageObject(speaker.avatar) : undefined,
+    })),
+  });
+}
+
+export function makeSoftwareApplication(
+  softwareApplication: Omit<SoftwareApplication, '@type'>
+): WithContext<SoftwareApplication> {
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'SoftwareApplication',
+    ...softwareApplication,
+    applicationCategory: 'Business',
+    offers: defaultOffers,
+  };
+}
+
+export function convertApiToStructuredDataSoftwareApplication(
+  api: ApiPageProps
+): WithContext<SoftwareApplication> {
+  return makeSoftwareApplication({
+    name: api.specURLsName,
+    url: `${baseUrl}/${api.product.slug}/api/${api.apiSlug}`,
+  });
+}
+
+export function makeArticle(article: Omit<Article, '@type'>): Article {
+  return {
+    '@type': 'Article',
+    ...article,
   };
 }
