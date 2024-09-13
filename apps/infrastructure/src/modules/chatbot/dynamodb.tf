0495c4e03f7528df12743b00ba73adbe8ecac45e
Devis Battisti <devis.battisti@uqido.com>
2024-09-13 12:47:49 +0200

    feat(chatbot): dynamodb configuration

--- apps/infrastructure/src/modules/chatbot/dynamodb.tf
+++ apps/infrastructure/src/modules/chatbot/dynamodb.tf
@@ -4,20 +4,15 @@ module "dynamodb_chatbot_queries" {
   source = "git::github.com/terraform-aws-modules/terraform-aws-dynamodb-table.git?ref=715399dbe24f6443820bf5de80f6100b35d56355" # v4.0.0
 
   billing_mode                = "PAY_PER_REQUEST"
-  deletion_protection_enabled = true
+  deletion_protection_enabled = false
 
   name                           = "ChatbotQueries"
   hash_key                       = "id"
-  range_key                      = "createdAt"
   server_side_encryption_enabled = true
 
   attributes = [
     {
       name = "id"
-      type = "S"
-    },
-    {
-      name = "createdAt"
       type = "S"
     },
   ]
