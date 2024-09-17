7e59439f081dc94749782030f80a4c4b7b6f54d6
christian-calabrese <christian.calabrese@pagopa.it>
2024-09-17 16:55:41 +0200

    fix: wrong reference to google api key ssm parameter

--- apps/infrastructure/src/modules/chatbot/lambda_chatbot.tf
+++ apps/infrastructure/src/modules/chatbot/lambda_chatbot.tf
@@ -18,7 +18,7 @@ locals {
     CHB_PROVIDER       = "aws"
     CHB_MODEL_ID       = "mistral.mistral-large-2402-v1:0"
     CHB_EMBED_MODEL_ID = "cohere.embed-multilingual-v3"
-    CHB_GOOGLE_API_KEY = module.gemini_api_key_ssm_parameter.ssm_parameter_name
+    CHB_GOOGLE_API_KEY = module.google_api_key_ssm_parameter.ssm_parameter_name
   }
 }
 
