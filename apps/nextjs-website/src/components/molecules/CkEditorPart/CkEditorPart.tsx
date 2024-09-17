a4134c79a37bf6525c6e1cb213ad1988f1c9ba55
Marco Ponchia <marco.ponchia@uqido.com>
2024-09-17 17:21:11 +0200

    Fix build error

--- apps/nextjs-website/src/components/molecules/CkEditorPart/CkEditorPart.tsx
+++ apps/nextjs-website/src/components/molecules/CkEditorPart/CkEditorPart.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { Box } from '@mui/material';
-import DOMPurify from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 
 export type CkEditorPartProps = {
   content: string;
