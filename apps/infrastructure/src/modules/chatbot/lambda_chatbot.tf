681dcf13de4e4dcc3b578988730a6ece2c98b81e
Devis Battisti <devis.battisti@uqido.com>
2024-09-20 12:20:45 +0200

    chore(chatbot): terraform fmt

--- apps/infrastructure/src/modules/chatbot/lambda_chatbot.tf
+++ apps/infrastructure/src/modules/chatbot/lambda_chatbot.tf
@@ -15,11 +15,11 @@ locals {
     # Be extremely careful when changing the provider
     # both the generation and the embedding models would be changed
     # embeddings size change would break the application and requires reindexing
-    CHB_PROVIDER              = "aws"
-    CHB_MODEL_ID              = "mistral.mistral-large-2402-v1:0"
-    CHB_EMBED_MODEL_ID        = "cohere.embed-multilingual-v3"
-    CHB_GOOGLE_API_KEY        = module.google_api_key_ssm_parameter.ssm_parameter_name
-    CHB_QUERY_TABLE_PREFIX    = local.prefix
+    CHB_PROVIDER           = "aws"
+    CHB_MODEL_ID           = "mistral.mistral-large-2402-v1:0"
+    CHB_EMBED_MODEL_ID     = "cohere.embed-multilingual-v3"
+    CHB_GOOGLE_API_KEY     = module.google_api_key_ssm_parameter.ssm_parameter_name
+    CHB_QUERY_TABLE_PREFIX = local.prefix
   }
 }
 
