811ba97cc7fa6869dab9b0f4dcd9ca5e66dd1c05
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2024-09-20 09:18:03 +0200

    Fix endpoint

--- apps/nextjs-website/src/lib/chatbot/chatbotFetch.ts
+++ apps/nextjs-website/src/lib/chatbot/chatbotFetch.ts
@@ -98,45 +98,6 @@ export const getQueries = (query: string) =>
     )
   );
 
-export const getCurrentSession = () =>
-  pipe(
-    R.ask<ChatbotEnv>(),
-    R.map(({ config: { CHATBOT_HOST: chatbotHost }, getAuthToken, fetch }) =>
-      pipe(
-        // handle any promise result
-        TE.tryCatch(() => getAuthToken(), E.toError),
-        TE.chainTaskK(
-          (authToken) => () =>
-            fetch(`${chatbotHost}/api/session`, {
-              method: 'GET',
-              headers: {
-                Authorization: `Bearer ${authToken}`,
-              },
-            })
-        ),
-        TE.chain((response) => {
-          if (response.status === 200) {
-            return TE.tryCatch(() => response.json(), E.toError);
-          } else {
-            return TE.left(makeError(response));
-          }
-        }),
-        TE.chainEitherK((json) =>
-          // decode the response with the given codec
-          pipe(
-            t.string.decode(json),
-            E.mapLeft((errors) => new Error(PR.failure(errors).join('\n')))
-          )
-        ),
-        TE.fold(
-          // eslint-disable-next-line functional/no-promise-reject
-          (errors) => () => Promise.reject(errors),
-          (result) => () => Promise.resolve(result)
-        )
-      )()
-    )
-  );
-
 export const patchFeedback = (feedback: boolean, queryId: string) =>
   pipe(
     R.ask<ChatbotEnv>(),
@@ -188,7 +149,7 @@ export const getSessions = (page: number, pageSize: number) =>
         TE.chainTaskK(
           (authToken) => () =>
             fetch(
-              `${chatbotHost}/api/sessions?${qs.stringify({
+              `${chatbotHost}/sessions?${qs.stringify({
                 page: page,
                 pageSize: pageSize,
               })}`,
