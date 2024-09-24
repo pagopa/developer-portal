366c8641d21437a254c089bfff4da693cbc8e04c
Sebastiano-Bertolin <56671015+Sebastiano-Bertolin@users.noreply.github.com>
2024-09-24 15:22:27 +0200

    [CAI-65] Chatbot History Layout (#1086)
    * Update chat input text
    
    * Update input text story
    
    * chore: add chandeset
    
    * ammend change
    
    * chore: add changeset
    
    * Fix send button position
    
    * add mobile style and remove badge
    
    * Fix build error
    
    * Update chatbot message
    
    * Update story
    
    * add changeset
    
    * Reduce timestamp font size
    
    * Cleanup imports
    
    * Update chat component with new layout
    
    * Improve componet layout and style
    
    * add changeset
    
    * Fix margins
    
    * Remove menu logic
    
    * Update layout
    
    * Update history button
    
    * Auto instant scroll to botton when chat opens
    
    * Fix chatobot sizes
    
    * Enter to send
    
    * Fix chatbot button border
    
    * Apply suggestions from code review
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Add date badge
    
    * Fix story
    
    * Update apps/nextjs-website/src/components/molecules/Chat/Chat.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Add mobile support
    
    * Apply suggestions from code review
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Remove imports
    
    * Add awaiting response animation
    
    * Improve dots generation
    
    * Fix as per review
    
    * Add chatbot feedback button atom
    
    * Add label
    
    * Apply suggestions from code review
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Add separator and feedback button behavuour
    
    * Add query idd
    
    * Add changeset
    
    * remove console log
    
    * Fix feedback management
    
    * Add copy to clipboard feature
    
    * add changeset
    
    * Fix translation
    
    * Fix first messagge md and messge margin
    
    * Fix input font size and send butto size and margin
    
    * Fix chatbot fab in mobile view
    
    * Apply suggestions from code review
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Fix lint error
    
    * Fix test
    
    * Apply suggestions from code review
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Add Chatbot History page
    
    * Add chatbot history list
    
    * Add changeset
    
    * Add Chatbot layout
    
    * chore: add changeset
    
    * Fix story
    
    * Apply suggestions from code review
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Create fixture file for chatbot Stories
    
    * Remove placeholder
    
    * Fix as per MR
    
    * Fix layout story
    
    * Fix story list item overflow
    
    * Hide menu voice if chatbot is not active
    
    * Add mobile view as per mock
    
    * Manage loading and empty history
    
    * add translation
    
    * Fix linting
    
    * Revert change
    
    * Add rerouting if chatbot not active
    
    ---------
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>

---
+++ apps/storybook-app/stories/organisms/ChatbotHistoryLayout.stories.tsx
@@ -0,0 +1,32 @@
+import { Decorator, Meta, StoryObj } from '@storybook/react';
+import { Box } from '@mui/material';
+import ChatbotHistoryLayout from 'nextjs-website/src/components/organisms/ChatbotHistoryLayout/ChatbotHistoryLayout';
+import { sessionsList } from '../fixtures/chatbotFixture';
+import React from 'react';
+
+const meta: Meta<typeof ChatbotHistoryLayout> = {
+  title: 'Organisms/ChatbotHistoryLayout',
+  component: ChatbotHistoryLayout,
+};
+
+const decorator: Decorator = (story) => (
+  <Box sx={{ padding: { xs: 0, md: '2rem' } }}>{story()}</Box>
+);
+
+export default meta;
+
+export const NewChatSession: StoryObj<typeof ChatbotHistoryLayout> = {
+  args: {
+    paginatedSessions: {
+      items: sessionsList,
+      page: 1,
+      pages: 5,
+      size: 44,
+    },
+    getSessionsByPage: (page: number) => {
+      console.log(page);
+      return null;
+    },
+  },
+  decorators: [decorator],
+};
