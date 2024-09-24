1634b99d3f82a9504d90fbfef31469c0ead1dd28
Marcello Bertoli <marcello.bertoli@uqido.com>
2024-09-24 17:51:49 +0200

    add url replace map single type

---
+++ apps/strapi-cms/src/api/url-replace-map/controllers/url-replace-map.ts
@@ -0,0 +1,7 @@
+/**
+ * url-replace-map controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::url-replace-map.url-replace-map');
---
+++ apps/strapi-cms/src/api/url-replace-map/routes/url-replace-map.ts
@@ -0,0 +1,7 @@
+/**
+ * url-replace-map router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::url-replace-map.url-replace-map');
