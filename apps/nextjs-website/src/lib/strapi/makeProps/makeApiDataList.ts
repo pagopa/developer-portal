bfb3a35831851992e39e3768ba643660109e9def
Marco Ponchia <marco.ponchia@uqido.com>
2024-09-25 10:43:53 +0200

    Update make functions

--- apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataList.ts
+++ apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataList.ts
@@ -1,18 +1,25 @@
 import { ApiPageProps } from '@/app/[productSlug]/api/[apiDataSlug]/page';
 import { StrapiApiDataList } from '../codecs/ApiDataListCodec';
+import { mergeProductWithStaticContent } from './makeProducts';
 
 export function makeApiDataListProps(
   apiDataList: StrapiApiDataList
 ): ReadonlyArray<ApiPageProps> {
   return apiDataList.data
     .filter((apiPage) => apiPage.attributes.apiRestDetail)
-    .map(({ attributes }) => ({
-      ...attributes,
-      apiDataSlug: attributes.apiRestDetail?.slug || '',
-      specURLs: attributes.apiRestDetail
-        ? [...attributes.apiRestDetail.specUrls.map((spec) => ({ ...spec }))]
-        : [],
-      specURLsName: attributes.title,
-      seo: attributes.seo,
-    }));
+    .map(({ attributes }) => {
+      const product = mergeProductWithStaticContent(
+        attributes.product.data.attributes
+      );
+      return {
+        ...attributes,
+        product,
+        apiDataSlug: attributes.apiRestDetail?.slug || '',
+        specURLs: attributes.apiRestDetail
+          ? [...attributes.apiRestDetail.specUrls.map((spec) => ({ ...spec }))]
+          : [],
+        specURLsName: attributes.title,
+        seo: attributes.seo,
+      };
+    });
 }
