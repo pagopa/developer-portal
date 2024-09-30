1e9b58dbee483175ce359b72c27e2781a2878ba6
Marco Ponchia <ponchia.marco1994@gmail.com>
2024-09-30 11:24:55 +0200

    [DEV-1920] Add structured data to pages (#1138)
    * Refactor guides codec files
    
    * Fix guides test
    
    * Add changeset
    
    * Fix guideList test
    
    * Fix guideListPages codec
    
    * Refactor ApiDataList and ApiDataListPages codecs
    
    * Fix file name
    
    * Refactor Solutions, SolutionListPage and CaseHistories codecs
    
    * Refactor caseHistories test using fixtures
    
    * Refactor Overviews codec
    
    * Refactor Homepage codec
    
    * Refactor QuickStartGuides codec
    
    * Move QuickStartGuidesCodec.ts in codecs folder
    
    * Refactor webinars codec
    
    * Add makeProps folder
    
    * Add changeset
    
    * Refactor SEO codec
    
    * Fix StrapiEnv file name
    
    * Refactor solution detail codec and SolutionTemplateProps
    
    * Add changeset
    
    * Rename StrapiApiData type to StrapiApiDataList
    
    * Rename const strapiSolutionsList to strapiSolutionListPage
    
    * Fix after merge with main branch
    
    * add schema-dts package
    
    * all codecs update
    
    * add new config vars
    
    * update default env with comment
    
    * add common structured data helper
    
    * add generator for structured data scripts
    
    * add structured data to homepage
    
    * codec update
    
    * product codecc
    
    * overview
    
    * guides
    
    * guides list
    
    * case histories
    
    * add structured data make functions
    
    * api data list pages
    
    * api data
    
    * add change set
    
    * webinar seo
    
    * tutorials
    
    * solutions
    
    * linter
    
    * quick start
    
    * overviews
    
    * api list
    
    * api detail page
    
    * guides, overviews, case histories
    
    * Update apps/nextjs-website/src/helpers/structuredData.helpers.ts
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * add use memo and fix breadcrumb
    
    * Remove useMemo
    
    * Update helpers
    
    * update structured data in homepage
    
    * add structured data to api page
    
    * Update apps/nextjs-website/src/lib/strapi/codecs/ProductCodec.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/lib/strapi/codecs/ProductCodec.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/lib/strapi/__tests__/fixtures/product.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/lib/strapi/__tests__/fixtures/solution.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update apps/nextjs-website/src/lib/strapi/__tests__/solutions.test.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Fix build error
    
    * Fix codecs
    
    * update fetches fix populate for seo
    
    * Fix changesets
    
    * update structured data helpers
    
    * add structured data to api
    
    * add structured data to guides
    
    * update structured data
    
    * update api and guide pages
    
    * update product layout
    
    * update helpers
    
    * Fix guide page
    
    * add structured data to overview
    
    * add structured data to quick start
    
    * add structured data to tutorials pages
    
    * add structured data to case history
    
    * add structured data to solutions
    
    * add structured data to webinars
    
    * update helper
    
    * add changeset
    
    * Update apps/nextjs-website/src/app/webinars/[webinarSlug]/page.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Add seo to structured data of webinar list page
    
    * remove old changeset
    
    ---------
    
    Co-authored-by: Marco Bottaro <marco.bottaro@uqido.com>
    Co-authored-by: t <t@t.t>
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    Co-authored-by: tommaso1 <tommasorosso1@gmail.com>

--- apps/nextjs-website/src/helpers/generateStructuredDataScripts.helpers.tsx
+++ apps/nextjs-website/src/helpers/generateStructuredDataScripts.helpers.tsx
@@ -1,23 +1,32 @@
 import { Thing } from 'schema-dts';
 import {
+  homeBreadCrumb,
   makeBreadcrumbList,
   makeWebPage,
   organizationWithContext,
   StructuredDataBreadcrumbList,
   StructuredDataWebPage,
 } from './structuredData.helpers';
+import { SEO } from '@/lib/types/seo';
 
 export function generateStructuredDataScripts(props: {
-  breadcrumbsItems: StructuredDataBreadcrumbList;
-  webPage: StructuredDataWebPage;
-  things?: Thing[];
+  breadcrumbsItems?: StructuredDataBreadcrumbList;
+  seo?: SEO;
+  things?: Array<Thing | undefined>;
 }) {
-  const { breadcrumbsItems, webPage, things } = props;
+  const { breadcrumbsItems, seo, things } = props;
+  const allBreadcrumbsItems = [homeBreadCrumb, ...(breadcrumbsItems || [])];
+  const webPage: StructuredDataWebPage = {
+    name: seo?.metaTitle,
+    description: seo?.metaDescription,
+    url: seo?.canonicalURL,
+    media: seo?.metaImage?.data?.attributes,
+  };
   const allThings: Thing[] = [
-    makeBreadcrumbList(breadcrumbsItems) as Thing,
+    makeBreadcrumbList(allBreadcrumbsItems) as Thing,
     makeWebPage(webPage) as Thing,
     organizationWithContext as Thing,
-    ...(things || []),
+    ...(things ? (things.filter(Boolean) as Thing[]) : []),
   ];
   return (
     <>
