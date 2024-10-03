8ab0ca06a34c4a7eac98e1dd379d22dde912c3e0
christian-calabrese <christian.calabrese@pagopa.it>
2024-10-03 12:07:19 +0200

    feat: change env variables

--- apps/infrastructure/src/modules/chatbot/data.tf
+++ apps/infrastructure/src/modules/chatbot/data.tf
@@ -28,6 +28,16 @@ data "aws_iam_policy_document" "lambda_s3_policy" {
   }
 }
 
+data "aws_iam_policy_document" "lambda_dynamodb_policy" {
+  statement {
+    sid       = "AllowDynamoDBItemOperations"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = [module.s3_bucket_llamaindex.s3_bucket_arn, module.s3_bucket_kb.s3_bucket_arn]
+  }
+}
+
+
 data "aws_caller_identity" "current" {}
 
 data "aws_availability_zones" "available" {
--- apps/infrastructure/src/modules/chatbot/lambda_chatbot.tf
+++ apps/infrastructure/src/modules/chatbot/lambda_chatbot.tf
@@ -15,9 +15,10 @@ locals {
     # Be extremely careful when changing the provider
     # both the generation and the embedding models would be changed
     # embeddings size change would break the application and requires reindexing
-    CHB_PROVIDER           = "aws"
-    CHB_MODEL_ID           = "mistral.mistral-large-2402-v1:0"
-    CHB_EMBED_MODEL_ID     = "cohere.embed-multilingual-v3"
+    CHB_PROVIDER           = "google"
+    CHB_MODEL_ID           = "models/gemini-1.5-flash"
+    CHB_EMBED_MODEL_ID     = "models/text-embedding-004"
+    CHB_REDIS_INDEX_NAME   = "gemini-index"
     CHB_GOOGLE_API_KEY     = module.google_api_key_ssm_parameter.ssm_parameter_name
     CHB_QUERY_TABLE_PREFIX = local.prefix
   }
