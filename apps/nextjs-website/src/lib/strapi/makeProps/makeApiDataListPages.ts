6d2f8b8b271a91056ed9c648ea8793b46be4748b
Marco Ponchia <ponchia.marco1994@gmail.com>
2024-10-04 17:44:47 +0200

    [DEV-1932] Refactor api data (#1164)
    * Update api data codecs
    
    * Fix test
    
    * Update make functions
    
    * update fetches
    
    * Update page
    
    * add changeset
    
    * Fix getApiDataParams
    
    * Update .changeset/proud-humans-search.md
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Fix after review
    
    * eslint
    
    ---------
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    Co-authored-by: Marcello Bertoli <marcello.bertoli@uqido.com>
    Co-authored-by: MarBert <41899883+MarBert@users.noreply.github.com>
    Co-authored-by: t <t@t.t>

--- apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataListPages.ts
+++ apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataListPages.ts
@@ -1,45 +1,50 @@
 import { ApiDataListTemplateProps } from '@/components/templates/ApiDataListTemplate/ApiDataListTemplate';
 import { StrapiApiDataListPages } from '../codecs/ApiDataListPagesCodec';
+import { mergeProductWithStaticContent } from './makeProducts';
 
 export function makeApiDataListPagesProps(
   apiDataListPages: StrapiApiDataListPages
 ): ReadonlyArray<ApiDataListTemplateProps> {
-  return apiDataListPages.data.map(({ attributes }) => ({
-    ...attributes,
-    hero: {
-      title: attributes.title,
-      subtitle: attributes.description || '',
-    },
-    product: attributes.product.data?.attributes || {
-      name: '',
-      slug: '',
-      shortName: '',
-    },
-    cards: attributes.apiData.data.map((item) => ({
-      target: (item.attributes.apiSoapUrl ? '_blank' : '_self') as
-        | '_blank'
-        | '_self',
-      tags: [
-        {
-          label: item.attributes.apiSoapUrl ? 'SOAP' : 'REST',
-        },
-      ],
-      title: item?.attributes?.title,
-      text: item?.attributes?.description || '',
-      icon: item?.attributes?.icon?.data?.attributes.url || '',
-      externalUrl: !!item.attributes.apiSoapUrl,
-      href:
-        item.attributes.apiSoapUrl ||
-        `/${attributes.product.data?.attributes.slug}/api/${item.attributes.apiRestDetail?.slug}`,
-    })),
-    bannerLinks: [
-      ...attributes.bannerLinks.map((bannerLink) => ({
-        content: bannerLink.content,
-        icon: bannerLink.icon.data.attributes,
-        theme: bannerLink.theme || 'dark',
-        title: bannerLink.title || '',
+  return apiDataListPages.data.map(({ attributes }) => {
+    const product = mergeProductWithStaticContent(
+      attributes.product.data.attributes
+    );
+    return {
+      ...attributes,
+      hero: {
+        title: attributes.title,
+        subtitle: attributes.description || '',
+      },
+      product,
+      apiRestDetailSlugs: attributes.apiData.data
+        .map(({ attributes }) => attributes.apiRestDetail?.slug)
+        .filter(Boolean) as readonly string[],
+      cards: attributes.apiData.data.map((item) => ({
+        target: (item.attributes.apiSoapUrl ? '_blank' : '_self') as
+          | '_blank'
+          | '_self',
+        tags: [
+          {
+            label: item.attributes.apiSoapUrl ? 'SOAP' : 'REST',
+          },
+        ],
+        title: item?.attributes?.title,
+        text: item?.attributes?.description || '',
+        icon: item?.attributes?.icon?.data?.attributes.url || '',
+        externalUrl: !!item.attributes.apiSoapUrl,
+        href:
+          item.attributes.apiSoapUrl ||
+          `/${attributes.product.data?.attributes.slug}/api/${item.attributes.apiRestDetail?.slug}`,
       })),
-    ],
-    seo: attributes.seo,
-  }));
+      bannerLinks: [
+        ...attributes.bannerLinks.map((bannerLink) => ({
+          content: bannerLink.content,
+          icon: bannerLink.icon.data.attributes,
+          theme: bannerLink.theme || 'dark',
+          title: bannerLink.title || '',
+        })),
+      ],
+      seo: attributes.seo,
+    };
+  });
 }
