9736ab4ae7e55bbc74c9e424273f0b4ca2d66d7b
t <t@t.t>
2024-10-08 11:43:20 +0200

    rename type

--- apps/nextjs-website/src/lib/strapi/makeProps/makeUrlReplaceMap.ts
+++ apps/nextjs-website/src/lib/strapi/makeProps/makeUrlReplaceMap.ts
@@ -1,10 +1,10 @@
 import { StrapiUrlReplaceMap } from '../codecs/UrlReplaceMapCodec';
 
-export type UrlReplacesMap = Record<string, string>;
+export type UrlReplaceMap = Record<string, string>;
 
 export function makeUrlReplaceMap(
   urlReplacemap: StrapiUrlReplaceMap
-): UrlReplacesMap {
+): UrlReplaceMap {
   const map = urlReplacemap.data.attributes.urlToGuide.reduce((map, obj) => {
     return {
       ...map,
@@ -14,6 +14,6 @@ export function makeUrlReplaceMap(
         obj.version ? `/${obj.version}` : ''
       }`,
     };
-  }, {} as UrlReplacesMap);
+  }, {} as UrlReplaceMap);
   return map;
 }
