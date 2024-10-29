2aef95433e405aed10a14bb1ac6db81e0a47d087
Marco Domenico Cirillo <59966344+mdciri@users.noreply.github.com>
2024-10-29 10:39:58 +0100

    [CAI-222] - Mistral (#1212)
    * Update modules
    
    * Update config
    
    * Update modules with aws guardrail
    
    * Update modules
    
    * Update poetry.lock
    
    * Update modules
    
    * Update datetime including time zone and time over the date
    
    * Update vector_database
    
    * Update modules
    
    * Restore .gitmodules

--- apps/chatbot/src/modules/models.py
+++ apps/chatbot/src/modules/models.py
@@ -25,14 +25,14 @@ assert PROVIDER in ["aws", "google"]
 GOOGLE_API_KEY = get_ssm_parameter(name=os.getenv("CHB_GOOGLE_API_KEY"))
 AWS_ACCESS_KEY_ID = os.getenv("CHB_AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("CHB_AWS_SECRET_ACCESS_KEY")
-CHB_AWS_DEFAULT_REGION = os.getenv("CHB_AWS_DEFAULT_REGION")
+AWS_BEDROCK_REGION = os.getenv("CHB_AWS_BEDROCK_REGION")
 AWS_GUARDRAIL_ID = os.getenv("CHB_AWS_GUARDRAIL_ID")
 AWS_GUARDRAIL_VERSION = os.getenv("CHB_AWS_GUARDRAIL_VERSION")
 
-MODEL_ID = os.getenv("CHB_MODEL_ID", "models/gemini-1.5-flash")
-MODEL_TEMPERATURE = os.getenv("CHB_MODEL_TEMPERATURE", "0.5")
-MODEL_MAXTOKENS = os.getenv("CHB_MODEL_MAXTOKENS", "512")
-EMBED_MODEL_ID = os.getenv("CHB_EMBED_MODEL_ID", "models/text-embedding-004")
+MODEL_ID = os.getenv("CHB_MODEL_ID")
+MODEL_TEMPERATURE = os.getenv("CHB_MODEL_TEMPERATURE", "0.3")
+MODEL_MAXTOKENS = os.getenv("CHB_MODEL_MAXTOKENS", "768")
+EMBED_MODEL_ID = os.getenv("CHB_EMBED_MODEL_ID")
 
 
 def get_llm():
@@ -47,12 +47,12 @@ def get_llm():
 
             def handle(self, event) -> None:
                 """Logic for handling event."""
-                if isinstance(event, (LLMCompletionEndEvent, LLMChatEndEvent,)):
-                    logging.info(f"RequestID: {event.response.raw["ResponseMetadata"]["RequestId"]}")
-                    logging.info(f"Stop Reason: {event.response.raw["stopReason"]}")
-                    logging.info(f"Input Tokens: {event.response.raw["usage"]["inputTokens"]}")
-                    logging.info(f"Output Tokens: {event.response.raw["usage"]["outputTokens"]}")
-                    logging.info(f"Latency (ms): {event.response.raw["metrics"]["latencyMs"]}")
+                if isinstance(event, (LLMCompletionEndEvent, LLMChatEndEvent)):
+                    logging.info(f"[{MODEL_ID}] Bedrock request id: {event.response.raw["ResponseMetadata"]["RequestId"]}")
+                    logging.info(f"[{MODEL_ID}] Stop Reason: {event.response.raw["stopReason"]}")
+                    logging.info(f"[{MODEL_ID}] Input Tokens: {event.response.raw["usage"]["inputTokens"]}")
+                    logging.info(f"[{MODEL_ID}] Output Tokens: {event.response.raw["usage"]["outputTokens"]}")
+                    logging.info(f"[{MODEL_ID}] Latency (ms): {event.response.raw["metrics"]["latencyMs"]}")
 
         root_dispatcher = get_dispatcher()
         root_dispatcher.add_event_handler(ModelEventHandler())
@@ -63,7 +63,7 @@ def get_llm():
             max_tokens=int(MODEL_MAXTOKENS),
             aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            region_name=CHB_AWS_DEFAULT_REGION
+            region_name=AWS_BEDROCK_REGION
         )
 
     else:
@@ -80,7 +80,7 @@ def get_llm():
             api_key=GOOGLE_API_KEY,
         )
 
-    logging.info(f"{MODEL_ID} LLM loaded successfully!")
+    logging.info(f"[models.py - get_llm] {MODEL_ID} LLM loaded successfully!")
 
     return llm
 
@@ -92,13 +92,13 @@ def get_embed_model():
             model_name = EMBED_MODEL_ID,
             aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            region_name=CHB_AWS_DEFAULT_REGION
+            region_name=AWS_BEDROCK_REGION
         )
     else:
         embed_model = GeminiEmbedding(
             api_key=GOOGLE_API_KEY,
             model_name=EMBED_MODEL_ID,
         )
-    logging.info(f"{EMBED_MODEL_ID} embegging model loaded successfully!")
+    logging.info(f"[models.py - get_embed_model] {EMBED_MODEL_ID} embegging model loaded successfully!")
 
     return embed_model
