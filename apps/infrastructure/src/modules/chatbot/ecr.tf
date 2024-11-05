378fe4d603cf8415179c187411c1cbd47e182db5
Devis Battisti <devis.battisti@uqido.com>
2024-11-05 17:26:47 +0100

    fix(chatbot): dockerfile

--- apps/infrastructure/src/modules/chatbot/ecr.tf
+++ apps/infrastructure/src/modules/chatbot/ecr.tf
@@ -1,4 +1,4 @@
-## ECR Container Registry for CMS Strapi
+## ECR Container Registry for Chatbot
 module "ecr" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-ecr.git?ref=9f4b587846551110b0db199ea5599f016570fefe" # v1.6.0
 
