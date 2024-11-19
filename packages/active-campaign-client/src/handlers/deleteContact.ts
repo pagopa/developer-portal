20e6f87261b99c02470af4d6cd7443dce84a8f3c
t <t@t.t>
2024-11-19 12:36:05 +0100

    Add delete and update contact lambdas + test

---
+++ packages/active-campaign-client/src/handlers/deleteContact.ts
@@ -0,0 +1,36 @@
+import { APIGatewayProxyResult, SQSEvent } from 'aws-lambda';
+import { acClient } from '../activeCampaignClient';
+
+export async function handler(event: {
+  readonly Records: SQSEvent['Records'];
+}): Promise<APIGatewayProxyResult> {
+  try {
+    const firstMessage = event.Records[0] ?? { body: '{}' };
+    // Parse request body
+    const { username } = JSON.parse(firstMessage.body);
+    console.log('email:', username);
+
+    const contactId = await acClient.getContactByEmail(username);
+    console.log('contactId:', contactId);
+
+    if (!contactId) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'Contact not found' }),
+      };
+    }
+
+    const response = await acClient.deleteContact(contactId);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error' }),
+    };
+  }
+}
