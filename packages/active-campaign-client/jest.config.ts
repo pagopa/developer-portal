ad4fb3d4ae50e53e8e4760ee43335734532b12c4
Marco Ponchia <ponchia.marco1994@gmail.com>
2024-11-21 15:00:03 +0100

    Update packages/active-campaign-client/jest.config.ts

--- packages/active-campaign-client/jest.config.ts
+++ packages/active-campaign-client/jest.config.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 
-// Load environment variables from .env.test file
+// Load environment variables from .env file
 dotenv.config({ path: '.env' });
 
 module.exports = {
