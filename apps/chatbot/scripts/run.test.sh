b3e82e1f7fc94a7f13b7725c3b19f439c39dbfe8
Devis Battisti <devis.battisti@uqido.com>
2024-11-25 10:13:06 +0100

    script run order

--- apps/chatbot/scripts/run.test.sh
+++ apps/chatbot/scripts/run.test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo '-=-=-=-=-=-=-= create redis index =-=-=-=-=-=-=-=-'
-./scripts/create_redis_index.sh
-
 echo '-=-=-=-=-=-=-=-=-= init DynamoDB -==-=-=-=-=-=-=-=-'
 ./scripts/dynamodb-init.sh
+
+echo '-=-=-=-=-=-=-= create redis index =-=-=-=-=-=-=-=-'
+./scripts/create_redis_index.sh
 
 echo '-=-=-=-=-=-=-=-=-=- run pytest -=-==-=-=-=-=-=-=-=-'
 pytest  -p no:warnings
