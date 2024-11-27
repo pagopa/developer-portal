3140e891ece77e7a614e24db648223313e12b12f
tommaso1 <tommasorosso1@gmail.com>
2024-11-27 17:22:25 +0100

    Update packages/active-campaign-client/src/__tests__/handlers/manageListSubscription.test.ts
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

--- packages/active-campaign-client/src/__tests__/handlers/manageListSubscription.test.ts
+++ packages/active-campaign-client/src/__tests__/handlers/manageListSubscription.test.ts
@@ -1,7 +1,7 @@
 import { manageListSubscription } from '../../handlers/manageListSubscription';
 
 // remove .skip to run the test, be aware it does real API calls
-describe('manage list subscription', () => {
+describe.skip('manage list subscription', () => {
   const cognitoUserId = '66ae52a0-f051-7080-04a1-465b3a4f44cc';
   const listName = 'test-webinar-1732097286071';
 
