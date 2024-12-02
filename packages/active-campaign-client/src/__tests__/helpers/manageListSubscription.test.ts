409655165daedda251e07c3cc049c60107404341
t <t@t.t>
2024-12-02 16:06:38 +0100

    pr comments

--- packages/active-campaign-client/src/__tests__/helpers/manageListSubscription.test.ts
+++ packages/active-campaign-client/src/__tests__/helpers/manageListSubscription.test.ts
@@ -1,26 +1,21 @@
 // remove .skip to run the test, be aware it does real API calls
-import { manageListSubscription } from '../../helpers/manageListSubscription';
+import {
+  addContactToList,
+  removeContactToList,
+} from '../../helpers/manageListSubscription';
 
 describe.skip('manage list subscription', () => {
   const cognitoUserId = process.env.COGNITO_USER_ID || '';
   const listName = process.env.LIST_NAME || '';
 
   it('should subscribe the contact to the list', async () => {
-    const result = await manageListSubscription(
-      cognitoUserId,
-      listName,
-      'subscribe'
-    );
+    const result = await addContactToList(cognitoUserId, listName);
 
     expect(result.statusCode).toBe(200);
   });
 
   it('should unsubscribe the contact from the list', async () => {
-    const result = await manageListSubscription(
-      cognitoUserId,
-      listName,
-      'unsubscribe'
-    );
+    const result = await removeContactToList(cognitoUserId, listName);
     expect(result.statusCode).toBe(200);
   });
 });
--- packages/active-campaign-client/src/helpers/manageListSubscription.ts
+++ packages/active-campaign-client/src/helpers/manageListSubscription.ts
@@ -1,13 +1,12 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
 import { acClient } from '../clients/activeCampaignClient';
 
-export async function manageListSubscription(
-  cognitoUserId: string,
-  listName: string,
-  action: 'subscribe' | 'unsubscribe'
+export async function addContactToList(
+  cognitoId: string,
+  listName: string
 ): Promise<APIGatewayProxyResult> {
   try {
-    const contactId = await acClient.getContactByCognitoId(cognitoUserId);
+    const contactId = await acClient.getContactByCognitoId(cognitoId);
     if (!contactId) {
       return {
         statusCode: 404,
@@ -17,19 +16,40 @@ export async function manageListSubscription(
 
     const listId = await acClient.getListIdByName(listName);
 
-    if (action == 'subscribe') {
-      const response = await acClient.addContactToList(contactId, listId);
+    const response = await acClient.addContactToList(contactId, listId);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error' }),
+    };
+  }
+}
+
+export async function removeContactToList(
+  cognitoId: string,
+  listName: string
+): Promise<APIGatewayProxyResult> {
+  try {
+    const contactId = await acClient.getContactByCognitoId(cognitoId);
+    if (!contactId) {
       return {
-        statusCode: 200,
-        body: JSON.stringify(response),
-      };
-    } else {
-      const response = await acClient.removeContactFromList(contactId, listId);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(response),
+        statusCode: 404,
+        body: JSON.stringify({ message: 'Contact not found' }),
       };
     }
+
+    const listId = await acClient.getListIdByName(listName);
+
+    const response = await acClient.removeContactFromList(contactId, listId);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
   } catch (error) {
     console.error('Error:', error);
     return {
