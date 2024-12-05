7cdede16257cc70a52348537b1c06e874e411c51
christian-calabrese <christian.calabrese@pagopa.it>
2024-12-05 18:49:50 +0100

    fix: adding https protocol in front of ac_sender_url

--- apps/infrastructure/src/modules/cms/ecs.tf
+++ apps/infrastructure/src/modules/cms/ecs.tf
@@ -50,7 +50,7 @@ resource "aws_ecs_task_definition" "cms_task_def" {
       ac_integration_is_enabled  = var.ac_integration_is_enabled
       ac_base_url                = var.ac_integration_is_enabled ? var.ac_base_url_param : module.secret_cms_transfer_token_salt.ssm_parameter_arn
       ac_api_key                 = var.ac_integration_is_enabled ? var.ac_api_key_param : module.secret_cms_transfer_token_salt.ssm_parameter_arn
-      ac_sender_url              = var.dns_domain_name
+      ac_sender_url              = "https://${var.dns_domain_name}"
   })
 }
 
