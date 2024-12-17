c59159897dd66547bcdc561d524eb0f6ffb705f7
christian-calabrese <christian.calabrese@pagopa.it>
2024-12-17 17:27:03 +0100

    chore: ran terraform fmt

--- apps/infrastructure/src/modules/active_campaign/cloudwatch_metrics_alarms.tf
+++ apps/infrastructure/src/modules/active_campaign/cloudwatch_metrics_alarms.tf
@@ -1,37 +1,37 @@
 # CloudWatch Metrics and Alarms for Active Campaign Sync Resources
 
 resource "aws_cloudwatch_metric_alarm" "pipe_failed" {
-  alarm_name                = "${local.prefix}-webinar-subs-pipe-errors"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 1
-  period                    = 60
-  statistic                 = "Sum"
-  threshold                 = 0
-  metric_name               = "ExecutionFailed"
-  namespace                 = "AWS/EventBridge/Pipes"
+  alarm_name          = "${local.prefix}-webinar-subs-pipe-errors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 0
+  metric_name         = "ExecutionFailed"
+  namespace           = "AWS/EventBridge/Pipes"
   dimensions = {
     PipeName = aws_pipes_pipe.dynamodb_to_sqs.name
   }
   alarm_description         = "This metric monitors the webinar subscriptions eventbridge pipe failures"
   insufficient_data_actions = []
-  alarm_actions       = [aws_sns_topic.alerts.arn]
+  alarm_actions             = [aws_sns_topic.alerts.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "dlq" {
-  alarm_name                = "${local.prefix}-sqs-messages-in-dlq"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 1
-  period                    = 60
-  statistic                 = "Sum"
-  threshold                 = 0
-  metric_name               = "ApproximateNumberOfMessagesVisible"
-  namespace                 = "AWS/SQS"
+  alarm_name          = "${local.prefix}-sqs-messages-in-dlq"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 0
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
   dimensions = {
     QueueName = aws_sqs_queue.fifo_dlq_queue.name
   }
   alarm_description         = "This metric monitors messages put in the dead letter queue"
   insufficient_data_actions = []
-  alarm_actions       = [aws_sns_topic.alerts.arn]
+  alarm_actions             = [aws_sns_topic.alerts.arn]
 }
 
 # SNS Topic for Alarms
