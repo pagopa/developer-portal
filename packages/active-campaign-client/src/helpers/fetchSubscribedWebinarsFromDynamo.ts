4e4933de9df2786f2a3706ee42e27b71b10e050b
tommaso1 <tommasorosso1@gmail.com>
2024-12-19 14:56:13 +0100

    [DEV-1614] Add bulk import of contacts (#1273)
    * sync user wip
    
    * sync user
    
    * changeset
    
    * Update .changeset/cuddly-cats-retire.md
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/src/index.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/src/index.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/src/helpers/resyncUser.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * pr comments
    
    * bulk add contact
    
    * bulk add contact
    
    * resync user
    
    * changeset
    
    * Update packages/active-campaign-client/src/clients/activeCampaignClient.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/src/handlers/resyncUserHandler.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * pr changes
    
    * Update packages/active-campaign-client/src/helpers/fetchSubscribedWebinarsFromDynamo.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/.env.example
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update packages/active-campaign-client/src/handlers/resyncUserHandler.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * pr changes
    
    ---------
    
    Co-authored-by: t <t@t.t>
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

---
+++ packages/active-campaign-client/src/helpers/fetchSubscribedWebinarsFromDynamo.ts
@@ -0,0 +1,29 @@
+import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
+import { APIGatewayProxyResult } from 'aws-lambda';
+
+export async function fetchSubscribedWebinarsFromDynamo(
+  username: string
+): Promise<APIGatewayProxyResult> {
+  try {
+    const dynamoClient = new DynamoDBClient({ region: process.env.AWS_REGION });
+    const command = new QueryCommand({
+      TableName: process.env.DYNAMO_WEBINARS_TABLE_NAME,
+      KeyConditionExpression: 'username = :username',
+      ExpressionAttributeValues: {
+        ':username': { S: username },
+      },
+    });
+
+    const response = await dynamoClient.send(command);
+    console.log('getWebinarSubscriptions', response);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.Items),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error' }),
+    };
+  }
+}
