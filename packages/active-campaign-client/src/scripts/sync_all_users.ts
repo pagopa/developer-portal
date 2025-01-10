7deef83b511d1ab0cfd8c5228ebe97e4914f515e
Marco Ponchia <ponchia.marco1994@gmail.com>
2025-01-10 10:04:10 +0100

    [DEV-2073] Add sync users script using tsnode (#1289)
    * sync user wip
    
    * sync user
    
    * changeset
    
    * Update .changeset/cuddly-cats-retire.md
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/src/index.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/src/index.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/src/helpers/resyncUser.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * pr comments
    
    * bulk add contact
    
    * bulk add contact
    
    * resync user
    
    * changeset
    
    * Update packages/active-campaign-client/src/clients/activeCampaignClient.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/src/handlers/resyncUserHandler.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * pr changes
    
    * Update packages/active-campaign-client/src/helpers/fetchSubscribedWebinarsFromDynamo.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * Update packages/active-campaign-client/.env.example
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update packages/active-campaign-client/src/handlers/resyncUserHandler.ts
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    * pr changes
    
    * add makeContactPayload
    
    * Add types
    
    * refactor AC client
    
    * add getListByName return type
    
    * refactor getListIdByName and add error manage to sqsQueueHandler
    
    * Add addOrUpdateContact helper
    
    * add getNewWebinarsAndUnsubsriptionLists
    
    * add addArrayOfListToContact and removeArrayOfListFromContact helpers
    
    * Update resync user handlers
    
    * Fix manageError
    
    * Fix after merge
    
    * add changeset
    
    * Fix list ac client call
    
    * Refactor env var for test
    
    * fix make payload
    
    * fix resync function
    
    * add sync user script
    
    * add changeset
    
    * add env
    
    * import users
    
    * Fix env
    
    * update script and configuration
    
    * Add changeset
    
    * update after review
    
    * Manage error during webinar fetching
    
    * remove try catch
    
    * Manage AC POST contactList error
    
    * Remove unused type
    
    ---------
    
    Co-authored-by: t <t@t.t>
    Co-authored-by: tommaso1 <tommasorosso1@gmail.com>
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

---
+++ packages/active-campaign-client/src/scripts/sync_all_users.ts
@@ -0,0 +1,150 @@
+// To run this script you need to have proper environment variables set up.
+// Must be logged in to AWS CLI and have the proper permissions to access the Cognito User Pool and DynamoDB tables.
+// Then run 'npm run sync-all-users -w active-campaign-client'
+
+/* eslint-disable */
+import { ActiveCampaignClient } from "../clients/activeCampaignClient";
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
+import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const activeCampaignClient = new ActiveCampaignClient(
+  process.env.AC_BASE_URL!,
+  process.env.AC_API_KEY!
+);
+
+const userPoolId = process.env.COGNITO_USER_POOL_ID!;
+
+// Initialize Cognito client
+const cognito = new CognitoIdentityServiceProvider({
+  region: process.env.AWS_REGION!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+  }
+});
+
+export async function getSubscribedWebinars(
+  username: string
+): Promise<any[]> {
+  try {
+    const dynamoClient = new DynamoDBClient({ region: process.env.AWS_REGION });
+    const command = new QueryCommand({
+      TableName: process.env.DYNAMO_WEBINARS_TABLE_NAME,
+      KeyConditionExpression: 'username = :username',
+      ExpressionAttributeValues: {
+        ':username': { S: username },
+      },
+    });
+
+    const response = await dynamoClient.send(command);
+    return response.Items as any[];
+  } catch (error) {
+    console.error('Error querying items by username:', error);
+    return [];
+  }
+}
+
+// Load all users from Cognito
+async function listAllUsers() {
+  try {
+    const allUsers = [];
+    let paginationToken = undefined;
+
+    do {
+      const params: any = {
+        UserPoolId: userPoolId,
+        PaginationToken: paginationToken
+      };
+
+      const response: any = await cognito.listUsers(params).promise();
+      allUsers.push(...response.Users);
+      paginationToken = response.PaginationToken;
+    } while (paginationToken);
+
+    return allUsers;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return [];
+  }
+}
+
+async function main() {
+  const users = await listAllUsers();
+
+  console.log(process.env.DYNAMO_WEBINARS_TABLE_NAME);
+
+  const usersAndWebinars = [];
+
+  for (const user of users) {
+    const subscribedWebinars = await getSubscribedWebinars(user.Username);
+    usersAndWebinars.push({
+      username: user,
+      subscribedWebinars: subscribedWebinars.map((webinar: any) => webinar?.webinarId?.S).filter(Boolean)
+    });
+  }
+
+  const subscribedWebinarNames = usersAndWebinars.flatMap((userAndWebinars) => userAndWebinars.subscribedWebinars);
+
+  const allLists: any = await activeCampaignClient.getLists();
+
+  const webinarIdByName = allLists.lists.reduce((acc: any, list: any) => {
+    acc[list.name] = Number(list.id);
+    return acc;
+  }, {});
+
+  console.log(webinarIdByName);
+
+  const emails = new Set();
+  // remove duplicates on emails
+  const uniqueUsersAndWebinars = usersAndWebinars.filter((userAndWebinars) => {
+    const email = userAndWebinars.username.Attributes.find((attr: any) => attr.Name === 'email')?.Value;
+    if (emails.has(email)) {
+      return false;
+    }
+    emails.add(email);
+    return true;
+  });
+
+  const usersToImportLimit = !!process.env.USERS_TO_IMPORT_LIMIT && parseInt(process.env.USERS_TO_IMPORT_LIMIT);
+  const limitedUniqueUsersAndWebinars = usersToImportLimit ? uniqueUsersAndWebinars
+    .filter((userAndWebinars) => userAndWebinars.subscribedWebinars.length > 0)
+    .slice(0, usersToImportLimit)
+    : uniqueUsersAndWebinars;
+
+  const acPayload = limitedUniqueUsersAndWebinars.map((userAndWebinars, index) => ({
+    contact: {
+      email: userAndWebinars.username.Attributes.find((attr: any) => attr.Name === 'email')?.Value,
+      firstName: userAndWebinars.username.Attributes.find((attr: any) => attr.Name === 'given_name')?.Value,
+      lastName: userAndWebinars.username.Attributes.find((attr: any) => attr.Name === 'family_name')?.Value,
+      phone: `cognito:${userAndWebinars.username.Username}`,
+      fieldValues: [
+        {
+          field: '2',
+          value: userAndWebinars.username.Attributes.find((attr: any) => attr.Name === 'custom:company_type')?.Value,
+        },
+        {
+          field: '1',
+          value: userAndWebinars.username.Attributes.find((attr: any) => attr.Name === 'custom:job_role')?.Value,
+        },
+        {
+          field: '3',
+          value: userAndWebinars.username.Attributes.find((attr: any) => attr.Name === 'custom:mailinglist_accepted')?.Value === 'true' ? 'TRUE' : 'FALSE',
+        },
+      ],
+    },
+    listIds: userAndWebinars.subscribedWebinars.map((webinar: any) => webinarIdByName[webinar]).filter(Boolean),
+  }));
+
+  const response = await activeCampaignClient.bulkAddContactToList(acPayload);
+
+  console.log(response);
+}
+
+// Execute the main function
+main().catch(error => {
+  console.error('Error executing script:', error);
+  process.exit(1);
+});
