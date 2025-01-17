f05543dde4645e9f1e021200439aa58d6b16fcdf
christian-calabrese <christian.calabrese@pagopa.it>
2025-01-17 12:48:03 +0100

    feat: implemented cloudfront and acm

---
+++ apps/infrastructure/src/modules/docs_redirect/acm.tf
@@ -0,0 +1,12 @@
+resource "aws_acm_certificate" "redirect" {
+  domain_name               = var.dns_domain_name
+  validation_method         = "DNS"
+  subject_alternative_names = [var.domain_to_redirect.from]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  # TLS certificate generated in us-east because it is related to the CDN which is a global resource
+  provider = aws.us-east-1
+}
