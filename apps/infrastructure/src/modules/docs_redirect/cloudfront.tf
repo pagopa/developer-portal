2290b731afbacff8f735f48c272d142723e549b9
christian-calabrese <christian.calabrese@pagopa.it>
2025-01-17 15:37:37 +0100

    chore: ran terraform fmt

--- apps/infrastructure/src/modules/docs_redirect/cloudfront.tf
+++ apps/infrastructure/src/modules/docs_redirect/cloudfront.tf
@@ -15,10 +15,10 @@ resource "aws_cloudfront_distribution" "redirect" {
     origin_id   = "hosting.gitbook.io"
 
     custom_origin_config {
-      http_port = 80
-      https_port = 443
+      http_port              = 80
+      https_port             = 443
       origin_protocol_policy = "match-viewer"
-      origin_ssl_protocols = ["TLSv1.1", "TLSv1.2"]
+      origin_ssl_protocols   = ["TLSv1.1", "TLSv1.2"]
     }
   }
 
@@ -27,17 +27,17 @@ resource "aws_cloudfront_distribution" "redirect" {
   comment             = "CloudFront distribution for ${var.domain_to_redirect.from} redirect."
   default_root_object = "index.html"
 
-  aliases = var.use_custom_certificate && var.domain_to_redirect.from != "" ? [ var.domain_to_redirect.from ] : []
+  aliases = var.use_custom_certificate && var.domain_to_redirect.from != "" ? [var.domain_to_redirect.from] : []
 
   default_cache_behavior {
     # HTTPS requests we permit the distribution to serve
-    allowed_methods            = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
-    cached_methods             = ["GET", "HEAD"]
-    target_origin_id           = "hosting.gitbook.io"
+    allowed_methods  = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "hosting.gitbook.io"
 
     cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # Managed-CachingDisabled
     origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3" # Managed-AllViewer
-    
+
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0     # min time for objects to live in the distribution cache
     default_ttl            = 3600  # default time for objects to live in the distribution cache
