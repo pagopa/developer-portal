7b41d44fe465ba7750f2f4ca7dabc6ab572ba08b
christian-calabrese <christian.calabrese@pagopa.it>
2025-01-20 12:17:57 +0100

    feat: add private dns zone

--- apps/infrastructure/src/modules/docs_redirect/acm.tf
+++ apps/infrastructure/src/modules/docs_redirect/acm.tf
@@ -1,11 +1,13 @@
-resource "aws_acm_certificate" "redirect" {
-  domain_name               = var.domain_to_redirect.from
-  validation_method         = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
+module "redirect_certificate" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-acm.git?ref=8d0b22f1f242a1b36e29b8cb38aaeac9b887500d" # v5.0.0
+  provider = {
+    aws = aws.us-east-1
   }
 
-  # TLS certificate generated in us-east because it is related to the CDN which is a global resource
-  provider = aws.us-east-1
+  domain_name = var.domain_to_redirect.from
+  zone_id     = aws_route53_zone.docs_pagopa_it.zone_id
+
+  wait_for_validation = false # https://github.com/terraform-aws-modules/terraform-aws-acm/blob/8d0b22f1f242a1b36e29b8cb38aaeac9b887500d/README.md?plain=1#L174
+  validation_method   = "DNS"
+  dns_ttl             = 3600
 }
--- apps/infrastructure/src/modules/docs_redirect/route53.tf
+++ apps/infrastructure/src/modules/docs_redirect/route53.tf
@@ -1,3 +1,15 @@
 resource "aws_route53_zone" "docs_pagopa_it" {
   name = "docs.pagopa.it"
 }
+
+resource "aws_route53_record" "www_website" {
+  zone_id = aws_route53_zone.docs_pagopa_it.zone_id
+  name    = var.domain_to_redirect.from
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.redirect.domain_name
+    zone_id                = aws_cloudfront_distribution.redirect.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
