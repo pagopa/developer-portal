62f3ca00693792cf1f486768525066deb75e6bdf
Marcello Bertoli <marcello.bertoli@uqido.com>
2025-01-22 14:39:53 +0100

    add default value to maotmo src

--- apps/nextjs-website/src/config.ts
+++ apps/nextjs-website/src/config.ts
@@ -8,7 +8,9 @@ See BrowserConfig.ts and BrowserEnv.ts as examples.
 // TODO: Add environment parser
 export const docsPath = process.env.PATH_TO_GITBOOK_DOCS;
 export const cookieDomainScript = process.env.NEXT_PUBLIC_COOKIE_DOMAIN_SCRIPT;
-export const matomoScriptSrc = process.env.NEXT_PUBLIC_MATOMO_SCRIPT_SRC;
+export const matomoScriptSrc =
+  process.env.NEXT_PUBLIC_MATOMO_SCRIPT_SRC ||
+  'https://cdn.matomo.cloud/pagopa.matomo.cloud/container_cT0RWuHZ_dev_d3f6dcc7290c5aa0bf857017.js';
 export const environment = process.env.ENVIRONMENT;
 export const docsAssetsPath = '/gitbook/docs';
 export const allowCrawler = process.env.ALLOW_CRAWLER === 'true';
