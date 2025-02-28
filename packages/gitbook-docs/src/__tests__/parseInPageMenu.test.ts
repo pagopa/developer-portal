09563473e2e7c4aac053e43960237d98efd2bb17
Marco Bottaro <marco.bottaro@uqido.com>
2025-02-28 14:18:39 +0100

    Add test to parseInPageMenu

--- packages/gitbook-docs/src/__tests__/parseInPageMenu.test.ts
+++ packages/gitbook-docs/src/__tests__/parseInPageMenu.test.ts
@@ -17,4 +17,15 @@ describe('parseInPageMenu', () => {
       new Markdoc.Tag('Heading', { level: 3, id: 'title-3' }, ['Title 3']),
     ]);
   });
+  it('should return only heading elements included in HEADING_LEVELS_TO_SHOW array', () => {
+    expect(
+      parseInPageMenu(
+        '# Title 1\n## Title 2\n### Title 3\n#### Title 4\nParagraph',
+        config
+      )
+    ).toStrictEqual([
+      new Markdoc.Tag('Heading', { level: 2, id: 'title-2' }, ['Title 2']),
+      new Markdoc.Tag('Heading', { level: 3, id: 'title-3' }, ['Title 3']),
+    ]);
+  });
 });
