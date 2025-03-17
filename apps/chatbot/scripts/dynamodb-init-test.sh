a7ef9851f6144700eca4a2543d6b5d0dfaf3e883
Devis Battisti <devis.battisti@uqido.com>
2025-03-17 15:58:46 +0100

    chore: use different dynamodb for tests

---
+++ apps/chatbot/scripts/dynamodb-init-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+aws dynamodb create-table \
+--endpoint-url http://dynamodb:8000 \
+--cli-input-json file://./docker/files/dynamodb_schemas/test/sessions.json \
+--region eu-south-1
+
+aws dynamodb create-table \
+--endpoint-url http://dynamodb:8000 \
+--cli-input-json file://./docker/files/dynamodb_schemas/test/queries.json \
+--region eu-south-1
+
+aws dynamodb create-table \
+--endpoint-url http://dynamodb:8000 \
+--cli-input-json file://./docker/files/dynamodb_schemas/test/salts.json \
+--region eu-south-1
+
+aws dynamodb list-tables \
+  --endpoint-url http://dynamodb:8000 \
+  --region eu-south-1
