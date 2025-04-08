60b6ca1ff91a96aaf8a0e16e5426d2556db8cfe2
Christian Calabrese <christian.calabrese@pagopa.it>
2025-04-08 17:39:00 +0200

    Use full environment name in cicd for easier reference gh actions

--- apps/infrastructure/src/modules/cicd/main.tf
+++ apps/infrastructure/src/modules/cicd/main.tf
@@ -1,7 +1,10 @@
 module "codebuild" {
   source = "github.com/pagopa/dx//infra/modules/github_selfhosted_runner_on_codebuild?ref=self-hosted-runner-on-aws-module" # to substitute with registry path when released
 
-  environment = var.environment_information
+  # The full environment name (dev, uat, prod) instead of the short one (d, u, p)
+  # allows to use the self-hosted runner for the current environment according using
+  # the standard "environment" input variable
+  environment = merge(var.environment_information, { env_short = var.environment })
   repository = {
     organization = split("/", var.github_repository)[0]
     name         = split("/", var.github_repository)[1]
@@ -12,6 +15,7 @@ module "codebuild" {
     private_subnets = var.vpc.private_subnets
   }
 
+  # Shared CodeConnection
   codeconnection_arn = "arn:aws:codeconnections:eu-south-1:195239627635:connection/1606e639-3a11-479d-ad75-b8a1921f0346"
 
   env_variables = var.chatbot_env_vars
