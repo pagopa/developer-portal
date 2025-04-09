385737d3012a7c0d26ee155140e94b28f2b973e1
Christian Calabrese <christian.calabrese@pagopa.it>
2025-04-09 10:21:04 +0200

    Fix comment in cicd

--- apps/infrastructure/src/modules/cicd/main.tf
+++ apps/infrastructure/src/modules/cicd/main.tf
@@ -2,7 +2,7 @@ module "codebuild" {
   source = "github.com/pagopa/dx//infra/modules/github_selfhosted_runner_on_codebuild?ref=self-hosted-runner-on-aws-module" # to substitute with registry path when released
 
   # The full environment name (dev, uat, prod) instead of the short one (d, u, p)
-  # allows to use the self-hosted runner for the current environment according using
+  # allows to use the self-hosted runner for the current environment according to
   # the standard "environment" input variable
   environment = merge(var.environment_information, { env_short = var.environment })
   repository = {
