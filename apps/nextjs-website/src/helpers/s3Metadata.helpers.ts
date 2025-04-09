933cd87061c358c869def6025978971ec62411bd
Marco Ponchia <marco.ponchia@uqido.com>
2025-04-09 12:29:04 +0200

    Update sitemap and s3Metadata helpers

--- apps/nextjs-website/src/helpers/s3Metadata.helpers.ts
+++ apps/nextjs-website/src/helpers/s3Metadata.helpers.ts
@@ -1,14 +1,18 @@
-import { credentials } from '@/config';
+import { credentials, region } from '@/config';
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 
-const s3Client = new S3Client({
-  region: process.env.AWS_REGION || 'us-east-1',
-  credentials: credentials,
-});
+const s3Client =
+  !!region && !!credentials
+    ? new S3Client({ region, credentials })
+    : new S3Client();
 const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME || '';
 
 export interface JsonMetadata {
   readonly path: string;
+  readonly dirName: string;
+  readonly contentS3Path: string;
+  readonly title: string;
+  readonly manuS3Path?: string; // check if this is optional
   readonly lastModified?: string;
 }
 
@@ -23,7 +27,6 @@ export async function fetchMetadataJsonFromS3(
         Key: key,
       })
     );
-
     // Convert stream to string
     const bodyContents = await response.Body?.transformToString();
     if (!bodyContents) {
@@ -32,6 +35,23 @@ export async function fetchMetadataJsonFromS3(
 
     return JSON.parse(bodyContents) as readonly JsonMetadata[];
   } catch (error) {
+    // eslint-disable-next-line functional/no-expression-statements
+    console.error('Error fetching metadata from S3:', error);
     return [];
   }
 }
+
+const S3_GUIDES_METADATA_JSON_PATH =
+  process.env.S3_GUIDES_METADATA_JSON_PATH || 'guides-metadata.json';
+const S3_SOLUTIONS_METADATA_JSON_PATH =
+  process.env.S3_SOLUTIONS_METADATA_JSON_PATH || 'solutions-metadata.json';
+const S3_RELEASE_NOTES_METADATA_JSON_PATH =
+  process.env.S3_RELEASE_NOTES_METADATA_JSON_PATH ||
+  'release-notes-metadata.json';
+
+export const getGuidesMetadata = async () =>
+  await fetchMetadataJsonFromS3(S3_GUIDES_METADATA_JSON_PATH);
+export const getSolutionsMetadata = async () =>
+  await fetchMetadataJsonFromS3(S3_SOLUTIONS_METADATA_JSON_PATH);
+export const getReleaseNotesMetadata = async () =>
+  await fetchMetadataJsonFromS3(S3_RELEASE_NOTES_METADATA_JSON_PATH);
