0897f093b60fb9ec70938279be408b49105b5a3f
Christian Calabrese <christian.calabrese@pagopa.it>
2025-04-18 15:54:40 +0200

    Extracted chatbot lambda resources from the module

--- apps/infrastructure/src/modules/chatbot/outputs.tf
+++ apps/infrastructure/src/modules/chatbot/outputs.tf
@@ -1,105 +1,110 @@
 # Lambda Function
 output "lambda_function_arn" {
   description = "The ARN of the Lambda Function"
-  value       = module.lambda_function.lambda_function_arn
+  value       = aws_lambda_function.chatbot_lambda.arn
 }
 
 output "lambda_function_arn_static" {
-  description = "The static ARN of the Lambda Function. Use this to avoid cycle errors between resources (e.g., Step Functions)"
-  value       = module.lambda_function.lambda_function_arn_static
+  description = "The static ARN of the Lambda Function. Use this when you don't need to access a function version"
+  value       = aws_lambda_function.chatbot_lambda.arn
 }
 
 output "lambda_function_invoke_arn" {
   description = "The Invoke ARN of the Lambda Function"
-  value       = module.lambda_function.lambda_function_invoke_arn
+  value       = aws_lambda_function.chatbot_lambda.invoke_arn
 }
 
 output "lambda_function_name" {
   description = "The name of the Lambda Function"
-  value       = module.lambda_function.lambda_function_name
+  value       = aws_lambda_function.chatbot_lambda.function_name
 }
 
 output "lambda_function_qualified_arn" {
   description = "The ARN identifying your Lambda Function Version"
-  value       = module.lambda_function.lambda_function_qualified_arn
+  value       = aws_lambda_function.chatbot_lambda.qualified_arn
 }
 
 output "lambda_function_qualified_invoke_arn" {
-  description = "The Invoke ARN identifying your Lambda Function Version"
-  value       = module.lambda_function.lambda_function_qualified_invoke_arn
+  description = "The invoke ARN identifying your Lambda Function Version"
+  value       = aws_lambda_function.chatbot_lambda.qualified_invoke_arn
 }
 
 output "lambda_function_version" {
   description = "Latest published version of Lambda Function"
-  value       = module.lambda_function.lambda_function_version
+  value       = aws_lambda_function.chatbot_lambda.version
 }
 
 output "lambda_function_last_modified" {
   description = "The date Lambda Function resource was last modified"
-  value       = module.lambda_function.lambda_function_last_modified
+  value       = aws_lambda_function.chatbot_lambda.last_modified
 }
 
 output "lambda_function_kms_key_arn" {
   description = "The ARN for the KMS encryption key of Lambda Function"
-  value       = module.lambda_function.lambda_function_kms_key_arn
+  value       = aws_lambda_function.chatbot_lambda.kms_key_arn
 }
 
 output "lambda_function_source_code_hash" {
   description = "Base64-encoded representation of raw SHA-256 sum of the zip file"
-  value       = module.lambda_function.lambda_function_source_code_hash
+  value       = aws_lambda_function.chatbot_lambda.source_code_hash
 }
 
 output "lambda_function_source_code_size" {
   description = "The size in bytes of the function .zip file"
-  value       = module.lambda_function.lambda_function_source_code_size
+  value       = aws_lambda_function.chatbot_lambda.source_code_size
 }
 
 # Lambda Function URL
 output "lambda_function_url" {
   description = "The URL of the Lambda Function URL"
-  value       = module.lambda_function.lambda_function_url
+  value       = null # La nostra funzione Lambda non ha URL diretto, usiamo API Gateway
 }
 
 output "lambda_function_url_id" {
-  description = "The Lambda Function URL generated id"
-  value       = module.lambda_function.lambda_function_url_id
+  description = "Lambda Function URL ID"
+  value       = null # La nostra funzione Lambda non ha URL diretto, usiamo API Gateway
 }
 
 # Lambda Layer
 output "lambda_layer_arn" {
   description = "The ARN of the Lambda Layer with version"
-  value       = module.lambda_function.lambda_layer_arn
+  value       = null # Non utilizziamo layer Lambda
 }
 
 output "lambda_layer_layer_arn" {
   description = "The ARN of the Lambda Layer without version"
-  value       = module.lambda_function.lambda_layer_layer_arn
+  value       = null # Non utilizziamo layer Lambda
 }
 
 output "lambda_layer_created_date" {
   description = "The date Lambda Layer resource was created"
-  value       = module.lambda_function.lambda_layer_created_date
+  value       = null # Non utilizziamo layer Lambda
 }
 
 output "lambda_layer_source_code_size" {
   description = "The size in bytes of the Lambda Layer .zip file"
-  value       = module.lambda_function.lambda_layer_source_code_size
+  value       = null # Non utilizziamo layer Lambda
 }
 
 output "lambda_layer_version" {
   description = "The Lambda Layer version"
-  value       = module.lambda_function.lambda_layer_version
+  value       = null # Non utilizziamo layer Lambda
 }
 
 # IAM Role
 output "lambda_role_arn" {
   description = "The ARN of the IAM role created for the Lambda Function"
-  value       = module.lambda_function.lambda_role_arn
+  value       = aws_iam_role.lambda_role.arn
 }
 
 output "lambda_role_name" {
   description = "The name of the IAM role created for the Lambda Function"
-  value       = module.lambda_function.lambda_role_name
+  value       = aws_iam_role.lambda_role.name
+}
+
+output "lambda_role_unique_id" {
+  description = "The unique id of the IAM role created for the Lambda Function"
+  value       = aws_iam_role.lambda_role.unique_id
 }
 
 output "lambda_env_variables" {
@@ -110,7 +115,7 @@ output "lambda_env_variables" {
 # CloudWatch Log Group
 output "lambda_cloudwatch_log_group_arn" {
   description = "The ARN of the Cloudwatch Log Group"
-  value       = module.lambda_function.lambda_cloudwatch_log_group_arn
+  value       = aws_cloudwatch_log_group.lambda_logs.arn
 }
 
 output "security_groups" {
