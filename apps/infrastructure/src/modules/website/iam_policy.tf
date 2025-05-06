0e8011b200b8fea5e6f4986b1ae69066d95553b4
Christian Calabrese <christian.calabrese@pagopa.it>
2025-05-06 11:57:59 +0200

    Fix website bucket policy (#1428)
    * Fix website bucket policy
    
    * Add changeset

--- apps/infrastructure/src/modules/website/iam_policy.tf
+++ apps/infrastructure/src/modules/website/iam_policy.tf
@@ -86,9 +86,7 @@ data "aws_iam_policy_document" "website_iam_policy" {
     actions = ["s3:GetObject", "s3:ListBucket"]
     resources = [
       aws_s3_bucket.website.arn,
-      "${aws_s3_bucket.website.arn}/*",
-      aws_s3_bucket.website_standalone.arn,
-      "${aws_s3_bucket.website_standalone.arn}/*"
+      "${aws_s3_bucket.website.arn}/*"
     ]
 
     principals {
