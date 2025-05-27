edf05fa73f21fea0c93c5bd9e9836def97d80c69
uolter <walter.traspadini@pagopa.it>
2025-05-27 11:24:31 +0200

    module outputs and first draft stendalone deploy action

--- apps/infrastructure/src/main.tf
+++ apps/infrastructure/src/main.tf
@@ -192,6 +192,8 @@ module "cicd" {
   lambda_code_opennext_bucket = module.website.lambda_code_opennext_bucket
   lambda_initializer_arn      = module.website.lambda_initializer.arn
 
+  assets_opennext_bucket = module.website.assets_opennext_bucket
+
   website_is_standalone = var.website_is_standalone
 
   chatbot_env_vars = var.create_chatbot ? module.chatbot[0].lambda_env_variables : {}
