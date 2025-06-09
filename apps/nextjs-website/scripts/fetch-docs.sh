b58377572d4bd4500229337ea0697a0ae3a1d42a
Marcello Bertoli <marcello.bertoli@uqido.com>
2025-06-09 15:40:47 +0200

    Update cp command in fetch and update

--- apps/nextjs-website/scripts/fetch-docs.sh
+++ apps/nextjs-website/scripts/fetch-docs.sh
@@ -35,4 +35,4 @@ dest='public/gitbook/'
 # create all the assets folder otherwise the copy will fail
 find docs -type d -path '*/.gitbook/assets' -exec mkdir -p $dest'{}' ';'
 # copy all the assets from docs to public assets of nextjs-website
-find docs -type f -path '*/.gitbook/*' -exec cp '{}' $dest'{}' ';'
+find docs -type d -path "*/.gitbook/includes" -prune -o -type f -path "*/.gitbook/*" -exec cp --parents '{}' "$dest" ';'
