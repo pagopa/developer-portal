762a28e3e9965b39a3dca3ae32749af8ee52d83e
uolter <walter.traspadini@pagopa.it>
2025-06-09 17:51:20 +0200

    bucket and cloudfront to store soap xml api descriptors

--- apps/infrastructure/src/modules/website/acm.tf
+++ apps/infrastructure/src/modules/website/acm.tf
@@ -22,3 +22,18 @@ resource "aws_acm_certificate" "auth" {
   # TLS certificate generated in us-east because it is related to the CDN which is a global resource
   provider = aws.us-east-1
 }
+
+
+# Route53 DNS validation records for the website ACM certificate
+
+resource "aws_acm_certificate" "static_contents" {
+  domain_name       = local.dns_domain_name_static_contents
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  # TLS certificate generated in us-east because it is related to the CDN which is a global resource
+  provider = aws.us-east-1
+}
--- apps/infrastructure/src/modules/website/cloudfront.tf
+++ apps/infrastructure/src/modules/website/cloudfront.tf
@@ -115,3 +115,67 @@ resource "aws_cloudfront_distribution" "website" {
     minimum_protocol_version       = var.use_custom_certificate ? "TLSv1.2_2021" : null
   }
 }
+
+
+# cloudfront distribution for standalone static content
+
+## Static website CDN
+resource "aws_cloudfront_distribution" "static_contents" {
+
+  origin {
+    domain_name = aws_s3_bucket.website_standalone.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.website_standalone.bucket
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.main["static"].cloudfront_access_identity_path
+    }
+  }
+
+
+  enabled         = true # enable CloudFront distribution
+  is_ipv6_enabled = true
+  comment         = "CloudFront distribution for the static contetnts."
+
+
+  aliases = [local.dns_domain_name_static_contents]
+
+
+  default_cache_behavior {
+    # HTTPS requests we permit the distribution to serve
+    allowed_methods            = ["GET", "HEAD", "OPTIONS"]
+    cached_methods             = ["GET", "HEAD"]
+    target_origin_id           = aws_s3_bucket.website_standalone.bucket
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.websites["static"].id
+
+    forwarded_values {
+      query_string = false
+      headers      = []
+      cookies {
+        forward = "none"
+      }
+    }
+
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0     # min time for objects to live in the distribution cache
+    default_ttl            = 3600  # default time for objects to live in the distribution cache
+    max_ttl                = 86400 # max time for objects to live in the distribution cache
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.website_viewer_request_handler["static"].arn
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = aws_acm_certificate.static_contents.arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+}
