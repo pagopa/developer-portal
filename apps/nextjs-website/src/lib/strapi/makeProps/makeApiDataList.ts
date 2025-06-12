19785261c3cdf44d0389d2f456d603cd3eed7725
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2025-06-12 15:22:18 +0200

    Add ApiSoapSection to ApiSection page

--- apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataList.ts
+++ apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataList.ts
@@ -2,22 +2,29 @@ import { ApiDataPageProps } from '@/app/[productSlug]/api/[apiDataSlug]/page';
 import { StrapiApiDataList } from '../codecs/ApiDataListCodec';
 import { makeBannerLinkProps } from '@/lib/strapi/makeProps/makeBannerLink';
 import { makeBaseProductWithoutLogoProps } from './makeProducts';
+import { makeApiSoapUrlList } from '@/lib/strapi/makeProps/makeApiSoapUrlList';
 
 export function makeApiDataListProps(
   strapiApiDataList: StrapiApiDataList
-): ReadonlyArray<ApiDataPageProps> {
-  return strapiApiDataList.data
+): Promise<ReadonlyArray<ApiDataPageProps>> {
+  const list = strapiApiDataList.data
     .filter((apiPage) => apiPage.attributes.apiRestDetail)
-    .map(({ attributes }) => {
+    .map(async ({ attributes }) => {
       const product = makeBaseProductWithoutLogoProps(attributes.product.data);
       return {
         ...attributes,
         product,
-        apiDataSlug: attributes.apiRestDetail?.slug || '',
-        specURLs: attributes.apiRestDetail
+        apiType: attributes.apiRestDetail ? 'rest' : 'soap',
+        apiDataSlug:
+          attributes.apiRestDetail?.slug || attributes.apiSoapDetail?.slug,
+        specUrls: attributes.apiRestDetail
           ? [...attributes.apiRestDetail.specUrls.map((spec) => ({ ...spec }))]
           : [],
-        specURLsName: attributes.title,
+        apiSoapUrl: attributes.apiSoapDetail?.repositoryUrl,
+        specUrlsName: attributes.title,
+        apiSoapWsdlUrlList: await makeApiSoapUrlList(
+          attributes.apiSoapDetail.dirName
+        ),
         bannerLinks:
           attributes.bannerLinks.length > 0
             ? attributes.bannerLinks.map(makeBannerLinkProps)
@@ -25,6 +32,7 @@ export function makeApiDataListProps(
                 makeBannerLinkProps
               ),
         seo: attributes.seo,
-      };
+      } satisfies ApiDataPageProps;
     });
+  return Promise.all(list);
 }
