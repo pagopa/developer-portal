05f6438168321163b005fd674f90265b495dfb53
uolter <walter.traspadini@pagopa.it>
2025-06-12 16:44:35 +0200

    propagator config

--- apps/nextjs-website/src/instrumentation.ts
+++ apps/nextjs-website/src/instrumentation.ts
@@ -1,5 +1,9 @@
 import { registerOTel } from '@vercel/otel';
+import { AWSXRayPropagator } from '@opentelemetry/propagator-aws-xray';
 
 export function register() {
-  const telemetry = registerOTel({ serviceName: 'next-app' });
+  const telemetry = registerOTel({
+    serviceName: 'next-app',
+    propagators: [new AWSXRayPropagator()],
+  });
 }
