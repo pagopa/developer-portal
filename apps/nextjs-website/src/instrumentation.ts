46a64c62b792720b4565a1991030d028f65d666a
uolter <walter.traspadini@pagopa.it>
2025-06-12 17:30:13 +0200

    removed optl config and set aggressive sampling

--- apps/nextjs-website/src/instrumentation.ts
+++ apps/nextjs-website/src/instrumentation.ts
@@ -1,9 +1,13 @@
 import { registerOTel } from '@vercel/otel';
 import { AWSXRayPropagator } from '@opentelemetry/propagator-aws-xray';
+import { AlwaysOnSampler } from '@opentelemetry/core';
+
+
 
 export function register() {
   const telemetry = registerOTel({
     serviceName: 'next-app',
     propagators: [new AWSXRayPropagator()],
+    traceSampler: new AlwaysOnSampler(),
   });
 }
