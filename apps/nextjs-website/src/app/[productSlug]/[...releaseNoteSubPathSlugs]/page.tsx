6d42487051b649ef1b74cd75b9a63ea4a3fd11af
Marco Ponchia <marco.ponchia@uqido.com>
2025-06-19 16:41:24 +0200

    Fix release note pages

--- apps/nextjs-website/src/app/[productSlug]/[...releaseNoteSubPathSlugs]/page.tsx
+++ apps/nextjs-website/src/app/[productSlug]/[...releaseNoteSubPathSlugs]/page.tsx
@@ -37,7 +37,9 @@ export const revalidate = REVALIDATE_LONG_INTERVAL;
 
 const PRODUCT_SLUG_PATH_INDEX = 1;
 const RELEASE_NOTE_SUB_PATH_INDEX = 2;
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<
+  ReleaseNotePageStaticParams[]
+> {
   const releaseNotes = await getReleaseNotesMetadata();
   const releaseNoteParams = releaseNotes
     .map(({ path }) => path.split('/'))
@@ -45,7 +47,7 @@ export async function generateStaticParams() {
     .map((paths) => {
       return {
         productSlug: paths[PRODUCT_SLUG_PATH_INDEX],
-        productGuidePage: paths.slice(RELEASE_NOTE_SUB_PATH_INDEX),
+        releaseNoteSubPathSlugs: paths.slice(RELEASE_NOTE_SUB_PATH_INDEX),
       };
     });
   return releaseNoteParams;
