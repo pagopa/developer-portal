30ccdc9fd92fbff8fdcb74fa7e3d8aab46d400ab
Sebastiano-Bertolin <56671015+Sebastiano-Bertolin@users.noreply.github.com>
2025-06-20 17:06:17 +0200

    [DEV-2297] Add ApiSoapSection component and component Storybook's story (#1456)
    * Refactor ApiSection into a generic section and rename the previous module to ApiRestSection
    
    * Add changeset
    
    * Update .changeset/purple-clocks-sniff.md
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Update apps/nextjs-website/src/components/molecules/ApiSection/ApiSection.tsx
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    
    * Move ApiRestSection to its own directory and rename its style.ts
    
    * Add fixture for ApiSoapSection story
    
    * Add ApiSoapSection component
    
    * Add ApiSoapSection Storybook story
    
    * Add changeset
    
    * Fix css href and change name of the wsdl fixtures folder
    
    * Move wsdl viewer into the wsdl forder
    
    * Fix text size and remove schema from wsdl view
    
    * Add chevron to menu items
    
    * Remove margin
    
    * Change line height and remove padding
    
    * Add link and fix component height
    
    * Add api repo link in apisoap section component
    
    * Update link copy and style
    
    * Fix link style
    
    * Improve footer link
    
    * Update wsdl-viewer to support xsd import recursively
    
    * Revert to first level xsd import resolution for source
    
    * Add scroll to initial position when new menu item is selected
    
    * Remove src folder
    
    * Remove dashed borders
    
    * Fix margin and add accent colo var
    
    * Fix port container padding
    
    * Include css directly in xsl
    
    * Remove css files
    
    * Refactor repoLink translation
    
    * Fix translation identifier
    
    * Add translation string for iframe title and pass target: '_blank' to LinkMui component
    
    * Update apps/nextjs-website/src/components/molecules/ApiSoapSection/ApiSoapSection.tsx
    
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>
    
    ---------
    
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>
    Co-authored-by: Marco Ponchia <ponchia.marco1994@gmail.com>

---
+++ apps/nextjs-website/src/components/molecules/ApiSoapSection/ApiSoapSection.tsx
@@ -0,0 +1,232 @@
+'use client';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import {
+  Box,
+  Link as LinkMui,
+  MenuItem,
+  MenuList,
+  Stack,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Product } from '@/lib/types/product';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+
+export type ApiSoapSectionProps = {
+  readonly product: Product;
+  readonly apisName: string;
+  readonly apisSlug: string;
+  readonly apiRepositoryUrl: string;
+  readonly apiUrls: {
+    name?: string;
+    url: string;
+  }[];
+};
+
+const ApiSoapSection = ({
+  product,
+  apisName,
+  apisSlug,
+  apiRepositoryUrl,
+  apiUrls,
+}: ApiSoapSectionProps) => {
+  const t = useTranslations();
+  const { palette } = useTheme();
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const [selectedItemUrl, setSelectedItemUrl] = useState(apiUrls[0].url);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const iframe = iframeRef.current;
+
+    const resizeIframe = () => {
+      if (iframe && iframe.contentWindow?.document?.body) {
+        const newHeight = iframe.contentWindow.document.body.scrollHeight;
+        // eslint-disable-next-line functional/immutable-data
+        iframe.style.height = `${newHeight}px`;
+      }
+    };
+
+    iframe?.addEventListener('load', resizeIframe);
+
+    return () => {
+      iframe?.removeEventListener('load', resizeIframe);
+    };
+  }, []);
+
+  useEffect(() => {
+    const specName = searchParams.get('spec');
+    if (specName) {
+      const decodedSpecName = decodeURIComponent(specName as string);
+      const spec = apiUrls.find((item) => item?.name === decodedSpecName);
+      if (spec) {
+        setSelectedItemUrl(spec.url);
+      }
+    }
+  }, [searchParams, apiUrls]);
+
+  const handleItemSelect = (urlItem: string) => {
+    setSelectedItemUrl(urlItem);
+
+    const spec = apiUrls.find((item) => item?.url === urlItem);
+
+    if (apisName && spec?.name) {
+      router.replace(
+        `/${product.slug}/api/${apisSlug}?spec=${encodeURIComponent(spec.name)}`
+      );
+    }
+
+    document.getElementById('soapApiStack')?.scroll(0, 0);
+  };
+
+  const selectedApi = useMemo(
+    () => apiUrls.find((item) => item?.url === selectedItemUrl) || apiUrls[0],
+    [selectedItemUrl, apiUrls]
+  );
+  const borderColor = palette.divider;
+
+  return (
+    <Box
+      sx={{
+        borderTop: { md: `1px solid ${borderColor}` },
+        marginTop: { xs: '-42px', sm: '-22px', md: 0 },
+        height: '100vh',
+      }}
+    >
+      {apiUrls.length > 1 && apisName && (
+        <Stack
+          id='soapApiStack'
+          direction='row'
+          justifyContent='flex-start'
+          alignContent='center'
+          sx={{
+            height: '100%',
+            overflowY: 'auto',
+          }}
+        >
+          <Box
+            sx={{
+              width: 280,
+              bgcolor: palette.background.default,
+              paddingTop: '2rem',
+              position: 'sticky',
+              top: 0,
+              height: 'calc(100% - 2rem)',
+            }}
+          >
+            <Typography
+              noWrap={true}
+              sx={{
+                paddingX: '2rem',
+                paddingY: '1rem',
+                color: palette.text.primary,
+                fontWeight: 600,
+                fontSize: '1rem',
+              }}
+            >
+              {apisName}
+            </Typography>
+            <MenuList dense>
+              {apiUrls.map((item) => (
+                <MenuItem
+                  key={item.url}
+                  onClick={() => handleItemSelect(item.url)}
+                  sx={{
+                    paddingX: '2rem',
+                    paddingY: '0.5rem',
+                    background:
+                      selectedItemUrl === item.url
+                        ? 'rgba(0, 115, 230, 0.08)'
+                        : 'transparent',
+                    '&:hover': {
+                      background:
+                        selectedItemUrl === item.url
+                          ? 'rgba(0, 115, 230, 0.08)'
+                          : 'rgba(0, 115, 230, 0.04)',
+                    },
+                  }}
+                >
+                  <Stack
+                    direction='row'
+                    justifyContent='space-between'
+                    alignItems='center'
+                    width='100%'
+                    spacing={1}
+                  >
+                    <Typography
+                      noWrap={true}
+                      sx={{
+                        fontWeight: 400,
+                        fontSize: '0.875rem',
+                      }}
+                    >
+                      {item.name || item.url}
+                    </Typography>
+                    <ArrowForwardIosIcon sx={{ fontSize: '0.75rem' }} />
+                  </Stack>
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Box>
+          <Stack
+            direction='column'
+            sx={{
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            <Box
+              sx={{
+                marginX: '100px',
+                paddingBlock: 4,
+                paddingTop: 7,
+                width: '90%',
+                minWidth: 800,
+              }}
+            >
+              <iframe
+                ref={iframeRef}
+                src={selectedApi.url}
+                style={{
+                  border: 'none',
+                  width: '100%',
+                }}
+                title={t('apiDataPage.soap.iframeTitle')}
+              />
+            </Box>
+            <Box
+              sx={{
+                paddingLeft: 20,
+                paddingBottom: 8,
+                paddingTop: 2,
+              }}
+            >
+              <LinkMui
+                component={Link}
+                underline='none'
+                href={apiRepositoryUrl}
+                target='_blank'
+              >
+                <Typography
+                  sx={{
+                    fontWeight: 600,
+                    fontSize: '0.625rem',
+                    textDecoration: 'underline',
+                  }}
+                >
+                  {t('apiDataPage.soap.repoLink')}
+                </Typography>
+              </LinkMui>
+            </Box>
+          </Stack>
+        </Stack>
+      )}
+    </Box>
+  );
+};
+
+export default ApiSoapSection;
