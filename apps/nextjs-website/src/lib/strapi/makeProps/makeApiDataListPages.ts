e33ae89e6359294611dfe9a58f05988e71ec3958
Sebastiano-Bertolin <56671015+Sebastiano-Bertolin@users.noreply.github.com>
2025-06-26 11:58:55 +0200

    [DEV-2358] API SOAP Section Feature Flag (#1490)
    * Add feature flag and relative env var
    
    * Add feature flag and relative env var
    
    * Fix makeApiDataListPages
    
    * Fix href expression
    
    * Fix merge conflicts

--- apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataListPages.ts
+++ apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataListPages.ts
@@ -2,6 +2,7 @@ import { ApiDataListPageTemplateProps } from '@/components/templates/ApiDataList
 import { StrapiApiDataListPages } from '../codecs/ApiDataListPagesCodec';
 import { makeBannerLinkProps } from '@/lib/strapi/makeProps/makeBannerLink';
 import { makeBaseProductWithoutLogoProps } from './makeProducts';
+import { soapApiPageActive } from '@/config';
 
 export function makeApiDataListPagesProps(
   strapiApiDataListPages: StrapiApiDataListPages
@@ -22,21 +23,28 @@ export function makeApiDataListPagesProps(
         )
         .filter(Boolean) as readonly string[],
       cards: attributes.apiData.data.map((item) => ({
-        target: '_self',
+        target: soapApiPageActive
+          ? '_self'
+          : ((item.attributes.apiSoapUrl ? '_blank' : '_self') as
+              | '_blank'
+              | '_self'),
         tags: [
           {
-            label: item.attributes.apiSoapDetail ? 'SOAP' : 'REST',
+            label: item.attributes.apiSoapUrl ? 'SOAP' : 'REST',
           },
         ],
         title: item?.attributes?.title,
         text: item?.attributes?.description || '',
         icon: item?.attributes?.icon?.data?.attributes.url || '',
         externalUrl: !!item.attributes.apiSoapUrl,
-        href: `/${attributes.product.data?.attributes.slug}/api/${
-          item.attributes.apiRestDetail
-            ? item.attributes.apiRestDetail?.slug
-            : item.attributes.apiSoapDetail?.slug
-        }`,
+        href: soapApiPageActive
+          ? `/${attributes.product.data?.attributes.slug}/api/${
+              item.attributes.apiRestDetail
+                ? item.attributes.apiRestDetail?.slug
+                : item.attributes.apiSoapDetail?.slug
+            }`
+          : item.attributes.apiSoapUrl ||
+            `/${attributes.product.data?.attributes.slug}/api/${item.attributes.apiRestDetail?.slug}`,
       })),
       bannerLinks: attributes.bannerLinks.map(makeBannerLinkProps),
       seo: attributes.seo,
