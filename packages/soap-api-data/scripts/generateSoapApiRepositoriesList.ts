7d444d4cd2d5f5897c71612b9828ce4c749072ef
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2025-07-07 10:54:28 +0200

    Move scripts out of gitbook-docs package

--- packages/soap-api-data/scripts/generateSoapApiRepositoriesList.ts
+++ packages/soap-api-data/scripts/generateSoapApiRepositoriesList.ts
@@ -1,5 +1,5 @@
 /* eslint-disable functional/no-expression-statements */
-import { fetchFromStrapi } from '../helpers/fetchFromStrapi';
+import { fetchFromStrapi } from 'gitbook-docs/helpers/fetchFromStrapi';
 import { writeFile, mkdir } from 'node:fs/promises';
 import { resolve } from 'path';
 
