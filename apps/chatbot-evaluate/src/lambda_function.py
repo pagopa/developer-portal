921792fb2f647439929cfa2ed3ba0b40ea31e4d8
mdciri <marco.cirillo@uqido.com>
2025-07-17 10:37:30 +0200

    Update lambda function

--- apps/chatbot-evaluate/src/lambda_function.py
+++ apps/chatbot-evaluate/src/lambda_function.py
@@ -1,17 +1,21 @@
-from src.modules.evaluate import evaluate
+import os
+import yaml
+from pathlib import Path
+
+from src.modules.judge import Judge
 from src.modules.logger import get_logger
 
 LOGGER = get_logger(__name__)
-
-
-def ping(payload):
-    return {"statusCode": 200, "result": "pong"}
+CWF = Path(__file__)
+ROOT = CWF.parent.parent.absolute().__str__()
+PROMPTS = yaml.safe_load(open(os.path.join(ROOT, "config", "prompts.yaml"), "r"))
+JUDGE = Judge(prompts=PROMPTS)
 
 
 def lambda_handler(event, context):
     LOGGER.debug(f"- - - - - - - - event: {event}")
 
-    result = evaluate(
+    result = JUDGE.evaluate(
         query_str=event.get("query_str", ""),
         response_str=event.get("response_str", ""),
         retrieved_context=event.get("retrieved_context", []),
