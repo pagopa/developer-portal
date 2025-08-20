0fdea3e94eccb45d313cf16bddda038c66439eb7
uolter <walter.traspadini@pagopa.it>
2025-08-20 12:35:43 +0200

    enabled content_based_deduplication

--- apps/infrastructure/src/modules/chatbot/sqs.tf
+++ apps/infrastructure/src/modules/chatbot/sqs.tf
@@ -1,7 +1,8 @@
 resource "aws_sqs_queue" "chatbot_evaluate_queue" {
-  name                  = "${local.prefix}-evaluate-queue.fifo"
-  fifo_queue            = true
-  deduplication_scope   = "messageGroup"
-  fifo_throughput_limit = "perMessageGroupId"
+  name                        = "${local.prefix}-evaluate-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+  deduplication_scope         = "messageGroup"
+  fifo_throughput_limit       = "perMessageGroupId"
 }
 
