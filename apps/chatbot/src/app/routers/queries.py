537b3289e52e38cae71f7208755e65d118d9e452
uolter <walter.traspadini@pagopa.it>
2025-08-20 12:20:32 +0200

    sqs send method with MessageGroupId

--- apps/chatbot/src/app/routers/queries.py
+++ apps/chatbot/src/app/routers/queries.py
@@ -107,7 +107,8 @@ async def query_creation(
             "messages": messages,
         }
         sqs_response = sqs_queue_evaluate.send_message(
-            MessageBody=json.dumps(evaluation_data)
+            MessageBody=json.dumps(evaluation_data),
+            MessageGroupId=session["id"]  # Required for FIFO queues
         )
         LOGGER.info(f"sqs response: {sqs_response}")
 
