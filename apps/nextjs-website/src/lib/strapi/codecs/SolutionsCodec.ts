9b3235550a30621af4cfc97b023d397a24a5aa75
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2025-08-26 17:25:02 +0200

    Remove some SolutionsCodecs

--- apps/nextjs-website/src/lib/strapi/codecs/SolutionsCodec.ts
+++ apps/nextjs-website/src/lib/strapi/codecs/SolutionsCodec.ts
@@ -1,30 +1,6 @@
 import * as t from 'io-ts/lib';
-import * as tt from 'io-ts-types';
 import { NullToUndefinedCodec } from './NullToUndefinedCodec';
 import { MediaCodec } from './MediaCodec';
-import { PaginationCodec } from './PaginationCodec';
-import {
-  BaseProductCodec,
-  ProductWithoutBannerLinksCodec,
-} from './ProductCodec';
-import { BlocksContentCodec } from './BlocksContentCodec';
-import { BannerLinkCodec } from './BannerLinkCodec';
-import { WebinarCodec } from './WebinarsCodec';
-import { CaseHistoriesComponentCodec } from '@/lib/strapi/codecs/CaseHistoriesComponentCodec';
-import { SEOCodec } from './SeoCodec';
-
-const StepCodec = t.strict({
-  title: t.string,
-  content: BlocksContentCodec,
-  products: t.strict({
-    data: t.array(BaseProductCodec),
-  }),
-});
-
-const StatCodec = t.strict({
-  title: t.string,
-  description: t.union([NullToUndefinedCodec, t.string]),
-});
 
 const BaseSolutionAttributesCodec = t.strict({
   slug: t.string,
@@ -39,44 +15,3 @@ const BaseSolutionAttributesCodec = t.strict({
 export const BaseSolutionCodec = t.strict({
   attributes: BaseSolutionAttributesCodec,
 });
-
-export const BaseSolutionWithProductsCodec = t.strict({
-  attributes: t.intersection([
-    BaseSolutionAttributesCodec,
-    t.strict({
-      products: t.strict({ data: t.array(ProductWithoutBannerLinksCodec) }),
-    }),
-  ]),
-});
-
-export const SolutionCodec = t.strict({
-  id: t.number,
-  attributes: t.intersection([
-    BaseSolutionAttributesCodec,
-    t.strict({
-      publishedAt: tt.DateFromISOString,
-      updatedAt: tt.DateFromISOString,
-      introductionToSteps: t.union([NullToUndefinedCodec, t.string]),
-      steps: t.array(StepCodec),
-      stats: t.array(StatCodec),
-      statsSource: t.union([NullToUndefinedCodec, t.string]),
-      bannerLinks: t.array(BannerLinkCodec),
-      webinars: t.strict({
-        data: t.array(WebinarCodec),
-      }),
-      products: t.strict({ data: t.array(ProductWithoutBannerLinksCodec) }),
-      caseHistories: t.union([
-        NullToUndefinedCodec,
-        CaseHistoriesComponentCodec,
-      ]),
-      seo: t.union([NullToUndefinedCodec, SEOCodec]),
-    }),
-  ]),
-});
-
-export const SolutionsCodec = t.strict({
-  data: t.array(SolutionCodec),
-  meta: PaginationCodec,
-});
-
-export type StrapiSolutions = t.TypeOf<typeof SolutionsCodec>;
