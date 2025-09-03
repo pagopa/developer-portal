47acca5d997bc4f03eef237f562ff0bece60d71e
Marcello Bertoli <marcello.bertoli@uqido.com>
2025-09-03 12:58:26 +0200

    add rapidoc and base component

--- apps/nextjs-website/src/components/atoms/ApiViewer/ApiViewer.tsx
+++ apps/nextjs-website/src/components/atoms/ApiViewer/ApiViewer.tsx
@@ -1,26 +1,71 @@
 'use client';
 import { FC } from 'react';
-import { API } from '@stoplight/elements';
-import '@stoplight/elements/styles.min.css';
+import { Box, useTheme } from '@mui/material';
 import { Product } from '@/lib/types/product';
+import 'rapidoc';
+
+// Extend JSX to recognize the custom element <rapi-doc>
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      'rapi-doc': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        'spec-url'?: string;
+        theme?: string;
+        'render-style'?: string;
+        'show-header'?: string;
+        'primary-color'?: string;
+      };
+    }
+  }
+}
 
 type ApiViewerProps = {
   specURL: string;
   product: Product;
   hideTryIt?: boolean;
 };
-const ApiViewer: FC<ApiViewerProps> = ({
-  hideTryIt = true,
-  specURL,
-  product,
-}) => {
+
+const ApiViewer: FC<ApiViewerProps> = ({ specURL, product }) => {
+  const { palette } = useTheme();
+  // function that return current API component type
   return (
-    <API
-      apiDescriptionUrl={specURL}
-      hideTryIt={hideTryIt}
-      basePath={`/${product.slug}/api`}
-      router={typeof window === 'undefined' ? 'memory' : 'hash'}
-    />
+    <Box
+      sx={{
+        mt: 2,
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+        overflow: 'hidden',
+      }}
+    >
+      <rapi-doc
+        spec-url={specURL}
+        /*theme='light'
+        nav-bg-color='#EBEEF5'
+        bg-color={palette.background.paper}
+        text-color={palette.text.primary}
+        nav-text-color={palette.text.primary}
+        regular-font='Titillium Web'
+        mono-font='Titillium Web'
+        font-size='largest'
+        allow-search='false'
+        allow-advanced-search='false'
+        render-style='focused'
+        show-header='false'
+        allow-authentication='false'
+        primary-color='#3b82f6'
+        scroll-y-offset='0'
+        auto-scroll='false'
+        allow-try='false'
+        allow-server-selection='false'
+        show-method-in-nav-bar='as-colored-block'
+        show-components='true'*/
+      ></rapi-doc>
+    </Box>
   );
 };
 
