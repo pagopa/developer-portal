37639e6b6498de8877c8801c541ec4515df4e2eb
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2025-09-03 12:59:54 +0200

    Add test and fixtures for makeApiDataListPages

--- apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataListPages.ts
+++ apps/nextjs-website/src/lib/strapi/makeProps/makeApiDataListPages.ts
@@ -2,49 +2,88 @@ import { ApiDataListPageTemplateProps } from '@/components/templates/ApiDataList
 import { makeBannerLinkProps } from '@/lib/strapi/makeProps/makeBannerLink';
 import { makeBaseProductWithoutLogoProps } from '@/lib/strapi/makeProps/makeProducts';
 import { StrapiApiDataListPages } from '@/lib/strapi/types/apiDataListPages';
+import _ from 'lodash';
+import { StrapiBaseApiData } from '../types/apiDataList';
+import { cons } from 'fp-ts/lib/ReadonlyNonEmptyArray';
+
+function makeApiDataListPageCard(item: StrapiBaseApiData, slug: string) {
+  if (
+    !item.attributes.title ||
+    (!item.attributes.apiRestDetail && !item.attributes.apiSoapDetail) ||
+    (!item.attributes.apiRestDetail?.slug &&
+      !item.attributes.apiSoapDetail?.slug)
+  ) {
+    // eslint-disable-next-line functional/no-expression-statements
+    console.error(
+      `Error processing API Data with ID "${item.id}": Missing title or API details`
+    );
+    return null;
+  }
+
+  return {
+    tags: [
+      {
+        label: item.attributes.apiSoapDetail ? 'SOAP' : 'REST',
+      },
+    ].filter((tag) => !!tag.label),
+    title: item?.attributes?.title,
+    text: item?.attributes?.description || '',
+    icon: item?.attributes?.icon?.data?.attributes.url || '',
+    href: `/${slug}/api/${
+      item.attributes.apiRestDetail
+        ? item.attributes.apiRestDetail?.slug
+        : item.attributes.apiSoapDetail?.slug
+    }`,
+  };
+}
 
 export function makeApiDataListPagesProps(
   strapiApiDataListPages: StrapiApiDataListPages
 ): ReadonlyArray<ApiDataListPageTemplateProps> {
-  return strapiApiDataListPages.data.map(({ attributes }) => {
-    return {
-      ...attributes,
-      hero: {
-        title: attributes.title,
-        subtitle: attributes.description || '',
-      },
-      product: makeBaseProductWithoutLogoProps(attributes.product.data),
-      apiDetailSlugs: attributes.apiData.data
-        .map(({ attributes }) =>
-          attributes.apiRestDetail
-            ? attributes.apiRestDetail.slug
-            : attributes.apiSoapDetail?.slug
-        )
-        .filter(Boolean) as readonly string[],
-      cards: attributes.apiData.data
-        .map((item) => ({
-          tags: [
-            {
-              label: item.attributes.apiSoapDetail
-                ? 'SOAP'
-                : item.attributes.apiRestDetail
-                ? 'REST'
-                : '',
-            },
-          ].filter((tag) => !!tag.label),
-          title: item?.attributes?.title,
-          text: item?.attributes?.description || '',
-          icon: item?.attributes?.icon?.data?.attributes.url || '',
-          href: `/${attributes.product.data?.attributes.slug}/api/${
-            item.attributes.apiRestDetail
-              ? item.attributes.apiRestDetail?.slug
-              : item.attributes.apiSoapDetail?.slug
-          }`,
-        }))
-        .filter((card) => card.title && !!card.tags),
-      bannerLinks: attributes.bannerLinks.map(makeBannerLinkProps),
-      seo: attributes.seo,
-      updatedAt: attributes.updatedAt,
-    };
-  });
+  return _.compact(
+    strapiApiDataListPages.data.map(({ attributes }) => {
+      const slug = attributes.product.data.attributes.slug;
+      if (!slug) {
+        // eslint-disable-next-line functional/no-expression-statements
+        console.error(
+          `Error processing API Data List Page with title "${attributes.title}": Missing product slug`
+        );
+        return null;
+      }
+
+      // eslint-disable-next-line functional/no-try-statements
+      try {
+        return {
+          ...attributes,
+          hero: {
+            title: attributes.title,
+            subtitle: attributes.description || '',
+          },
+          product: makeBaseProductWithoutLogoProps(attributes.product.data),
+          apiDetailSlugs: attributes.apiData.data
+            .map(({ attributes }) =>
+              attributes.apiRestDetail
+                ? attributes.apiRestDetail.slug
+                : attributes.apiSoapDetail?.slug
+            )
+            .filter(Boolean) as readonly string[],
+          cards: _.compact(
+            attributes.apiData.data.map((item) =>
+              makeApiDataListPageCard(item, slug)
+            )
+          ),
+          bannerLinks: attributes.bannerLinks.map(makeBannerLinkProps),
+          seo: attributes.seo,
+          updatedAt: attributes.updatedAt,
+        };
+      } catch (error) {
+        // eslint-disable-next-line functional/no-expression-statements
+        console.error(
+          `Error processing API Data List Page with title "${attributes.title}":`,
+          error
+        );
+        return null;
+      }
+    })
+  );
 }
