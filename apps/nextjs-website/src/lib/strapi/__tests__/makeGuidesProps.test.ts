8b631b528319d89d34b918b733c7b17f6328dd68
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2025-09-04 11:02:51 +0200

    Add guard for missing slugs in makeGuides

--- apps/nextjs-website/src/lib/strapi/__tests__/makeGuidesProps.test.ts
+++ apps/nextjs-website/src/lib/strapi/__tests__/makeGuidesProps.test.ts
@@ -4,6 +4,10 @@ import {
   strapiGuideData,
 } from '@/lib/strapi/__tests__/fixtures/guides';
 import { makeGuides } from '@/lib/strapi/makeProps/makeGuides';
+import {
+  makeGuidesWithProductWithEmptySlug,
+  makeGuidesWithProductWithUndefinedSlug,
+} from '@/lib/strapi/__tests__/factories/guides';
 
 describe('makeGuides', () => {
   it('should return an empty array when no guides are provided', () => {
@@ -15,5 +19,15 @@ describe('makeGuides', () => {
     const result = makeGuides(strapiGuideData);
     expect(result).toHaveLength(1);
     expect(result).toEqual(guideProps);
+  });
+
+  it('should return an empty array when the product slug is an empty string', () => {
+    const result = makeGuides(makeGuidesWithProductWithEmptySlug());
+    expect(result).toEqual([]);
+  });
+
+  it('should return an empty array when the product slug is undefined', () => {
+    const result = makeGuides(makeGuidesWithProductWithUndefinedSlug());
+    expect(result).toEqual([]);
   });
 });
--- apps/nextjs-website/src/lib/strapi/makeProps/makeGuides.ts
+++ apps/nextjs-website/src/lib/strapi/makeProps/makeGuides.ts
@@ -1,28 +1,51 @@
+/* eslint-disable functional/no-try-statements */
+/* eslint-disable functional/no-expression-statements */
 import { GuideDefinition } from '@/helpers/makeDocs.helpers';
 import { makeBannerLink } from '@/lib/strapi/makeProps/makeBannerLink';
 import { makeBaseProductWithoutLogo } from './makeProducts';
 import { StrapiGuides } from '@/lib/strapi/types/guide';
+import _ from 'lodash';
 
 export function makeGuides(
   strapiGuides: StrapiGuides
 ): readonly GuideDefinition[] {
-  return strapiGuides.data.map(({ attributes }) => {
-    const product = makeBaseProductWithoutLogo(attributes.product.data);
+  return _.compact(
+    strapiGuides.data.map(({ attributes }) => {
+      if (!attributes.slug) {
+        console.error('guide slug is missing:', attributes);
+        return null;
+      }
 
-    return {
-      product,
-      guide: {
-        name: attributes.title,
-        slug: attributes.slug,
-      },
-      versions: attributes.versions,
-      bannerLinks:
-        attributes.bannerLinks.length > 0
-          ? attributes.bannerLinks.map(makeBannerLink)
-          : attributes.product.data.attributes.bannerLinks?.map(
-              makeBannerLink
-            ) || [],
-      seo: attributes.seo || undefined,
-    };
-  });
+      if (!attributes.product.data.attributes.slug) {
+        console.error('product slug is missing:', attributes.product.data);
+        return null;
+      }
+
+      try {
+        const product = makeBaseProductWithoutLogo(attributes.product.data);
+        return {
+          product,
+          guide: {
+            name: attributes.title,
+            slug: attributes.slug,
+          },
+          versions: attributes.versions,
+          bannerLinks:
+            attributes.bannerLinks.length > 0
+              ? attributes.bannerLinks.map(makeBannerLink)
+              : attributes.product.data.attributes.bannerLinks?.map(
+                  makeBannerLink
+                ) || [],
+          seo: attributes.seo,
+        };
+      } catch (error) {
+        console.error(
+          'error creating guide definition for:',
+          attributes,
+          error
+        );
+        return null;
+      }
+    })
+  );
 }
