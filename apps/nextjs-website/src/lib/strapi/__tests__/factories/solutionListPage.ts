c62ec80354d9c26be7e831c7edb28b836ad14ba5
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2025-09-04 15:58:19 +0200

    Fix linting error

--- apps/nextjs-website/src/lib/strapi/__tests__/factories/solutionListPage.ts
+++ apps/nextjs-website/src/lib/strapi/__tests__/factories/solutionListPage.ts
@@ -83,7 +83,7 @@ export function solutionListPageWithMissingSolutionSlug(): StrapiSolutionListPag
                   .attributes,
                 title: 'Valid Solution',
               },
-            }
+            },
           ],
         },
       },
