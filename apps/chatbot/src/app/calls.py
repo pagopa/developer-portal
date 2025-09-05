de3b44f36b4ece7594c8af7395b02a9fd22aa02b
Devis Battisti <devis.battisti@uqido.com>
2025-09-05 12:51:05 +0200

    remove logs

--- apps/chatbot/src/app/calls.py
+++ apps/chatbot/src/app/calls.py
@@ -23,13 +23,13 @@ def chatbot_generate(payload: dict) -> dict:
         response_json = response.json()
         LOGGER.info(f"[calls] local lambda generate response_json={response_json}")
         response = response_json["result"]
+        # LOGGER.debug(f"[calls] lambda client generate response={response}")
     else:
         response = lambda_client.invoke(
             FunctionName=SETTINGS.chatbot_generate_lambda_name,
             InvocationType="Event",
             Payload=bytes(str(lambda_event), encoding="utf-8"),
         )
-        LOGGER.info(f"[calls] lambda client generate response={response}")
 
     return response
 
@@ -46,7 +46,7 @@ def chatbot_mask_pii(string: str) -> str:
             "http://core:8080/2015-03-31/functions/function/invocations",
             json=lambda_event,
         )
-        LOGGER.info(f"[calls] lambda mask_pii response={response.json()}")
+        # LOGGER.debug(f"[calls] lambda mask_pii response={response.json()}")
         response_json = response.json()
         response = response_json["result"]
     else:
