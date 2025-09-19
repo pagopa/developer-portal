18630baf4f516d586adf745b9e8a8752a55187cf
Marcello Bertoli <marcello.bertoli@uqido.com>
2025-09-19 11:55:19 +0200

    fix to tests

--- apps/nextjs-website/src/lib/strapi/__tests__/fixtures/overviews.ts
+++ apps/nextjs-website/src/lib/strapi/__tests__/fixtures/overviews.ts
@@ -23,6 +23,7 @@ export const strapiOverviews = {
           data: {
             attributes: {
               ...product,
+              tags: { data: [] },
               bannerLinks: generateBannerLinks(1),
               overview: {
                 data: {
@@ -123,6 +124,7 @@ export const strapiOverviews = {
               {
                 attributes: {
                   title: 'Tutorial 1',
+                  tags: { data: [] },
                   slug: 'tutorial-1',
                   image: {
                     data: mediaJpeg(),
