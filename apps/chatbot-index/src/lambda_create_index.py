d3e29e70b244ac35d91f2e0b0c0b6229b21f5878
mdciri <marco.cirillo@uqido.com>
2025-09-24 17:47:47 +0200

    Add create index lambda function in chatbot-index

---
+++ apps/chatbot-index/src/lambda_create_index.py
@@ -0,0 +1,18 @@
+import json
+from typing import Tuple, List, Dict
+
+from src.modules.logger import get_logger
+from src.modules.documents import read_file_from_s3
+from src.modules.vector_index import DiscoveryVectorIndex
+
+
+LOGGER = get_logger(__name__)
+VECTOR_INDEX = DiscoveryVectorIndex()
+
+
+def lambda_handler(event, context):
+    LOGGER.debug(f"event: {event}")
+
+    VECTOR_INDEX.create_index()
+
+    return {"statusCode": 200, "result": True, "event": event}
