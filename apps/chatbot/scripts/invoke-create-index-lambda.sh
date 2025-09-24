0c97dd6bf31bc0090caf10cff87ade19a65f3f30
mdciri <marco.cirillo@uqido.com>
2025-09-24 18:29:33 +0200

    Update invoke create index script

--- apps/chatbot/scripts/invoke-create-index-lambda.sh
+++ apps/chatbot/scripts/invoke-create-index-lambda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -X POST "http://localhost:8082/2015-03-31/functions/function/invocations" \
+curl -X POST "http://localhost:8083/2015-03-31/functions/function/invocations" \
 -H "Content-Type: application/json" \
 -d '{}'
