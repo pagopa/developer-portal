95ee796f679b769393257e1fa7c244df76cc53a9
mdciri <marco.cirillo@uqido.com>
2025-09-25 12:09:35 +0200

    Update settings

--- apps/chatbot-index/src/modules/settings.py
+++ apps/chatbot-index/src/modules/settings.py
@@ -4,9 +4,10 @@ import yaml
 from pathlib import Path
 from pydantic_settings import BaseSettings
 
-from src.modules.utils import get_ssm_parameter
+from src.modules.logger import get_logger
 
 
+LOGGER = get_logger(__name__)
 CWF = Path(__file__)
 ROOT = CWF.parent.parent.parent.absolute().__str__()
 PARAMS = yaml.safe_load(open(os.path.join(ROOT, "config", "params.yaml"), "r"))
@@ -15,6 +16,52 @@ AWS_SESSION = boto3.Session(
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     region_name=os.getenv("AWS_DEFAULT_REGION", "eu-south-1"),
 )
+SSM_CLIENT = boto3.client("ssm")
+
+
+def get_ssm_parameter(name: str | None, default: str | None = None) -> str | None:
+    """
+    Retrieves a specific value from AWS Systems Manager's Parameter Store.
+
+    :param name: The name of the parameter to retrieve.
+    :param default: The default value to return if the parameter is not found.
+    :return: The value of the requested parameter.
+    """
+
+    LOGGER.info(f"get_ssm_parameter {name}...")
+
+    if name is None:
+        name = "/none/param"
+    try:
+        # Get the requested parameter
+        response = SSM_CLIENT.get_parameter(Name=name, WithDecryption=True)
+        value = response["Parameter"]["Value"]
+    except SSM_CLIENT.exceptions.ParameterNotFound:
+        LOGGER.warning(
+            f"Parameter {name} not found in SSM, returning default: {default}"
+        )
+        return default
+
+    return value
+
+
+def put_ssm_parameter(name: str, value: str) -> None:
+    """
+    Puts a specific value into AWS Systems Manager's Parameter Store.
+    :param name: The name of the parameter to put.
+    :param value: The value to store in the parameter.
+    """
+
+    LOGGER.debug(f"Putting parameter {name} to SSM")
+    try:
+        SSM_CLIENT.put_parameter(
+            Name=name,
+            Value=value,
+            Type="String",
+            Overwrite=True,
+        )
+    except Exception as e:
+        LOGGER.error(e)
 
 
 class ChatbotSettings(BaseSettings):
