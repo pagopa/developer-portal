550b0f1fec768bb1e4fbb412794006c548234b9a
mdciri <marco.cirillo@uqido.com>
2025-09-25 13:50:53 +0200

    Update chatbot-index settings

--- apps/chatbot-index/src/modules/settings.py
+++ apps/chatbot-index/src/modules/settings.py
@@ -14,7 +14,7 @@ PARAMS = yaml.safe_load(open(os.path.join(ROOT, "config", "params.yaml"), "r"))
 AWS_SESSION = boto3.Session(
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_DEFAULT_REGION", "eu-south-1"),
+    region_name=os.getenv("AWS_REGION", "eu-south-1"),
 )
 SSM_CLIENT = boto3.client("ssm")
 
