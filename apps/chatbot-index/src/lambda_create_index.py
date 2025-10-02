e5a962b05ba93587099aded29a36d6a844559449
uolter <walter.traspadini@pagopa.it>
2025-10-02 14:22:33 +0200

    fix wrong import

--- apps/chatbot-index/src/lambda_create_index.py
+++ apps/chatbot-index/src/lambda_create_index.py
@@ -2,20 +2,20 @@ from src.modules.logger import get_logger
 from src.modules.vector_index import DiscoveryVectorIndex
 # open telemetry
 from opentelemetry import trace
-from opentelemetry.instrumentation.aws_lambda import AwsLambdaInstrumentor
+from opentelemetry.trace.status import Status, StatusCode
 
-AwsLambdaInstrumentor().instrument()
 
+tracer = trace.get_tracer(__name__)
 
 LOGGER = get_logger(__name__)
 VECTOR_INDEX = DiscoveryVectorIndex()
 
 
 def lambda_handler(event, context):
-    tracer = trace.get_tracer(__name__)
+    
     LOGGER.debug(f"event: {event}")
 
-    with tracer.start_as_current_span("my_lambda_span"):
+    with tracer.start_as_current_span("build_index_redis") as span:
         VECTOR_INDEX.create_index()
 
         return {"statusCode": 200, "result": True, "event": event}
