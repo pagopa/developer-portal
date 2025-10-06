cabe5c6a8defb2ae0e0f150555803b3f3eade77f
Devis Battisti <devis.battisti@uqido.com>
2025-10-06 16:25:18 +0200

    dynamodb region

--- apps/chatbot/scripts/dynamodb-init-local.sh
+++ apps/chatbot/scripts/dynamodb-init-local.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
+
+AWS_REGION=us-east-1
+
 aws dynamodb create-table \
 --endpoint-url http://dynamodb:8000 \
 --cli-input-json file://./docker/files/dynamodb_schemas/local/sessions.json \
---region eu-south-1
+--region $AWS_REGION
 
 aws dynamodb update-time-to-live \
   --endpoint-url http://dynamodb:8000 \
   --table-name chatbot-local-sessions \
   --time-to-live-specification "Enabled=true,AttributeName=expiresAt" \
-  --region eu-south-1
+  --region $AWS_REGION
 
 aws dynamodb create-table \
---endpoint-url http://dynamodb:8000 \
---cli-input-json file://./docker/files/dynamodb_schemas/local/queries.json \
---region eu-south-1
+  --endpoint-url http://dynamodb:8000 \
+  --cli-input-json file://./docker/files/dynamodb_schemas/local/queries.json \
+  --region $AWS_REGION
 
 aws dynamodb update-time-to-live \
   --endpoint-url http://dynamodb:8000 \
   --table-name chatbot-local-queries \
   --time-to-live-specification "Enabled=true,AttributeName=expiresAt" \
-  --region eu-south-1
+  --region $AWS_REGION
 
 aws dynamodb create-table \
 --endpoint-url http://dynamodb:8000 \
 --cli-input-json file://./docker/files/dynamodb_schemas/local/salts.json \
---region eu-south-1
+--region $AWS_REGION
 
 aws dynamodb update-time-to-live \
   --endpoint-url http://dynamodb:8000 \
   --table-name chatbot-local-salts \
   --time-to-live-specification "Enabled=true,AttributeName=expiresAt" \
-  --region eu-south-1
+  --region $AWS_REGION
 
 aws dynamodb list-tables \
   --endpoint-url http://dynamodb:8000 \
-  --region eu-south-1
+  --region $AWS_REGION
--- apps/chatbot/scripts/dynamodb-init-test.sh
+++ apps/chatbot/scripts/dynamodb-init-test.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-echo "-=-=-=-= AWS_ENDPOINT_URL: $AWS_ENDPOINT_URL -=-=-=-=-=-=-"
+AWS_REGION=us-east-1
 
 aws dynamodb create-table \
 --endpoint-url $AWS_ENDPOINT_URL \
 --cli-input-json file://./docker/files/dynamodb_schemas/test/sessions.json \
---region eu-south-1
+--region $AWS_REGION
 
 aws dynamodb update-time-to-live \
   --endpoint-url $AWS_ENDPOINT_URL \
   --table-name chatbot-test-sessions \
   --time-to-live-specification "Enabled=true,AttributeName=expiresAt" \
-  --region eu-south-1
+  --region $AWS_REGION
 
 aws dynamodb create-table \
---endpoint-url $AWS_ENDPOINT_URL \
---cli-input-json file://./docker/files/dynamodb_schemas/test/queries.json \
---region eu-south-1
+  --endpoint-url $AWS_ENDPOINT_URL \
+  --cli-input-json file://./docker/files/dynamodb_schemas/test/queries.json \
+  --region $AWS_REGION
 
 aws dynamodb update-time-to-live \
   --endpoint-url $AWS_ENDPOINT_URL \
   --table-name chatbot-test-queries \
   --time-to-live-specification "Enabled=true,AttributeName=expiresAt" \
-  --region eu-south-1
+  --region $AWS_REGION
 
 aws dynamodb create-table \
 --endpoint-url $AWS_ENDPOINT_URL \
 --cli-input-json file://./docker/files/dynamodb_schemas/test/salts.json \
---region eu-south-1
+--region $AWS_REGION
 
 aws dynamodb update-time-to-live \
   --endpoint-url $AWS_ENDPOINT_URL \
   --table-name chatbot-test-salts \
   --time-to-live-specification "Enabled=true,AttributeName=expiresAt" \
-  --region eu-south-1
+  --region $AWS_REGION
 
 aws dynamodb list-tables \
   --endpoint-url $AWS_ENDPOINT_URL \
-  --region eu-south-1
+  --region $AWS_REGION
