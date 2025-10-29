00b423f55e5ec4ecdfd423d40a51c5f14ae5cf66
Marco Ponchia <marco.ponchia@uqido.com>
2025-10-29 16:34:57 +0100

    Update helper

--- apps/nextjs-website/src/helpers/webinar.helpers.tsx
+++ apps/nextjs-website/src/helpers/webinar.helpers.tsx
@@ -3,6 +3,7 @@ import { Webinar } from '@/lib/types/webinar';
 
 const COMING_SOON_START_TIME_DELTA_MS = 39 * 30 * 60 * 1000;
 const CHECK_WEBINAR_STATUS_INTERVAL_MS = 500;
+const POLLING_WEBINAR_VIDEO_INTERVAL_MS = 3 * 1000; // every 3 seconds
 
 export enum WebinarState {
   future,
@@ -17,6 +18,27 @@ export const useWebinar = () => {
   const [webinarState, setWebinarState] = useState<WebinarState>(
     WebinarState.unknown
   );
+  const [webinarVideoVisible, setWebinarVideoVisible] =
+    useState<boolean>(false);
+
+  const updateWebinarVideoVisible = async (url?: string): Promise<void> => {
+    if (!url) {
+      setWebinarVideoVisible(false);
+      return;
+    }
+    const vimeoPattern = /vimeo\.com\/(\d+)/;
+    if (vimeoPattern.test(url)) {
+      setWebinarVideoVisible(true);
+      return;
+    }
+    // eslint-disable-next-line functional/no-try-statements
+    try {
+      const response = await fetch(url, { method: 'GET' });
+      setWebinarVideoVisible(response.ok);
+    } catch {
+      setWebinarVideoVisible(false);
+    }
+  };
 
   const startDateTimestamp =
     webinar?.startDateTime && new Date(webinar.startDateTime).getTime();
@@ -51,9 +73,23 @@ export const useWebinar = () => {
       setWebinarState(handleWebinarState());
     }, CHECK_WEBINAR_STATUS_INTERVAL_MS);
 
-    // Cleanup the interval when the component is unmounted
+    // Cleanup the intervals when the component is unmounted
     return () => clearInterval(intervalId);
   }, [webinar]);
 
-  return { webinarState, setWebinar };
+  useEffect(() => {
+    updateWebinarVideoVisible(webinar?.playerSrc);
+    const pollingIntervalId = setInterval(() => {
+      if (
+        webinar &&
+        [WebinarState.live, WebinarState.comingSoon].includes(webinarState)
+      ) {
+        updateWebinarVideoVisible(webinar.playerSrc);
+      }
+    }, POLLING_WEBINAR_VIDEO_INTERVAL_MS);
+
+    return () => clearInterval(pollingIntervalId);
+  }, [webinarState]);
+
+  return { webinarState, setWebinar, webinarVideoVisible };
 };
