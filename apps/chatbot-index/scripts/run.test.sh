85ac7e592a9574b63cf08297e6a2032702b8be34
Devis Battisti <devis.battisti@uqido.com>
2025-11-04 12:33:29 +0100

    working tests

--- apps/chatbot-index/scripts/run.test.sh
+++ apps/chatbot-index/scripts/run.test.sh
@@ -2,9 +2,4 @@
 
 ./scripts/s3-init.sh > /dev/null 2>&1
 
-echo '-=-=-=-= run pytest'
-# poetry run pytest src
-
-curl -XPOST "http://localhost:8080/2015-03-31/functions/function/invocations" \
-  -H "Content-Type: application/json" \
-  -d @scripts/event.json
+poetry run pytest src
