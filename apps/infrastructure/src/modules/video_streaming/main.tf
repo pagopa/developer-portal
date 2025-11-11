d837daa572029059a59b689aa7b111ff81eede9e
uolter <walter.traspadini@pagopa.it>
2025-11-11 17:30:34 +0100

    fix envvar name in ivs lambda

--- apps/infrastructure/src/modules/video_streaming/main.tf
+++ apps/infrastructure/src/modules/video_streaming/main.tf
@@ -380,9 +380,9 @@ resource "aws_lambda_function" "ivs_video_processing_function" {
 
   environment {
     variables = {
-      VIDEO_BASE_URL           = "https://${var.custom_domain_name}"
-      STRAPI_API_URL           = var.strapi_api_url
-      STRASTRAPI_IVS_API_TOKEN = aws_ssm_parameter.strapi_api_key.name
+      VIDEO_BASE_URL       = "https://${var.custom_domain_name}"
+      STRAPI_API_URL       = var.strapi_api_url
+      STRAPI_IVS_API_TOKEN = aws_ssm_parameter.strapi_api_key.name
     }
   }
 
