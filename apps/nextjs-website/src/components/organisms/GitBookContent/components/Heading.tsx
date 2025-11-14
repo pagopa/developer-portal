6a6b8381b52e4d373660e0938e6fe792d9907749
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2025-11-14 16:58:42 +0100

    Fix linting

--- apps/nextjs-website/src/components/organisms/GitBookContent/components/Heading.tsx
+++ apps/nextjs-website/src/components/organisms/GitBookContent/components/Heading.tsx
@@ -2,18 +2,13 @@ import { HeadingProps } from 'gitbook-docs/markdoc/schema/heading';
 import { ReactNode } from 'react';
 import ContentHeading from '@/components/atoms/ContentHeading/ContentHeading';
 
-
 export const SubHeading = ({
   level,
   id,
   children,
 }: HeadingProps<ReactNode>) => {
   return (
-    <ContentHeading
-      level={level}
-      id={id}
-      skipH1={true}
-    >
+    <ContentHeading level={level} id={id} skipH1={true}>
       {children}
     </ContentHeading>
   );
