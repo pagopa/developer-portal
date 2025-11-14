5f76a2100e24be0adb5b6f06cce4fda7213ff843
tommaso1 <tommasorosso1@gmail.com>
2025-11-14 16:23:18 +0100

    Refactor release note and solution page components to remove unused metadata handling and enforce dynamic rendering (#1837)
    Co-authored-by: Your Name <youremail@yourdomain.com>
    Co-authored-by: marcobottaro <39835990+marcobottaro@users.noreply.github.com>

--- apps/nextjs-website/src/app/[productSlug]/[...releaseNoteSubPathSlugs]/page.tsx
+++ apps/nextjs-website/src/app/[productSlug]/[...releaseNoteSubPathSlugs]/page.tsx
@@ -24,34 +24,15 @@ import {
   makeMetadataFromStrapi,
 } from '@/helpers/metadata.helpers';
 import { BreadcrumbSegment } from '@/lib/types/path';
-import { baseUrl, REVALIDATE_LONG_INTERVAL } from '@/config';
+import { baseUrl } from '@/config';
 import PageNotFound from '@/app/not-found';
-import { getReleaseNotesMetadata } from '@/helpers/s3Metadata.helpers';
 
 type ReleaseNotePageStaticParams = {
   productSlug: string;
   releaseNoteSubPathSlugs: string[];
 };
-// TODO: remove when release notes metadata will be managed in strapi
-export const revalidate = REVALIDATE_LONG_INTERVAL;
+export const dynamic = 'force-dynamic';
 
-const PRODUCT_SLUG_PATH_INDEX = 1;
-const RELEASE_NOTE_SUB_PATH_INDEX = 2;
-export async function generateStaticParams(): Promise<
-  ReleaseNotePageStaticParams[]
-> {
-  const releaseNotes = await getReleaseNotesMetadata();
-  const releaseNoteParams = releaseNotes
-    .map(({ path }) => path.split('/'))
-    .filter((paths) => paths.length > RELEASE_NOTE_SUB_PATH_INDEX)
-    .map((paths) => {
-      return {
-        productSlug: paths[PRODUCT_SLUG_PATH_INDEX],
-        releaseNoteSubPathSlugs: paths.slice(RELEASE_NOTE_SUB_PATH_INDEX),
-      };
-    });
-  return releaseNoteParams;
-}
 export async function generateMetadata({
   params,
 }: {
