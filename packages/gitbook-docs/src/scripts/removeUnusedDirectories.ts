797f4f36b628bf1f9e7ba245cd3b62e8cc1e1188
Marcello Bertoli <marcello.bertoli@uqido.com>
2025-11-25 09:50:15 +0100

    switch to putS3File

--- packages/gitbook-docs/src/scripts/removeUnusedDirectories.ts
+++ packages/gitbook-docs/src/scripts/removeUnusedDirectories.ts
@@ -17,7 +17,7 @@ import {
   deleteS3Directory,
   downloadS3File,
   makeS3Client,
-  writeMetadataJson,
+  putS3File,
 } from '../helpers/s3Bucket.helper';
 import { S3Client } from '@aws-sdk/client-s3';
 import path from 'path';
@@ -248,7 +248,7 @@ async function main() {
         getS3Client()
       );
     });
-    await writeMetadataJson(
+    await putS3File(
       dirNames,
       S3_DIRNAME_FILE_PATH,
       S3_BUCKET_NAME!,
