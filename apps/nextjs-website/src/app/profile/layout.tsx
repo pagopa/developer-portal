4e59651e110500f89cabb2ee32399d39f8f9987c
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2025-11-25 17:32:20 +0100

    Move ProfileMenu into profile/layout

--- apps/nextjs-website/src/app/profile/layout.tsx
+++ apps/nextjs-website/src/app/profile/layout.tsx
@@ -1,8 +1,40 @@
 'use client';
+import PageNotFound from '@/app/not-found';
 import React, { ReactNode } from 'react';
-import ProfileMenu from '@/components/organisms/ProfileMenu/ProfileMenu';
+import { Box } from '@mui/material';
+import { usePathname } from 'next/navigation';
+import Spinner from '@/components/atoms/Spinner/Spinner';
+import DesktopProfileMenu from '@/components/molecules/DesktopProfileMenu/DesktopProfileMenu';
+import MobileProfileMenu from '@/components/molecules/MobileProfileMenu/MobileProfileMenu';
+import EContainer from '@/editorialComponents/EContainer/EContainer';
+import { useUser } from '@/helpers/user.helper';
+
 const ProfileLayout = ({ children }: { children: ReactNode }) => {
-  return <ProfileMenu>{children}</ProfileMenu>;
+  const { user, loading, userFullName } = useUser();
+
+  const pathname = usePathname();
+
+  if (!loading && !user) {
+    return <PageNotFound />;
+  }
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <MobileProfileMenu userFullName={userFullName} />
+      <EContainer>
+        <Box sx={{ display: 'flex', mt: { xs: '60px', md: 0 }, width: '100%' }}>
+          <DesktopProfileMenu
+            currentPathname={pathname}
+            userFullName={userFullName}
+          />
+          {children}
+        </Box>
+      </EContainer>
+    </>
+  );
 };
 
 export default ProfileLayout;
