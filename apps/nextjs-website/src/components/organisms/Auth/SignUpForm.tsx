b54e2259ebd483d64a912f51d744b1481223ba2f
Marco Ponchia <marco.ponchia@uqido.com>
2025-12-02 18:21:02 +0100

    Add checkbox for survey in sign up form

--- apps/nextjs-website/src/components/organisms/Auth/SignUpForm.tsx
+++ apps/nextjs-website/src/components/organisms/Auth/SignUpForm.tsx
@@ -301,6 +301,18 @@ const SignUpForm = ({
                   label={t('auth.signUp.confirmComunications')}
                   sx={{ alignItems: 'flex-start' }}
                 />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name='surveyAccepted'
+                      checked={userData.surveyAccepted}
+                      sx={{ marginTop: '-4px' }}
+                      onChange={handleInputChange}
+                    />
+                  }
+                  label={t('auth.signUp.confirmSurvey')}
+                  sx={{ alignItems: 'flex-start', marginTop: 2 }}
+                />
               </Grid>
               <Stack pt={3}>
                 <Stack direction='row' justifyContent='center'>
