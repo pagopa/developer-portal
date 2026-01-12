a577d58deb00c42fc5eb84606db3c64459048d2b
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2026-01-12 15:25:46 +0100

    Remove commented-out Grafana resources from Terraform files
    Eliminated unused Grafana-related Route53 and Secrets Manager resources that were commented out in the Terraform configuration. This cleanup improves code readability and removes unnecessary clutter.

--- apps/infrastructure/src/modules/langfuse/route53.tf
+++ apps/infrastructure/src/modules/langfuse/route53.tf
@@ -9,11 +9,3 @@ resource "aws_route53_record" "langfuse" {
     evaluate_target_health = true
   }
 }
-
-# resource "aws_route53_record" "grafana" {
-#   name    = "grafana.${var.custom_domain_name}"
-#   zone_id = var.custom_domain_id
-#   type    = "CNAME"
-#   records = [aws_apprunner_service.grafana.service_url]
-#   ttl     = 300
-# }
