48861538961e24d239da6fd8b20c1fb91bc0a2e5
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2026-01-12 10:09:51 +0100

    Enable Route53 record for langfuse.
    Uncommented and activated the previously disabled Route53 record configuration for langfuse. This enables DNS resolution via the associated load balancer.

--- apps/infrastructure/src/modules/langfuse/route53.tf
+++ apps/infrastructure/src/modules/langfuse/route53.tf
@@ -1,14 +1,14 @@
-# resource "aws_route53_record" "langfuse" {
-#   name    = local.langfuse_domain_name
-#   zone_id = var.custom_domain_id
-#   type    = "A"
-#
-#   alias {
-#     name                   = module.langfuse_load_balancer.dns_name
-#     zone_id                = module.langfuse_load_balancer.zone_id
-#     evaluate_target_health = true
-#   }
-# }
+resource "aws_route53_record" "langfuse" {
+  name    = local.langfuse_domain_name
+  zone_id = var.custom_domain_id
+  type    = "A"
+
+  alias {
+    name                   = module.langfuse_load_balancer.dns_name
+    zone_id                = module.langfuse_load_balancer.zone_id
+    evaluate_target_health = true
+  }
+}
 
 # resource "aws_route53_record" "grafana" {
 #   name    = "grafana.${var.custom_domain_name}"
