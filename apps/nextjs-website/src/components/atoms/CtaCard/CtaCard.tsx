f24aef6cc080656034f0a2e24e70931317040b63
Marco Ponchia <marco.ponchia@uqido.com>
2026-01-12 16:52:22 +0100

    refactor: update CtaCard and SolutionStep components for improved styling

--- apps/nextjs-website/src/components/atoms/CtaCard/CtaCard.tsx
+++ apps/nextjs-website/src/components/atoms/CtaCard/CtaCard.tsx
@@ -97,6 +97,7 @@ const CtaCard = ({
                     sx={{
                       paddingY: 0.2,
                       maxHeight: 'auto',
+                      maxWidth: 'auto',
                       marginBottom: 1,
                     }}
                   />
