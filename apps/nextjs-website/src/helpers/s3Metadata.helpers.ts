70ca68bbfe47092b5dc1a35f528f99801d8d7cf6
Marcello Bertoli <marcello.bertoli@uqido.com>
2026-01-21 12:48:47 +0100

    hardcoded path to metadata

--- apps/nextjs-website/src/helpers/s3Metadata.helpers.ts
+++ apps/nextjs-website/src/helpers/s3Metadata.helpers.ts
@@ -169,13 +169,9 @@ export async function fetchMetadataFromCDN<T>(
   return await requestPromise;
 }
 
-const S3_GUIDES_METADATA_JSON_PATH =
-  process.env.S3_GUIDES_METADATA_JSON_PATH || 'guides-metadata.v5.json';
-const S3_SOLUTIONS_METADATA_JSON_PATH =
-  process.env.S3_SOLUTIONS_METADATA_JSON_PATH || 'solutions-metadata.v5.json';
-const S3_RELEASE_NOTES_METADATA_JSON_PATH =
-  process.env.S3_RELEASE_NOTES_METADATA_JSON_PATH ||
-  'release-notes-metadata.v5.json';
+const S3_GUIDES_METADATA_JSON_PATH = 'guides-metadata.v5.json';
+const S3_SOLUTIONS_METADATA_JSON_PATH = 'solutions-metadata.v5.json';
+const S3_RELEASE_NOTES_METADATA_JSON_PATH = 'release-notes-metadata.v5.json';
 const S3_SOAP_API_METADATA_JSON_PATH =
   process.env.S3_SOAP_API_METADATA_JSON_PATH ||
   'soap-api/soap-api-metadata.json';
