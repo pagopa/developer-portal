22399984e4264085736db2cd6dd148107046b67f
mdciri <marco.cirillo@uqido.com>
2026-01-27 14:51:15 +0100

    Update spaces and indentation

--- apps/chatbot-monitor/src/modules/test_lambda.py
+++ apps/chatbot-monitor/src/modules/test_lambda.py
@@ -1,25 +1,26 @@
 import requests
 import json
 
+
 def test_lambda_invocation_create_trace():
     url = "http://localhost:8080/2015-03-31/functions/function/invocations"
     headers = {"Content-Type": "application/json"}
-    
+
     with open("files/event_create_trace.json", "r") as f:
         payload = json.load(f)
-    
+
     response = requests.post(url, headers=headers, json=payload)
-    
+
     assert response.status_code == 200
 
 
 def test_lambda_invocation_add_score():
     url = "http://localhost:8080/2015-03-31/functions/function/invocations"
     headers = {"Content-Type": "application/json"}
-    
+
     with open("files/event_add_score.json", "r") as f:
         payload = json.load(f)
-    
+
     response = requests.post(url, headers=headers, json=payload)
-    
+
     assert response.status_code == 200
