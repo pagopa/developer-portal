d329d8b66979b9745f908d89a0da1ad809b068d5
Marco Bottaro <marco.bottaro@uqido.com>
2026-02-09 16:31:27 +0100

    Fix Rapidoc rendering issue by optimizing ApiViewer loading

---
+++ .changeset/happy-groups-agree.md
@@ -0,0 +1,5 @@
+---
+"nextjs-website": patch
+---
+
+Add english locale to DEV and UAT environments
--- apps/nextjs-website/src/locales.ts
+++ apps/nextjs-website/src/locales.ts
@@ -1,6 +1,9 @@
+import { isProduction } from '@/config';
+
 export const SUPPORTED_LOCALES = [
   {
     code: 'it',
     label: 'IT',
   },
+  ...(isProduction ? [] : [{ code: 'en', label: 'EN' }]),
 ];
