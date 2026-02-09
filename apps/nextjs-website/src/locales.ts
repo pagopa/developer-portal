866255445238fc1e7b615582ce557015fae7404e
Marco Bottaro <marco.bottaro@uqido.com>
2026-02-09 16:42:45 +0100

    Add English locale to DEV and UAT environments

--- apps/nextjs-website/src/locales.ts
+++ apps/nextjs-website/src/locales.ts
@@ -1,6 +1,10 @@
+import { isProduction } from '@/config';
+
 export const SUPPORTED_LOCALES = [
   {
     code: 'it',
     label: 'IT',
   },
+  // TODO: Remove this condition when the English version of the website will be ready to be published in production
+  ...(isProduction ? [] : [{ code: 'en', label: 'EN' }]),
 ];
