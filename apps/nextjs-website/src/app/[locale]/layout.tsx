545aa2e451091b233533c35c18df69b9654b567c
marcobottaro <39835990+marcobottaro@users.noreply.github.com>
2026-02-10 18:14:22 +0100

    Apply suggestion from @marcobottaro

--- apps/nextjs-website/src/app/[locale]/layout.tsx
+++ apps/nextjs-website/src/app/[locale]/layout.tsx
@@ -125,7 +125,7 @@ export default async function RootLayout({
                 ? MATOMO_TAG_MANAGER_SCRIPT
                 : PREVIOUS_MATOMO_TAG_MANAGER_SCRIPT,
             }}
-            strategy="lazyOnload"
+            strategy='lazyOnload'
           />
         )}
       </head>
