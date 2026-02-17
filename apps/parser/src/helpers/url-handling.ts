4fa6fa89f0ab848c40382bf5067dae7fdb7db8c5
Noemi Manara <noemi.manara@gmail.com>
2026-02-17 17:21:11 +0100

    Minor fix

--- apps/parser/src/helpers/url-handling.ts
+++ apps/parser/src/helpers/url-handling.ts
@@ -162,7 +162,7 @@ export function buildVisitKey(rawUrl: string): string {
     const url = new URL(rawUrl);
     url.hash = "";
     url.hostname = url.hostname.replace(/^www\./, "");
-    return RemoveAnchorsFromUrl(url.hostname.toString()+'/'+url.pathname.toString());
+    return RemoveAnchorsFromUrl(url.hostname.toString()+url.pathname.toString());
   } catch (error) {
     console.warn(`Failed to build visit key for URL: ${rawUrl}`, error);
     return rawUrl;
