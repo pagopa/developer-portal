4fd32ba6e2aa362ddc4dba7fea10b9db37216c23
batdevis <devis.battisti@uqido.com>
2026-02-19 11:19:54 +0100

    parser script example

---
+++ apps/parser/src/main.ts
@@ -0,0 +1,5 @@
+const PARSER_VECTOR_INDEX_NAME = process.env.PARSER_VECTOR_INDEX_NAME;
+const URL = process.env.URL;
+
+console.log("PARSER_VECTOR_INDEX_NAME:", PARSER_VECTOR_INDEX_NAME);
+console.log("URL:", URL);
--- apps/parser/src/modules/errors.ts
+++
@@ -1,4 +0,0 @@
-export function handleError(error: unknown) {
-  console.error('Parser terminated with an error:', error);
-  process.exitCode = 1;
-}
