5ee31ce36b9b925d3b34e870aadd29807f0e71b0
Marcello Bertoli <marcello.bertoli@uqido.com>
2026-02-19 09:45:33 +0100

    add storybook component

---
+++ apps/storybook-app/stories/atoms/ChatbotChip.stories.tsx
@@ -0,0 +1,22 @@
+import { Decorator, Meta, StoryObj } from '@storybook/nextjs';
+import ChatbotChip from 'nextjs-website/src/components/atoms/ChatbotChip/ChatbotChip';
+import { nextIntlContextDecorator } from '../next-intl-context.helper';
+import React from 'react';
+
+const meta: Meta<typeof ChatbotChip> = {
+  title: 'Atoms/ChatButton',
+  component: ChatbotChip,
+};
+
+export default meta;
+
+const decorator: Decorator = (story) => (
+  <div style={{ backgroundColor: '#FFFFFF', padding: '2rem' }}>{story()}</div>
+);
+
+export const Showcase: StoryObj<typeof ChatbotChip> = {
+  args: {
+    label: 'Storico pagamenti',
+  },
+  decorators: [decorator, nextIntlContextDecorator],
+};
