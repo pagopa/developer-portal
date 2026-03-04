29ebba620a2faccd9b962f8a9a31c67763ac4dfd
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2026-03-04 10:23:59 +0100

    Add tests for isExternalLink helper function

---
+++ apps/nextjs-website/src/__tests__/helpers/navigation.helpers.test.ts
@@ -0,0 +1,35 @@
+import { isExternalLink } from '../../helpers/navigation.helpers';
+
+describe('isExternalLink', () => {
+  const currentHost = 'example.com';
+
+  it('returns true for http external link', () => {
+    expect(isExternalLink(currentHost, 'http://external.com')).toBe(true);
+  });
+
+  it('returns true for https external link', () => {
+    expect(isExternalLink(currentHost, 'https://external.com')).toBe(true);
+  });
+
+  it('returns false for http link to current host', () => {
+    expect(isExternalLink(currentHost, 'http://example.com/page')).toBe(false);
+  });
+
+  it('returns false for https link to current host', () => {
+    expect(isExternalLink(currentHost, 'https://example.com/other')).toBe(
+      false
+    );
+  });
+
+  it('returns false for relative link', () => {
+    expect(isExternalLink(currentHost, '/internal')).toBe(false);
+  });
+
+  it('returns false for mailto link', () => {
+    expect(isExternalLink(currentHost, 'mailto:test@example.com')).toBe(false);
+  });
+
+  it('returns false for anchor link', () => {
+    expect(isExternalLink(currentHost, '#section')).toBe(false);
+  });
+});
