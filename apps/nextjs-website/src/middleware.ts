8d0a0447000fe6d0290bd435984da81e90dbdee0
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2026-03-04 17:27:01 +0100

    Refactor authentication handling: remove user redirect logic from components and implement middleware for authenticated user redirection

--- apps/nextjs-website/src/middleware.ts
+++ apps/nextjs-website/src/middleware.ts
@@ -1,12 +1,36 @@
 /* eslint-disable functional/no-expression-statements */
 import { NextRequest, NextResponse } from 'next/server';
-import { defaultLocale } from '@/config';
+import { amplifyConfig, defaultLocale } from '@/config';
 import { SUPPORTED_LOCALES } from '@/locales';
+
+const guestPaths = [
+  '/auth/login',
+  '/auth/sign-up',
+  '/auth/password-reset',
+  '/auth/expired-code',
+  '/auth/account-activated',
+  '/auth/change-password',
+];
 
 // This middleware redirects URLs without locale prefixes to include the default locale,
 // maintaining backwards compatibility with legacy indexed URLs and CMS' resources URLs
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+
+  if (guestPaths.some((path) => pathname.includes(path))) {
+    const clientId = amplifyConfig.Auth.userPoolWebClientId;
+    const lastAuthUserCookie = request.cookies.get(
+      `CognitoIdentityServiceProvider.${clientId}.LastAuthUser`
+    );
+
+    if (lastAuthUserCookie) {
+      const locale = SUPPORTED_LOCALES.find((loc) =>
+        request.url.includes(`/${loc.langCode}/`)
+      )?.langCode;
+      return NextResponse.redirect(new URL(`/${locale ?? ''}`, request.url));
+    }
+  }
+
   const pathnameHasLocale = SUPPORTED_LOCALES.some(
     (locale) =>
       pathname.startsWith(`/${locale.langCode}/`) ||
