9546f7b5c51e5aa0f266a475e9d361bca4665b48
Walter Traspadini <walter.traspadini@pagopa.it>
2026-03-05 11:06:20 +0100

    [DEV-3767] Fix permission deploy cognito lambda functions (#2106)
    * Update iam policy deploy website
    
    * fix iam policy
    
    * + changeset

--- apps/infrastructure/src/modules/website/iam_policy.tf
+++ apps/infrastructure/src/modules/website/iam_policy.tf
@@ -63,7 +63,8 @@ resource "aws_iam_policy" "deploy_website" {
         ]
         Effect = "Allow"
         Resource = [
-          "arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:ac-${var.environment}-*"
+          "arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:ac-${var.environment}-*",
+          "arn:aws:lambda:${var.aws_region}:${data.aws_caller_identity.current.account_id}:function:cognito_*",
         ]
       },
       {
