3a89b63a543f19a1d3eff83e455142ea4411ecd9
batdevis <devis.battisti@uqido.com>
2026-03-06 10:17:25 +0100

    put s3 product file

--- apps/chatbot/scripts/s3-init.sh
+++ apps/chatbot/scripts/s3-init.sh
@@ -7,3 +7,6 @@ aws s3api create-bucket --bucket $CHB_AWS_S3_BUCKET_NAME_STATIC_CONTENT \
   --endpoint-url=$AWS_ENDPOINT_URL \
   --region $AWS_REGION
 
+aws s3 cp ./docker/files/s3/it/synced-products-response.json s3://$CHB_AWS_S3_BUCKET_NAME_STATIC_CONTENT/it/synced-products-response.json \
+  --endpoint-url=$AWS_ENDPOINT_URL \
+  --region $AWS_REGION
