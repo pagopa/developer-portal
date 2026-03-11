2a0eefe70be0679de3924127dd217136b3865c62
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2026-03-11 15:00:21 +0100

    Set submitting state to false when after onConfirmLogin completes

--- apps/nextjs-website/src/components/organisms/Auth/ConfirmLogin.tsx
+++ apps/nextjs-website/src/components/organisms/Auth/ConfirmLogin.tsx
@@ -54,6 +54,7 @@ const ConfirmLogin = ({
       } else if (e.name === 'NotAuthorizedException') {
         setErrors((prev) => ({ ...prev, codeError: true }));
       }
+      setSubmitting(false);
     });
   }, [onConfirmLogin, code]);
 
