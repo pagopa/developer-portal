3443518235902d108b982b2e9172182526fdfd46
Noemi Manara <noemi.manara@gmail.com>
2026-03-13 12:19:06 +0100

    Update compose and files for desired behavior

--- apps/structured-data-cleaner/scripts/s3-init.sh
+++ apps/structured-data-cleaner/scripts/s3-init.sh
@@ -7,6 +7,10 @@ aws s3api create-bucket --bucket $S3_BUCKET_NAME \
   --endpoint-url=$AWS_ENDPOINT_URL \
   --region $AWS_REGION
 
-aws s3 cp ./docker/files/s3/guides-metadata.json s3://$S3_BUCKET_NAME/guides-metadata.json \
+aws s3 cp --recursive ./files/s3/$CHB_INDEX_ID/parser/example.com/ s3://$S3_BUCKET_NAME/$CHB_INDEX_ID/parser/example.com/ \
+  --endpoint-url=$AWS_ENDPOINT_URL \
+  --region $AWS_REGION
+  
+aws s3 cp --recursive ./files/s3/$CHB_INDEX_ID/extractor/example.com/ s3://$S3_BUCKET_NAME/$CHB_INDEX_ID/extractor/example.com/ \
   --endpoint-url=$AWS_ENDPOINT_URL \
   --region $AWS_REGION
