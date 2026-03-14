f6e344e7db3f67929a0280ff15fd1f4ad3b63281
uolter <walter.traspadini@pagopa.it>
2026-03-14 17:49:35 +0100

    lambda evaluate new ragas max retries and max workers env variables

--- apps/infrastructure/src/modules/chatbot/lambda_evaluate.tf
+++ apps/infrastructure/src/modules/chatbot/lambda_evaluate.tf
@@ -124,6 +124,8 @@ resource "aws_lambda_function" "chatbot_evaluate_lambda" {
       CHB_AWS_SSM_GOOGLE_API_KEY      = module.google_api_key_ssm_parameter.ssm_parameter_name
       CHB_AWS_SSM_LANGFUSE_PUBLIC_KEY = module.langfuse_public_key.ssm_parameter_name
       CHB_AWS_SSM_LANGFUSE_SECRET_KEY = module.langfuse_secret_key.ssm_parameter_name
+      RAGAS_MAX_RETRIES               = 3
+      RAGAS_MAX_WORKERS               = 2
       RAGAS_DO_NOT_TRACK              = "True"
     }
   }
