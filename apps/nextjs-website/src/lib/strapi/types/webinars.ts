7aa1d850a872adffec0cf0da350c2009fdf189cb
Marco Ponchia <marco.ponchia@uqido.com>
2026-03-25 17:16:39 +0100

    Add chapters and VTT content support to webinar types and props

--- apps/nextjs-website/src/lib/strapi/types/webinars.ts
+++ apps/nextjs-website/src/lib/strapi/types/webinars.ts
@@ -4,6 +4,7 @@ import { StrapiRelatedLinks } from '@/lib/strapi/types/link';
 import { StrapiSeo } from '@/lib/strapi/types/seo';
 import { StrapiWebinarCategory } from '@/lib/strapi/types/webinarCategory';
 import { Paginated } from '@/lib/strapi/types/paginated';
+import { Chapter } from '@/lib/types/webinar';
 
 type StrapiWebinarSpeaker = {
   readonly id: number;
@@ -62,6 +63,7 @@ export type StrapiWebinar = {
     readonly webinarCategory: { readonly data?: StrapiWebinarCategory };
     readonly headerImage: { readonly data?: StrapiMedia };
     readonly updatedAt: string;
+    readonly chapters?: readonly Chapter[];
   };
 };
 
