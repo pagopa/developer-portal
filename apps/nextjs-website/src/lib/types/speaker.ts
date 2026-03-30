d7a4c83bf46e9f0eee32ec8d123158db000d05ba
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2026-03-30 17:17:33 +0200

    Refactor: remove unused type definitions for auth and speaker

--- apps/nextjs-website/src/lib/types/speaker.ts
+++
@@ -1,9 +0,0 @@
-import { BlocksContent } from '@strapi/blocks-react-renderer';
-import { Media } from '@/lib/media/types';
-
-export type Speaker = {
-  readonly name: string;
-  readonly jobTitle: string;
-  readonly description?: BlocksContent;
-  readonly avatar?: Media;
-};
