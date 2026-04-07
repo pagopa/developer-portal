f4f5373f7d53de5e6d9cab218856b998c66b9c57
Marco Bottaro <marco.bottaro@uqido.com>
2026-04-07 16:55:39 +0200

    Update StrapiWebinars type to change id to number and make startDatetime and endDatetime optional

--- apps/ivs-functions/src/types/strapiWebinars.ts
+++ apps/ivs-functions/src/types/strapiWebinars.ts
@@ -1,10 +1,10 @@
 export type StrapiWebinars = {
   readonly data: readonly {
-    readonly id: string;
+    readonly id: number;
     readonly documentId: string;
     readonly slug: string;
-    readonly startDatetime: string;
-    readonly endDatetime: string;
+    readonly startDatetime?: string;
+    readonly endDatetime?: string;
   }[];
   readonly meta: {
     readonly pagination: {
