33992cd85d013f3b41185e24b6ba7d5aa1370086
Sebastiano Bertolin <sebastiano.bertolin@uqido.com>
2026-04-15 16:35:21 +0200

    Remove console logs

--- packages/gitbook-docs/src/scripts/generateUrlParsingMetadata.ts
+++ packages/gitbook-docs/src/scripts/generateUrlParsingMetadata.ts
@@ -42,8 +42,6 @@ export function generateUrlPath(
   locale?: string
 ): string {
   const restOfPath = sitePathFromLocalPath(filePath, dirName, undefined);
-  console.log(`Rest of path for file ${filePath}: ${restOfPath}`);
-  console.log(`Version name: ${versionName}`);
   switch (metadataType) {
     case MetadataType.Guide:
       return [
