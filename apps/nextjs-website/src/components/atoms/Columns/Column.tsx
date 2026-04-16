9451e961e64e09000c211fb8b508cda6088349bd
Marco Ponchia <marco.ponchia@uqido.com>
2026-04-16 12:26:42 +0200

    refactor: remove unused theme imports and format Box component for better readability

--- apps/nextjs-website/src/components/atoms/Columns/Column.tsx
+++ apps/nextjs-website/src/components/atoms/Columns/Column.tsx
@@ -1,12 +1,18 @@
 'use client';
 import { ReactNode } from 'react';
 import { ColumnProps } from 'gitbook-docs/markdoc/schema/columns';
-import { Box, useTheme } from '@mui/material';
+import { Box } from '@mui/material';
 
 const Column = ({ children, width }: ColumnProps<ReactNode>) => {
-  const { palette } = useTheme();
   return (
-    <Box sx={{ display: 'flex', width: { xs: 'auto', sm: width }, flexDirection: 'column', gap: '16px' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        width: { xs: 'auto', sm: width },
+        flexDirection: 'column',
+        gap: '16px',
+      }}
+    >
       {children}
     </Box>
   );
--- apps/nextjs-website/src/components/atoms/Columns/Columns.tsx
+++ apps/nextjs-website/src/components/atoms/Columns/Columns.tsx
@@ -1,12 +1,17 @@
 'use client';
 import { ReactNode } from 'react';
 import { ColumnsProps } from 'gitbook-docs/markdoc/schema/columns';
-import { Box, useTheme } from '@mui/material';
+import { Box } from '@mui/material';
 
 const Columns = ({ children }: ColumnsProps<ReactNode>) => {
-  const { palette } = useTheme();
   return (
-    <Box sx={{ display: 'flex', flexDirection: { xs: 'column', sm: 'row' }, gap: '16px' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column', sm: 'row' },
+        gap: '16px',
+      }}
+    >
       {children}
     </Box>
   );
