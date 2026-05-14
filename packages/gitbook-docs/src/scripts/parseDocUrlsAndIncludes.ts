87acdfbd0ef89055a65c2c351f12ebf5aac428ae
Marcello Bertoli <marcello.bertoli@uqido.com>
2026-05-14 10:05:23 +0200

    fix issues with searching for spaceid in file

--- packages/gitbook-docs/src/scripts/parseDocUrlsAndIncludes.ts
+++ packages/gitbook-docs/src/scripts/parseDocUrlsAndIncludes.ts
@@ -39,11 +39,7 @@ async function recursiveParseMarkdownFiles(
         fullPath,
         guideMetadata
           ? guideMetadata
-          : metadata.find((data) =>
-              fullPath.includes(
-                data.spaceId?.length > 0 ? data.spaceId : data.dirName
-              )
-            )
+          : metadata.find((data) => fullPath.includes(data.dirName))
       );
     } else if (item.isFile() && fullPath.endsWith('.md')) {
       if (!guideMetadata) return;
