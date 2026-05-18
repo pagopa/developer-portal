c71bf3cff6cc31034199e52e0d9cc8ebb9b78523
uolter <walter.traspadini@pagopa.it>
2026-05-18 15:27:38 +0200

    removed module strapi migration and its reference

--- apps/infrastructure/src/modules/strapi_migration/cloudwatch_log_group.tf
+++
@@ -1,7 +0,0 @@
-# CloudWatch Log Group for Strapi CMS
-module "cms_log_group" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-cloudwatch.git//modules/log-group?ref=bf969da953bdbea229392255d2b36e7b720e917e" # v4.3.0
-
-  name              = "/cms/strapi-v4"
-  retention_in_days = 7
-}
