b0dcd112babfaba2d22e2d2471caa48453ee11d8
Marcello Bertoli <marcello.bertoli@uqido.com>
2026-06-10 12:19:38 +0200

    add monitoring accepted field

--- apps/infrastructure/src/modules/auth/cognito_user.tf
+++ apps/infrastructure/src/modules/auth/cognito_user.tf
@@ -128,6 +128,14 @@ resource "aws_cognito_user_pool" "devportal" {
   }
 
   schema {
+    name                     = "webinar_monitoring_accepted"
+    attribute_data_type      = "Boolean"
+    developer_only_attribute = false
+    mutable                  = true
+    required                 = false
+  }
+
+  schema {
     name                     = "job_role"
     attribute_data_type      = "String"
     developer_only_attribute = false
