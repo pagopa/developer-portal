700d8949ea7f178d95ffcb44e48af2d14fdb92c8
Simone Salsi <simone.salsi@dgsspa.com>
2026-09-11 11:51:38 +0200

    Refactor for lint + Remove comments

--- apps/nextjs-website/src/components/atoms/QualtricsIntercept/QualtricsIntercept.tsx
+++ apps/nextjs-website/src/components/atoms/QualtricsIntercept/QualtricsIntercept.tsx
@@ -1,22 +1,20 @@
-"use client";
-import { useUser } from "@/helpers/user.helper";
-import { useEffect } from "react";
+'use client';
+import { useUser } from '@/helpers/user.helper';
+import { useEffect } from 'react';
 
 const QualtricsIntercept = () => {
   const { user } = useUser();
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src =
-      'https://zn1dbdheb2vgg54ei-pagopa.siteintercept.qualtrics.com/SIE/?Q_ZID=ZN_1dbDHeb2VGg54EI';
-    
-    if (user && user.attributes["custom:survey_accepted"]) {
-      // TODO: Add condition: Only do this is user is not on their first login
+    const script = Object.assign(document.createElement('script'), {
+      type: 'text/javascript',
+      src: 'https://zn1dbdheb2vgg54ei-pagopa.siteintercept.qualtrics.com/SIE/?Q_ZID=ZN_1dbDHeb2VGg54EI',
+    });
+
+    if (user && user.attributes['custom:survey_accepted']) {
       document.body.appendChild(script);
-      console.log("Starting Survey Program");
     }
-    
+
     return () => {
       // Optional cleanup if the component is unmounted.
       script.remove();
